e4a7bac55fa69bf0dfb2bdadd5b5ca775d97e7c1
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-13 13:57:29 +0900

    test

--- baekjoon/1026.cpp
+++ baekjoon/1026.cpp
@@ -7,14 +7,8 @@ int main()
 	cin.tie(NULL), cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	int N, sum = 0;
-	cin >> N;
-	vector<int> A(N), B(N);
-	loop(i, N) cin >> A[i];
-	loop(i, N) cin >> B[i];
-	sort(A, A + N);
-	sort(B, B + N, greater<int>());
-	loop(i, N) sum += A[i] * B[i];
-	cout << sum;
+	loop(i, 10) loop(j, 10)
+		cout << "hello" << endl;
+
 	return 0;
 }
