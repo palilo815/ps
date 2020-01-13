63c63971523781ae43c3e9fab6e3cc079db89b5f
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-13 14:04:36 +0900

    보물

--- baekjoon/1026.cpp
+++ baekjoon/1026.cpp
@@ -4,17 +4,17 @@ using namespace std;
 
 int main()
 {
-	cin.tie(NULL), cout.tie(NULL);
-	ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    ios::sync_with_stdio(false);
 
-	int N, sum = 0;
-	cin >> N;
-	vector<int> A(N), B(N);
-	loop(i, N) cin >> A[i];
-	loop(i, N) cin >> B[i];
-	sort(A, A + N);
-	sort(B, B + N, greater<int>());
-	loop(i, N) sum += A[i] * B[i];
-	cout << sum;
-	return 0;
+    int N, sum = 0;
+    cin >> N;
+    vector<int> A(N), B(N);
+    loop(i, N) cin >> A[i];
+    loop(i, N) cin >> B[i];
+    sort(A, A + N);
+    sort(B, B + N, greater<int>());
+    loop(i, N) sum += A[i] * B[i];
+    cout << sum;
+    return 0;
 }
