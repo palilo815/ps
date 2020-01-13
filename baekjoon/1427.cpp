8b8975fc75c8f14b2a47eb60e96009bcd470acdb
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-13 16:01:18 +0900

    소트인사이드

---
+++ baekjoon/1427.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    sort(s.begin(), s.end(), greater<char>());
+    cout << s;
+    return 0;
+}
