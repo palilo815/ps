1698a10ea6577f436b37f8febf347b8a53a4cbf6
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-13 17:11:23 +0900

    N 찍기

---
+++ baekjoon/2741.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        for(int i=1; i<=N; i++)
+            System.out.println(i);
+    }
+}
