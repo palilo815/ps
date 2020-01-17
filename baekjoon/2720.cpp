37af72b43c61c7f038bfc228e6212d5fc27cc876
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-17 10:12:25 +0900

    세탁소 사장 동혁

--- baekjoon/2720.cpp
+++ baekjoon/2720.cpp
@@ -10,9 +10,9 @@ int main()
     while (T--) {
         int Q = 0, D = 0, N = 0;
         int C; cin >> C;
-        Q += C % 25; C /= 25;
-        D += C % 10; C /= 10;
-        N += C % 5; C /= 5;
+        Q += C / 25; C %= 25;
+        D += C / 10; C %= 10;
+        N += C / 5; C %= 5;
         cout << Q << ' ' << D << ' ' << N << ' ' << C << '\n';
     }
     return 0;
