06791b0762cfcd4d183eb08ae607def2e3230e95
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-17 17:09:57 +0900

    체스판 조각

--- baekjoon/3004.cpp
+++ baekjoon/3004.cpp
@@ -5,8 +5,8 @@ int main()
 {
     int n; cin >> n;
     if (n % 2)
-        cout << (1 + n / 2) * (1 + n / 2);
-    else
         cout << (1 + n / 2) * (2 + n / 2);
+    else
+        cout << (1 + n / 2) * (1 + n / 2);
     return 0;
 }
