5699b526682901bb2b5d2f777cae2845ea798dd5
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-18 00:33:45 +0900

    택시 기하학

---
+++ baekjoon/3053.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#define PI = 3.14159265358979323846;
+using namespace std;
+
+int main()
+{
+    int r; cin >> r;
+    double circle_U = PI * r * r;
+    double circle_T = 2 * r * r;
+    printf("%6lf\n%6lf", circle_U, circle_T);
+    return 0;
+}
