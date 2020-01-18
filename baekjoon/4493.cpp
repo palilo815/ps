3945f0636cb182cf8aad6f46afe3aecc9d568d0d
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-18 15:54:50 +0900

    가위 바위 보?

---
+++ baekjoon/4493.cpp
@@ -0,0 +1,27 @@
+#include< iostream>
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int T; cin >> T;
+    while (T--) {
+        int A = 0, B = 0;
+        int n; cin >> n;
+        while (n--) {
+            char a, b; cin >> a >> b;
+            if (a == b) continue;
+            if ((a == 'P' && b == 'R') || (a == 'R' && b == 'S') || (a == 'S' && b == 'P')) ++A;
+            else ++B;
+        }
+        if (A == B) cout << "TIE\n";
+        else {
+            cout << "Player ";
+            if (A > B) cout << "1\n";
+            else cout << "2\n";
+        }
+    }
+    return 0;
+}
