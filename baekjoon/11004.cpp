05a79ff50dd160740289f67e65d68f2f24eb17ba
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-20 16:52:41 +0900

    K번째 수

--- baekjoon/11004.cpp
+++ baekjoon/11004.cpp
@@ -1,38 +1,29 @@
-#include <bits/stdc++.h> 
-#define loop(i,n) for(int i=0;i<n;++i)
+#include <iostream>
+#include <queue>
 using namespace std;
-
-int arr[5000000];
-
-int pivot(int l, int r)
-{
-    int x = arr[r], i = l;
-    for (int j = l; j <= r - 1; j++) {
-        if (arr[j] <= x) {
-            swap(arr[i], arr[j]);
-            ++i;
-        }
-    }
-    swap(arr[i], arr[r]);
-    return i;
-}
-int kth(int l, int r, int k)
-{
-    if (k > 0 && k <= r - l + 1) {
-        int idx = pivot(l, r);
-        if (idx - l == k - 1) return arr[idx];
-        if (idx - l > k - 1) return kth(l, idx - 1, k);
-        return kth(idx + 1, r, k - idx + l - 1);
-    }
-}
 
 int main()
 {
     cin.tie(NULL), cout.tie(NULL);
     ios::sync_with_stdio(false);
 
-    int N, K; cin >> N >> K;
-    loop(i, N) cin >> arr[i];
-    cout << kth(0, N - 1, K);
+    priority_queue<int> pq;
+    int n, k, x; cin >> n >> k;
+
+    // k개는 그냥 넣고
+    // n-k개는 비교하면서 넣는다.
+    n -= k;
+    while (k--) {
+        cin >> x;
+        pq.push(x);
+    }
+    while (n--) {
+        cin >> x;
+        if (x <= pq.top()) {
+            pq.pop();
+            pq.push(x);
+        }
+    }
+    cout << pq.top();
     return 0;
 }
