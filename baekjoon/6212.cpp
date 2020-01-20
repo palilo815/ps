36fb7ffc2a9f6af00c0ede4f9a153231baeb487c
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-20 22:29:36 +0900

    Dream Counting

--- baekjoon/6212.cpp
+++ baekjoon/6212.cpp
@@ -1,6 +1,6 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <iostream>
 #define loop(i,n) for(int i=0;i<n;++i)
+using namespace std;
 
 int arr[10];
 void counting(int n)
@@ -15,8 +15,7 @@ int main()
     cin.tie(NULL), cout.tie(NULL);
     ios::sync_with_stdio(false);
 
-    int a, b;
-    cin >> a >> b;
+    int a, b; cin >> a >> b;
     for (int i = a; i <= b; ++i)
         counting(i);
     loop(i, 10) cout << arr[i] << ' ';
