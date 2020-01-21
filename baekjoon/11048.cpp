55650889b0908705fbf3154fdccc43386635e0e0
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-22 00:07:29 +0900

    이동하기

---
+++ baekjoon/11048.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#define LOOP(i,n) for(int i=1;i<=n;++i)
+using namespace std;
+
+int candy[1001][1001], DP[1001][1001];
+int main()
+{
+    cin.tie(NULL), cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int row, col; cin >> row >> col;
+    LOOP(i, row) LOOP(j, col) cin >> candy[i][j];
+    DP[0][0] = candy[0][0];
+    LOOP(i, row) LOOP(j, col) {
+        int tmp = max(DP[i - 1][j], DP[i][j - 1]);
+        tmp = max(tmp, DP[i - 1][j - 1]);
+        DP[i][j] = tmp + candy[i][j];
+    }
+    cout << DP[row][col];
+}
