82e152eefb87ffe74027e395f4fb5c87553344ef
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-22 02:06:11 +0900

    과자 사기

---
+++ baekjoon/17450.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#define loop(i,n) for(int i=0;i<n;++i)
+using namespace std;
+
+int main()
+{
+    char name[3] = { 'S','N','U' };
+    double price[3];
+    loop(i, 3) {
+        int c, w; cin >> c >> w;
+        if (c >= 5000) c -= 500;
+        price[i] = 1.0 * w / c;
+    }
+
+    int idx = 2;
+    loop(i, 2)
+        if (price[i] > price[idx])
+            idx = i;
+    cout << name[idx];
+    return 0;
+}
