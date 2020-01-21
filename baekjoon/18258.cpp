dc94a5f7c34d066f2cedcac3ef92093bbadfa168
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-22 01:34:06 +0900

    큐 2

---
+++ baekjoon/18258.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define loop(i,n) for(int i=0;i<n;++i)
+
+int main()
+{
+    cin.tie(NULL), cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int n, num;
+    string order;
+    queue<int> q;
+    cin >> n;
+    while (n--) {
+        cin >> order;
+        if (order == "push") {
+            cin >> num;
+            q.push(num);
+        }
+        else if (order == "pop") {
+            if (q.empty()) cout << "-1\n";
+            else {
+                cout << q.front() << '\n';
+                q.pop();
+            }
+        }
+        else if (order == "size")
+            cout << q.size() << '\n';
+        else if (order == "empty")
+            cout << q.empty() << '\n';
+        else if (order == "front") {
+            if (q.empty()) cout << "-1\n";
+            else cout << q.front() << '\n';
+        }
+        else {
+            if (q.empty()) cout << "-1\n";
+            else cout << q.back() << '\n';
+        }
+    }
+    return 0;
+}
