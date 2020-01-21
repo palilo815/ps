4baa9b2b4208c3b8516e2c9ac71c760666319cb8
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-21 15:58:20 +0900

    유레카 이론

--- baekjoon/10430.py
+++ baekjoon/10430.py
@@ -1,5 +1,22 @@
-A, B, C = map(int, input().split())
-print((A + B) % C)
-print((A % C + B % C) % C)
-print((A * B) % C)
-print((A % C * B % C) % C)
+def solve(num):
+    for a in range(1, 1000):
+        A = a * (a + 1) // 2
+        if 3 * A > num:
+            break
+        for b in range(a, 1000):
+            B = b * (b + 1) // 2
+            if A + 2 * B > num:
+                break
+            for c in range(b, 1000):
+                C = c * (c + 1) // 2
+                if A + B + C == num:
+                    return 1
+                if A + B + C > num:
+                    break
+    return 0
+
+
+N = int(input())
+for n in range(N):
+    K = int(input())
+    print(solve(K))
