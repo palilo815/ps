5cd2dbaaa98767f9f6945e120ec7e6cf3d12001b
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-22 17:25:40 +0900

    ACM 호텔

--- baekjoon/10250.py
+++ baekjoon/10250.py
@@ -1,6 +1,6 @@
 T = int(input())
 for _ in range(T):
     r, c, n = map(int, input().split())
-    num = n // r + 1
-    floor = n % r
+    num = (n - 1) // r + 1
+    floor = (n - 1) % r + 1
     print(floor * 100 + num)
