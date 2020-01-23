8fe37d7c15dc9da3e192c0f98330e691ed0a6cab
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-23 21:21:44 +0900

    세 수

---
+++ baekjoon/2000/2985.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    if (a + b == c) printf("%d+%d=%d", a, b, c);
+    else if (a - b == c) printf("%d-%d=%d", a, b, c);
+    else if (a * b == c) printf("%d*%d=%d", a, b, c);
+    else if (a / b == c) printf("%d/%d=%d", a, b, c);
+    else if (a == b + c) printf("%d=%d+%d", a, b, c);
+    else if (a == b - c) printf("%d=%d-%d", a, b, c);
+    else if (a == b * c) printf("%d=%d*%d", a, b, c);
+    else if (a == b / c) printf("%d=%d/%d", a, b, c);
+    return 0;
+}
