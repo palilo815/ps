1b952cbe5b470039c81c4bb75f27982bb540356a
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-23 15:14:14 +0900

    A+B - 8

---
+++ baekjoon/11000/11022.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int T, a, b;
+    scanf("%d", &T);
+    for (int i = 1;i <= T;++i) {
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d + %d = %d\n", i, a, b, a + b);
+    }
+    return 0;
+}
