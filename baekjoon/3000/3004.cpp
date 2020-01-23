432d9090156439ef27903d423e4e408cb1a23c23
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-24 01:07:39 +0900

    체스판 조각

---
+++ baekjoon/3000/3004.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n; cin >> n;
+    if (n % 2)
+        cout << (1 + n / 2) * (2 + n / 2);
+    else
+        cout << (1 + n / 2) * (1 + n / 2);
+    return 0;
+}
