d5791aa9c3220248f8f7474c7f2b3a8e92ef0c77
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-23 15:49:07 +0900

    delete

--- baekjoon/14182.py
+++
@@ -1,9 +0,0 @@
-while 1:
-    n = int(input())
-    if n == 0:
-        break
-    if n > 5000000:
-        n = n * 8 // 10
-    elif n > 1000000:
-        n = n * 9 // 10
-    print(n)
--- baekjoon/14215.py
+++
@@ -1,4 +0,0 @@
-a = list(map(int, input().split()))
-a.sort()
-a[2] = min(a[2], a[0] + a[1] - 1)
-print(sum(a))
