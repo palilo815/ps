c6b64c923ac1c64cd01060c1071377f27e8f9134
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-23 14:38:14 +0900

    보물

---
+++ baekjoon/1000/1026.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define loop(i,n) for(int i=0;i<n;++i)
+
+int main()
+{
+    cin.tie(NULL), cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int N, sum = 0;
+    cin >> N;
+    vector<int> A(N), B(N);
+    loop(i, N) cin >> A[i];
+    loop(i, N) cin >> B[i];
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end(), greater<int>());
+    loop(i, N) sum += A[i] * B[i];
+    cout << sum;
+    return 0;
+}
