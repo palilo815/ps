4276959ea85e56def405b31110a7bd54181ddf4a
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-27 15:20:24 +0900

    상근이의 친구들

---
+++ baekjoon/5000/5717.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    while (1) {
+        scanf("%d %d", &a, &b);
+        if (a == 0) break;
+        printf("%d\n", a + b);
+    }
+    return 0;
+}
