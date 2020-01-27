6ce2bfd7d45bd97a599da5902b79e06463bf01e3
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-27 23:39:16 +0900

    delete

--- baekjoon/7523.py
+++
@@ -1,6 +0,0 @@
-T = int(input())
-for t in range(1, T + 1):
-    print("Scenario #" + str(t) + ":")
-    a, b = map(int, input().split())
-    print((a + b) * (b - a + 1) // 2)
-    print()
--- baekjoon/7567.py
+++
@@ -1,8 +0,0 @@
-s = input()
-h = 10
-for i in range(1, len(s)):
-    if s[i] == s[i - 1]:
-        h += 5
-    else:
-        h += 10
-print(h)
