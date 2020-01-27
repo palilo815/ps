e1fb241bdd56c0e9e4a4fd5d3b065769a6c339aa
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-27 21:12:59 +0900

    피보나치 수 4

---
+++ baekjoon/10000/10826.py
@@ -0,0 +1,34 @@
+def multiply(m):
+    global f
+    x = f[0][0] * m[0][0] + f[0][1] * m[1][0]
+    y = f[0][0] * m[0][1] + f[0][1] * m[1][1]
+    z = f[1][0] * m[0][0] + f[1][1] * m[1][0]
+    w = f[1][0] * m[0][1] + f[1][1] * m[1][1]
+    f[0][0] = x
+    f[0][1] = y
+    f[1][0] = z
+    f[1][1] = w
+
+
+def power(n):
+    if n == 0 or n == 1:
+        return
+    global f
+    m = [[1, 1], [1, 0]]
+    power(n // 2)
+    multiply(f)
+    if n % 2 == 1:
+        multiply(m)
+
+
+def fib(n):
+    global f
+    if n == 0:
+        return 0
+    power(n - 1)
+    return f[0][0]
+
+
+f = [[1, 1], [1, 0]]
+N = int(input())
+print(fib(N))
