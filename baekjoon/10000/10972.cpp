273007d2ed34ebc4b1a688e1d29d484302241d78
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-27 23:21:40 +0900

    다음 순열

---
+++ baekjoon/10000/10972.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define loop(i,n) for(int i=0;i<n;++i)
+using namespace std;
+const int max_N = 10000;
+
+int arr[max_N];
+int main()
+{
+    cin.tie(NULL), cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int n; cin >> n;
+    loop(i, n) cin >> arr[i];
+    if (next_permutation(arr, arr + n))
+        loop(i, n) cout << arr[i] << ' ';
+    else cout << -1;
+    return 0;
+}
