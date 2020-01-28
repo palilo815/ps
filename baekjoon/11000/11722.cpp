8e7d20665e98fc7e071186b71e9da3e0f7a0cc0e
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-28 17:51:52 +0900

    가장 긴 감소하는 부분 수열

---
+++ baekjoon/11000/11722.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define loop(i,n) for(int i=0;i<n;++i)
+using namespace std;
+
+const int max_N = 1000;
+int arr[max_N], T[max_N];
+int ceil_elem(int s, int e, int key) {
+    while (e - s > 1) {
+        int mid = s + (e - s) / 2;
+        if (T[mid] <= key)
+            e = mid;
+        else
+            s = mid;
+    }
+    return e;
+}
+int main()
+{
+    cin.tie(NULL), cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int N; cin >> N;
+    loop(i, N) cin >> arr[i];
+    int len = 1;
+    T[0] = arr[0];
+    for (int i = 1;i < N;++i) {
+        if (arr[i] > T[0])
+            T[0] = arr[i];
+        else if (arr[i] < T[len - 1])
+            T[len++] = arr[i];
+        else
+            T[ceil_elem(-1, len - 1, arr[i])] = arr[i];
+    }
+    cout << len;
+    return 0;
+}
