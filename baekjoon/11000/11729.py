804ef5bcf2d0015fc1e468d6226404ee86c7742e
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-28 18:22:29 +0900

    하노이 탑 이동 순서

---
+++ baekjoon/11000/11729.py
@@ -0,0 +1,12 @@
+def hanoi(num, u, v):
+    if num == 1:
+        print(u, v)
+    else:
+        hanoi(num - 1, u, 6 - u - v)
+        print(u, v)
+        hanoi(num - 1, 6 - u - v, v)
+
+
+n = int(input())
+print(pow(2, n) - 1)
+hanoi(n, 1, 3)
