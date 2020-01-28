5a076937853315754703f3e65f1452a2df5e94a4
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-28 20:17:05 +0900

    수 정렬하기 4

---
+++ baekjoon/11000/11931.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <algorithm>
+#define loop(i,n) for(int i=0;i<n;++i)
+using namespace std;
+const int max_N = 1000000;
+
+int arr[max_N];
+int main()
+{
+    int N; scanf("%d", &N);
+    loop(i, N) scanf("%d", &arr[i]);
+    sort(arr, arr + N, greater<int>());
+    loop(i, N) printf("%d\n", arr[i]);
+    return 0;
+}
