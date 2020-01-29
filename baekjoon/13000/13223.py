23939235352da67572b0a57a33d898fde9b3cbad
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-29 17:24:31 +0900

    소금 폭탄

--- baekjoon/13000/13223.py
+++ baekjoon/13000/13223.py
@@ -6,6 +6,8 @@ for i in range(2, 0, -1):
         b[i] += 60
         b[i - 1] -= 1
 b[0] = (b[0] - a[0] + 24) % 24
+if b[0] == b[1] == b[2] == 0:
+    b[0] = 24
 for i in range(3):
     print("{:02d}".format(b[i]), end='')
     if i != 2:
