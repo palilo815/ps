4d572eb023179a9987c5a1646113897a88ac81a7
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-29 16:40:51 +0900

    1로 만들기

--- baekjoon/1000/1463.cpp
+++ baekjoon/1000/1463.cpp
@@ -1,25 +1,24 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
 using namespace std;
+const int max_N = 1000000;
 
-int* num;
-void MakeOne(int n)
-{
-    if (n % 3 == 0) num[n] = 1 + min(num[n / 3], num[n - 1]);
-    else if (n % 2 == 0) num[n] = 1 + min(num[n / 2], num[n - 1]);
-    else num[n] = 1 + num[n - 1];
-}
+int DP[max_N + 1];
 int main()
 {
-    cin.tie(NULL);
-    std::ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    ios::sync_with_stdio(false);
 
-    int n;
-    cin >> n;
-    num = new int[n + 1];
-    num[1] = 0;
-    for (int i = 2; i <= n;i++)
-        MakeOne(i);
-    cout << num[n];
+    // basis : DP[1] = 0
+
+    int n; cin >> n;
+    for (int i = 2; i <= n; ++i) {
+        int cnt = DP[i - 1];
+        if (i % 2 == 0 && DP[i / 2] < cnt)
+            cnt = DP[i / 2];
+        if (i % 3 == 0 && DP[i / 3] < cnt)
+            cnt = DP[i / 3];
+        DP[i] = cnt + 1;
+    }
+    cout << DP[n];
     return 0;
 }
