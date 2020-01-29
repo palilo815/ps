74317fc40c841922fb162dc5466c08aa4d682bb8
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-29 17:18:05 +0900

    소금 폭탄

---
+++ baekjoon/13000/13223.py
@@ -0,0 +1,12 @@
+a = list(map(int, input().split(':')))
+b = list(map(int, input().split(':')))
+for i in range(2, 0, -1):
+    b[i] -= a[i]
+    if b[i] < 0:
+        b[i] += 60
+        b[i - 1] -= 1
+b[0] = (b[0] - a[0] + 24) % 24
+for i in range(3):
+    print("{:02d}".format(b[i]), end='')
+    if i != 2:
+        print(':', end='')
