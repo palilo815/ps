8417d31016f38687e2c523d98db3142c11a5ffaf
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-30 16:59:15 +0900

    소가 길을 건너간 이유 2

--- baekjoon/14000/14468.cpp
+++ baekjoon/14000/14468.cpp
@@ -15,10 +15,14 @@ int main()
             lst.push_back(cow);
         }
         else {
+            // 소의 출발점과 도착점 사이의 다른 소의 개수를 더해준다.
             int cnt = lst.size() - 1;
             auto it = lst.begin();
             while (*it != cow) ++it, --cnt;
             ans += cnt;
+            // 현재 소를 x, 나중에 나올 소를 y라 하면
+            // y x x y 혹은 x x y y의 경우밖에 없다.
+            // 절대 둘의 경로가 겹치지 않는다.
             lst.erase(it);
         }
     }
