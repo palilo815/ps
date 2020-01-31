44bd89587fadb0f9d44c2b3efc629c3fa76314f5
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-01-31 14:45:24 +0900

    피자 (Large)

--- baekjoon/14000/14607.py
+++ baekjoon/14000/14607.py
@@ -1,10 +1,2 @@
-def happy(h):
-    if h <= 1:
-        return 0
-    l = h // 2
-    r = h - l
-    return l * r + happy(l) + happy(r)
-
-
 n = int(input())
-print(happy(n))
+print(n * (n - 1) // 2)
