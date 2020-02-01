791c0d3ff127211414d4999662295b100a4f32a7
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-02-01 17:22:30 +0900

    Simple Arithmetic

---
+++ baekjoon/15000/15858.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define loop(i,n) for(int i=0;i<n;++i)
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ll a, b, c; cin >> a >> b >> c;
+
+    ll D = a * b;
+    ll E = D / c;
+    D -= E * c;
+    cout << E << '.';
+    double F = 1.0 * D / c;
+    loop(i, 10) {
+        F *= 10;
+        cout << int(F);
+        while (F >= 1) --F;
+    }
+    return 0;
+}
