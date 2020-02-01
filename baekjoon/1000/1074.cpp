55820c8839bbe42be5d07ac3eb348f646249829e
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-02-01 17:44:39 +0900

    Z

--- baekjoon/1000/1074.cpp
+++ baekjoon/1000/1074.cpp
@@ -1,32 +1,26 @@
-#include<iostream>
-#include<algorithm>
-#include<math.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-long long Zsum(long long n, long long r, long long c)
+int z_sum(int len, int r, int c)
 {
-    if (n == 1)
-        return 2 * r + c;
-    long long half_len = pow(2, n - 1);
-    long long sum = 0;
-    if (r >= half_len) {
-        r -= half_len;
-        sum += 2 * pow(half_len, 2);
+    if (len == 1) return 2 * r + c;
+    int half = len / 2;
+    int ret = 0;
+    if (r >= half) {
+        r -= half;
+        ret += 2 * pow(half, 2);
     }
-    if (c >= half_len) {
-        c -= half_len;
-        sum += pow(half_len, 2);
+    if (c >= half) {
+        c -= half;
+        ret += pow(half, 2);
     }
-    return sum + Zsum(n - 1, r, c);
+    return ret + z_sum(len / 2, r, c);
 }
 
 int main()
 {
-    cin.tie(NULL);
-    std::ios::sync_with_stdio(false);
-
-    long long N, r, c;
+    int N, r, c;
     cin >> N >> r >> c;
-    cout << Zsum(N, r, c);
+    cout << z_sum(pow(2, N), r, c);
     return 0;
 }
