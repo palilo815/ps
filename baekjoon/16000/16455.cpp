9afcc763e5f7e91a7b56adb1dc081a8fc299c67a
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-02-04 15:02:25 +0900

    K번째 수 찾는 함수

---
+++ baekjoon/16000/16455.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int pivot(vector<int>& arr, int l, int r)
+{
+	int x = arr[r], i = l;
+	for (int j = l; j <= r - 1; ++j) {
+		if (arr[j] <= x) {
+			swap(arr[i], arr[j]);
+			++i;
+		}
+	}
+	swap(arr[i], arr[r]);
+	return i;
+}
+int kth_smallest(vector<int>& arr, int l, int r, int k)
+{
+	int idx = pivot(arr, l, r);
+	if (idx - l == k - 1)
+		return arr[idx];
+	if (idx - l > k - 1)
+		return kth_smallest(arr, l, idx - 1, k);
+	return kth_smallest(arr, idx + 1, r, k - idx + l - 1);
+}
+int kth(vector<int>& a, int k)
+{
+	return kth_smallest(a, 0, a.size() - 1, k);
+}
