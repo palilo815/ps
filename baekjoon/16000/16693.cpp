190d35af6be818a05d9555a6ac3ccdaa55ad404d
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-02-04 16:26:51 +0900

    Pizza Deal

--- baekjoon/16000/16693.cpp
+++ baekjoon/16000/16693.cpp
@@ -6,6 +6,9 @@ int main()
     double a, r, p1, p2;
     scanf("%lf %lf %lf %lf", &a, &p1, &r, &p2);
     double slice = a / p1, whole = PI * r * r / p2;
+
+    // slice == whole인 경우가 없다고 문제에 있으므로
+    // 오차범위를 설정해줄 필요가 없다.
     if (slice > whole) printf("Slice of ");
     else printf("Whole ");
     printf("pizza");
