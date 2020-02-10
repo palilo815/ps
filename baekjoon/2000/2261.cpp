ae5b5470338e66bf362d63900cf6a644f499bce5
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-02-10 15:05:18 +0900

    가장 가까운 두 점

--- baekjoon/2000/2261.cpp
+++ baekjoon/2000/2261.cpp
@@ -1,44 +1,57 @@
 #include <bits/stdc++.h>
 #define loop(i,n) for(int i=0;i<n;++i)
-#define P pair<int,int>
 using namespace std;
-const int max_N = 100000;
+typedef pair<int, int> p;
 
-int dist(P a, P b)
+const int max_N = 100000;
+p dot[max_N];
+
+auto cmp_y = [](p a, p b)
+{
+    if (a.second == b.second) return a.first < b.first;
+    return a.second < b.second;
+};
+int dist(p a, p b)
 {
     int dx = a.first - b.first, dy = a.second - b.second;
     return dx * dx + dy * dy;
 }
+int main()
+{
+    cin.tie(NULL), cout.tie(NULL);
+    ios::sync_with_stdio(false);
 
-P dots[max_N];
-int main() {
+    // 출처)
+    // https://www.acmicpc.net/blog/view/25
+
     int N; cin >> N;
-    loop(i, N) cin >> dots[i].first >> dots[i].second;
-    sort(dots, dots + N);
-    set<P> candidate = { dots[0],dots[1] };
-    int ans = dist(dots[0], dots[1]);
+    loop(i, N) cin >> dot[i].first >> dot[i].second;
+    sort(dot, dot + N);
+
+    set<p, decltype(cmp_y)> st(cmp_y);
+    st.insert(dot[0]), st.insert(dot[1]);
+    int ans = dist(dot[0], dot[1]);
     int start = 0;
+
     for (int i = 2; i < N; ++i) {
-        P now = dots[i];
+        p& here = dot[i];
         while (start < i) {
-            P p = dots[start];
-            int x = now.first - p.first;
-            if (x * x > ans) {
-                candidate.erase(p);
-                start += 1;
-            }
+            int dx = dot[start].first - here.first;
+            if (dx * dx > ans) st.erase(dot[start++]);
             else break;
         }
-        int d = (int)sqrt((double)ans) + 1;
-        auto lower_point = P(-100000, now.second - d);
-        auto upper_point = P(100000, now.second + d);
-        auto lower = candidate.lower_bound(lower_point);
-        auto upper = candidate.upper_bound(upper_point);
-        for (auto it = lower; it != upper; it++) {
-            int d = dist(now, *it);
+
+        int d = ceil(sqrt(ans));
+        p lower_p = { -10000,here.second - d };
+        p upper_p = { 10000,here.second + d };
+        auto lower = st.lower_bound(lower_p);
+        auto upper = st.upper_bound(upper_p);
+
+        for (auto it = lower; it != upper; ++it) {
+            int d = dist(here, *it);
             if (d < ans) ans = d;
         }
-        candidate.insert(now);
+        st.insert(here);
     }
     cout << ans;
     return 0;
