5bb4b14535b4b52085295d3a375e9970a5727429
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-02-10 23:12:20 +0900

    홍익대학교 지하캠퍼스

---
+++ baekjoon/17000/17833.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define loop(i,n) for(int i=0;i<n;++i)
+using namespace std;
+typedef pair<int, int> p;
+
+const int max_N = 2000;
+const int max_M = 2000;
+
+struct building {
+    int height, cost, u, v;
+};
+
+int cost[max_N + 1];
+building edge[max_M];
+
+int main()
+{
+    cin.tie(NULL), cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    // Dijkstra로 풀었으나 DP로 접근하는게 더 나은 듯
+
+    int N, src, dst, M;
+    cin >> N >> src >> dst >> M;
+    loop(i, M)
+        cin >> edge[i].height >> edge[i].cost >> edge[i].u >> edge[i].v;
+
+    fill(cost + 1, cost + N + 1, INT_MAX);
+    cost[src] = 0;
+
+    priority_queue<p, vector<p>, greater<p>> pq;
+    pq.push({ 0,src });
+
+    int ans = -1;
+    while (!pq.empty()) {
+        auto [c, d] = pq.top(); pq.pop();
+        if (cost[d] < c) continue;
+        if (d == dst) { ans = c; break; }
+
+        loop(i, M) {
+            auto& e = edge[i];
+            if (d >= e.u && d + e.height - e.u <= N) {
+                int D = d - e.u + e.v, C = c + e.cost;
+                if (cost[D] > C) {
+                    cost[D] = C;
+                    pq.push({ C,D });
+                }
+            }
+            if (d >= e.v && d + e.height - e.v <= N) {
+                int D = d - e.v + e.u, C = c + e.cost;
+                if (cost[D] > C) {
+                    cost[D] = C;
+                    pq.push({ C,D });
+                }
+            }
+        }
+    }
+    cout << ans;
+    return 0;
+}
