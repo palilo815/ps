e68de4e493cf39925715e419bb4bb6b685b9cea7
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-02-13 16:51:45 +0900

    수열과 쿼리 17

---
+++ baekjoon/14000/14438.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int max_N = 100000;
+
+int arr[max_N];
+int segT[131072 * 2 - 1];
+
+int build_segT(int l, int r, int idx)
+{
+    if (l == r) return segT[idx] = arr[l];
+
+    int mid = l + (r - l) / 2;
+    return segT[idx] = min(build_segT(l, mid, 2 * idx + 1), build_segT(mid + 1, r, 2 * idx + 2));
+}
+int minQ(int ql, int qr, int l, int r, int idx)
+{
+    if (ql <= l && r <= qr) return segT[idx];
+    if (r < ql || qr < l) return INT_MAX;
+    int mid = l + (r - l) / 2;
+    return min(minQ(ql, qr, l, mid, 2 * idx + 1), minQ(ql, qr, mid + 1, r, 2 * idx + 2));
+}
+void update(int ui, int uv, int l, int r, int idx)
+{
+    if (l == r) {
+        arr[ui] = uv;
+        segT[idx] = uv;
+    }
+    else {
+        int mid = l + (r - l) / 2;
+        if (ui <= mid) update(ui, uv, l, mid, 2 * idx + 1);
+        else update(ui, uv, mid + 1, r, 2 * idx + 2);
+        segT[idx] = min(segT[2 * idx + 1], segT[2 * idx + 2]);
+    }
+
+}
+int main()
+{
+    cin.tie(NULL), cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int N; cin >> N;
+    for (int i = 0; i < N; ++i)
+        cin >> arr[i];
+    build_segT(0, N - 1, 0);
+
+    int Q; cin >> Q;
+    while (Q--) {
+        int q, a, b; cin >> q >> a >> b;
+        if (q == 1) update(a - 1, b, 0, N - 1, 0);
+        else cout << minQ(a - 1, b - 1, 0, N - 1, 0) << '\n';
+    }
+    return 0;
+}
