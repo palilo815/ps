163b178fcdd4854a13467856dca4c947981cad0c
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-02-18 17:46:19 +0900

    분수

---
+++ baekjoon/18000/18117.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+def pow_10(k, mod):
+    if k == 0:
+        return 1
+    ret = 10 if k % 2 else 1
+    half = pow_10(k // 2, mod)
+    return (ret * half * half) % mod
+
+for t in range(int(input())):
+    a, b = map(int, input().split())
+    i, n = map(int, input().split())
+    tmp = (a * pow_10(i - 1, b)) % b
+    for _ in range(n):
+        tmp *= 10
+        print(tmp // b, end="")
+        tmp %= b
+    print()
