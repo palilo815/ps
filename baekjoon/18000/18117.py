ec2f3231ef77a049d46459cbe7a83d27be54cebc
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-02-18 17:47:08 +0900

    분수

--- baekjoon/18000/18117.py
+++ baekjoon/18000/18117.py
@@ -8,6 +8,9 @@ def pow_10(k, mod):
     half = pow_10(k // 2, mod)
     return (ret * half * half) % mod
 
+# a/b 의 k번째 숫자
+# = (a * 10^(k-1) % b * 10) // b
+
 for t in range(int(input())):
     a, b = map(int, input().split())
     i, n = map(int, input().split())
