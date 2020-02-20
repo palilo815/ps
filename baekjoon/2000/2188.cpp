498b0eb2ec91e08abb91341448931ca7ae28f5e8
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-02-20 17:24:11 +0900

    축사 배정

---
+++ baekjoon/2000/2188.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int max_N = 500;
+const int max_M = 500;
+
+vector<int> adj[max_N + 1];
+
+bool visited[max_M + 1];
+int N_match[max_N + 1];
+int M_match[max_M + 1];
+
+bool DFS(int u)
+{
+    if (visited[u]) return false;
+    visited[u] = true;
+    for(int v : adj[u])
+        if (!M_match[v] || DFS(M_match[v])) {
+            N_match[u] = v;
+            M_match[v] = u;
+            return true;
+        }
+    return false;
+}
+int main()
+{
+    cin.tie(NULL), cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int N, M; cin >> N >> M;
+    for (int u = 1; u <= N; ++u) {
+        int cnt; cin >> cnt;
+        while (cnt--) {
+            int v; cin >> v;
+            adj[u].emplace_back(v);
+        }
+    }
+
+    int ans = 0;
+    for (int i = 1; i <= N; ++i) {
+        memset(visited, 0, sizeof(visited));
+        if (DFS(i)) ++ans;
+    }
+    cout << ans;
+    return 0;
+}
