16b01b3bd97a5c488f6b30e72242196ef7d4a660
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-02-20 18:20:59 +0900

    열혈강호 2

---
+++ baekjoon/11000/11376.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int max_N = 1000;
+const int max_M = 1000;
+
+vector<int> adj[max_N + 1];
+bool visited[max_M + 1];
+int M_match[max_M + 1];
+
+bool DFS(int u)
+{
+    if (visited[u]) return false;
+    visited[u] = true;
+    for (int v : adj[u])
+        if (!M_match[v] || DFS(M_match[v])) {
+            M_match[v] = u;
+            return true;
+        }
+    return false;
+}
+int main()
+{
+    cin.tie(NULL), cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int N, M; cin >> N >> M;
+    for (int u = 1; u <= N; ++u) {
+        int cnt; cin >> cnt;
+        while (cnt--) {
+            int v; cin >> v;
+            adj[u].push_back(v);
+        }
+    }
+
+    int ans = 0;
+    for (int u = 1; u <= N; ++u) for (int j = 0; j < 2; ++j) {
+        memset(visited, 0, sizeof(visited));
+        if (DFS(u)) ++ans;
+    }
+    cout << ans;
+    return 0;
+}
