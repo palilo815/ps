ec78247499a4ad029be7caab0741db7844c1408a
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-02-22 17:50:36 +0900

    1, 2, 3 더하기 9

---
+++ baekjoon/16000/16195.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define loop(i,n) for(int i=0;i<n;++i)
+#define scanf scanf_s
+
+const int mod = 1000000009;
+const int max_N = 1000;
+const int max_M = 1000;
+
+int DP[max_N + 3][max_M + 1];
+
+int main() {
+    DP[0][0] = 1;
+    loop(m, max_M) loop(n, max_N)
+        if (DP[n][m]) {
+            DP[n + 1][m + 1] = (DP[n + 1][m + 1] + DP[n][m]) % mod;
+            DP[n + 2][m + 1] = (DP[n + 2][m + 1] + DP[n][m]) % mod;
+            DP[n + 3][m + 1] = (DP[n + 3][m + 1] + DP[n][m]) % mod;
+        }
+
+    int T; scanf("%d", &T);
+    while (T--) {
+        int n, m; scanf("%d %d", &n, &m);
+        int ans = 0;
+        for (int i = 1; i <= m; ++i)
+            ans = (ans + DP[n][i]) % mod;
+        printf("%d\n", ans);
+    }
+    return 0;
+}
