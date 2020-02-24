f0d43cefa3eb435d182f3bd45029a0740e5a52b9
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-02-24 23:29:12 +0900

    라면 사기 (Small)

---
+++ baekjoon/18000/18185.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define scanf scanf_s
+inline int min(int a, int b) {
+    return (a < b ? a : b);
+}
+inline int min(int a, int b, int c) {
+    b = min(b, c);
+    return (a < b ? a : b);
+}
+int main() {
+    int N; scanf("%d", &N);
+    int a3 = 0, a5 = 0; // 이전 이전 케이스
+    int b3 = 0, b5 = 0; // 이전 케이스
+
+    int x, ans = 0;
+    while (N--) {
+        scanf("%d", &x);
+        int buy_5 = min(b3, x);
+        b3 -= buy_5, b5 += buy_5, ans += 2 * buy_5, x -= buy_5;
+
+        int buy_7 = min(a5, b5, x);
+        b5 -= buy_7, ans += 2 * buy_7, x -= buy_7;
+
+        ans += 3 * x;
+        a3 = b3, a5 = b5;
+        b3 = x, b5 = buy_5;
+    }
+    printf("%d", ans);
+    return 0;
+}
