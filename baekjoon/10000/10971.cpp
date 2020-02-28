e9ac49503cc03e5b007a64b2dd43b932839924e3
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-02-28 15:26:41 +0900

    외판원 순회 2

--- baekjoon/10000/10971.cpp
+++ baekjoon/10000/10971.cpp
@@ -1,33 +1,29 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 #define loop(i,n) for(int i=0;i<n;++i)
 using namespace std;
-const int max_N = 10;
 
-int n, dist[max_N][max_N], cache[max_N][1 << max_N];
-int TSP(int here, int visited)
+const int MAX = 10;
+int N, cost[MAX][MAX], cache[MAX][1 << MAX];
+
+int TSP(int u, int visited)
 {
-    if (visited == (1 << n) - 1) {
-        if (dist[here][0])
-            return dist[here][0];
-        return INT_MAX / 2;
-    }
-    int& ret = cache[here][visited];
+    if (visited == (1 << N) - 1)
+        return (cost[u][0] ? cost[u][0] : INT_MAX / 2);
+
+    int& ret = cache[u][visited];
     if (ret) return ret;
+
     ret = INT_MAX / 2;
-    loop(next, n) {
-        if (dist[here][next] == 0 || visited & (1 << next)) continue;
-        int cand = dist[here][next] + TSP(next, visited + (1 << next));
-        ret = min(ret, cand);
-    }
+    loop(v, N) if (cost[u][v] && !(visited & (1 << v)))
+        ret = min(ret, cost[u][v] + TSP(v, visited + (1 << v)));
     return ret;
 }
-int main()
-{
+int main() {
     cin.tie(NULL), cout.tie(NULL);
     ios::sync_with_stdio(false);
 
-    cin >> n;
-    loop(i, n) loop(j, n) cin >> dist[i][j];
+    cin >> N;
+    loop(i, N) loop(j, N) cin >> cost[i][j];
     cout << TSP(0, 1);
     return 0;
 }
