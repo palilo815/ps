3de0ef38e806afa85722b5b8f56a1595fb8fb34e
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-02-28 19:03:58 +0900

    감정이입

--- baekjoon/14000/14623.cpp
+++ baekjoon/14000/14623.cpp
@@ -1,35 +1,15 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
 using namespace std;
 
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
-
-    string a, b; cin >> a >> b;
-    reverse(a.begin(), a.end());
-    reverse(b.begin(), b.end());
-
-    vector<int> ans(100);
-    int len_a = a.size(), len_b = b.size();
-
-    // a * b
-    loop(i, len_b)
-        // b에서 1을 발견하면 그 위치만큼 a를 shift한 뒤 ans에 더해준다.
-        if (b[i] == '1')
-            loop(j, len_a)
-            ans[i + j] += a[j] - '0';
-    // 자릿수 올림
-    loop(i, 100)
-        if (ans[i] > 1) {
-            ans[i + 1] += ans[i] / 2;
-            ans[i] %= 2;
-        }
-
-    // 첫 자리가 0이면 전부 제거하고 출력
-    while (ans.back() == 0) ans.pop_back();
-    for (auto it = ans.rbegin(); it != ans.rend(); ++it)
-        cout << *it;
+int main() {
+    bitset<32> a;
+    bitset<32> b;
+    cin >> a >> b;
+    long long A = a.to_ullong(), B = b.to_ullong();
+    bitset<64> C(A * B);
+    string ans = C.to_string();
+    int i = 0;
+    while (ans[i] == '0') ++i;
+    while (i < ans.size()) cout << ans[i++];
     return 0;
 }
