09ed4a0f9e1a42e585046e53100c9c673bcb570d
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-02-28 16:45:39 +0900

    타일 채우기

--- baekjoon/2000/2133.cpp
+++ baekjoon/2000/2133.cpp
@@ -1,45 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
-int Cache[8][31];
 
-// 켜진 비트 수 return
-int NofB(int n)
-{
-    int ret = 0;
-    while (n) { n = n & (n - 1); ++ret; }
-    return ret;
-}
+const int max_N = 30;
+int cache[max_N][1 << 3];
 
 // 현재 타일 상태는 mask, n줄을 더 채워야함.
-int DP(int Mask, int n)
+int DP(int n, int mask)
 {
     // n==0 : 다 채운 경우
     // n==1 : 어차피 경우의 수는 하나
-    if (n <= 1) return 1;
-    int& ret = Cache[Mask][n];
+    if (n <= 0) return 1;
+    int& ret = cache[n][mask];
     if (ret != -1) return ret;
 
-    int num1 = NofB(Mask);
+    int num = __builtin_popcount(mask);
     // ㅁ : 타일, . : 빈 자리
     // 상 -> 하 방향으로 채운다.
 
     // .ㅁㅁ  ㅁ.ㅁ  ㅁㅁ.
     // 빈 공간에 세로 타일을 넣는 경우 한 가지.
-    if (num1 == 2)
-        ret = DP(Mask ^ 7, n - 1);
+    if (num == 2) ret = DP(n - 1, mask ^ 7);
     // ..ㅁ  ㅁ..
     // 빈 공간을 가로 타일로 완전 채우는 경우
     // 또는 세로 타일 두 개를 넣는 경우
     // .ㅁ. 이 경우는 세로타일 두 개 밖에 안됨.
-    else if (num1 == 1) {
-        if (Mask == 2) ret = DP(5, n - 1);
-        else ret = DP(0, n - 1) + DP(Mask ^ 7, n - 1);
+    else if (num == 1) {
+        if (mask == 2) ret = DP(n - 1, 5);
+        else ret = DP(n - 1, 0) + DP(n - 1, mask ^ 7);
     }
     // ...
     // 가로 타일을 왼쪽 또는 오른쪽에 놓는 경우 두 가지
     // 세로 타일 세 개를 놓는 경우
-    else
-        ret = DP(3, n) + DP(6, n) + DP(0, n - 2);
+    else ret = DP(n, 3) + DP(n, 6) + DP(n - 2, 0);
     return ret;
 }
 int main()
@@ -47,9 +39,9 @@ int main()
     cin.tie(NULL), cout.tie(NULL);
     ios::sync_with_stdio(false);
 
-    memset(Cache, -1, sizeof(Cache));
+    memset(cache, -1, sizeof(cache));
     int n; cin >> n;
     if (n % 2) cout << 0;
-    else cout << DP(0, n);
+    else cout << DP(n - 1, 0);
     return 0;
 }
