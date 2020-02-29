63c13dacda47073bde9c6ea5b6d81fe91470ef1c
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-02-29 13:49:20 +0900

    고속도로

--- baekjoon/1000/1884.cpp
+++ baekjoon/1000/1884.cpp
@@ -15,7 +15,7 @@ int main() {
     int K, N, M; cin >> K >> N >> M;
     while (M--) {
         int u, v, d, c; cin >> u >> v >> d >> c;
-        adj[u].push_back({ c,d,v });
+        adj[u].push_back({ d,c,v });
     }
     
     fill(&DP[0][0], &DP[max_N + 1][0], INT_MAX);
@@ -23,22 +23,20 @@ int main() {
     priority_queue<tup, vector<tup>, greater<tup>> pq;
     pq.push({ 0,0,1 });
 
+    int ans = -1;
     while (!pq.empty()) {
-        auto [c, d, u] = pq.top(); pq.pop();
-        if (c > K) break;
-        if (DP[u][c] < d) continue;
+        auto [d, c, u] = pq.top(); pq.pop();
+        if (c > K || DP[u][c] < d) continue;
+        if (u == N) { ans = d; break; }
 
-        for (auto& [cost, dist, v] : adj[u]) {
+        for (auto& [dist, cost, v] : adj[u]) {
             int C = c + cost, D = d + dist;
             if (DP[v][C] > D) {
                 DP[v][C] = D;
-                pq.push({ C,D,v });
+                pq.push({ D,C,v });
             }
         }
     }
-
-    int ans = INT_MAX;
-    for (int i = 0; i <= K; ++i) ans = min(ans, DP[N][i]);
-    cout << (ans == INT_MAX ? -1 : ans);
+    cout << ans;
     return 0;
 }
