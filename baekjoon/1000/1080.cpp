59fe2e8f7a382a02c5a66310ec5df0a19d3c6c5c
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-03-01 17:33:29 +0900

    행렬

--- baekjoon/1000/1080.cpp
+++ baekjoon/1000/1080.cpp
@@ -1,34 +1,37 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <bits/stdc++.h>
 #define loop(i,n) for(int i=0;i<n;++i)
+using namespace std;
 
-char matA[50][50], matB[50][50];
-void rev(int y, int x, int& ans)
-{
-    ++ans;
+const int MAX = 50;
+
+char A[MAX][MAX], B[MAX][MAX];
+
+void rev(int y, int x) {
     loop(i, 3) loop(j, 3) {
-        if (matA[y + i][x + j] == '0') matA[y + i][x + j] = '1';
-        else matA[y + i][x + j] = '0';
+        if (A[y + i][x + j] == '0') A[y + i][x + j] = '1';
+        else A[y + i][x + j] = '0';
     }
 }
-int main()
-{
+int main() {
     cin.tie(NULL), cout.tie(NULL);
     ios::sync_with_stdio(false);
 
-    int ans = 0, row, col;
+    int row, col;
     cin >> row >> col;
-    loop(i, row) loop(j, col) cin >> matA[i][j];
-    loop(i, row) loop(j, col) cin >> matB[i][j];
-    
-    // 뒤집기 
+    loop(i, row) loop(j, col) cin >> A[i][j];
+    loop(i, row) loop(j, col) cin >> B[i][j];
+
+    // 뒤집기
+    int ans = 0;
     loop(i, row - 2) loop(j, col - 2)
-        if (matA[i][j] != matB[i][j])
-            rev(i, j, ans);
+        if (A[i][j] != B[i][j]) {
+            rev(i, j);
+            ++ans;
+        }
 
     // 뒤집은 후에도 같지 않다면 -1
     loop(i, row) loop(j, col)
-        if (matA[i][j] != matB[i][j]) {
+        if (A[i][j] != B[i][j]) {
             ans = -1;
             break;
         }
