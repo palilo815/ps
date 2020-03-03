c5346dd0192b12f924bbafd177d14030d5e73e20
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-03-03 17:23:25 +0900

    크게 만들기

--- baekjoon/2000/2812.cpp
+++ baekjoon/2000/2812.cpp
@@ -1,27 +1,22 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
 using namespace std;
 
-const int max_N = 500000;
-
-char num[max_N];
-char r_max[max_N];
 
 int main() {
     cin.tie(NULL), cout.tie(NULL);
     ios::sync_with_stdio(false);
 
     int N, K; cin >> N >> K;
-    loop(i, N) cin >> num[i];
-    for (int i = N - 1; i > 0; --i)
-        r_max[i - 1] = max(num[i], r_max[i]);
-
-    string ans;
-    loop(i, N) {
-        if (num[i] < r_max[i] && K) --K;
-        else ans += num[i];
+    string num, ans;
+    cin >> num;
+    for (char c : num) {
+        while (K && !ans.empty() && ans.back() < c) {
+            ans.pop_back();
+            --K;;
+        }
+        ans += c;
     }
-    N = ans.size();
-    loop(i, N - K) cout << ans[i];
+    while (K--) ans.pop_back();
+    cout << ans;
     return 0;
 }
