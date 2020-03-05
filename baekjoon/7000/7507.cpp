0b6d41161318e30735a27832612cab15bfe5918f
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-03-05 14:31:02 +0900

    올림픽 게임

--- baekjoon/7000/7507.cpp
+++ baekjoon/7000/7507.cpp
@@ -1,38 +1,36 @@
 #include <bits/stdc++.h>
 #define loop(i,n) for(int i=0;i<n;++i)
-#define tup tuple<int,int,int>
 using namespace std;
+typedef tuple<int, int, int> tup;
 
 tup arr[50000];
-int main()
-{
+int main() {
     cin.tie(NULL), cout.tie(NULL);
     ios::sync_with_stdio(false);
-    
-    int T, N;
-    cin >> T;
+
+    int T; cin >> T;
     loop(t, T) {
-        cin >> N;
+        int N; cin >> N;
         loop(i, N) {
             int d, s, e; cin >> d >> s >> e;
-            arr[i] = make_tuple(d, e, s);
+            arr[i] = { d,e,s };
         }
         sort(arr, arr + N);
 
         int ans = 0;
         // 현재 날짜와 시간
-        int Day = -1, Time = 0;
+        int _day = -1, _time = 0;
         loop(i, N) {
-            int d = get<0>(arr[i]), e = get<1>(arr[i]), s = get<2>(arr[i]);
+            auto [d, e, s] = arr[i];
 
             // 날짜가 변하면 시간도 초기화
-            if (d != Day) Day = d, Time = 0;
+            if (d != _day) _day = d, _time = 0;
 
             // 시작 시간이 이미 지난 경우 무시
-            if (s < Time) continue;
+            if (s < _time) continue;
 
             // i번 경기를 본다
-            Time = e;
+            _time = e;
             ++ans;
         }
         cout << "Scenario #" << t + 1 << ":\n";
