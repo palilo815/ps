17a4bb06001a0cafe594067ffce7419b91923cfe
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-03-05 19:09:14 +0900

    암기왕

--- baekjoon/2000/2776.cpp
+++ baekjoon/2000/2776.cpp
@@ -1,26 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define loop(i,n) for(int i=0;i<n;++i)
 
-int main()
-{
+const int max_N = 1000000;
+
+int arr[max_N];
+
+int main() {
     cin.tie(NULL), cout.tie(NULL);
     ios::sync_with_stdio(false);
 
     int T; cin >> T;
     while (T--) {
-        set<int> st;
         int N; cin >> N;
-        while (N--) {
+        for (int i = 0; i < N; ++i) cin >> arr[i];
+        sort(arr, arr + N);
+        int M; cin >> M;
+        while (M--) {
             int x; cin >> x;
-            st.insert(x);
-        }
-        cin >> N;
-        while (N--) {
-            int x; cin >> x;
-            if (st.find(x) == st.end())
-                cout << 0 << '\n';
-            else cout << 1 << '\n';
+            cout << binary_search(arr, arr + N, x) << '\n';
         }
     }
     return 0;
