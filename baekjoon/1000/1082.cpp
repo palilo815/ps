7fd8a2cd75b4e825694d9bbeab5bbc0847fe36e4
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-03-05 22:52:15 +0900

    방 번호

--- baekjoon/1000/1082.cpp
+++ baekjoon/1000/1082.cpp
@@ -3,45 +3,38 @@
 using namespace std;
 
 int price[10];
-int selected[10];
-int minQ(int s, int e)
-{
-    int ret = INT_MAX;
-    for (int i = s; i <= e; ++i)
-        ret = min(ret, price[i]);
-    return ret;
-}
+
 int main()
 {
     int N; cin >> N;
     loop(i, N) cin >> price[i];
     int money; cin >> money;
-
     if (N == 1) { cout << 0; return 0; }
 
-    int min_cost = minQ(1, N - 1);
-    if (money < min_cost) { cout << 0; return 0; }
+    int a = N - 1;
+    for (int i = N - 2; i > 0; --i) if (price[i] < price[a])
+        a = i;
+    if (money < price[a]) { cout << 0; return 0; }
+    int b = (price[0] < price[a] ? 0 : a);
 
-    int len = 1 + ((money - min_cost) / min(min_cost, price[0]));
-    for (int i = N - 1; i > 0; --i) {
-        if (price[i] * len <= money) {
-            selected[i] = len;
-            len = 0;
-            break;
-        }
-        int tmp = minQ(0, i - 1), cnt = 0;
-        while (len && (money - price[i] >= tmp * (len - 1))) {
-            money -= price[i];
-            ++cnt, --len;
-        }
-        selected[i] = cnt;
+    vector<int> ans;
+    ans.emplace_back(a);
+    money -= price[a];
+    while (1) {
+        if (money - price[b] < 0) break;
+        ans.emplace_back(b);
+        money -= price[b];
     }
-    bool flag = true;
-    for (int i = N - 1; i > 0; --i) while (selected[i]--) {
-        cout << i;
-        flag = false;
+
+    for (int i = 0; money && i < ans.size(); ++i) {
+        int cur = ans[i];
+        for (int nxt = N - 1; nxt > cur; --nxt)
+            if (money >= price[nxt] - price[cur]) {
+                money -= price[nxt] - price[cur];
+                ans[i] = nxt;
+                break;
+            }
     }
-    if (flag) len = 1;
-    while (len--) cout << 0;
+    for (int x : ans) cout << x;
     return 0;
 }
