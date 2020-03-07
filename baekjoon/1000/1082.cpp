6bcb3a4dbb8cb3c0cd7a8eaebb6c17a42bb318a2
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-03-07 15:55:34 +0900

    방 번호

--- baekjoon/1000/1082.cpp
+++ baekjoon/1000/1082.cpp
@@ -1,27 +1,24 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
 using namespace std;
 
 int price[10];
 
-int main()
-{
+int main() {
     int N; cin >> N;
-    loop(i, N) cin >> price[i];
+    for (int i = 0; i < N; ++i)
+        cin >> price[i];
     int money; cin >> money;
-    if (N == 1) { cout << 0; return 0; }
 
     int a = N - 1;
     for (int i = N - 2; i > 0; --i) if (price[i] < price[a])
         a = i;
-    if (money < price[a]) { cout << 0; return 0; }
+    if (a == 0 || money < price[a]) { cout << 0; return 0; }
     int b = (price[0] < price[a] ? 0 : a);
 
     vector<int> ans;
     ans.emplace_back(a);
     money -= price[a];
-    while (1) {
-        if (money - price[b] < 0) break;
+    while (money >= price[b]) {
         ans.emplace_back(b);
         money -= price[b];
     }
