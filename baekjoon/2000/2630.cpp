0d42bb989e4905244395c176cb91d2cbacb53828
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-03-08 14:09:16 +0900

    색종이 만들기

--- baekjoon/2000/2630.cpp
+++ baekjoon/2000/2630.cpp
@@ -1,48 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
-int COLOR(int** arr, int x, int y, int len)
-{
-    int color = arr[x][y];
-    for (int i = x; i < x + len; i++) {
-        for (int j = y; j < y + len; j++) {
-            if (color != arr[i][j])
-                return -1;
-        }
-    }
-    return color;
+#define loop(i,n) for(int i=0;i<n;++i)
+
+const int max_N = 128;
+
+int white, blue;
+int colour[max_N][max_N];
+
+bool is_same(int r, int c, int len) {
+    int x = colour[r][c];
+    loop(i, len) loop(j, len)
+        if (colour[r + i][c + j] != x)
+            return false;
+    return true;
 }
-void NUM_ColoredPaper(int** arr, int x, int y, int len, int* sum_white, int* sum_blue)
-{
-    switch (COLOR(arr, x, y, len)) {
-    case -1:
-        NUM_ColoredPaper(arr, x, y, len / 2, sum_white, sum_blue);
-        NUM_ColoredPaper(arr, x, y + len / 2, len / 2, sum_white, sum_blue);
-        NUM_ColoredPaper(arr, x + len / 2, y, len / 2, sum_white, sum_blue);
-        NUM_ColoredPaper(arr, x + len / 2, y + len / 2, len / 2, sum_white, sum_blue);
-        break;
-    case 0:
-        (*sum_white)++;
-        break;
-    case 1:
-        (*sum_blue)++;
-        break;
+void solve(int r, int c, int len) {
+    if (is_same(r, c, len)) {
+        colour[r][c] ? ++blue : ++white;
+        return;
     }
+    len /= 2;
+    solve(r, c, len);
+    solve(r, c + len, len);
+    solve(r + len, c, len);
+    solve(r + len, c + len, len);
 }
-int main()
-{
-    int sum_white = 0, sum_blue = 0;
-    int len;
-    scanf("%d", &len);
-
-    int** arr = malloc(sizeof(int*) * len);
-    for (int i = 0; i < len; i++)
-        arr[i] = malloc(sizeof(int) * len);
-    for (int i = 0; i < len; i++)
-        for (int j = 0; j < len; j++)
-            scanf("%d", &arr[i][j]);
-
-    NUM_ColoredPaper(arr, 0, 0, len, &sum_white, &sum_blue);
-
-    printf("%d\n%d", sum_white, sum_blue);
+int main() {
+    int N; scanf("%d", &N);
+    loop(i, N) loop(j, N) scanf("%d", &colour[i][j]);
+    solve(0, 0, N);
+    printf("%d\n%d", white, blue);
     return 0;
 }
