b36a3b6eedbae74ed1994dbb9df58373433e4791
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-03-13 13:12:05 +0900

    체스판 다시 칠하기

--- baekjoon/1000/1018.cpp
+++ baekjoon/1000/1018.cpp
@@ -1,49 +1,32 @@
 #include <algorithm>
 #include <iostream>
+#define loop(i,n) for(int i=0;i<n;++i)
 using namespace std;
 
-char board_W[8][8];
+const int MAX = 50;
 
-int Recolor(char **arr, int a, int b) {
-    int count_W = 0, count_B = 0;
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            if (arr[a + i][b + j] == board_W[i][j])
-                count_B++;
-            else
-                count_W++;
-        }
-    }
-    return min(count_W, count_B);
+char board[MAX][MAX];
+char tmp[8][8];
+
+int solve(int r, int c) {
+    int W = 0, B = 0;
+    loop(i, 8) loop(j, 8)
+        board[r + i][c + j] == tmp[i][j] ? ++B : ++W;
+    return min(W, B);
 }
 
 int main() {
-    cin.tie(NULL);
-    std::ios::sync_with_stdio(false);
-
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            if ((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1))
-                board_W[i][j] = 'W';
-            else
-                board_W[i][j] = 'B';
-        }
+    loop(i, 8) loop(j, 8) {
+        if ((i + j) % 2) tmp[i][j] = 'B';
+        else tmp[i][j] = 'W';
     }
+    
+    int row, col; cin >> row >> col;
+    loop(i, row) loop(j, col) cin >> board[i][j];
 
-    int row, col;
-    cin >> row >> col;
-    char **arr = new char *[row];
-    for (int i = 0; i < row; i++)
-        arr[i] = new char[col];
-
-    for (int i = 0; i < row; i++)
-        for (int j = 0; j < col; j++)
-            cin >> arr[i][j];
-
-    int minimum = 64;
-    for (int i = 0; i <= row - 8; i++)
-        for (int j = 0; j <= col - 8; j++)
-            minimum = min(minimum, Recolor(arr, i, j));
-    cout << minimum;
+    int ans = 64;
+    loop(i, row - 7) loop(j, col - 7)
+        ans = min(ans, solve(i, j));
+    cout << ans;
     return 0;
 }
