ffbd895755aac09c04d58c4e50b83bb213949725
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-03-14 17:51:30 +0900

    벽 부수고 이동하기

--- baekjoon/2000/2206.cpp
+++ baekjoon/2000/2206.cpp
@@ -1,29 +1,33 @@
-#include<iostream>
-#include<queue>
-using namespace std;
+#include <iostream>
+#include <queue>
 #define loop(i,n) for(int i=0;i<n;++i)
+using namespace std;
 
-char maze[1000][1000];
-bool visited[2][1000][1000];
-int mov[4][2] = { -1,0,0,-1,0,1,1,0 };
+const int MAX = 1000;
+const int mov[4][2] = { -1,0,0,-1,0,1,1,0 };
 
-int main()
-{
+bool wall[MAX][MAX];
+bool visited[2][MAX][MAX];
+
+int main() {
     cin.tie(NULL), cout.tie(NULL);
-    std::ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
 
     int row, col; cin >> row >> col;
-    loop(i, row) loop(j, col)
-        cin >> maze[i][j];
+    char x;
+    loop(i, row) loop(j, col) {
+        cin >> x;
+        if (x == '1') wall[i][j] = true;
+    }
 
     queue<int> q;
-    q.push(0), q.push(-1);
+    q.emplace(0), q.emplace(-1);
     int ans = 1;
     while (1) {
         int crash = q.front(); q.pop();
         if (crash == -1) {
-            if (q.empty()) break;
-            ++ans, q.push(-1);
+            if (q.empty()) { ans = -1; break; }
+            ++ans, q.emplace(-1);
             continue;
         }
 
@@ -34,18 +38,16 @@ int main()
         loop(i, 4) {
             int R = r + mov[i][0], C = c + mov[i][1];
             if (R < 0 || R >= row || C < 0 || C >= col || visited[crash][R][C]) continue;
-            if (maze[R][C] == '0') {
+            if (!wall[R][C] && !visited[crash][R][C]) {
                 visited[crash][R][C] = true;
-                q.push(crash * 1024 * 1024 + R * 1024 + C);
+                q.emplace(crash * 1024 * 1024 + R * 1024 + C);
             }
-            else {
-                if (crash) continue;
+            else if (wall[R][C] && !crash && !visited[1][R][C]) {
                 visited[1][R][C] = true;
-                q.push(1024 * 1024 + R * 1024 + C);
+                q.emplace(1024 * 1024 + R * 1024 + C);
             }
         }
     }
-    if (q.empty()) cout << -1;
-    else cout << ans;
+    cout << ans;
     return 0;
 }
