0d8ae5b45a7a11f9fb1e6213817d74bbc2214685
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-03-17 15:45:38 +0900

    연구소

--- baekjoon/14000/14502.cpp
+++ baekjoon/14000/14502.cpp
@@ -1,52 +1,50 @@
 #include <bits/stdc++.h>
 #define loop(i,n) for(int i=0;i<n;++i)
 using namespace std;
-typedef pair<int, int> P;
-const int max_N = 8;
+typedef pair<int, int> p;
 
-int M[max_N][max_N], tmp[max_N][max_N];
+const int max_N = 8;
+const int mov[4][2] = { -1,0,0,-1,0,1,1,0 };
+
 int row, col, ans;
-int mov[4][2] = { -1,0,0,-1,0,1,1,0 };
-int safe_area()
-{
+int M[max_N][max_N], tmp[max_N][max_N];
+
+int safe_area() {
     memcpy(tmp, M, sizeof(M));
-    queue<P> q;
-    loop(i, row) loop(j, col)
-        if (M[i][j] == 2) q.push({ i,j });
+    queue<p> q;
+    loop(i, row) loop(j, col) if (M[i][j] == 2)
+        q.emplace(i, j);
 
     while (!q.empty()) {
-        int r = q.front().first, c = q.front().second; q.pop();
+        auto [r, c] = q.front(); q.pop();
         loop(i, 4) {
             int R = r + mov[i][0], C = c + mov[i][1];
             if (R < 0 || R >= row || C < 0 || C >= col || tmp[R][C]) continue;
             tmp[R][C] = 2;
-            q.push({ R,C });
-
+            q.emplace(R, C);
         }
     }
 
     int ret = 0;
     loop(i, row) loop(j, col)
-        if (tmp[i][j] == 0) ++ret;
+        if (!tmp[i][j]) ++ret;
     return ret;
 }
-void set_wall(int pos, int n)
-{
+void set_wall(int pos, int n) {
     if (n == 3) {
         ans = max(ans, safe_area());
         return;
     }
     for (int i = pos; i < row * col; ++i) {
         int r = i / col, c = i % col;
-        if (M[r][c] == 0) {
+        if (!M[r][c]) {
             M[r][c] = 1;
             set_wall(i + 1, n + 1);
             M[r][c] = 0;
         }
     }
 }
-int main()
-{
+int main() {
     cin.tie(NULL), cout.tie(NULL);
     ios::sync_with_stdio(false);
 
