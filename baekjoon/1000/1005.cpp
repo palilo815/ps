1014b10163e69fe472a9ff088ccc13a074d386cb
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-03-19 20:36:40 +0900

    ACM Craft

--- baekjoon/1000/1005.cpp
+++ baekjoon/1000/1005.cpp
@@ -1,54 +1,38 @@
 #include<bits/stdc++.h>
+#define LOOP(i,n) for(int i=1;i<=n;++i)
 using namespace std;
-#define loop(i,n) for(int i=0;i<n;++i)
-#define P pair<int,int>
 
-//cost : 건물 짓는 시간, Pre : 먼저 지어놔야 하는 건물 개수
-int V, Cost[1001], Pre[1001];
-vector<int> adj[1001];
+const int max_N = 1000;
 
-int main()
-{
+vector<int> rev[max_N + 1];
+int t[max_N + 1];
+int total[max_N + 1];
+
+int DFS(int v) {
+    int cost = 0;
+    for (int u : rev[v]) {
+        if (total[u] == -1) DFS(u);
+        cost = max(cost, total[u]);
+    }
+    return total[v] = t[v] + cost;
+}
+int main() {
     cin.tie(NULL), cout.tie(NULL);
     ios::sync_with_stdio(false);
 
-    int t, E, goal;
-    cin >> t;
-    while (t-- > 0) {
-        for (auto& v : adj) v.clear();
-        memset(Pre, 0, sizeof(Pre));
-
-        cin >> V >> E;
-        for (int i = 1; i <= V; ++i) cin >> Cost[i];
-        while (E-- > 0) {
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-            Pre[v]++;
+    int T; cin >> T;
+    while (T--) {
+        int N, M; cin >> N >> M;
+        memset(total, -1, sizeof(int) * (N + 1));
+        LOOP(i, N) rev[i].clear();
+        LOOP(i, N) cin >> t[i];
+        while (M--) {
+            int u, v; cin >> u >> v;
+            rev[v].emplace_back(u);
         }
 
-        cin >> goal;
-        priority_queue<P, vector<P>, greater<P>> pq;
-        // !Pre[i] == i를 짓기 위해 필요한 테크 건물들을 다 지었다.
-        // pq에 건설 가능한 건물들을 넣는다.
-        for (int i = 1; i <= V; ++i)
-            if (!Pre[i]) pq.push(make_pair(Cost[i], i));
-        while (1) {
-            // 지금 지은 건물은 build, 현재 시각 T
-            int T = pq.top().first, build = pq.top().second; pq.pop();
-            // 목표 건물 goal을 지으면 끝
-            if (build == goal) { cout << T << '\n'; break; }
-
-            // bulid를 지음으로써 추가로 건설 가능한 건물들을 pq에 추가
-            loop(i, adj[build].size()) {
-                int next = adj[build][i];
-                Pre[next]--;
-                if (!Pre[next]) {
-                    int nextT = T + Cost[next];
-                    pq.push(make_pair(nextT, next));
-                }
-            }
-        }
+        int dst; cin >> dst;
+        cout << DFS(dst) << '\n';
     }
     return 0;
 }
