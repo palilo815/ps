db5e84b850f6329fa750612b439a3cd4d3fe28d3
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-03-23 14:38:51 +0900

    RGB거리

--- baekjoon/1000/1149.cpp
+++ baekjoon/1000/1149.cpp
@@ -1,43 +1,21 @@
-#include<iostream>
-#include<algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-int n;
-int** cost;
-int** cache;
 
-int MinCost(int x, int color)
-{
-    if (x == n) return 0;
+int main() {
+    cin.tie(NULL), cout.tie(NULL);
+    ios::sync_with_stdio(false);
 
-    int& ret = cache[x][color];
-    if (ret != 0) return ret;
-    if (color == 0)
-        return ret = min(cost[x][1] + MinCost(x + 1, 1), cost[x][2] + MinCost(x + 1, 2));
-    else if (color == 1)
-        return ret = min(cost[x][0] + MinCost(x + 1, 0), cost[x][2] + MinCost(x + 1, 2));
-    else
-        return ret = min(cost[x][0] + MinCost(x + 1, 0), cost[x][1] + MinCost(x + 1, 1));
-}
+    int r, g, b, rr, gg, bb;
+    int R = 0, G = 0, B = 0;
 
-int main()
-{
-    cin.tie(NULL);
-    std::ios::sync_with_stdio(false);
-
-    cin >> n;
-    cost = new int* [n];
-    cache = new int* [n];
-
-    for (int i = 0; i < n; i++)
-        cost[i] = new int[3];
-    for (int i = 0; i < n; i++)
-        cache[i] = new int[3]{};
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < 3; j++)
-            cin >> cost[i][j];
-
-    int ans = min(cost[0][0] + MinCost(1, 0), cost[0][1] + MinCost(1, 1));
-    ans = min(ans, cost[0][2] + MinCost(1, 2));
-    cout << ans;
+    int N; cin >> N;
+    while (N--) {
+        cin >> r >> g >> b;
+        rr = min(G, B) + r;
+        gg = min(B, R) + g;
+        bb = min(R, G) + b;
+        R = rr, G = gg, B = bb;
+    }
+    cout << min({ R,G,B });
     return 0;
 }
