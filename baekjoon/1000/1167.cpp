2aa3dfedce720623d2832c290ef9a6c6e286cf63
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-03-25 16:25:09 +0900

    트리의 지름

--- baekjoon/1000/1167.cpp
+++ baekjoon/1000/1167.cpp
@@ -1,18 +1,30 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> 
 using namespace std;
-typedef tuple<int, int, int> tup;
+typedef pair<int, int> p;
 
 const int max_V = 100000;
 
-vector<tup> adj[max_V + 1];
+vector<p> adj[max_V + 1];
+int dia[max_V + 1];
+bool visited[max_V + 1];
 
-int DFS(int u, int p) {
-    int ret = 0;
-    for (auto& [w, v, h] : adj[u]) if(v != p) {
-        if (!h) h = DFS(v, u);
-        ret = max(ret, h + w);
+int BFS(int src, int V) {
+    memset(visited, 0, sizeof(visited));
+    memset(dia, 0, sizeof(dia));
+
+    queue<int> q;
+    q.emplace(src);
+    visited[src] = true;
+
+    while (!q.empty()) {
+        int u = q.front(); q.pop();
+        for (auto [w, v] : adj[u]) if (!visited[v]) {
+            visited[v] = true;
+            dia[v] += dia[u] + w;
+            q.emplace(v);
+        }
     }
-    return ret;
+    return int(max_element(dia + 1, dia + V + 1) - dia);
 }
 int main() {
     cin.tie(NULL), cout.tie(NULL);
@@ -26,13 +38,12 @@ int main() {
             cin >> v;
             if (v == -1) break;
             cin >> w;
-            adj[u].emplace_back(w, v, 0);
+            adj[u].emplace_back(w, v);
         }
     }
-    
-    int ans = 0;
-    for (int i = 1; i <= V; ++i)
-        ans = max(ans, DFS(i, 0));
-    cout << ans;
+
+    int src = BFS(1, V);
+    int ans = BFS(src, V);
+    cout << dia[ans];
     return 0;
 }
