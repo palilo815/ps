eaa6c7689e80bd0324e2937ec58d1784eaa1d457
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-03-28 16:07:09 +0900

    팰린드롬 만들기

--- baekjoon/1000/1213.cpp
+++ baekjoon/1000/1213.cpp
@@ -1,42 +1,35 @@
 #include<bits/stdc++.h>
-using namespace std;
 #define loop(i,n) for(int i=0;i<n;++i)
+using namespace std;
 
 int cnt[26];
-void print_str(vector<char>& vt, char c) {
-    for (char x : vt) cout << x;
-    if (c) cout << c;
-    for (int i = vt.size() - 1; i >= 0; --i) cout << vt[i];
-}
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
-
-    vector<char> vt;
+int main() {
     string s; cin >> s;
-    loop(i, s.size()) ++cnt[s[i] - 'A'];
+    for (char c : s) ++cnt[c - 'A'];
 
-    // 홀수인 문자의 개수를 센다.
     int odd = 0;
-    loop(i, 26) if (cnt[i] % 2) ++odd;
-
-    // 만약에 odd == 1이면 홀수인 문자를 중앙에 넣어야 한다.
-    if (odd <= 1) {
-        // odd == 0인 경우
-        // c는 그대로 '\0'인 상태로 남아있는다.
-        char c = 0;
-        loop(i, 26) {
-            if (cnt[i] % 2 == 1) {
-                c = i + 'A';
-                --cnt[i];
-            }
-            int n = cnt[i] / 2;
-            while (n--) vt.push_back(i + 'A');
-        }
-        print_str(vt, c);
+    char odd_char = '\0';
+    loop(i, 26) if (cnt[i] % 2) {
+        ++odd;
+        odd_char = i + 'A';
     }
-    // odd == 2면 불가능하다.
-    else cout << "I'm Sorry Hansoo";
+
+    if (odd > 1) {
+        cout << "I'm Sorry Hansoo";
+        return 0;
+    }
+
+    stack<char> stk;
+    loop(i, 26)  while (cnt[i] >= 2) {
+        cout << (char)(i + 'A');
+        stk.emplace(i + 'A');
+        cnt[i] -= 2;
+    }
+
+    if (odd_char) cout << odd_char;
+
+    while (!stk.empty()) {
+        cout << stk.top(); stk.pop();
+    }
     return 0;
 }
