1f8aa1b526a01a09dc27c09ef828d9ab07cdcea3
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-03-29 18:05:26 +0900

    N-Queen

--- baekjoon/9000/9663.cpp
+++ baekjoon/9000/9663.cpp
@@ -1,51 +1,26 @@
 #include <iostream>
 using namespace std;
-#define loop(i,n) for(int i=0;i<n;++i)
 
-int n, board[15][15], ans;
+int N, board[15][15], ans;
 
-// 내 공격범위의 칸을 num으로 칠한다.
-void colour(int r, int c, int num)
-{
-    for (int i = r + 1; i < n; ++i)
-        if (!board[i][c]) board[i][c] = num;
-    for (int i = 1;r + i < n && c - i >= 0;++i)
-        if (!board[r + i][c - i]) board[r + i][c - i] = num;
-    for (int i = 1;r + i < n && c + i < n;++i)
-        if (!board[r + i][c + i]) board[r + i][c + i] = num;
+void check(int r, int c, int val) {
+    for (int i = r + 1; i < N; ++i) board[i][c] += val;
+    for (int i = r + 1, j = c - 1; i < N && j >= 0; ++i, --j) board[i][j] += val;
+    for (int i = r + 1, j = c + 1; i < N && j < N; ++i, ++j) board[i][j] += val;
 }
-
-// board에서 num을 전부 지운다.
-void erase(int r, int c, int num)
-{
-    for (int i = r + 1; i < n; ++i)
-        if (board[i][c] == num) board[i][c] = 0;
-    for (int i = 1;r + i < n && c - i >= 0;++i)
-        if (board[r + i][c - i] == num) board[r + i][c - i] = 0;
-    for (int i = 1;r + i < n && c + i < n;++i)
-        if (board[r + i][c + i] == num) board[r + i][c + i] = 0;
-}
-
-// 퀸을 놓는다. 현재 놓여있는 퀸은 count개다.
-void placing(int count)
-{
-    if (count == n) {
+void placing(int row) {
+    if (row == N) {
         ++ans;
         return;
     }
-    loop(i, n)
-        if (!board[count][i]) {
-            colour(count, i, count + 1);
-            placing(count + 1);
-            erase(count, i, count + 1);
-        }
+    for (int i = 0; i < N; ++i) if (!board[row][i]) {
+        check(row, i, 1);
+        placing(row + 1);
+        check(row, i, -1);
+    }
 }
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
-
-    cin >> n;
+int main() {
+    cin >> N;
     placing(0);
     cout << ans;
     return 0;
