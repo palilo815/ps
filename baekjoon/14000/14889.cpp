c01c0431717ded456627d6317bc2e7c6bf2de4fb
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-03-30 15:01:14 +0900

    스타트와 링크

--- baekjoon/14000/14889.cpp
+++ baekjoon/14000/14889.cpp
@@ -1,39 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define loop(i,n) for(int i=0;i<n;++i)
 using namespace std;
 
-int n, arr[20][20];
-bool TeamS[20];
-int DFS(int prev, int team)
-{
-    TeamS[prev] = true;
-    int ret;
-    if (team == n / 2) {
-        int Start = 0, Link = 0;
-        loop(i, n) loop(j, n) {
-            if (TeamS[i] && TeamS[j]) Start += arr[i][j];
-            else if (!TeamS[i] && !TeamS[j]) Link += arr[i][j];
-        }
-        ret = abs(Start - Link);
-    }
-    else {
-        ret = INT_MAX;
-        for (int i = prev + 1; i < n; ++i)
-            ret = min(ret, DFS(i, team + 1));
-    }
-    TeamS[prev] = false;
-    return ret;
-}
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+int power[20][20];
 
-    cin >> n;
-    loop(i, n) loop(j, n) cin >> arr[i][j];
+int main() {
+    int N; cin >> N;
+    loop(i, N) loop(j, N) cin >> power[i][j];
+
+    vector<bool> select(N);
+    loop(i, N / 2) select[i] = true;
+
     int ans = INT_MAX;
-    loop(i, n / 2)
-        ans = min(ans, DFS(i, 1));
+    do {
+        int start = 0, link = 0;
+        loop(i, N) loop(j, N)
+            if (select[i] && select[j]) start += power[i][j];
+            else if (!select[i] && !select[j]) link += power[i][j];
+        ans = min(ans, abs(start - link));
+    } while (prev_permutation(select.begin(), select.end()));
     cout << ans;
     return 0;
 }
