77ebd15a67ff4fc19d910496b7da011c42e34a48
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-03-31 14:44:53 +0900

    로프

--- baekjoon/2000/2217.cpp
+++ baekjoon/2000/2217.cpp
@@ -1,25 +1,25 @@
-#include<iostream>
-#include<algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    cin.tie(NULL);
-    std::ios::sync_with_stdio(false);
+const int max_N = 100000;
 
-    int n;
-    cin >> n;
-    int* weight = new int[n];
-    for (int i = 0; i < n; i++)
-        cin >> weight[i];
-    sort(weight, weight + n);
-    int count = 1, max = 0;
-    for (int i = n - 1; i >= 0; i--) {
-        int tmp = weight[i] * count;
-        if (tmp > max)
-            max = tmp;
-        count++;
+int w[max_N];
+
+int main() {
+    cin.tie(NULL), cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int N; cin >> N;
+    for (int i = 0; i < N; ++i) cin >> w[i];
+    sort(w, w + N);
+
+    int cnt = 1, ans = 0;
+    for (int i = N - 1; i >= 0; --i) {
+        int tmp = w[i] * cnt;
+        if (tmp > ans)
+            ans = tmp;
+        ++cnt;
     }
-    cout << max;
+    cout << ans;
     return 0;
 }
