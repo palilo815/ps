8545acc6531585734a573541bb343054ad63a52a
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-01 13:09:47 +0900

    집합의 표현

--- baekjoon/1000/1717.cpp
+++ baekjoon/1000/1717.cpp
@@ -1,42 +1,28 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-#define loop(i,n) for(int i=0;i<n;++i)
 
-struct DisjointSet {
-    vector<int> parent, rank;
-    DisjointSet(int n) : parent(n), rank(n, 1) {
-        loop(i, n) parent[i] = i;
-    }
-    int Find(int u) {
-        if (u == parent[u]) return u;
-        return parent[u] = Find(parent[u]);
-    }
-    void Merge(int u, int v) {
-        u = Find(u), v = Find(v);
-        if (u == v) return;
-        if (rank[u] > rank[v]) swap(u, v);
-        parent[u] = v;
-        if (rank[u] == rank[v]) ++rank[v];
-    }
-};
+const int max_N = 1000000;
 
+int parent[max_N + 1];
 
-int main()
-{
+int find_rt(int u) {
+    if (u == parent[u]) return u;
+    return parent[u] = find_rt(parent[u]);
+}
+void merge(int u, int v) {
+    u = find_rt(u), v = find_rt(v);
+    parent[u] = v;
+}
+int main() {
     cin.tie(NULL), cout.tie(NULL);
     ios::sync_with_stdio(false);
 
-    int n, m; cin >> n >> m;
-    DisjointSet Dset(n + 1);
-    while (m--) {
-        int op, u, v;
-        cin >> op >> u >> v;
-        if (op) {
-            if (Dset.Find(u) == Dset.Find(v)) cout << "YES\n";
-            else cout << "NO\n";
-        }
-        else
-            Dset.Merge(u, v);
+    int N, M; cin >> N >> M;
+    for (int i = 0; i <= N; ++i) parent[i] = i;
+    while (M--) {
+        int op, u, v; cin >> op >> u >> v;
+        if (op) cout << (find_rt(u) == find_rt(v) ? "YES" : "NO") << '\n';
+        else merge(u, v);
     }
     return 0;
 }
