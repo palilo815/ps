dad5e128dc8c3aae760b52b8ecee03863a7c2230
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-01 15:17:38 +0900

    비슷한 단어

--- baekjoon/2000/2607.cpp
+++ baekjoon/2000/2607.cpp
@@ -1,26 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define loop(i,n) for(int i=0;i<n;++i)
 
-int key_cnt[26], s_cnt[26];
-int main()
-{
+int A[26], B[26];
+int main() {
     cin.tie(NULL), cout.tie(NULL);
     ios::sync_with_stdio(false);
 
     int N; cin >> N;
     string key; cin >> key;
-    loop(i, key.size()) ++key_cnt[key[i] - 'A'];
 
+    for (char c : key) ++A[c - 'A'];
     int ans = 0;
-    while (N-- > 1) {
-        memset(s_cnt, 0, sizeof(s_cnt));
+    while (--N) {
+        memset(B, 0, sizeof(B));
         string s; cin >> s;
-        loop(i, s.size()) ++s_cnt[s[i] - 'A'];
+        for (char c : s) ++B[c - 'A'];
 
         int gap = 0;
-        loop(i, 26)
-            gap += abs(key_cnt[i] - s_cnt[i]);
+        for (int i = 0; i < 26; ++i)
+            gap += abs(A[i] - B[i]);
         if (gap <= 2 && abs((int)s.size() - (int)key.size()) <= 1) ++ans;
     }
     cout << ans;
