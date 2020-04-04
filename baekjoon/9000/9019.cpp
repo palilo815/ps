5cf5ec3ae37e0ca64c06ea01515e1e37d9da3c5f
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-04 16:08:13 +0900

    DSLR

--- baekjoon/9000/9019.cpp
+++ baekjoon/9000/9019.cpp
@@ -2,89 +2,54 @@
 using namespace std;
 #define loop(i,n) for(int i=0;i<n;++i)
 
-int cache[10000];
-inline int D(int n)
-{
-    return (n * 2) % 10000;
-}
-inline int S(int n)
-{
-    if (n != 0) return n - 1;
-    return 9999;
-}
-inline int L(int n)
-{
+int cache_N[10000];
+char cache_C[10000];
+
+inline int D(int n) { return (n * 2) % 10000; }
+inline int S(int n) { return n ? n - 1 : 9999; }
+inline int L(int n) {
     int msb = n / 1000;
     return (n - msb * 1000) * 10 + msb;
 }
-inline int R(int n)
-{
+inline int R(int n) {
     int mlb = n % 10;
     return n / 10 + mlb * 1000;
 }
-void pushing(queue<int>& q, int n)
-{
-    int tmp;
-    tmp = D(n);
-    if (cache[tmp] == -1) {
-        cache[tmp] = n;
-        q.push(tmp);
-    }
-    tmp = S(n);
-    if (cache[tmp] == -1) {
-        cache[tmp] = n;
-        q.push(tmp);
-    }
-    tmp = L(n);
-    if (cache[tmp] == -1) {
-        cache[tmp] = n;
-        q.push(tmp);
-    }
-    tmp = R(n);
-    if (cache[tmp] == -1) {
-        cache[tmp] = n;
-        q.push(tmp);
-    }
+void pushing(queue<int>& q, int u, int v, char order) {
+    if (cache_C[v]) return;
+    q.emplace(v);
+    cache_C[v] = order;
+    cache_N[v] = u;
 }
-void trace_back(int last)
-{
-    int curr = last, prev = cache[last];
-    stack<char> stk;
-    while (prev != -2) {
-        if (D(prev) == curr) stk.push('D');
-        else if (S(prev) == curr) stk.push('S');
-        else if (L(prev) == curr) stk.push('L');
-        else stk.push('R');
-        curr = prev;
-        prev = cache[prev];
-    }
-    while (!stk.empty()) {
-        cout << stk.top();
-        stk.pop();
-    }
-    cout << '\n';
+void print_ans(int u, int v) {
+    if (u == v) return;
+    print_ans(u, cache_N[v]);
+    cout << cache_C[v];
 }
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
-    int t, u, v;
-    cin >> t;
-    while (t--) {
-        memset(cache, -1, sizeof(cache));
-        cin >> u >> v;
+    int T; cin >> T;
+    while (T--) {
+        memset(cache_C, 0, sizeof(cache_C));
+        int src, dst; cin >> src >> dst;
+
         queue<int> q;
-        q.push(u);
-        cache[u] = -2;
+        q.emplace(src);
+        cache_C[src] = '~';
+
         while (1) {
-            int n = q.front(); q.pop();
-            if (n == v) {
-                trace_back(n);
-                break;
-            }
-            pushing(q, n);
+            int u = q.front(); q.pop();
+            if (u == dst) break;
+            pushing(q, u, D(u), 'D');
+            pushing(q, u, S(u), 'S');
+            pushing(q, u, L(u), 'L');
+            pushing(q, u, R(u), 'R');
         }
+
+        print_ans(src, dst);
+        cout << '\n';
     }
     return 0;
 }
