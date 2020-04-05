4721290ddfc78efb6e8a8b6f9f7e04889d809369
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-05 14:09:33 +0900

    녹색 옷 입은 애가 젤다지?

--- baekjoon/4000/4485.cpp
+++ baekjoon/4000/4485.cpp
@@ -1,40 +1,45 @@
 #include <bits/stdc++.h>
 #define loop(i,n) for(int i=0;i<n;++i)
-#define P pair<int,int>
 using namespace std;
-    
-int M[125][125], dist[125][125], mov[4][2] = { -1,0,0,-1,0,1,1,0 };
-int Dijkstra(int N)
-{
-    loop(i, N) loop(j, N) dist[i][j] = INT32_MAX;
+typedef tuple<int, int, int> tup;
+
+const int max_N = 125;
+const int mov[4][2] = { -1,0,0,-1,0,1,1,0 };
+
+int M[max_N][max_N];
+int dist[max_N][max_N];
+
+int Dijkstra(int N) {
+    loop(i, N) memset(dist[i], 0x3f, sizeof(int) * N);
     dist[0][0] = M[0][0];
-    priority_queue<P, vector<P>, greater<P>> pq;
-    pq.push({ M[0][0],0 });
+
+    priority_queue<tup, vector<tup>, greater<tup>> pq;
+    pq.emplace(M[0][0], 0, 0);
+
     while (1) {
-        int cost = pq.top().first, r = pq.top().second / 128, c = pq.top().second % 128; pq.pop();
-        if (r == N - 1 && c == N - 1) return cost;
-        if (dist[r][c] < cost) continue;
+        auto [d, r, c] = pq.top(); pq.pop();
+        if (dist[r][c] < d) continue;
+        if (r == N - 1 && c == N - 1) return d;
+
         loop(i, 4) {
             int R = r + mov[i][0], C = c + mov[i][1];
             if (R < 0 || R >= N || C < 0 || C >= N) continue;
-            int next_cost = cost + M[R][C];
-            if (dist[R][C] > next_cost) {
-                dist[R][C] = next_cost;
-                pq.push({ next_cost,R * 128 + C });
+            int D = d + M[R][C];
+            if (dist[R][C] > D) {
+                dist[R][C] = D;
+                pq.emplace(D, R, C);
             }
         }
     }
 }
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
     int num = 0, N;
     while (cin >> N) {
         if (N == 0) break;
-        loop(i, N) loop(j, N)
-            cin >> M[i][j];
+        loop(i, N) loop(j, N) cin >> M[i][j];
         cout << "Problem " << ++num << ": " << Dijkstra(N) << '\n';
     }
     return 0;
