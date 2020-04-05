123e9a6fb876807734a0fb6828a0cc33a5355636
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-05 13:39:20 +0900

    미확인 도착지

--- baekjoon/9000/9370.cpp
+++ baekjoon/9000/9370.cpp
@@ -1,46 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef pair<int, int> p;
-typedef tuple<int, int, int> tup;
 
 const int max_V = 2000;
 
 vector<p> adj[max_V + 1];
-int dist[max_V + 1];
-bool possible[max_V + 1];
+int d1[max_V + 1], d2[max_V + 1];
 
-void Dijkstra(int V, int s, int g, int h) {
-    memset(possible, 0, V + 1);
-    fill(dist + 1, dist + V + 1, INT_MAX);
+void Dijkstra(int V, int s, int dist[]) {
+    memset(dist, 0x3f, sizeof(int) * (V + 1));
     dist[s] = 0;
 
-    priority_queue<tup, vector<tup>, greater<tup>> pq;
-    pq.emplace(0, s, 0);
+    priority_queue<p, vector<p>, greater<p>> pq;
+    pq.emplace(0, s);
 
     while (!pq.empty()) {
-        auto [d, u, pass] = pq.top(); pq.pop();
+        auto [d, u] = pq.top(); pq.pop();
         if (dist[u] < d) continue;
 
         for (auto [w, v] : adj[u]) {
-            bool flag = ((u == g && v == h) || (u == h && v == g)) ?
-                true : pass;
             int D = d + w;
-
             if (dist[v] > D) {
                 dist[v] = D;
-                possible[v] = flag;
-                pq.emplace(D, v, flag);
-            }
-            else if (dist[v] == D && flag) {
-                possible[v] = flag;
-                pq.emplace(D, v, flag);
+                pq.emplace(D, v);
             }
         }
     }
 }
 int main() {
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
     int T; cin >> T;
     while (T--) {
@@ -54,12 +43,21 @@ int main() {
             adj[v].emplace_back(w, u);
         }
 
-        Dijkstra(V, src, g, h);
+        Dijkstra(V, src, d1);
+
+        int m = 0;
+        for (auto [w, node] : adj[g]) if (node == h) {
+            m = w;
+            break;
+        }
 
         vector<int> ans;
         while (dst--) {
             int x; cin >> x;
-            if (possible[x]) ans.emplace_back(x);
+            Dijkstra(V, x, d2);
+            int tmp = d1[x] - m;
+            if (d1[g] + d2[h] == tmp || d1[h] + d2[g] == tmp)
+                ans.emplace_back(x);
         }
 
         sort(ans.begin(), ans.end());
