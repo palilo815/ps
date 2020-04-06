9da587e8d363362f85e686be2891a309d204df22
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-06 17:45:33 +0900

    온라인 판매

---
+++ baekjoon/1000/1246.py
@@ -0,0 +1,12 @@
+N, M = map(int, input().split())
+price = []
+for i in range(M):
+    price.append(int(input()))
+price.sort(reverse=True)
+money, ans = 0, 0
+for i in range(M):
+    tmp = price[i] * (i + 1)
+    if tmp >= money:
+        money = tmp
+        ans = price[i]
+print(ans, money)
