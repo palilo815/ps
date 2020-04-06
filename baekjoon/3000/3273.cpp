6339483506240f33c10f5ad758298929da56cf90
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-06 13:51:07 +0900

    두 수의 합

--- baekjoon/3000/3273.cpp
+++ baekjoon/3000/3273.cpp
@@ -8,6 +8,7 @@ int arr[max_N];
 int main() {
     cin.tie(0), cout.tie(0);
     ios::sync_with_stdio(0);
+
     int N; cin >> N;
     for (int i = 0; i < N; ++i) cin >> arr[i];
     sort(arr, arr + N);
