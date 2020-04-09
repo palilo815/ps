a39166cfcc965dfef825333ba72036b10c3708da
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-09 22:21:17 +0900

    Breed Counting

---
+++ baekjoon/11000/11969.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N, Q = map(int, input().split())
+psum = [[0, 0, 0]]
+while N:
+    N -= 1
+    x = int(input())
+    psum.append(psum[-1].copy())
+    psum[-1][x - 1] += 1
+while Q:
+    Q -= 1
+    l, r = map(int, input().split())
+    for i in range(3):
+        print(psum[r][i] - psum[l - 1][i], end=' ')
+    print()
