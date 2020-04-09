dcad0b29580bbdfd6ad379a0c085c95837fadd4c
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-09 20:51:01 +0900

    블록 놀이

--- baekjoon/16000/16951.py
+++ baekjoon/16000/16951.py
@@ -1,13 +1,19 @@
 import sys
+
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
 h = list(map(int, input().split()))
-ans = 0
+ans = 1000
+
 for i in range(N):
     curr = h[i]
+    if curr - K * i <= 0:
+        continue
+    cnt = i
     for j in range(i, N):
-        if h[j] == curr:
-            curr += K
-    ans = max(ans, (curr - h[i]) // K)
-print(N - ans)
+        if h[j] != curr:
+            cnt += 1
+        curr += K
+    ans = min(ans, cnt)
+print(ans)
