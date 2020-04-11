272c94fb7b1bff934c618d7ea27412b753339e59
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-11 17:56:34 +0900

    직사각형과 쿼리

--- baekjoon/14000/14846.py
+++ baekjoon/14000/14846.py
@@ -6,10 +6,10 @@ mat = []
 for i in range(N):
     mat.append(list(map(int, input().split())))
 
-DP = [[[0] * 10 for j in range(N + 1)] for k in range(N + 1)]
+DP = [[[0] * 11 for j in range(N + 1)] for k in range(N + 1)]
 for i in range(1, N + 1):
     for j in range(1, N + 1):
-        for k in range(1, 10):
+        for k in range(1, 11):
             DP[i][j][k] = -DP[i - 1][j - 1][k] + DP[i][j - 1][k] + DP[i - 1][j][k]
         DP[i][j][mat[i - 1][j - 1]] += 1
 
@@ -18,7 +18,7 @@ while Q:
     Q -= 1
     x1, y1, x2, y2 = map(int, input().split())
     ans = 0
-    for k in range(1, 10):
+    for k in range(1, 11):
         if DP[x2][y2][k] + DP[x1 - 1][y1 - 1][k] - DP[x2][y1 - 1][k] - DP[x1 - 1][y2][k]:
             ans += 1
     print(ans)
