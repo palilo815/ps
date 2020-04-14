712ae30cfa45f298366d9642007bdd039d5e3ebd
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-15 02:10:27 +0900

    나는야 포켓몬 마스터 이다솜

--- baekjoon/1000/1620.cpp
+++ baekjoon/1000/1620.cpp
@@ -1,33 +1,29 @@
-#include<iostream>
-#include<algorithm>
-#include<string>
-#include<unordered_map>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    cin.tie(NULL);
-    std::ios::sync_with_stdio(false);
+const int max_N = 100000;
 
-    int n, m;
-    string s;
-    cin >> n >> m;
-    string* arr = new string[n];
+string arr[max_N];
+
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
+
     unordered_map<string, int> ump;
-
-    for (int i = 0; i < n;i++) {
-        cin >> s;
-        arr[i] = s;
-        ump[s] = i + 1;
+    int N, Q; cin >> N >> Q;
+    for (int i = 0; i < N; ++i) {
+        cin >> arr[i];
+        ump[arr[i]] = i + 1;
     }
-    while (m-- > 0) {
-        cin >> s;
-        if (s[0] < 'A') {
-            int index = stoi(s);
-            cout << arr[index - 1] << '\n';
+
+    while (Q--) {
+        string q; cin >> q;
+        if (q[0] < 'A') {
+            int idx = stoi(q);
+            cout << arr[idx - 1];
         }
-        else
-            cout << ump.find(s)->second << '\n';
+        else cout << ump.find(q)->second;
+        cout << '\n';
     }
     return 0;
 }
