72031d1384726c070540eca4b563b402e69bfe34
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-14 13:02:30 +0900

    팩토리얼 0의 개수

--- baekjoon/11000/11687.py
+++ baekjoon/11000/11687.py
@@ -7,10 +7,17 @@ def solve(n):
 
 
 M = int(input())
-ans = 5 * M
-while True:
-    cnt = solve(ans)
+ans = -1
+l, r = 1, 100000000
+while l <= r:
+    m = (l + r) >> 1
+    n = 5 * m
+    cnt = solve(n)
     if cnt <= M:
-        break
-    ans -= 5
-print(ans if cnt == M else -1)
+        if cnt == M:
+            ans = n
+            break
+        l = m + 1
+    else:
+        r = m - 1
+print(ans)
