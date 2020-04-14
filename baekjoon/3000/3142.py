9e50c74a07b7a3e2ee49156d752678a10ea6f5c1
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-14 14:19:57 +0900

    즐거운 삶을 위한 노력

--- baekjoon/3000/3142.py
+++ baekjoon/3000/3142.py
@@ -1,5 +1,6 @@
 input = __import__('sys').stdin.readline
 
+# factor[i] = i를 나누어 떨어지게 하는 가장 작은 소수
 factor = [0] * 1000001
 for i in range(2, 1000001):
     if factor[i]:
@@ -8,9 +9,14 @@ for i in range(2, 1000001):
     for j in range(i * i, 1000001, i):
         factor[j] = i
 
+# multiplied[i] = 현재 기계에 i가 곱해져 있는가?
+# (소수만 곱하므로 소수가 아닌 인덱스는 사용하지 않는다.)
 multiplied = [False] * 1000001
-N = int(input())
+
+# prime_cnt = (multiplied[i] == True)인 i의 개수
 prime_cnt = 0
+
+N = int(input())
 for _ in range(N):
     num = int(input())
     while num > 1:
