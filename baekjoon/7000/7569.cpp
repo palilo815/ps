d50eadbb35e6c95d20de0257ba7d01a7a9d3e521
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-15 15:55:56 +0900

    토마토

--- baekjoon/7000/7569.cpp
+++ baekjoon/7000/7569.cpp
@@ -1,35 +1,44 @@
 #include <bits/stdc++.h>
 #define loop(i,n) for(int i=0;i<n;++i)
-#define tup tuple<int,int,int>
 using namespace std;
 
-int box[100][100][100], mov[6][3] = { -1,0,0, 0,-1,0, 0,0,-1, 0,0,1, 0,1,0, 1,0,0 };
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+const int MAX = 100;
+const int bar = 1 << 7;
+const int mov[6][3] = { -1,0,0, 0,-1,0, 0,0,-1, 0,0,1, 0,1,0, 1,0,0 };
+
+int box[MAX][MAX][MAX];
+
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
     int height, row, col;
     cin >> col >> row >> height;
-    queue<tup> q;
+
+    queue<int> q;
     loop(h, height) loop(r, row) loop(c, col) {
         cin >> box[h][r][c];
-        if (box[h][r][c] == 1) q.push({ h,r,c });
+        if (box[h][r][c] == 1)
+            q.emplace(h * bar * bar + r * bar + c);
     }
+    q.emplace(-1);
 
     int ans = 0;
-    q.push({ -1,-1,-1 });
     while (1) {
-        int h = get<0>(q.front()), r = get<1>(q.front()), c = get<2>(q.front()); q.pop();
+        int h = q.front(); q.pop();
         if (h == -1) {
             if (q.empty()) break;
-            ++ans; q.push({ h,r,c });
+            ++ans; q.emplace(-1);
             continue;
         }
+
+        int c = h % bar; h /= bar;
+        int r = h % bar; h /= bar;
+
         loop(i, 6) {
             int H = h + mov[i][0], R = r + mov[i][1], C = c + mov[i][2];
             if (H < 0 || H >= height || R < 0 || R >= row || C < 0 || C >= col || box[H][R][C]) continue;
-            q.push({ H,R,C });
+            q.emplace(H * bar * bar + R * bar + C);
             box[H][R][C] = 1;
         }
     }
