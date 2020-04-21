83f62458858b78753647dd9259d0d42af8d75af6
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-21 18:20:59 +0900

    애너그램 만들기

--- baekjoon/1000/1919.cpp
+++ baekjoon/1000/1919.cpp
@@ -1,21 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main()
-{
-    cin.tie(NULL);
-    std::ios::sync_with_stdio(false);
-
-    string s1, s2;
-    cin >> s1 >> s2;
-    int alpha[2][26] = {};
-    for (int i = 0; i < s1.length(); i++)
-        alpha[0][s1[i] - 'a']++;
-    for (int i = 0; i < s2.length(); i++)
-        alpha[1][s2[i] - 'a']++;
-    int ans = 0;
-    for (int i = 0; i < 26; i++)
-        ans += abs(alpha[0][i] - alpha[1][i]);
-    cout << ans;
-    return 0;
+int cnt[2][26];
+int main() {
+	string s1, s2; cin >> s1 >> s2;
+	for (char c : s1) ++cnt[0][c - 'a'];
+	for (char c : s2) ++cnt[1][c - 'a'];
+	int ans = 0;
+	for (int i = 0; i < 26; i++)
+		ans += abs(cnt[0][i] - cnt[1][i]);
+	cout << ans;
+	return 0;
 }
