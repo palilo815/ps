5872d6b7deab9b9ddf26beedca347b67e928a0cd
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-21 14:52:55 +0900

    달빛 여우

--- baekjoon/16000/16118.cpp
+++ baekjoon/16000/16118.cpp
@@ -5,12 +5,12 @@ typedef tuple<int, int, int> tup;
 
 const int max_N = 4000;
 
-int dist[3][max_N + 1];
+int dist[max_N + 1][3];
 vector<p> adj[max_N + 1];
 
 int main() {
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
     int N, M; cin >> N >> M;
     while (M--) {
@@ -19,24 +19,23 @@ int main() {
         adj[v].emplace_back(w, u);
     }
 
-    for (int i = 0; i < 3; ++i)
-        fill(dist[i] + 1, dist[i] + N + 1, INT_MAX);
-    dist[0][1] = dist[1][1] = 0;
+    fill(&dist[1][0], &dist[N + 1][0], INT_MAX);
+    dist[1][0] = dist[1][1] = 0;
 
     priority_queue<tup, vector<tup>, greater<tup>>pq;
     pq.emplace(0, 1, 1), pq.emplace(0, 1, 0);
 
     while (!pq.empty()) {
         auto [d, u, speed] = pq.top(); pq.pop();
-        if (dist[speed][u] < d) continue;
+        if (dist[u][speed] < d) continue;
 
         int cost = pow(2, speed);
         speed = 2 - speed;
 
         for (auto [w, v] : adj[u]) {
             int D = d + w * cost;
-            if (dist[speed][v] > D) {
-                dist[speed][v] = D;
+            if (dist[v][speed] > D) {
+                dist[v][speed] = D;
                 pq.emplace(D, v, speed);
             }
         }
@@ -44,7 +43,7 @@ int main() {
 
     int ans = 0;
     for (int i = 1; i <= N; ++i)
-        if (dist[1][i] < min(dist[0][i], dist[2][i]))
+        if (dist[i][1] < min(dist[i][0], dist[i][2]))
             ++ans;
     cout << ans;
     return 0;
