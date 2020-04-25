c2c2c5209c8214785992a9025172de2b08c06094
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-25 15:24:51 +0900

    지각하면 안 돼

--- baekjoon/12000/12763.cpp
+++ baekjoon/12000/12763.cpp
@@ -1,58 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef pair<int, int> p;
 typedef tuple<int, int, int> tup;
+
 const int max_N = 100;
 const int max_M = 10000;
 
 vector<tup> adj[max_N + 1];
-int dist[max_N + 1][max_M + 1];
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+p min_t[max_N + 1];
+p min_m[max_N + 1];
 
-    int N, T, M, L;
-    cin >> N >> T >> M >> L;
-    while (L--) {
-        int u, v, t, m;
-        cin >> u >> v >> t >> m;
-        adj[u].push_back({ t,m,v });
-        adj[v].push_back({ t,m,u });
-    }
+int main() {
+	cin.tie(0), cout.tie(0);
+	ios::sync_with_stdio(0);
 
-    // dist[idx][money] = idx 건물에 money를 사용해서 도달할 수 있는 최단거리
-    for (int i = 1; i <= N; ++i)
-        for (int j = 0; j <= M; ++j)
-            dist[i][j] = INT_MAX;
-    dist[1][0] = 0;
+	int N, T, M, L;
+	cin >> N >> T >> M >> L;
+	while (L--) {
+		int u, v, t, m;
+		cin >> u >> v >> t >> m;
+		adj[u].emplace_back(m, t, v);
+		adj[v].emplace_back(m, t, u);
+	}
 
-    priority_queue<tup, vector<tup>, greater<tup>>pq;
-    pq.push({ 0,0,1 });
-    while (!pq.empty()) {
-        int time_u = get<0>(pq.top()), cost_u = get<1>(pq.top()), u = get<2>(pq.top()); pq.pop();
+	fill_n(min_t + 2, N - 1, make_pair(T, M));
+	fill_n(min_m + 2, N - 1, make_pair(M, T));
 
-        // 도착지인 N에 도착하더라도 계속 탐색해야 한다.
-        if (u == N || dist[u][cost_u] < time_u) continue;
+	priority_queue<tup, vector<tup>, greater<tup>>pq;
+	pq.emplace(0, 0, 1);
 
-        for (tup p : adj[u]) {
-            int time_v = time_u + get<0>(p), cost_v = cost_u + get<1>(p);
-            // time, money 제한이 걸리면 pass
-            if (time_v > T || cost_v > M) continue;
-            int v = get<2>(p);
-            if (dist[v][cost_v] > time_v) {
-                pq.push({ time_v,cost_v,v });
-                dist[v][cost_v] = time_v;
-            }
-        }
-    }
+	while (!pq.empty()) {
+		auto [m_u, t_u, u] = pq.top(); pq.pop();
+		if (u == N) { cout << m_u; return 0; }
 
-    // 찾은 경로 중 가장 돈을 덜 쓰는 방법을 출력한다.
-    int ans = -1;
-    for (int i = 0; i <= M; ++i)
-        if (dist[N][i] != INT_MAX) {
-            ans = i;
-            break;
-        }
-    cout << ans;
-    return 0;
+		for (auto [m, t, v] : adj[u]) {
+			int m_v = m + m_u, t_v = t + t_u, flag = 0;
+			if (m_v > M || t_v > T) continue;
+
+			p new_t = { t_v,m_v };
+			p new_m = { m_v,t_v };
+			if (new_t < min_t[v]) min_t[v] = new_t, flag = 1;
+			if (new_m < min_m[v]) min_m[v] = new_m, flag = 1;
+			if (flag) pq.emplace(m_v, t_v, v);
+		}
+	}
+	cout << -1;
+	return 0;
 }
