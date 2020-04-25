3130d178751f2a1a6f70cd0925a0e1fb913f56bc
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-25 12:53:44 +0900

    미확인 도착지

--- baekjoon/9000/9370.cpp
+++ baekjoon/9000/9370.cpp
@@ -5,28 +5,8 @@ typedef pair<int, int> p;
 const int max_V = 2000;
 
 vector<p> adj[max_V + 1];
-int d1[max_V + 1], d2[max_V + 1];
+int dist[max_V + 1];
 
-void Dijkstra(int V, int s, int dist[]) {
-    memset(dist, 0x3f, sizeof(int) * (V + 1));
-    dist[s] = 0;
-
-    priority_queue<p, vector<p>, greater<p>> pq;
-    pq.emplace(0, s);
-
-    while (!pq.empty()) {
-        auto [d, u] = pq.top(); pq.pop();
-        if (dist[u] < d) continue;
-
-        for (auto [w, v] : adj[u]) {
-            int D = d + w;
-            if (dist[v] > D) {
-                dist[v] = D;
-                pq.emplace(D, v);
-            }
-        }
-    }
-}
 int main() {
     cin.tie(0), cout.tie(0);
     ios::sync_with_stdio(0);
@@ -39,25 +19,35 @@ int main() {
         for (int i = 1; i <= V; ++i) adj[i].clear();
         while (E--) {
             int u, v, w; cin >> u >> v >> w;
+            w *= 2;
+            if ((u == g && v == h) || (v == g && u == h)) --w;
             adj[u].emplace_back(w, v);
             adj[v].emplace_back(w, u);
         }
 
-        Dijkstra(V, src, d1);
+        memset(dist, 0x3e, sizeof(int) * (V + 1));
+        dist[src] = 0;
 
-        int m = 0;
-        for (auto [w, node] : adj[g]) if (node == h) {
-            m = w;
-            break;
+        priority_queue<p, vector<p>, greater<p>> pq;
+        pq.emplace(0, src);
+
+        while (!pq.empty()) {
+            auto [d, u] = pq.top(); pq.pop();
+            if (dist[u] < d) continue;
+
+            for (auto [w, v] : adj[u]) {
+                int D = d + w;
+                if (dist[v] > D) {
+                    dist[v] = D;
+                    pq.emplace(D, v);
+                }
+            }
         }
 
         vector<int> ans;
         while (dst--) {
             int x; cin >> x;
-            Dijkstra(V, x, d2);
-            int tmp = d1[x] - m;
-            if (d1[g] + d2[h] == tmp || d1[h] + d2[g] == tmp)
-                ans.emplace_back(x);
+            if (dist[x] & 1) ans.emplace_back(x);
         }
 
         sort(ans.begin(), ans.end());
