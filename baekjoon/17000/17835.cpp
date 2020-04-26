6100d7d8bc576614513d0d273e5b9f67c19c1ed9
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-26 12:43:53 +0900

    면접보는 승범이네

--- baekjoon/17000/17835.cpp
+++ baekjoon/17000/17835.cpp
@@ -1,53 +1,47 @@
 #include<bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
-#define ll long long
-#define P pair<ll,int>
 using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
 
-vector<P> graph[100001];
-ll dist[100001];
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+const int max_N = 100000;
+
+vector<pii> rev[max_N + 1];
+ll dist[max_N + 1];
+
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
     int V, E, K; cin >> V >> E >> K;
-    for (int i = 1;i <= V;++i)
-        dist[i] = INT64_MAX;
     while (E--) {
         int u, v, w; cin >> u >> v >> w;
-        graph[v].push_back({ u,w });	// reverse order
+        rev[v].emplace_back(w, u);
     }
 
-    // 면접 장소를 전부 pq에 넣고 Dijkstra로 찾는다.
-    priority_queue<P, vector<P>, greater<P>> pq;
+    memset(dist, 0x3f, sizeof(dist));
+    priority_queue <pli, vector<pli>, greater<pli>> pq;
+
     while (K--) {
         int x; cin >> x;
-        pq.push({ 0,x });
+        pq.emplace(0, x);
         dist[x] = 0;
     }
+
     while (!pq.empty()) {
-        ll cost = pq.top().first;
-        int here = pq.top().second; pq.pop();
-        if (dist[here] < cost) continue;
-        loop(i, graph[here].size()) {
-            int there = graph[here][i].first;
-            ll w = graph[here][i].second;
-            if (dist[there] > cost + w) {
-                dist[there] = cost + w;
-                pq.push({ cost + w,there });
+        auto [d, u] = pq.top(); pq.pop();
+        if (dist[u] < d) continue;
+
+        for (auto [w, v] : rev[u]) {
+            ll D = d + w;
+            if (dist[v] > D) {
+                dist[v] = D;
+                pq.emplace(D, v);
             }
         }
     }
 
-    int ans = 0;
-    ll MAX = -1;
-    for (int i = 1; i <= V; ++i) {
-        if (dist[i] > MAX) {
-            MAX = dist[i];
-            ans = i;
-        }
-    }
-    cout << ans << '\n' << MAX;
+    auto ans = max_element(dist + 1, dist + V + 1);
+    cout << ans - dist << '\n' << *ans;
     return 0;
 }
