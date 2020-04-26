40a2f46eba0f9efd2d0ba40ab590703e853f69aa
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-26 13:15:05 +0900

    Underground Cables

--- baekjoon/4000/4722.cpp
+++ baekjoon/4000/4722.cpp
@@ -1,42 +1,40 @@
 #include <bits/stdc++.h>
 #define loop(i,n) for(int i=0;i<n;++i)
 using namespace std;
-typedef tuple<int, int, int> tup;
+
 const int max_N = 1000;
 
-int parent[max_N];
-int V[max_N][2];
-tup E[(max_N - 1) * (max_N / 2)];
-int find_rt(int u)
-{
-    if (u == parent[u]) return u;
-    return parent[u] = find_rt(parent[u]);
-}
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+int x[max_N], y[max_N];
+int dist[max_N];
+bool conn[max_N];
 
-    while(1){
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
+
+    while (1) {
         int N; cin >> N;
-        if (N == 0) break;
-        loop(i, N) parent[i] = i;
-        loop(i, N) cin >> V[i][0] >> V[i][1];
+        if (!N) break;
+        loop(i, N) cin >> x[i] >> y[i];
 
-        int idx = 0;
-        loop(i, N - 1) for (int j = i + 1; j < N; ++j) {
-            int dx = V[i][0] - V[j][0], dy = V[i][1] - V[j][1];
-            E[idx++] = { dx * dx + dy * dy,i,j };
-        }
-        sort(E, E + idx);
-
+        memset(conn, 0, N);
+        memset(dist, 0x3f, sizeof(int) * N);
+        dist[0] = 0;
+        
         double ans = 0;
-        loop(i, idx) {
-            int u = get<1>(E[i]), v = get<2>(E[i]);
-            u = find_rt(u), v = find_rt(v);
-            if (u == v) continue;
-            parent[u] = v;
-            ans += sqrt(get<0>(E[i]));
+        int cnt = N;
+        while (cnt--) {
+            int k = -1;
+            loop(i, N) if (!conn[i] && (k == -1 || dist[i] < dist[k]))
+                k = i;
+
+            ans += sqrt(dist[k]);
+            conn[k] = true;
+
+            loop(i, N) if (!conn[i]) {
+                int dx = x[i] - x[k], dy = y[i] - y[k];
+                dist[i] = min(dist[i], dx * dx + dy * dy);
+            }
         }
         cout << fixed << setprecision(2) << ans << '\n';
     }
