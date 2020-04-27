4f683d1c3335331a1f5aebd3268023dd9a7b60fa
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-27 13:48:57 +0900

    제국

--- baekjoon/10000/10776.cpp
+++ baekjoon/10000/10776.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef pair<int, int> p;
 typedef tuple<int, int, int> tup;
 
 const int max_N = 2000;
 const int max_K = 200;
 
 vector<tup> adj[max_N + 1];
-int dist[max_N + 1][max_K];
+p dist[max_N + 1];
 
 int main() {
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
     int K, N, M; cin >> K >> N >> M;
     while (M--) {
@@ -20,25 +21,23 @@ int main() {
     }
     int src, dst; cin >> src >> dst;
 
-    for (int i = 1; i <= N; ++i)
-        fill(dist[i], dist[i] + K, INT_MAX);
-    dist[src][0] = 0;
-
+    memset(dist, 0x3f, sizeof(dist));
     priority_queue<tup, vector<tup>, greater<tup>> pq;
     pq.emplace(0, 0, src);
 
     int ans = -1;
     while (!pq.empty()) {
         auto [d, h, u] = pq.top(); pq.pop();
-        if (dist[u][h] < d) continue;
         if (u == dst) { ans = d; break; }
 
         for (auto [w1, w2, v] : adj[u]) {
             int D = d + w1, H = h + w2;
-            if (H < K && dist[v][H] > D) {
-                dist[v][H] = D;
-                pq.emplace(D, H, v);
-            }
+            if (H >= K) continue;
+
+            int flag = 0;
+            if (dist[v].first > D) dist[v].first = D, flag = 1;
+            if (dist[v].second > H) dist[v].second = H, flag = 1;
+            if (flag) pq.emplace(D, H, v);
         }
     }
     cout << ans;
