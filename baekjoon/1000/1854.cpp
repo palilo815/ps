abe4134265c846e6ca67006e42ae684d824f7d34
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-27 13:24:54 +0900

    K번째 최단경로 찾기

--- baekjoon/1000/1854.cpp
+++ baekjoon/1000/1854.cpp
@@ -1,48 +1,45 @@
 #include <bits/stdc++.h>
-#define LOOP(i,n) for(int i=1;i<=n;++i)
 using namespace std;
 typedef pair<int, int> P;
+
 const int max_N = 1000;
 
 vector<P> adj[max_N + 1];
 priority_queue<int> dist[max_N + 1];
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
 
-    int N, M, k;
-    cin >> N >> M >> k;
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
+    int N, M, k; cin >> N >> M >> k;
     while (M--) {
         int u, v, w; cin >> u >> v >> w;
-        adj[u].push_back({ v,w });
+        adj[u].emplace_back(w, v);
     }
     
     priority_queue<P, vector<P>, greater<P>> pq;
-    pq.push({ 0,1 });
-    dist[1].push(0);
+    pq.emplace(0, 1);
+    dist[1].emplace(0);
 
     while (!pq.empty()) {
-        int u = pq.top().second, d = pq.top().first; pq.pop();
-        for (P edge : adj[u]) {
-            int v = edge.first, D = d + edge.second;
+        auto [d, u] = pq.top(); pq.pop();
+        if (d > dist[u].top()) continue;
 
-            // v로 가는 경로가 k개 미만이라면 바로 추가.
+        for (auto [w, v] : adj[u]) {
+            int D = d + w;
             if (dist[v].size() < k) {
-                dist[v].push(D);
-                pq.push({ D, v });
+                dist[v].emplace(D);
+                pq.emplace(D, v);
             }
-            // k개라면, D를 포함한 k+1개 경로 중 최댓값을 버려야 한다.
             else if (dist[v].top() > D) {
                 dist[v].pop();
-                dist[v].push(D);
-                pq.push({ D,v });
+                dist[v].emplace(D);
+                pq.emplace(D, v);
             }
         }
     }
-    
-    LOOP(i, N)
+
+    for (int i = 1; i <= N; ++i)
         cout << ((dist[i].size() == k) ? dist[i].top() : -1) << '\n';
     return 0;
 }
