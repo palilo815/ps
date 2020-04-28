e30238859ca1c619bbc74c1fa2ca9c3152637f58
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-28 13:20:50 +0900

    의약품 수송

--- baekjoon/16000/16227.cpp
+++ baekjoon/16000/16227.cpp
@@ -5,12 +5,13 @@ typedef tuple<int, int, int> tup;
 
 const int max_N = 1000;
 
-int dist[max_N + 2][101];
+int min_dist[max_N + 2];
+int min_dust[max_N + 2];
 vector<p> adj[max_N + 2];
 
 int main() {
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
     int N, M; cin >> N >> M;
     while (M--) {
@@ -20,27 +21,26 @@ int main() {
         adj[v].emplace_back(w, u);
     }
 
-    fill(&dist[0][0], &dist[N + 2][0], INT_MAX);
-    dist[0][0] = 0;
+    memset(min_dist + 1, 0x3f, sizeof(int) * (N + 1));
+    memset(min_dust + 1, 0x3f, sizeof(int) * (N + 1));
 
     priority_queue<tup, vector<tup>, greater<tup>> pq;
     pq.emplace(0, 0, 0);
+
     while (1) {
         auto [d, dust, u] = pq.top(); pq.pop();
-        if (dist[u][dust] < d) continue;
+        if (min_dist[u] < d && min_dust[u] < dust) continue;
         if (u == N + 1) { cout << d; break; }
 
+        if (dust) pq.emplace(d + 5, 0, u);
         for (auto [w, v] : adj[u]) {
-            int D = d + w;
-            if (dust + w <= 100 && dist[v][dust + w] > D) {
-                dist[v][dust + w] = D;
-                pq.emplace(D, dust + w, v);
-            }
-            D += 5;
-            if (dist[v][w] > D) {
-                dist[v][w] = D;
-                pq.emplace(D, w, v);
-            }
+            int D = d + w, DUST = dust + w;
+            if (DUST > 100) continue;
+
+            int flag = 0;
+            if (min_dist[v] > D) min_dist[v] = D, flag = 1;
+            if (min_dust[v] > DUST) min_dust[v] = DUST, flag = 1;
+            if (flag) pq.emplace(D, DUST, v);
         }
     }
     return 0;
