7e681d4f2d040f486f0d8a0e4293dc8f339888ed
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-04-29 17:12:56 +0900

    양팔저울

--- baekjoon/2000/2629.cpp
+++ baekjoon/2000/2629.cpp
@@ -1,49 +1,37 @@
 #include<bits/stdc++.h>
-using namespace std;
 #define loop(i,n) for(int i=0;i<n;++i)
+using namespace std;
 
-// 측정 가능한 무게
-bool weight[15001];
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+const int max_W = 15000;
 
-    int N; cin >> N;
-    int range = 0; // 현재 만들 수 있는 최대 무게
-    while (N--) {
-        int w; cin >> w;
-        vector<int> tmp;
+bool DP[max_W + 1];
+bool tmp[max_W + 1];
 
-        loop(i, range + 1)
-            if (weight[i]) {
-                // i를 만들 수 있으면
-                // abs(i-w)와 i+w도 만들 수 있다.
-                int next = abs(w - i);
-                if (!weight[next]) tmp.push_back(next);
-                next = w + i;
-                if (!weight[next]) tmp.push_back(next);
-            }
+int main() {
+	cin.tie(0), cout.tie(0);
+	ios::sync_with_stdio(0);
 
-        range = max(range, w);
-        if (!tmp.empty())
-            range = max(range, tmp.back());
+	int N; cin >> N;
+	DP[0] = true;
 
-        weight[w] = true;
-        for (int x : tmp)
-            weight[x] = true;
-    }
-    vector<char> ans;
-    cin >> N;
-    while (N--) {
-        int w; cin >> w;
-        if (w > 15000) ans.push_back('N');
-        else {
-            if (weight[w]) ans.push_back('Y');
-            else ans.push_back('N');
-        }
-    }
-    for (char c : ans)
-        cout << c << ' ';
-    return 0;
+	while (N--) {
+		int w; cin >> w;
+		loop(i, max_W + 1) if (DP[i]) {
+			if (i + w <= max_W) tmp[i + w] = true;
+			tmp[abs(i - w)] = true;
+		}
+		DP[w] = true;
+
+		loop(i, max_W + 1) {
+			DP[i] |= tmp[i];
+			tmp[i] = false;
+		}
+	}
+
+	int Q; cin >> Q;
+	while (Q--) {
+		int w; cin >> w;
+		cout << (DP[w] ? 'Y' : 'N') << ' ';
+	}
+	return 0;
 }
