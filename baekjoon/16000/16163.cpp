57788bd911a0c5cdf4c9874a51c0d20df474223a
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-05-01 17:34:16 +0900

    #15164번_제보

--- baekjoon/16000/16163.cpp
+++ baekjoon/16000/16163.cpp
@@ -7,33 +7,33 @@ char s[max_N * 2 + 1];
 int p[max_N * 2 + 1];
 
 int main() {
-	cin.tie(0), cout.tie(0);
-	ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
-	string ori; cin >> ori;
-	int N = 1;
-	for (char c : ori) {
-		s[N] = c;
-		N += 2;
-	}
+    string ori; cin >> ori;
+    int N = 1;
+    for (char c : ori) {
+        s[N] = c;
+        N += 2;
+    }
 
-	int l = 0, r = -1;
-	for (int i = 0; i < N; ++i) {
-		int k;
-		if (i > r) k = 0;
-		else k = min(p[l + r - i], r - i);
+    int l = 0, r = -1;
+    for (int i = 0; i < N; ++i) {
+        int k;
+        if (i > r) k = 0;
+        else k = min(p[l + r - i], r - i);
 
-		while (i - k > 0 && i + k < N - 1 && s[i - k - 1] == s[i + k + 1])
-			++k;
+        while (i - k > 0 && i + k < N - 1 && s[i - k - 1] == s[i + k + 1])
+            ++k;
 
-		p[i] = k;
-		if (i + k > r)
-			l = i - k, r = i + k;
-	}
+        p[i] = k;
+        if (i + k > r)
+            l = i - k, r = i + k;
+    }
 
-	long long ans = 0;
-	for (int i = 0; i < N; ++i)
-		ans += (p[i] + 1) / 2;
-	cout << ans;
-	return 0;
+    long long ans = 0;
+    for (int i = 0; i < N; ++i)
+        ans += (p[i] + 1) / 2;
+    cout << ans;
+    return 0;
 }
