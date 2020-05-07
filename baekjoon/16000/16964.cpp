3ce45bb354de2c77a3782e46e5e78cc27d246e07
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-05-07 16:50:21 +0900

    DFS 스페셜 저지

--- baekjoon/16000/16964.cpp
+++ baekjoon/16000/16964.cpp
@@ -10,33 +10,33 @@ int path[max_N];
 int order[max_N];
 
 bool cmp(const int a, const int b) {
-	return order[a] < order[b];
+    return order[a] < order[b];
 }
 int valid() {
-	if (idx == N + 1) return 1;
-	int u = path[idx++];
-	LOOP(i, adj[u].size() - 1) {
-		int v = path[idx];
-		if (v != adj[u][i] || !valid()) return 0;
-	}
-	return 1;
+    if (idx == N + 1) return 1;
+    int u = path[idx++];
+    LOOP(i, adj[u].size() - 1) {
+        int v = path[idx];
+        if (v != adj[u][i] || !valid()) return 0;
+    }
+    return 1;
 }
 int main() {
-	cin.tie(0), cout.tie(0);
-	ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
-	cin >> N;
-	adj[1].emplace_back(0);
-	LOOP(i, N - 1) {
-		int u, v; cin >> u >> v;
-		adj[u].emplace_back(v);
-		adj[v].emplace_back(u);
-	}
-	LOOP(i, N) {
-		cin >> path[i];
-		order[path[i]] = i;
-	}
-	LOOP(i, N) sort(adj[i].begin(), adj[i].end(), cmp);
-	cout << (order[1] == 1 ? valid() : 0);
-	return 0;
+    cin >> N;
+    adj[1].emplace_back(0);
+    LOOP(i, N - 1) {
+        int u, v; cin >> u >> v;
+        adj[u].emplace_back(v);
+        adj[v].emplace_back(u);
+    }
+    LOOP(i, N) {
+        cin >> path[i];
+        order[path[i]] = i;
+    }
+    LOOP(i, N) sort(adj[i].begin(), adj[i].end(), cmp);
+    cout << (order[1] == 1 ? valid() : 0);
+    return 0;
 }
