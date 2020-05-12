4988146bc9880436de694073313ed886ce38cb13
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-05-13 03:26:57 +0900

    섬 여행

--- baekjoon/14000/14619.cpp
+++ baekjoon/14000/14619.cpp
@@ -10,34 +10,34 @@ vector<int> adj[max_N];
 int DP[max_N][max_K];
 
 int solve(int u, int k) {
-	if (k == 0) return h[u];
-	int& ret = DP[u][k];
-	if (ret != INF) return ret;
+    if (k == 0) return h[u];
+    int& ret = DP[u][k];
+    if (ret != INF) return ret;
 
-	for (int v : adj[u])
-		ret = min(ret, solve(v, k - 1));
-	return ret;
+    for (int v : adj[u])
+        ret = min(ret, solve(v, k - 1));
+    return ret;
 }
 int main() {
-	cin.tie(0), cout.tie(0);
-	ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
-	int N, M; cin >> N >> M;
-	for (int i = 1; i <= N; ++i)
-		cin >> h[i];
-	while (M--) {
-		int u, v; cin >> u >> v;
-		adj[u].emplace_back(v);
-		adj[v].emplace_back(u);
-	}
+    int N, M; cin >> N >> M;
+    for (int i = 1; i <= N; ++i)
+        cin >> h[i];
+    while (M--) {
+        int u, v; cin >> u >> v;
+        adj[u].emplace_back(v);
+        adj[v].emplace_back(u);
+    }
 
-	memset(DP, 0x3f, sizeof(DP));
+    memset(DP, 0x3f, sizeof(DP));
 
-	int T; cin >> T;
-	while (T--) {
-		int u, k; cin >> u >> k;
-		int ans = solve(u, k);
-		cout << (ans == INF ? -1 : ans) << '\n';
-	}
-	return 0;
+    int T; cin >> T;
+    while (T--) {
+        int u, k; cin >> u >> k;
+        int ans = solve(u, k);
+        cout << (ans == INF ? -1 : ans) << '\n';
+    }
+    return 0;
 }
