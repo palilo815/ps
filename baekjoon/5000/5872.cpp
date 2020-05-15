c3bc5d8a9228c13923584e94f11a63dcfb9ef557
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-05-15 23:39:40 +0900

    Clumsy Cows

---
+++ baekjoon/5000/5872.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+
+const int M = 100001;
+
+char s[M];
+
+int main() {
+    scanf("%s", s);
+    int stack = 0, ans = 0;
+    for (int i = 0; s[i]; ++i) {
+        if (s[i] == '(') ++stack;
+        else {
+            if (stack == 0) ++stack, ++ans;
+            else --stack;
+        }
+    }
+    printf("%d", ans + stack / 2);
+    return 0;
+}
