077989181a632ca15876659b06e526a0f06722ea
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-05-15 21:01:27 +0900

    파일 합치기

--- baekjoon/11000/11066.cpp
+++ baekjoon/11000/11066.cpp
@@ -1,33 +1,38 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=1;i<=n;++i)
+#define LOOP(i,n) for(int i=1;i<=n;++i)
 using namespace std;
 
-const int max_N = 500;
-int DP[max_N + 1][max_N + 1], psum[max_N + 1];
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+const int max_N = 501;
+
+int s[max_N];
+int DP[max_N][max_N];
+int p[max_N][max_N];
+
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
     int T; cin >> T;
     while (T--) {
         int N; cin >> N;
-        for (int i = 1;i <= N;++i) {
+        LOOP(i, N) {
             int x; cin >> x;
-            psum[i] = psum[i - 1] + x;
+            s[i] = s[i - 1] + x;
         }
+        LOOP(i, N) p[i][i] = i;
 
-        // ** DP[a][b] == [a,b]의 파일을 1개로 만드는 비용 **
-        // DP[i][i+gap]을 구하는 방법:
-        // tmp : [i,i+gap]의 파일을 딱 두개로 만드는 최솟값
-        // psum[i+gap] - psum[i-1] : 두개의 파일을 합치는 비용
-        for (int gap = 1; gap < N; ++gap) {
-            for (int i = 1; i + gap <= N; ++i) {
-                int tmp = INT_MAX;
-                for (int j = i; j < i + gap; ++j)
-                    tmp = min(tmp, DP[i][j] + DP[j + 1][i + gap]);
-                DP[i][i + gap] = tmp + psum[i + gap] - psum[i - 1];
-            }
+        // DP[i][j] : [i,j] 파일을 합치는 최소 비용
+        // DP[i][j] = min(DP[i][k] + DP[k][j] + sum[i,j])
+
+        LOOP(len, N) LOOP(i, N - len) {
+            int j = i + len;
+            DP[i][j] = INT_MAX;
+            for (int k = p[i][j - 1]; k <= p[i + 1][j]; ++k)
+                if (DP[i][j] > DP[i][k] + DP[k + 1][j]) {
+                    DP[i][j] = DP[i][k] + DP[k + 1][j];
+                    p[i][j] = k;
+                }
+            DP[i][j] += s[j] - s[i - 1];
         }
         cout << DP[1][N] << '\n';
     }
