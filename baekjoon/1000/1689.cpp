319781c20bd086ef3be7e679815dae3abebdbf21
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-05-15 23:30:59 +0900

    겹치는 선분

---
+++ baekjoon/1000/1689.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int max_N = 1000000;
+
+int s[max_N], e[max_N];
+
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
+
+    int N; cin >> N;
+    for (int i = 0; i < N; ++i)
+        cin >> s[i] >> e[i];
+
+    sort(s, s + N);
+    sort(e, e + N);
+
+    int i = 0, j = 0, ans = 0, cnt = 0;
+    while (i < N) {
+        if (s[i] < e[j]) {
+            ++i;
+            ans = max(ans, ++cnt);
+        }
+        else if (s[i] >= e[j])
+            ++j, --cnt;
+    }
+    cout << ans;
+    return 0;
+}
