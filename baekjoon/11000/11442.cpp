7e97700a9e40cbc8afdad49cb85de27263f51a19
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-05-17 22:57:39 +0900

    홀수번째 피보나치 수의 합

---
+++ baekjoon/11000/11442.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#define loop(i,n) for(int i=0;i<n;++i)
+typedef long long ll;
+
+const int mod = 1000000007;
+
+ll A[2][2];
+ll B[2][2];
+ll res[2][2];
+
+void multiply(ll x[][2], ll y[][2]) {
+    B[0][0] = B[0][1] = B[1][0] = B[1][1] = 0;
+    loop(i, 2) loop(k, 2) loop(j, 2) {
+        B[i][j] += x[i][k] * y[k][j];
+        B[i][j] %= mod;
+    }
+    x[0][0] = B[0][0], x[0][1] = B[0][1];
+    x[1][0] = B[1][0], x[1][1] = B[1][1];
+}
+int main() {
+    ll n; scanf("%lld", &n);
+    n += (n & 1);
+
+    res[0][0] = res[1][1] = 1;
+    res[0][1] = res[1][0] = 0;
+
+    A[0][0] = A[0][1] = A[1][0] = 1;
+    A[1][1] = 0;
+
+    while (n) {
+        if (n & 1) multiply(res, A);
+        multiply(A, A);
+        n /= 2;
+    }
+    printf("%lld", res[0][1]);
+    return 0;
+}
