d4bb35603b8af4a161e67cc4dff3122858dd2d10
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-05-17 16:48:16 +0900

    리그 오브 레전설 (Large)

--- baekjoon/17000/17272.cpp
+++ baekjoon/17000/17272.cpp
@@ -12,29 +12,29 @@ ll B[max_M][max_M];
 ll res[max_M][max_M];
 
 void multiply(ll x[][max_M], ll y[][max_M]) {
-	memset(B, 0, sizeof(B));
-	loop(i, M) loop(k, M) loop(j, M) {
-		B[i][j] += x[i][k] * y[k][j];
-		B[i][j] %= mod;
-	}
-	memcpy(x, B, sizeof(B));
+    memset(B, 0, sizeof(B));
+    loop(i, M) loop(k, M) loop(j, M) {
+        B[i][j] += x[i][k] * y[k][j];
+        B[i][j] %= mod;
+    }
+    memcpy(x, B, sizeof(B));
 }
 int main() {
-	ll N; cin >> N >> M;
+    ll N; cin >> N >> M;
 
-	A[0][0] = A[0][M - 1] = 1;
-	loop(i, M - 1) A[i + 1][i] = 1;
-	loop(i, M) res[i][i] = 1;
+    A[0][0] = A[0][M - 1] = 1;
+    loop(i, M - 1) A[i + 1][i] = 1;
+    loop(i, M) res[i][i] = 1;
 
-	N -= M - 1;
-	while (N > 0) {
-		if (N & 1) multiply(res, A);
-		multiply(A, A);
-		N /= 2;
-	}
+    N -= M - 1;
+    while (N > 0) {
+        if (N & 1) multiply(res, A);
+        multiply(A, A);
+        N /= 2;
+    }
 
-	ll ans = 0;
-	loop(i, M) ans = (ans + res[0][i]) % mod;
-	cout << ans;
-	return 0;
+    ll ans = 0;
+    loop(i, M) ans = (ans + res[0][i]) % mod;
+    cout << ans;
+    return 0;
 }
