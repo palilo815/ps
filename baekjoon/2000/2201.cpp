a338501dae6d92c399dc475b11f2df3d096afef4
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-05-21 13:19:22 +0900

    이친수 찾기

--- baekjoon/2000/2201.cpp
+++ baekjoon/2000/2201.cpp
@@ -1,26 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef pair<ll, ll> p;
 
-int main()
-{
-    vector<p> DP;
-    DP.push_back({ 1,1 });
+int main() {
     ll k; cin >> k;
     ll cnt = 1;
+
+    vector<pair<ll, ll>> DP = { {1,1} };
     while (cnt < k) {
         auto [zero, one] = DP.back();
-        DP.push_back({ zero + one, zero });
+        DP.emplace_back(zero + one, zero);
         cnt += zero;
     }
     
     ++k;
-    for (int i = DP.size() - 1; i >= 0; --i) {
-        auto [zero, one] = DP[i];
-        if (k > zero) {
+    for (auto it = DP.rbegin(); it != DP.rend(); ++it) {
+        if (k > it->first) {
             cout << 1;
-            k -= zero;
+            k -= it->first;
         }
         else cout << 0;
     }
