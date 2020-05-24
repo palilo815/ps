61a0d5b5412b500e89451b39229c742bbc2c6ef9
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-05-24 22:00:28 +0900

    수열

---
+++ baekjoon/7000/7976.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define loop(i,n) for(int i=0;i<n;++i)
+using namespace std;
+
+const int max_N = 1000000;
+
+int arr[max_N];
+int cnt[max_N][2];
+
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
+
+    int N, K; cin >> N >> K;
+    loop(i, N) cin >> arr[i];
+
+    loop(s, K) for (int i = s; i < N; i += K)
+        ++cnt[s][arr[i] & 1];
+
+    int odd = 0x3f3f3f3f, even = 0;
+    loop(i, K) {
+        int O = min(odd + cnt[i][1], even + cnt[i][0]);
+        int E = min(even + cnt[i][1], odd + cnt[i][0]);
+        odd = O, even = E;
+    }
+    cout << even;
+    return 0;
+}
