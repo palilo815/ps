a6f25f0316587764708e2988cf366b940ed76e98
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-06-06 02:13:09 +0900

    모자이크

--- baekjoon/2000/2539.cpp
+++ baekjoon/2000/2539.cpp
@@ -6,39 +6,39 @@ typedef pair<int, int> p;
 vector<p> pos;
 
 int solve(int len) {
-	int ret = 0;
-	auto x = pos.begin();
-	while (x != pos.end()) {
-		++ret;
-		x = lower_bound(x, pos.end(), make_pair(x->first + len, 0));
-	}
-	return ret;
+    int ret = 0;
+    auto x = pos.begin();
+    while (x != pos.end()) {
+        ++ret;
+        x = lower_bound(x, pos.end(), make_pair(x->first + len, 0));
+    }
+    return ret;
 }
 int main() {
-	cin.tie(0), cout.tie(0);
-	ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
-	int R, C, K, N;
-	cin >> R >> C >> K >> N;
+    int R, C, K, N;
+    cin >> R >> C >> K >> N;
 
-	pos.resize(N);
-	for (auto& [c, r] : pos)
-		cin >> r >> c;
-	sort(pos.begin(), pos.end());
+    pos.resize(N);
+    for (auto& [c, r] : pos)
+        cin >> r >> c;
+    sort(pos.begin(), pos.end());
 
-	int l = 0, r = 2000000;
-	for (auto [c, r] : pos)
-		l = max(l, r);
+    int l = 0, r = 2000000;
+    for (auto [c, r] : pos)
+        l = max(l, r);
 
-	int ans;
-	while (l <= r) {
-		int m = (l + r) >> 1;
-		if (solve(m) <= K) {
-			ans = m;
-			r = m - 1;
-		}
-		else l = m + 1;
-	}
-	cout << ans;
-	return 0;
+    int ans;
+    while (l <= r) {
+        int m = (l + r) >> 1;
+        if (solve(m) <= K) {
+            ans = m;
+            r = m - 1;
+        }
+        else l = m + 1;
+    }
+    cout << ans;
+    return 0;
 }
