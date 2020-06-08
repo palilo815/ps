16c068d379b1c002648009adaee4c87fef4fdd8d
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-06-08 16:38:35 +0900

    곱셈

--- baekjoon/1000/1629.cpp
+++ baekjoon/1000/1629.cpp
@@ -2,14 +2,14 @@
 typedef unsigned long long ull;
 
 int main() {
-	ull A, B, C;
-	scanf("%llu %llu %llu", &A, &B, &C);
+    ull A, B, C;
+    scanf("%llu %llu %llu", &A, &B, &C);
 
-	ull ans = 1;
-	while (B) {
-		if (B & 1) ans = (ans * A) % C;
-		A = A * A % C;
-		B >>= 1;
-	}
-	printf("%llu", ans);
+    ull ans = 1;
+    while (B) {
+        if (B & 1) ans = (ans * A) % C;
+        A = A * A % C;
+        B >>= 1;
+    }
+    printf("%llu", ans);
 }
