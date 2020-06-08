ec1f84618ec319a3a0c4319fd34fecfa1f01030f
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-06-08 16:38:20 +0900

    곱셈

---
+++ baekjoon/1000/1629.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+typedef unsigned long long ull;
+
+int main() {
+	ull A, B, C;
+	scanf("%llu %llu %llu", &A, &B, &C);
+
+	ull ans = 1;
+	while (B) {
+		if (B & 1) ans = (ans * A) % C;
+		A = A * A % C;
+		B >>= 1;
+	}
+	printf("%llu", ans);
+}
