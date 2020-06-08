8e1aa357e38f0d874e98486b13f6bd907953365e
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-06-08 17:13:35 +0900

    나이순 정렬

--- baekjoon/10000/10814.cpp
+++ baekjoon/10000/10814.cpp
@@ -1,25 +1,22 @@
 #include <bits/stdc++.h>
 #define loop(i,n) for(int i=0;i<n;++i)
 using namespace std;
-const int max_N = 100000;
 
+const int max_N = 100000;
 typedef struct {
     int age;
     string name;
-}member;
-bool cmp(member a, member b)
-{
-    return a.age < b.age;
-}
+} member;
+
 member arr[max_N];
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
     int N; cin >> N;
     loop(i, N) cin >> arr[i].age >> arr[i].name;
-    stable_sort(arr, arr + N, cmp);
+    stable_sort(arr, arr + N, [](member a, member b) {return a.age < b.age;});
     loop(i, N) cout << arr[i].age << ' ' << arr[i].name << '\n';
     return 0;
 }
