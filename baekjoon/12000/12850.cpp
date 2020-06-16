8b7fd68d33c1aa8c98c7f71a6cc754dd97562986
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-06-16 10:03:46 +0900

    본대 산책2

--- baekjoon/12000/12850.cpp
+++ baekjoon/12000/12850.cpp
@@ -1,42 +1,39 @@
-#include <stdio.h>
-#include <string.h>
+#include <cstdio>
+#include <cstring>
 #define loop(i,n) for(int i=0;i<n;++i)
-typedef long long ll;
+using ll = long long;
 
 const int mod = 1000000007;
 const int N = 8;
 
-ll mat[N][N] = {
-{0,1,0,1,0,0,0,0},
-{1,0,1,1,0,0,0,0},
-{0,1,0,1,1,1,0,0},
-{1,1,1,0,0,1,0,0},
-{0,0,1,0,0,1,1,0},
-{0,0,1,1,1,0,0,1},
-{0,0,0,0,1,0,0,1},
-{0,0,0,0,0,1,1,0}
+ll A[N][N] = {
+    {0, 1, 0, 1, 0, 0, 0, 0},
+    {1, 0, 1, 1, 0, 0, 0, 0},
+    {0, 1, 0, 1, 1, 1, 0, 0},
+    {1, 1, 1, 0, 0, 1, 0, 0},
+    {0, 0, 1, 0, 0, 1, 1, 0},
+    {0, 0, 1, 1, 1, 0, 0, 1},
+    {0, 0, 0, 0, 1, 0, 0, 1},
+    {0, 0, 0, 0, 0, 1, 1, 0}
 };
+ll B[N][N], res[N][N];
 
-void multiply(ll A[][N], ll B[][N]) {
-    ll tmp[N][N];
-    loop(i, N) loop(j, N) {
-        ll x = 0;
-        loop(k, N) x += (A[i][k] * B[k][j]) % mod;
-        tmp[i][j] = x % mod;
+void mul(ll x[][N], ll y[][N]) {
+    memset(B, 0, sizeof(B));
+    loop(i, N) loop(k, N) loop(j, N) {
+        B[i][j] += x[i][k] * y[k][j];
+        B[i][j] %= mod;
     }
-    memcpy(A, tmp, sizeof(tmp));
-}
-void power(int D) {
-    if (D == 1) return;
-    ll tmp[N][N];
-    memcpy(tmp, mat, sizeof(mat));
-    power(D / 2);
-    multiply(mat, mat);
-    if (D % 2) multiply(mat, tmp);
+    memmove(x, B, sizeof(B));
 }
 int main() {
     int D; scanf("%d", &D);
-    power(D);
-    printf("%lld", mat[0][0]);
+    loop(i, N) res[i][i] = 1;
+    while (D) {
+        if (D & 1) mul(res, A);
+        mul(A, A);
+        D /= 2;
+    }
+    printf("%lld", res[0][0]);
     return 0;
 }
