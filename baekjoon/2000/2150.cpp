97807dc0301a305f9c65352d7fba2c458cc73286
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-06-28 17:23:51 +0900

    Strongly Connected Component

--- baekjoon/2000/2150.cpp
+++ baekjoon/2000/2150.cpp
@@ -1,69 +1,58 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define loop(i,n) for(int i=0;i<n;++i)
 
-int V;
-stack<int> stk; // stack by finish time
-bool visited[10001];
-vector<int> adj[10001], rev[10001];
-vector<vector<int>> ANS;
+const int max_V = 10001;
 
-// Kosaraju's Algorithm
-// 1. 정방향 그래프로 DFS 탐색.
-//    여기서 빨리 끝난 vertex 순서대로 stack에 집어넣음
-// 2. stack에서 순서대로 꺼내며 역방향 그래프로 DFS탐색
-void DFS1(int start)
-{
-    visited[start] = true;
-    loop(i, adj[start].size()) {
-        int To = adj[start][i];
-        if (!visited[To])
-            DFS1(To);
+vector<int> adj[max_V];
+int disc[max_V], low[max_V];
+bool on_stack[max_V];
+int t;
+stack<int> stk;
+vector<vector<int>> ans;
+
+void dfs(int u) {
+    disc[u] = low[u] = ++t;
+    stk.emplace(u);
+    on_stack[u] = true;
+
+    for (int& v : adj[u]) {
+        if (!disc[v]) dfs(v);
+        if (on_stack[v]) low[u] = min(low[u], low[v]);
     }
-    stk.push(start);
-}
 
-void DFS2(int start, vector<int>& vt)
-{
-    visited[start] = true;
-    vt.push_back(start);
-    loop(i, rev[start].size()) {
-        int To = rev[start][i];
-        if (!visited[To])
-            DFS2(To, vt);
+    if (disc[u] == low[u]) {
+        vector<int> tmp;
+        while (1) {
+            int x = stk.top(); stk.pop();
+            on_stack[x] = false;
+            tmp.emplace_back(x);
+            if (x == u) break;
+        }
+        ans.emplace_back(tmp);
     }
 }
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
-    int E;
-    cin >> V >> E;
+    int V, E; cin >> V >> E;
     while (E--) {
         int u, v; cin >> u >> v;
-        adj[u].push_back(v);
-        rev[v].push_back(u);
+        adj[u].emplace_back(v);
     }
-    for (int i = 1; i <= V; ++i)
-        if (!visited[i])
-            DFS1(i);
-    memset(visited, 0, sizeof(visited));
 
-    while (!stk.empty()) {
-        int Start = stk.top(); stk.pop();
-        if (visited[Start]) continue;
-        vector<int> tmp;
-        DFS2(Start, tmp);
-        sort(tmp.begin(), tmp.end());
-        ANS.push_back(tmp);
-    }
-    sort(ANS.begin(), ANS.end());
-    cout << ANS.size() << '\n';
-    loop(i, ANS.size()) {
-        loop(j, ANS[i].size())
-            cout << ANS[i][j] << ' ';
-        cout << "-1\n";
+    for (int i = 1; i <= V; ++i)
+        if (!disc[i])
+            dfs(i);
+
+    for (auto& v : ans) sort(v.begin(), v.end());
+    sort(ans.begin(), ans.end(), [] (const auto & a, const auto & b) -> bool { return a[0] < b[0]; });
+
+    cout << ans.size();
+    for (auto& v : ans) {
+        cout << '\n';
+        for (int& x : v) cout << x << ' ';
+        cout << -1;
     }
     return 0;
 }
