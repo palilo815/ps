d20615229aed707ab8153586b8569014a7833ee3
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-07-02 00:24:26 +0900

    선분 교차 2

--- baekjoon/17000/17387.cpp
+++ baekjoon/17000/17387.cpp
@@ -1,40 +1,34 @@
 #include <cstdio>
-typedef long long ll;
+#define swap(a,b) do{auto tmp=a;a=b;b=tmp;}while(0)
+using ll = long long;
 
-ll x[4], y[4];
-int ccw(int i1, int i2, int i3) {
-	ll ret = (x[i1] * y[i2] + x[i2] * y[i3] + x[i3] * y[i1]) -
-		(y[i1] * x[i2] + y[i2] * x[i3] + y[i3] * x[i1]);
-	if (ret > 0) return 1;
-	else if (ret < 0) return -1;
-	return 0;
-}
-int inside(int i1, int m, int i2) {
-	ll xl, xr, yd, yu;
-	if (x[i1] < x[i2])
-		xl = x[i1], xr = x[i2];
-	else
-		xl = x[i2], xr = x[i1];
-	if (y[i1] < y[i2])
-		yd = y[i1], yu = y[i2];
-	else
-		yd = y[i2], yu = y[i1];
-	return xl <= x[m] && x[m] <= xr && yd <= y[m] && y[m] <= yu;
+struct p {
+    ll x, y;
+    bool operator >(const p& rhs) const {
+        return x == rhs.x ? y > rhs.y : x > rhs.x;
+    }
+    bool operator <=(const p& rhs) const {
+        return x == rhs.x ? y <= rhs.y : x < rhs.x;
+    }
+};
+int ccw(p& a, p& b, p& c) {
+    ll ret = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+    return ret ? (ret > 0 ? 1 : -1) : 0;
 }
 int main() {
-	for (int i = 0; i < 4; ++i)
-		scanf("%lld %lld", &x[i], &y[i]);
+    p a[4];
+    for (int i = 0; i < 4; ++i)
+        scanf("%lld %lld", &a[i].x, &a[i].y);
 
-	int c1 = ccw(0, 1, 2), c2 = ccw(0, 1, 3);
-	if ((c1 == 0 && inside(0, 2, 1)) || (c2 == 0 && inside(0, 3, 1))) {
-		putchar('1');
-		return 0;
-	}
-	int c3 = ccw(2, 3, 0), c4 = ccw(2, 3, 1);
-	if ((c3 == 0 && inside(2, 0, 3)) || (c4 == 0 && inside(2, 1, 3))) {
-		putchar('1');
-		return 0;
-	}
-	printf("%d", (c1 != c2 && c3 != c4));
-	return 0;
+    int A = ccw(a[0], a[1], a[2]) * ccw(a[0], a[1], a[3]);
+    int B = ccw(a[2], a[3], a[0]) * ccw(a[2], a[3], a[1]);
+    int ans;
+    if (A | B) ans = A <= 0 && B <= 0;
+    else {
+        if (a[0] > a[1]) swap(a[0], a[1]);
+        if (a[2] > a[3]) swap(a[2], a[3]);
+        ans = a[0] <= a[3] && a[2] <= a[1];
+    }
+    printf("%d", ans);
+    return 0;
 }
