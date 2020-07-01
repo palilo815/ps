0d4cdef9241691e913b948669a83d0c76364009b
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-07-01 17:23:56 +0900

    수열과 쿼리 38

--- baekjoon/18000/18917.cpp
+++ baekjoon/18000/18917.cpp
@@ -1,18 +1,18 @@
 #include <cstdio>
 
 int main() {
-	int Q; scanf("%d", &Q);
-	long long res_sum = 0;
-	int res_xor = 0;
+    int Q; scanf("%d", &Q);
+    long long res_sum = 0;
+    int res_xor = 0;
 
-	while (Q--) {
-		int q; scanf("%d", &q);
-		if (q <= 2) {
-			int x; scanf("%d", &x);
-			res_sum += (q == 1 ? x : -x);
-			res_xor ^= x;
-		}
-		else printf("%lld\n", q == 3 ? res_sum : res_xor);
-	}
-	return 0;
+    while (Q--) {
+        int q; scanf("%d", &q);
+        if (q <= 2) {
+            int x; scanf("%d", &x);
+            res_sum += (q == 1 ? x : -x);
+            res_xor ^= x;
+        }
+        else printf("%lld\n", q == 3 ? res_sum : res_xor);
+    }
+    return 0;
 }
