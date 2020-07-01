4af305996dc75e9571eebf53e9d22d51abd33b9e
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-07-02 01:56:19 +0900

    Update ccw.cpp

--- Algorithm/ccw.cpp
+++ Algorithm/ccw.cpp
@@ -5,18 +5,18 @@ using namespace std;
 using p = pair<int, int>;
 
 int ccw(int x1, int y1, int x2, int y2, int x3, int y3) {
-    int ret = (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1);
-    return ret ? (ret > 0 ? 1 : -1) : 0;
+	int ret = (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1);
+	return ret ? (ret > 0 ? 1 : -1) : 0;
 }
 int ccw(p& a, p& b, p& c) {
-    int ret = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
-    return ret ? (ret > 0 ? 1 : -1) : 0;
+	int ret = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+	return ret ? (ret > 0 ? 1 : -1) : 0;
 }
 bool cross(p& a, p& b, p& c, p& d) {
-    int A = ccw(a, b, c) * ccw(a, b, d);
-    int B = ccw(c, d, a) * ccw(c, d, b);
-    if (A | B) return A <= 0 && B <= 0;
-    if (a > b) swap(a, b);
-    if (c > d) swap(c, d);
-    return a <= d && c <= b;
+	int A = ccw(a, b, c) * ccw(a, b, d);
+	int B = ccw(c, d, a) * ccw(c, d, b);
+	if (A | B) return A <= 0 && B <= 0;
+	if (a > b) swap(a, b);
+	if (c > d) swap(c, d);
+	return a <= d && c <= b;
 }
