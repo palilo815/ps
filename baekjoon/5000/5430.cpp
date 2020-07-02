1dbf0e2b7775786efb1434021fafd11e9d57510a
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-07-02 19:14:25 +0900

    AC

--- baekjoon/5000/5430.cpp
+++ baekjoon/5000/5430.cpp
@@ -1,59 +1,43 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
 using namespace std;
 
-int main() {
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
-    
-    deque<int> dq;
-    string order;
-    int T; cin >> T;
-    while (T--) {
-        dq.clear();
-        bool front = true, valid = true;
-        int x, len; char c;
-        cin >> order >> len >> c;
-        while (len--) {
-            cin >> x;
-            dq.emplace_back(x);
-            if (len) cin >> c;
-        }
-        cin >> c;
+const int M = 100000;
 
-        for (char c : order) {
-            if (c == 'R') front = !front;
-            else {
-                if (dq.empty()) {
-                    valid = false;
-                    break;
-                }
-                if (front) dq.pop_front();
-                else dq.pop_back();
-            }
-        }
+int dq[M];
+int d, i, j;
+char c;
 
-        if (!valid) {
-            cout << "error\n";
-            continue;
-        }
+bool error() {
+    string s; cin >> s;
+    cin >> j >> c;
+    for (i = 0; i < j; ++i)
+        cin >> dq[i] >> c;
+    if (!j) cin >> c;
 
-        cout << '[';
-        if (front) {
-            auto s = dq.begin(), e = dq.end();
-            while (s != e) {
-                if (s != dq.begin()) cout << ',';
-                cout << *s++;
-            }
+    i = 0, d = 1;
+    for (char& c : s) {
+        if (c == 'R') {
+            swap(i, j);
+            j += (d = -d);
         }
-        else {
-            auto s = dq.rbegin(), e = dq.rend();
-            while (s != e) {
-                if (s != dq.rbegin()) cout << ',';
-                cout << *s++;
-            }
-        }
-        cout << "]\n";
+        else if (i == j) return true;
+        i += d;
     }
+
+    for (cout << '['; i ^ j; i += d) {
+        cout << dq[i];
+        if ((i + d) ^ j) cout << ',';
+    }
+    cout << "]\n";
+    return false;
+}
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
+
+    int T; cin >> T;
+    while (T--)
+        if (error())
+            cout << "error\n";
     return 0;
 }
