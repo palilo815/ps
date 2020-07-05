cc016d2b38952fd3fc483dbb7bfffbb87fffd016
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-07-06 08:08:43 +0900

    LCA 2

--- baekjoon/11000/11438.cpp
+++ baekjoon/11000/11438.cpp
@@ -1,66 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int max_N = 100000;
+const int max_N = 100001;
 
-vector<int> adj[max_N + 1];
-vector<int> Euler_tour;
-int occurrence[max_N + 1];
+vector<int> adj[max_N];
 
-int cnt;
-int Euler_idx[max_N + 1];
-int original_idx[max_N];
+int sz[max_N], par[max_N], dep[max_N], top[max_N], idx[max_N];
 
-int segT[262144 * 2 - 1];
-
-void touring(int u, int p)
-{
-    original_idx[cnt] = u;
-    Euler_idx[u] = cnt;
-    occurrence[u] = Euler_tour.size();
-    ++cnt;
-    Euler_tour.push_back(Euler_idx[u]);
-    for (int v : adj[u]) if (v != p) {
-        touring(v, u);
-        Euler_tour.push_back(Euler_idx[u]);
-    }
+int dfs(int u, int p) {
+    par[u] = p, sz[u] = 1;
+    for (int& v : adj[u])
+        if (v ^ p)
+            sz[u] += dfs(v, u);
+    return sz[u];
 }
-int build_segT(int l, int r, int idx)
-{
-    if (l == r) return segT[idx] = Euler_tour[l];
-    int m = l + (r - l) / 2;
-    return segT[idx] = min(build_segT(l, m, 2 * idx + 1), build_segT(m + 1, r, 2 * idx + 2));
+void hld(int u, int p, int rt, int i, int d) {
+    dep[u] = d, top[u] = rt, idx[u] = i;
+    int H = -1;
+    for (int& v : adj[u])
+        if (v ^ p && (H == -1 || sz[v] > sz[H]))
+            H = v;
+
+    if (~H) hld(H, u, rt, i + 1, d);
+    for (int& v : adj[u])
+        if (v ^ p && v ^ H)
+            hld(v, u, v, 0, d + 1);
 }
-int query(int ql, int qr, int l, int r, int idx)
-{
-    if (ql <= l && r <= qr) return segT[idx];
-    if (qr < l || r < ql) return INT_MAX;
-    int m = l + (r - l) / 2;
-    return min(query(ql, qr, l, m, 2 * idx + 1), query(ql, qr, m + 1, r, 2 * idx + 2));
-}
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
     int N; cin >> N;
-    for (int i = 0; i < N - 1; ++i) {
+    for (int i = 1; i < N; ++i) {
         int u, v; cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        adj[u].emplace_back(v);
+        adj[v].emplace_back(u);
     }
 
-    Euler_tour.reserve(2 * N - 1);
-    touring(1, -1);
-    build_segT(0, 2 * N - 2, 0);
+    dfs(1, 0);
+    hld(1, 0, 1, 0, 0);
 
     int Q; cin >> Q;
     while (Q--) {
-        int a, b; cin >> a >> b;
-        int ql = occurrence[a], qr = occurrence[b];
-        if (ql > qr) swap(ql, qr);
-        int q = query(ql, qr, 0, 2 * N - 2, 0);
-        cout << original_idx[q] << '\n';
+        int u, v; cin >> u >> v;
+        while (top[u] ^ top[v])
+            dep[u] < dep[v] ? (v = par[top[v]]) : (u = par[top[u]]);
+        cout << (idx[u] < idx[v] ? u : v) << '\n';
     }
     return 0;
 }
