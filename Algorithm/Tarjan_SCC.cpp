14ade9888fbcd6508e263c01243c666113f79aa8
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-07-05 15:45:58 +0900

    Update Tarjan_SCC.cpp

--- Algorithm/Tarjan_SCC.cpp
+++ Algorithm/Tarjan_SCC.cpp
@@ -3,32 +3,30 @@ using namespace std;
 
 const int max_V = 10000;
 
-int cnt;
+int t;
 vector<int> adj[max_V + 1];
 stack<int> stk;
-bool in_stack[max_V + 1];
+bool on_stack[max_V + 1];
 int disc[max_V + 1], low[max_V + 1];
 
 vector<vector<int>> ans;
 
-void DFS(int u) {
-    disc[u] = low[u] = ++cnt;
-    stk.push(u);
-    in_stack[u] = true;
-    for (int v : adj[u]) {
-        if (!disc[v]) {
-            DFS(v);
-            low[u] = min(low[u], low[v]);
-        }
-        else if (in_stack[v])
-            low[u] = min(low[u], disc[v]);
+void dfs(int u) {
+    disc[u] = low[u] = ++t;
+    on_stack[u] = true;
+    stk.emplace(u);
+
+    for (int& v : adj[u]) {
+        if (!disc[v]) dfs(v);
+        if (on_stack[v]) low[u] = min(low[u], low[v]);
     }
+
     if (disc[u] == low[u]) {
         vector<int> tmp;
         while (1) {
             int x = stk.top(); stk.pop();
             tmp.emplace_back(x);
-            in_stack[x] = false;
+            on_stack[x] = false;
             if (x == u) break;
         }
         ans.emplace_back(tmp);
@@ -42,12 +40,13 @@ int main() {
     ios::sync_with_stdio(false);
 
     int V, E; cin >> V >> E;
-    while(E--) {
+    while (E--) {
         int u, v; cin >> u >> v;
         adj[u].emplace_back(v);
     }
-    for (int i = 1; i <= V; ++i) if (!disc[i])
-        DFS(i);
+    for (int i = 1; i <= V; ++i)
+        if (!disc[i])
+            dfs(i);
     for (vector<int>& v : ans)
         sort(v.begin(), v.end());
     sort(ans.begin(), ans.end(), cmp);
