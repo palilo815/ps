12a392a2f70ff07e33829814b37e61e6c6cb6fec
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-07-06 20:49:17 +0900

    8진수, 10진수, 16진수

--- baekjoon/11000/11816.cpp
+++ baekjoon/11000/11816.cpp
@@ -1,32 +1,7 @@
-#include <bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
-using namespace std;
+#include <cstdio>
 
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
-
-    string s; cin >> s;
-    if (s[0] == '0') {
-        if (s[1] == 'x') {
-            reverse(s.begin(), s.end());
-            int Ans = 0;
-            loop(i, s.size() - 2) {
-                int n = s[i] - '0';
-                if (n > 9) n -= 39;
-                Ans += n * pow(16, i);
-            }
-            cout << Ans;
-        }
-        else {
-            reverse(s.begin(), s.end());
-            int Ans = 0;
-            loop(i, s.size() - 1)
-                Ans += (s[i] - '0') * pow(8, i);
-            cout << Ans;
-        }
-    }
-    else cout << s;
+int main() {
+    int x; scanf("%i", &x);
+    printf("%d", x);
     return 0;
 }
