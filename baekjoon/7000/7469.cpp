15ed4fbdba901d553392775b5ad94fdcf3979423
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-07-08 23:34:54 +0900

    K번째 수

--- baekjoon/7000/7469.cpp
+++ baekjoon/7000/7469.cpp
@@ -1,86 +1,46 @@
 #include <bits/stdc++.h>
+#define loop(i,n) for(int i=0;i<n;++i)
+#define x first
+#define y second
 using namespace std;
+using p = pair<int, int>;
 
 const int max_N = 100000;
+const size_t LEN = 131072;
 
-int arr[max_N];
-vector<int> segT[131072 * 2 - 1];
+p a[max_N];
+int table[17][LEN];
 
-vector<int> merge_segT(vector<int>& a, vector<int>& b)
-{
-    int len_a = a.size(), len_b = b.size();
-    vector<int> ret;
-    ret.reserve(len_a + len_b);
-    int i = 0, j = 0;
-    while (i < len_a && j < len_b) {
-        if (a[i] < b[j]) ret.push_back(a[i++]);
-        else ret.push_back(b[j++]);
-    }
-    while (i < len_a) ret.push_back(a[i++]);
-    while (j < len_b) ret.push_back(b[j++]);
-    return ret;
-}
-void build_segT(int l, int r, int idx)
-{
-    if (l == r) {
-        segT[idx].push_back(arr[l]);
-        return;
-    }
-    int m = l + (r - l) / 2;
-    build_segT(l, m, 2 * idx + 1);
-    build_segT(m + 1, r, 2 * idx + 2);
-    segT[idx] = merge_segT(segT[2 * idx + 1], segT[2 * idx + 2]);
-}
-void query(vector<vector<int>*>& vt, int ql, int qr, int l, int r, int idx)
-{
-    if (ql <= l && r <= qr) {
-        vt.push_back(&segT[idx]);
-        return;
-    }
-    if (qr < l || r < ql) return;
-    int m = l + (r - l) / 2;
-    query(vt, ql, qr, l, m, 2 * idx + 1);
-    query(vt, ql, qr, m + 1, r, 2 * idx + 2);
-    return;
-}
-int kth_num(int ql, int qr, int k, int N)
-{
-    // [ql,qr] 범위의 segT의 포인터를 vt에 담는다.
-    vector<vector<int>*> vt;
-    query(vt, ql, qr, 0, N - 1, 0);
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
-    // 이분탐색으로 k번째 요소를 찾는다.
-    // cnt : m보다 작은 숫자의 개수
-    int s = -1000000000, e = 1000000000;
-    int ret = 0;
-    while (s <= e) {
-        int m = s + (e - s) / 2;
-        int cnt = 0;
-        for (auto ptr : vt)
-            cnt += lower_bound(ptr->begin(), ptr->end(), m) - ptr->begin();
-        if (cnt > k - 1)
-            e = m - 1;
-        else {
-            ret = m;
-            s = m + 1;
+    int N, M; cin >> N >> M;
+    loop(i, N) {
+        cin >> a[i].x;
+        a[i].y = i;
+    }
+
+    int L = 1, D = -1;
+    while (L < N) L <<= 1, ++D;
+
+    sort(a, a + N);
+    loop(i, N) table[0][i] = a[i].y;
+    memset(table[0] + N, 0x3f, sizeof(int) * (L - N));
+
+    for (int d = 0, k = 1; d < D; ++d, k <<= 1)
+        for (int* t = table[d]; t < table[d] + L; t += k << 1)
+            merge(t, t + k, t + k, t + k * 2, t + LEN);
+
+    while (M--) {
+        int i, j, k, d = D, l = 0, r = L; cin >> i >> j >> k;
+        for (--i, --j, --k; d >= 0; --d) {
+            int m = (l + r) >> 1;
+            int cnt = upper_bound(table[d] + l, table[d] + m, j) -
+                      lower_bound(table[d] + l, table[d] + m, i);
+            k >= cnt ? (l = m, k -= cnt) : (r = m);
         }
-    }
-    return ret;
-}
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
-
-    int N, Q; cin >> N >> Q;
-    for (int i = 0; i < N; ++i)
-        cin >> arr[i];
-
-    build_segT(0, N - 1, 0);
-
-    while (Q--) {
-        int a, b, k; cin >> a >> b >> k;
-        cout << kth_num(a - 1, b - 1, k, N) << '\n';
+        cout << a[l].x << '\n';
     }
     return 0;
 }
