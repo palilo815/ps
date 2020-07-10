574efec71bbfad5b5261d008d802082fcb832309
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-07-10 11:59:00 +0900

    최솟값과 최댓값

--- baekjoon/2000/2357.cpp
+++ baekjoon/2000/2357.cpp
@@ -1,44 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef pair<int, int> p;
 
-const int max_N = 100000;
+const int max_N = 1e5;
 
-int arr[max_N];
-p segT[131072 * 2 - 1];
+int N, M;
+int minT[max_N << 1];
+int maxT[max_N << 1];
 
-inline p min_max(p a, p b) {
-    return { min(a.first,b.first),max(a.second,b.second) };
-}
-p build_segT(int l, int r, int idx) {
-    if (l == r) return segT[idx] = { arr[l],arr[l] };
-    int m = l + (r - l) / 2;
-    int c1 = 2 * idx + 1, c2 = 2 * idx + 2;
-    build_segT(l, m, c1);
-    build_segT(m + 1, r, c2);
-    return segT[idx] = min_max(segT[c1], segT[c2]);
-}
-p query(int ql, int qr, int l, int r, int idx) {
-    if (ql <= l && r <= qr) return segT[idx];
-    if (qr < l || r < ql) return { INT_MAX,INT_MIN };
-    int m = l + (r - l) / 2;
-    int c1 = 2 * idx + 1, c2 = 2 * idx + 2;
-    return min_max(query(ql, qr, l, m, c1), query(ql, qr, m + 1, r, c2));
+pair<int, int> query(int l, int r) {
+    int x = INT_MAX, y = INT_MIN;
+    for (l += N, r += N; l < r; l >>= 1, r >>= 1) {
+        if (l & 1) {
+            x = min(x, minT[l]), y = max(y, maxT[l]);
+            ++l;
+        }
+        if (r & 1) {
+            --r;
+            x = min(x, minT[r]), y = max(y, maxT[r]);
+        }
+    }
+    return {x, y};
 }
 int main() {
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
-    int N, M; cin >> N >> M;
-    for (int i = 0; i < N; ++i)
-        cin >> arr[i];
+    cin >> N >> M;
+    for (int i = N; i < N << 1; ++i)
+        cin >> minT[i];
+    copy(minT + N, minT + N + N, maxT + N);
 
-    build_segT(0, N - 1, 0);
+    for (int i = N - 1; i; --i) {
+        minT[i] = min(minT[i << 1], minT[i << 1 | 1]);
+        maxT[i] = max(maxT[i << 1], maxT[i << 1 | 1]);
+    }
 
     while (M--) {
         int ql, qr; cin >> ql >> qr;
-        p ans = query(ql - 1, qr - 1, 0, N - 1, 0);
-        cout << ans.first << ' ' << ans.second << '\n';
+        auto [x, y] = query(ql - 1, qr);
+        cout << x << ' ' << y << '\n';
     }
     return 0;
 }
