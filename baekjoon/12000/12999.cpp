3bb87eb3f52c54856101f103c3744ffda089f7cb
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-07-16 20:39:57 +0900

    화려한 마을3

--- baekjoon/12000/12999.cpp
+++ baekjoon/12000/12999.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
-#define LOOP(i,n) for(int i=1;i<=n;++i)
+#define loop(i,n) for(int i=0;i<n;++i)
 using namespace std;
 struct query {
     int l, r, i;
+    query(int l, int r, int i) : l(l), r(r), i(i) {}
 };
 
-const int max_N = 1e5 + 1;
+const int max_N = 1e5;
 const int max_p = 2e5 + 1;
 
 int p[max_N], cnt[max_p], total[max_N];
 
-query q[max_N];
+vector<query> q[317];
 int ans[max_N], res;
 
 void insert(int i) {
@@ -25,35 +26,46 @@ void erase(int i) {
     --total[cnt[n]];
     ++total[--cnt[n]];
 }
+void solve(vector<query>& q) {
+    if (q.empty()) return;
+    sort(q.begin(), q.end(), [&](auto & a, auto & b) -> bool {
+        return a.r == b.r ? a.l < b.l : a.r < b.r;
+    });
+
+    res = 0;
+    memset(cnt, 0, sizeof(cnt));
+    memset(total, 0, sizeof(total));
+
+    int l = q[0].l, r = q[0].r, sz = q.size();
+    for (int i = l; i < r; ++i)
+        insert(i);
+    ans[q[0].i] = res;
+
+    for (int j = 1; j ^ sz; ++j) {
+        auto& [L, R, i] = q[j];
+        while (l > L) insert(--l);
+        while (R > r) insert(r++);
+        while (l < L) erase(l++);
+        while (R < r) erase(--r);
+        ans[i] = res;
+    }
+}
 int main() {
     cin.tie(0), cout.tie(0);
     ios::sync_with_stdio(0);
 
     int N, Q; cin >> N >> Q;
-    LOOP(i, N) cin >> p[i];
-    LOOP(i, N) p[i] += 100000;
-    LOOP(i, Q) q[i].i = i;
-    LOOP(i, Q) cin >> q[i].l >> q[i].r;
+    loop(i, N) cin >> p[i];
+    loop(i, N) p[i] += 100000;
 
     int sqrt_N = sqrt(N);
-    sort(q + 1, q + Q + 1, [&](auto & a, auto & b) -> bool {
-        int A = a.l / sqrt_N, B = b.l / sqrt_N;
-        return A == B ? a.r < b.r : A < B;
-    });
-
-    int l = q[1].l, r = q[1].r;
-    for (int i = l; i <= r; ++i)
-        insert(i);
-    ans[q[1].i] = res;
-
-    for (int j = 2; j <= Q; ++j) {
-        auto& [L, R, i] = q[j];
-        while (l > L) insert(--l);
-        while (R > r) insert(++r);
-        while (l < L) erase(l++);
-        while (R < r) erase(r--);
-        ans[i] = res;
+    loop(i, Q) {
+        int l, r; cin >> l >> r;
+        --l;
+        q[l / sqrt_N].emplace_back(l, r, i);
     }
-    LOOP(i, Q) cout << ans[i] << '\n';
+
+    loop(i, sqrt_N + 1) solve(q[i]);
+    loop(i, Q) cout << ans[i] << '\n';
     return 0;
 }
