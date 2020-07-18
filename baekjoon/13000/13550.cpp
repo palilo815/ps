f654dc7b497433a8078761d4132552e5187bf958
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-07-19 01:44:49 +0900

    수열과 쿼리 7

---
+++ baekjoon/13000/13550.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#define loop(i,n) for(int i=0;i<n;++i)
+using namespace std;
+struct query {
+    int l, r, i;
+    query(int l, int r, int i): l(l), r(r), i(i) {}
+};
+
+const int max_N = 1e5;
+const int max_K = 1e6;
+const int max_sq = 317;
+
+vector<query> vt[max_sq];
+int K, a[max_N + 1], ans[max_N];
+int fst[max_K], lst[max_K];
+
+void solve(vector<query>& vt, int s, int e) {
+    memset(fst, -1, sizeof(int) * K);
+    memset(lst, -1, sizeof(int) * K);
+    sort(vt.begin(), vt.end(), [&](auto & a, auto & b) -> bool {
+        return a.r < b.r;
+    });
+
+    int res2 = 0;
+    for (auto& [l, r, i] : vt) {
+        for (; s < r; ++s) {
+            lst[a[s]] = s;
+            if (s >= e)
+                ~fst[a[s]] ? (res2 = max(res2, s - fst[a[s]])) : (fst[a[s]] = s);
+        }
+
+        int res1 = 0;
+        for (int j = min(r, e); l < j; ++l)
+            res1 = max(res1, lst[a[l]] - l);
+        ans[i] = max(res1, res2);
+    }
+}
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
+
+    int N; cin >> N >> K;
+    loop(i, N) cin >> a[i + 1];
+    loop(i, N) a[i + 1] = (a[i] + a[i + 1]) % K;
+    N = sqrt(N) + 1;
+
+    int Q; cin >> Q;
+    loop(i, Q) {
+        int l, r; cin >> l >> r;
+        --l;
+        vt[l / N].emplace_back(l, ++r, i);
+    }
+
+    loop(i, N + 1) if (!vt[i].empty())
+        solve(vt[i], i * N, (i + 1) * N);
+
+    loop(i, Q) cout << ans[i] << '\n';
+    return 0;
+}
