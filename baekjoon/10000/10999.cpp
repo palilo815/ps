8bf88115f5826c16f9255c0bb23549bb5e7e0b73
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-07-22 14:50:03 +0900

    구간 합 구하기 2

--- baekjoon/10000/10999.cpp
+++ baekjoon/10000/10999.cpp
@@ -1,48 +1,53 @@
 #include <bits/stdc++.h>
+#define len (r-l)
+#define left (i<<1)
+#define right (i<<1|1)
 using namespace std;
 using ll = long long;
 
-const int M = 1000001;
+const int seg_N = 1048576;
 
-int N;
-ll fen1[M], fen2[M];
+ll segT[seg_N << 1], lazy[seg_N << 1];
 
-void update(ll fenwick[], int i, ll v) {
-    for (; i <= N; i += i & -i)
-        fenwick[i] += v;
+ll query(int l, int r, int ql, int qr, int i) {
+    if (qr <= l || r <= ql) return 0;
+    if (ql <= l && r <= qr) return segT[i] + lazy[i] * len;
+    if (lazy[i]) {
+        if (l + 1 ^ r) lazy[left] += lazy[i], lazy[right] += lazy[i];
+        segT[i] += lazy[i] * len;
+        lazy[i] = 0;
+    }
+
+    int m = (l + r) >> 1;
+    return query(l, m, ql, qr, left) + query(m, r, ql, qr, right);
 }
-ll read(ll fenwick[], int i) {
-    ll ret = 0;
-    for (; i; i -= i & -i)
-        ret += fenwick[i];
-    return ret;
-}
-void Q1(int l, int r, ll v) {
-    update(fen1, l, v), update(fen1, r + 1, -v);
-    update(fen2, l, v * (l - 1)), update(fen2, r + 1, -v * r);
-}
-ll Q2(int l, int r) {
-    return read(fen1, r) * r - read(fen2, r) -
-           read(fen1, l - 1) * (l - 1) + read(fen2, l - 1);
+void update(int l, int r, int ql, int qr, ll qv, int i) {
+    if (qr <= l || r <= ql) return;
+    if (ql <= l && r <= qr) {lazy[i] += qv; return;}
+
+    int m = (l + r) >> 1;
+    update(l, m, ql, qr, qv, left), update(m, r, ql, qr, qv, right);
+    segT[i] = segT[left] + segT[right] + lazy[left] * (len >> 1) + lazy[right] * (len >> 1);
 }
 int main() {
     cin.tie(0), cout.tie(0);
     ios::sync_with_stdio(0);
 
-    int Q, QQ; cin >> N >> Q >> QQ;
-    Q += QQ;
-    for (int i = 1; i <= N; ++i) {
-        ll x; cin >> x;
-        Q1(i, i, x);
-    }
+    int N, Q, Q2; cin >> N >> Q >> Q2;
+    Q += Q2;
+    for (int i = seg_N; i < seg_N + N; ++i)
+        cin >> segT[i];
+
+    for (int i = seg_N - 1; i; --i)
+        segT[i] = segT[left] + segT[right];
 
     for (int i = 0, q, l, r; i < Q; ++i) {
-        cin >> q >> l >> r;
+        cin >> q >> l >> r; --l;
         if (q == 1) {
             ll v; cin >> v;
-            Q1(l, r, v);
+            update(0, seg_N, l, r, v, 1);
         }
-        else  cout << Q2(l, r) << '\n';
+        else cout << query(0, seg_N, l, r, 1) << '\n';
     }
     return 0;
 }
