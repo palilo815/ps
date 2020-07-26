acbe400d7db619f48a167e179d83c9ca57c264d5
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-07-26 13:49:04 +0900

    나는 친구가 적다 (Large)

--- baekjoon/16000/16172.cpp
+++ baekjoon/16000/16172.cpp
@@ -1,59 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> get_partial_match(string& needle)
-{
-    int M = needle.size();
-    vector<int> pi(M);
-    int begin = 1, matched = 0;
-    while (begin + matched < M) {
-        if (needle[begin + matched] == needle[matched]) {
-            ++matched;
-            pi[begin + matched - 1] = matched;
-        }
-        else {
-            if (matched == 0) ++begin;
-            else {
-                begin += matched - pi[matched - 1];
-                matched = pi[matched - 1];
-            }
-        }
-    }
-    return pi;
-}
-bool kmp_search(string& haystack, string& needle)
-{
-    int N = haystack.size(), M = needle.size();
-    vector<int> pi = get_partial_match(needle);
-    int begin = 0, matched = 0;
-    while (begin <= N - M) {
-        if (matched < M && haystack[begin + matched] == needle[matched]) {
-            ++matched;
-            if (matched == M) return true;
-        }
-        else {
-            if (matched == 0) ++begin;
-            else {
-                begin += matched - pi[matched - 1];
-                matched = pi[matched - 1];
-            }
-        }
-    }
-    return false;
-}
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
-    string hay = "", needle;
-    char x;
-    while (cin.peek() != '\n') {
-        cin >> x;
-        if (x < '0' || x > '9') hay += x;
+    string s, k; cin >> s >> k;
+    s.erase(remove_if(s.begin(), s.end(), [&](char c) {
+        return '0' <= c && c <= '9';
+    }), s.end());
+
+    int M = k.size();
+    vector<int> lps(M);
+    for (int i = 1, j = 0; i < M; ++i) {
+        while (j && k[i] ^ k[j]) j = lps[j - 1];
+        if (k[i] == k[j]) lps[i] = ++j;
     }
-    cin.ignore();
-    cin >> needle;
-    cout << kmp_search(hay, needle);
+
+    int N = s.size();
+    for (int i = 0, j = 0; i < N; ++i) {
+        while (j && s[i] ^ k[j]) j = lps[j - 1];
+        if (s[i] == k[j] && ++j == M) {
+            cout << 1;
+            return 0;
+        }
+    }
+    cout << 0;
     return 0;
 }
