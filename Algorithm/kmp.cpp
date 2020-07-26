21f2c49dd9b89463fefab0de9e9ffd489d8da804
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-07-26 13:48:47 +0900

    Create kmp.cpp

---
+++ Algorithm/kmp.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
+
+    string s, k; cin >> s >> k;
+
+    int M = k.size();
+    vector<int> lps(M);
+    for (int i = 1, j = 0; i < M; ++i) {
+        while (j && k[i] ^ k[j]) j = lps[j - 1];
+        if (k[i] == k[j]) lps[i] = ++j;
+    }
+
+    int N = s.size();
+    for (int i = 0, j = 0; i < N; ++i) {
+        while (j && s[i] ^ k[j]) j = lps[j - 1];
+        if (s[i] == k[j] && ++j == M) {
+            cout << 1;
+            return 0;
+        }
+    }
+    cout << 0;
+    return 0;
+}
