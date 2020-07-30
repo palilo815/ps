5c1e18dffbe0e0a13ef1e8788ec56003e2824a6c
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-07-30 21:21:01 +0900

    돌 게임

--- baekjoon/9000/9655.cpp
+++ baekjoon/9000/9655.cpp
@@ -1,14 +1,7 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define loop(i,n) for(int i=0;i<n;++i)
+#include <cstdio>
 
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
-
-    int n; cin >> n;
-    if (n % 2) cout << "SK";
-    else cout << "CY";
+int main() {
+    int N; scanf("%d", &N);
+    printf("%s", N & 1 ? "SK" : "CY");
     return 0;
 }
