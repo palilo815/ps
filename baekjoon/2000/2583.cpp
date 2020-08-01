9468ff55ebccc7cb311a2184a72b7fdf9f0292e3
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-08-01 17:06:33 +0900

    영역 구하기

--- baekjoon/2000/2583.cpp
+++ baekjoon/2000/2583.cpp
@@ -1,45 +1,41 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=0; i<n; ++i)
+#define loop(i,n) for(int i=0;i<n;++i)
 using namespace std;
 
-int row, col;
-int mov[4][2] = { -1,0,0,-1,0,1,1,0 };
-bool colour[100][100];
+const int mx = 100;
+const int mov[4][2] = { -1, 0, 0, -1, 0, 1, 1, 0};
 
-int DFS(int r, int c)
-{
-    colour[r][c] = true;
+int row, col;
+bool v[mx][mx];
+
+int dfs(int r, int c) {
+    v[r][c] = true;
     int ret = 1;
     loop(i, 4) {
         int R = r + mov[i][0], C = c + mov[i][1];
-        if (R < 0 || R >= row || C < 0 || C >= col || colour[R][C]) continue;
-        ret += DFS(R, C);
+        if (~R && R ^ row && ~C && C ^ col && !v[R][C])
+            ret += dfs(R, C);
     }
     return ret;
 }
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
-int main()
-{
-    cin.tie(NULL);
-    ios::sync_with_stdio(false);
-
-    int N;
-    cin >> row >> col >> N;
-    while (N--) {
-        int r1, c1, r2, c2; cin >> c1 >> r1 >> c2 >> r2;
+    int K; cin >> row >> col >> K;
+    for (int r1, c1, r2, c2; K--;) {
+        cin >> c1 >> r1 >> c2 >> r2;
         for (int i = r1; i < r2; ++i)
             for (int j = c1; j < c2; ++j)
-                colour[i][j] = true;
+                v[i][j] = true;
     }
 
     vector<int> ans;
-    loop(i, row) loop(j, col)
-        if (!colour[i][j])
-            ans.push_back(DFS(i, j));
+    loop(i, row) loop(j, col) if (!v[i][j])
+        ans.emplace_back(dfs(i, j));
     sort(ans.begin(), ans.end());
 
     cout << ans.size() << '\n';
-    for(int x : ans)
-        cout << x << ' ';
+    for (int& x : ans) cout << x << ' ';
     return 0;
 }
