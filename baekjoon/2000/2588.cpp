0b9cbe5df8e7d16fef0abca839bc3c16db9a7386
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-08-01 16:26:38 +0900

    곱셈

--- baekjoon/2000/2588.cpp
+++ baekjoon/2000/2588.cpp
@@ -1,13 +1,7 @@
-import java.util.Scanner;
+#include <cstdio>
 
-public class Main {
-    public static void main(String[]args) {
-        Scanner scanner = new Scanner(System.in);
-        int num1 = scanner.nextInt();
-        int num2 = scanner.nextInt();
-        System.out.println(num1 * (num2 % 10));
-        System.out.println(num1 * ((num2 % 100) / 10));
-        System.out.println(num1 * (num2 / 100));
-        System.out.println(num1 * num2);
-    }
+int main() {
+    int a, b; scanf("%d%d", &a, &b);
+    printf("%d\n%d\n%d\n%d", a * (b % 10), a * (b % 100 / 10), a * (b / 100), a * b);
+    return 0;
 }
