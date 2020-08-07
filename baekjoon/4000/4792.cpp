1fe8dc3ef8161d82c6bf9b99b1b3d96ae3eeecab
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-08-07 15:15:24 +0900

    레드 블루 스패닝 트리

--- baekjoon/4000/4792.cpp
+++ baekjoon/4000/4792.cpp
@@ -1,61 +1,49 @@
-#include <iostream>
-#define LOOP(i,n) for(int i=1;i<=n;++i)
+#include <bits/stdc++.h>
 using namespace std;
 
-const int max_N = 1000;
+const int mxN = 1001;
 
-int blue[max_N + 1], red[max_N + 1], MST[max_N + 1];
+int* par, * cnt;
+int blue[mxN], red[mxN], mst[mxN];
 
-int find_rt(int u, int parent[])
-{
-    if (u == parent[u]) return u;
-    return parent[u] = find_rt(parent[u], parent);
+int _find(int u) {
+    return ~par[u] ? (par[u] = _find(par[u])) : u;
 }
-void union_tr(int u, int v, int parent[], int& cnt)
-{
-    u = find_rt(u, parent), v = find_rt(v, parent);
-    if (u != v) {
-        parent[u] = v;
-        ++cnt;
-    }
-}
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
-    while (1) {
-        int N, M, K; cin >> N >> M >> K;
+    for (int N, M, K;;) {
+        cin >> N >> M >> K;
         if (N == 0) break;
-        LOOP(i, N) blue[i] = i;
-        LOOP(i, N) red[i] = i;
-        LOOP(i, N) MST[i] = i;
-        int cnt_B = 0, cnt_R = 0, cnt_MST = 0;
 
-        while (M--) {
-            char c; int u, v;
-            cin >> c >> u >> v;
-            int* parent = (c == 'B' ? blue : red);
-            int& cnt = (c == 'B' ? cnt_B : cnt_R);
+        memset(blue + 1, -1, sizeof(int) * N);
+        memset(red + 1, -1, sizeof(int) * N);
+        memset(mst + 1, -1, sizeof(int) * N);
+        int cnt_B = 0, cnt_R = 0, cnt_mst = 0;
 
-            union_tr(u, v, MST, cnt_MST);
-            union_tr(u, v, parent, cnt);
+        par = mst;
+        for (int u, v, U, V; M--; par = mst) {
+            char c; cin >> c >> U >> V;
+            u = _find(U), v = _find(V);
+            if (u ^ v) par[u] = v, ++cnt_mst;
+
+            c == 'B' ? (par = blue, cnt = &cnt_B) : (par = red, cnt = &cnt_R);
+            u = _find(U), v = _find(V);
+            if (u ^ v) par[u] = v, ++*cnt;
         }
-
         /*
-            cnt_MST : red, blue 신경 끄고 spanning-tree를 만들 때 edge 개수
+            cnt_mst : red, blue 신경 끄고 spanning-tree를 만들 때 edge 개수
                       만약 N-1이 아니라면 애초에 spanning-tree를 만들 수 없다.
             cnt_R,B : red, blue만 이용해서 spanning-tree를 만들 때 edge 개수
-                      만약 cnt_MST == N-1이라면
+                      만약 cnt_mst == N-1이라면
                       cnt_R : red edge를 최대한 사용해서 만든 spanning-tree의 red edge 개수
                              ( == blue를 최소한으로 사용)
                       cnt_B : blue edge를 최대한 사용해서 만든 spanning-tree의 blue edge 개수
 
             blue의 최소, 최대 범위 안에 K가 있어야 한다.
         */
-
-        cout << (cnt_MST == N - 1 && (N - 1 - cnt_R <= K && K <= cnt_B)) << '\n';
+        cout << (cnt_mst == N - 1 && (N - 1 - cnt_R <= K && K <= cnt_B)) << '\n';
     }
-
     return 0;
 }
