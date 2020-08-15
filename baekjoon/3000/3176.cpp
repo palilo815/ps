6a1a4050f58a537e7bd4a53884da6a6e68fb6445
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-08-15 14:19:13 +0900

    도로 네트워크

--- baekjoon/3000/3176.cpp
+++ baekjoon/3000/3176.cpp
@@ -1,112 +1,92 @@
 #include <bits/stdc++.h>
+#define left (i<<1)
+#define right (i<<1|1)
 using namespace std;
-typedef pair<int, int> p;
+using p = pair<int, int>;
 
-const int M = 100001;
+const int mx = 1e5 + 1;
 
-vector<p> adj[M];
-int sz[M], parent[M], dist[M];
+vector<p> adj[mx];
+int t, sz[mx], par[mx], top[mx], dist[mx], idx[mx];
+int minT[mx << 1], maxT[mx << 1];
 
-vector<int> chain[M];
-int depth[M];
-int chain_number[M], chain_index[M];
-
-vector<p> segT[M];
-
-int DFS(int u, int p) {
-    parent[u] = p;
-    sz[u] = 1;
-    for (auto [w, v] : adj[u]) if (v != p) {
-        dist[v] = w;
-        sz[u] += DFS(v, u);
-    }
-    return sz[u];
+void dfs(int u, int p) {
+	par[u] = p, sz[u] = 1;
+	for (auto& [w, v] : adj[u])
+		if (v ^ p) {
+			dfs(v, u);
+			dist[v] = w;
+			sz[u] += sz[v];
+		}
 }
-void HLD(int u, int p, int ch, int d) {
-    depth[u] = d;
-    chain_number[u] = ch;
-    chain_index[u] = chain[ch].size();
-    chain[ch].emplace_back(u);
+void hld(int u, int p) {
+	int H = 0;
+	for (auto& [_, v] : adj[u])
+		if (v ^ p && sz[v] > sz[H])
+			H = v;
+	for (auto& [_, v] : adj[u])
+		if (v ^ p && v ^ H)
+			hld(v, u);
 
-    int H = -1;
-    for (auto [w, v] : adj[u]) if (v != p)
-        if (H == -1 || sz[v] > sz[H])
-            H = v;
+	if (!top[u]) top[u] = u;
+	if (H) top[H] = top[u], hld(H, u);
 
-    if (H != -1)
-        HLD(H, u, ch, d);
-    for (auto [w, v] : adj[u]) if (v != p && v != H)
-        HLD(v, u, v, d + 1);
+	par[u] = p, idx[u] = t++;
 }
-p min_max(const p& a, const p& b) {
-    p ret;
-    ret.first = min(a.first, b.first);
-    ret.second = max(a.second, b.second);
-    return ret;
+int mn_query(int l, int r) {
+	int ret = INT_MAX;
+	for (l += t, r += t; l ^ r; l >>= 1, r >>= 1) {
+		if (l & 1) ret = min(ret, minT[l++]);
+		if (r & 1) ret = min(ret, minT[--r]);
+	}
+	return ret;
 }
-void build_segT(int ch, int l, int r, int idx) {
-    if (l == r) {
-        segT[ch][idx] = { dist[chain[ch][l]],dist[chain[ch][l]] };
-        return;
-    }
-
-    int m = (l + r) / 2;
-    int l_child = 2 * idx + 1, r_child = 2 * idx + 2;
-
-    build_segT(ch, l, m, l_child);
-    build_segT(ch, m + 1, r, r_child);
-    segT[ch][idx] = min_max(segT[ch][l_child], segT[ch][r_child]);
+int mx_query(int l, int r) {
+	int ret = INT_MIN;
+	for (l += t, r += t; l ^ r; l >>= 1, r >>= 1) {
+		if (l & 1) ret = max(ret, maxT[l++]);
+		if (r & 1) ret = max(ret, maxT[--r]);
+	}
+	return ret;
 }
-p query(int ch, int ql, int qr, int l, int r, int idx) {
-    if (ql <= l && r <= qr) return segT[ch][idx];
-    if (qr < l || r < ql) return { INT_MAX,INT_MIN };
-
-    int m = (l + r) / 2;
-    return min_max(query(ch, ql, qr, l, m, 2 * idx + 1), query(ch, ql, qr, m + 1, r, 2 * idx + 2));
-}
-p solve(int u, int v) {
-    p ret = { INT_MAX, INT_MIN };
-    while (chain_number[u] != chain_number[v]) {
-        if (depth[u] > depth[v]) {
-            ret = min_max(ret, query(chain_number[u], 0, chain_index[u], 0, chain[chain_number[u]].size() - 1, 0));
-            u = parent[chain_number[u]];
-        }
-        else {
-            ret = min_max(ret, query(chain_number[v], 0, chain_index[v], 0, chain[chain_number[v]].size() - 1, 0));
-            v = parent[chain_number[v]];
-        }
-    }
-    if (chain_index[u] > chain_index[v]) swap(u, v);
-    ret = min_max(ret, query(chain_number[u], chain_index[u] + 1, chain_index[v], 0, chain[chain_number[u]].size() - 1, 0));
-    return ret;
+void solve(int u, int v) {
+	int x = INT_MAX, y = INT_MIN;
+	while (top[u] ^ top[v]) {
+		int& n = sz[top[u]] < sz[top[v]] ? u : v;
+		x = min(x, mn_query(idx[n], idx[top[n]] + 1));
+		y = max(y, mx_query(idx[n], idx[top[n]] + 1));
+		n = par[top[n]];
+	}
+	if (idx[u] > idx[v]) swap(u, v);
+	x = min(x, mn_query(idx[u], idx[v]));
+	y = max(y, mx_query(idx[u], idx[v]));
+	cout << x << ' ' << y << '\n';
 }
 int main() {
-    cin.tie(0), cout.tie(0);
-    ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	ios::sync_with_stdio(0);
 
-    int N; cin >> N;
-    for (int i = 1; i < N; ++i) {
-        int u, v, w; cin >> u >> v >> w;
-        adj[u].emplace_back(w, v);
-        adj[v].emplace_back(w, u);
-    }
+	int N; cin >> N;
+	for (int i = 1, u, v, w; i < N; ++i) {
+		cin >> u >> v >> w;
+		adj[u].emplace_back(w, v);
+		adj[v].emplace_back(w, u);
+	}
 
-    DFS(1, 0);
-    HLD(1, 0, 1, 0);
+	dfs(1, 0);
+	hld(1, 0);
 
-    for (int i = 1; i <= N; ++i) {
-        int s = chain[i].size();
-        if (s == 0) continue;
+	for (int i = 1; i <= N; ++i)
+		minT[t + idx[i]] = maxT[t + idx[i]] = dist[i];
+	for (int i = t - 1; i; --i) {
+		minT[i] = min(minT[left], minT[right]);
+		maxT[i] = max(maxT[left], maxT[right]);
+	}
 
-        segT[i].resize(2 * pow(2, ceil(log2(s))) - 1);
-        build_segT(i, 0, s - 1, 0);
-    }
-
-    int Q; cin >> Q;
-    while (Q--) {
-        int u, v; cin >> u >> v;
-        p ans = solve(u, v);
-        cout << ans.first << ' ' << ans.second << '\n';
-    }
-    return 0;
+	int Q; cin >> Q;
+	for (int u, v; Q--;) {
+		cin >> u >> v;
+		solve(u, v);
+	}
+	return 0;
 }
