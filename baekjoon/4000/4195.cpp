70c4883edbf5acc8b83957afe12fb2085694f4e8
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-08-19 20:46:02 +0900

    친구 네트워크

--- baekjoon/4000/4195.cpp
+++ baekjoon/4000/4195.cpp
@@ -1,62 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define loop(i,n) for(int i=0;i<n;++i)
 
-struct DisjointSet {
-    vector<int> parent, rank, size;
-    DisjointSet(int n) : parent(n), rank(n, 1), size(n, 1) {
-        loop(i, n) parent[i] = i;
-    }
-    int Find(int u) {
-        if (u == parent[u]) return u;
-        return parent[u] = Find(parent[u]);
-    }
-    void Merge(int u, int v) {
-        u = Find(u), v = Find(v);
-        if (u == v) return;
-        if (rank[u] > rank[v]) swap(u, v);
-        parent[u] = v;
-        if (rank[u] == rank[v]) ++rank[v];
-        size[v] += size[u];
-    }
-    int Fr_num(int u) {
-        u = Find(u);
-        return size[u];
-    }
-};
+const int mx = 1e5;
 
+int par[mx << 1 | 1];
 
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+int _find(int u) {
+    return par[u] < 0 ? u : (par[u] = _find(par[u]));
+}
+void solve() {
+    unordered_map<string, int> mp;
+    int cnt = 0;
 
-    int t, F;
-    cin >> t;
-    while (t--) {
-        map<string, int> Fnw;
-        map<string, int>::iterator it;
-        string s1, s2;
-        int n1, n2, num = 0;
-        cin >> F;
-        DisjointSet Dset(2 * F);
-        while (F--) {
-            cin >> s1 >> s2;
-            it = Fnw.find(s1);
-            if (it == Fnw.end()) {
-                n1 = num++;
-                Fnw[s1] = n1;
-            }
-            else n1 = (*it).second;
-            it = Fnw.find(s2);
-            if (it == Fnw.end()) {
-                n2 = num++;
-                Fnw[s2] = n2;
-            }
-            else n2 = (*it).second;
-            Dset.Merge(n1, n2);
-            cout << Dset.Fr_num(n1) << '\n';
+    int N; cin >> N;
+    memset(par + 1, -1, sizeof(int) * N * 2);
+    for (string a, b; N--;) {
+        cin >> a >> b;
+        int u = mp[a], v = mp[b];
+
+        if (!u) u = mp[a] = ++cnt;
+        if (!v) v = mp[b] = ++cnt;
+
+        u = _find(u), v = _find(v);
+        if (u ^ v) {
+            if (u < v) swap(u, v);
+            par[v] += par[u];
+            par[u] = v;
         }
+        cout << -par[v] << '\n';
     }
+}
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
+
+    int T; cin >> T;
+    while (T--) solve();
     return 0;
 }
