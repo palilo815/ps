de1468c78b742ce3f623c06d624cadd311e94aa0
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-08-21 13:39:20 +0900

    최소 스패닝 트리

--- baekjoon/1000/1197.cpp
+++ baekjoon/1000/1197.cpp
@@ -1,40 +1,35 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
 using namespace std;
-typedef pair<int, int> p;
+struct edge {
+    int u, v, w;
+};
 
-const int max_V = 10000;
+const int mxV = 1e4 + 1;
+const int mxE = 1e5;
 
-vector<p> adj[max_V + 1];
-bool added[max_V + 1];
-int dist[max_V + 1];
+int par[mxV];
+edge e[mxE];
 
+int _find(int u) {
+    return ~par[u] ? (par[u] = _find(par[u])) : u;
+}
 int main() {
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
     int V, E; cin >> V >> E;
-    while (E--) {
-        int u, v, w; cin >> u >> v >> w;
-        adj[u].emplace_back(w, v);
-        adj[v].emplace_back(w, u);
-    }
+    for (int i = 0; i < E; ++i)
+        cin >> e[i].u >> e[i].v >> e[i].w;
 
-    fill_n(dist + 1, V, INT_MAX);
-    dist[1] = 0;
-    long long ans = 0;
-    int cnt = V;
+    sort(e, e + E, [&](auto & a, auto & b) {
+        return a.w < b.w;
+    });
+    memset(par + 1, -1, sizeof(int) * V);
 
-    while (cnt--) {
-        int u = -1;
-        for (int i = 1; i <= V; ++i) if (!added[i] && (u == -1 || dist[i] < dist[u]))
-            u = i;
-
-        ans += dist[u];
-        added[u] = true;
-
-        for (auto [w, v] : adj[u]) if (!added[v] && dist[v] > w)
-            dist[v] = w;
+    int ans = 0;
+    for (int i = 0; i < E; ++i) {
+        int u = _find(e[i].u), v = _find(e[i].v);
+        if (u ^ v) par[u] = v, ans += e[i].w;
     }
     cout << ans;
     return 0;
