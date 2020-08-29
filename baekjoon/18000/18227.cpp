d0d41fcb630982eb53cd77673ea55e786332ff45
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-08-29 21:58:25 +0900

    성대나라의 물탱크

--- baekjoon/18000/18227.cpp
+++ baekjoon/18000/18227.cpp
@@ -1,54 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int max_N = 200001;
+const int mxN = 2e5 + 1;
 
-vector<int> adj[max_N];
-int _time;
-int d[max_N];
-int in[max_N];
-int out[max_N];
-int fenwick[max_N];
+vector<int> adj[mxN];
+int t, d[mxN], in[mxN], out[mxN], fenwick[mxN];
 
-void DFS(int u, int p) {
+void dfs(int u, int p) {
     d[u] = d[p] + 1;
-    in[u] = ++_time;
-    for (int v : adj[u]) if (v != p)
-        DFS(v, u);
-    out[u] = _time;
+    in[u] = ++t;
+    for (int v : adj[u])
+        if (v ^ p)
+            dfs(v, u);
+    out[u] = t;
 }
-void update(int i, int w) {
-    while (i <= _time) {
-        fenwick[i] += w;
-        i += i & -i;
-    }
+void update(int i) {
+    for (; i <= t; i += i & -i)
+        ++fenwick[i];
 }
-long long read(int i) {
-    long long ret = 0;
-    while (i) {
+int read(int i) {
+    int ret = 0;
+    for (; i; i -= i & -i)
         ret += fenwick[i];
-        i -= i & -i;
-    }
     return ret;
 }
 int main() {
     cin.tie(0), cout.tie(0);
     ios::sync_with_stdio(0);
 
-    int N, rt; cin >> N >> rt;
-    for (int i = 1; i < N; ++i) {
-        int u, v; cin >> u >> v;
+    int N, root; cin >> N >> root;
+    for (int i = 1, u, v; i < N; ++i) {
+        cin >> u >> v;
         adj[u].emplace_back(v);
         adj[v].emplace_back(u);
     }
 
-    DFS(rt, 0);
+    dfs(root, 0);
 
     int Q; cin >> Q;
-    while (Q--) {
-        int q, i; cin >> q >> i;
-        if (q == 1) update(in[i], 1);
-        else cout << d[i] * (read(out[i]) - read(in[i] - 1)) << '\n';
+    for (int q, i; Q; --Q) {
+        cin >> q >> i;
+        if (q == 1) update(in[i]);
+        else cout << (long long)d[i] * (read(out[i]) - read(in[i] - 1)) << '\n';
     }
     return 0;
 }
