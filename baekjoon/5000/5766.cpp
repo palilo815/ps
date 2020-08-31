f4165ffcc664fd2c4084a4f1cc370071faff17d2
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-08-31 17:20:12 +0900

    할아버지는 유명해!

--- baekjoon/5000/5766.cpp
+++ baekjoon/5000/5766.cpp
@@ -2,24 +2,23 @@
 #define loop(i,n) for(int i=0;i<n;++i)
 using namespace std;
 
-// 랭킹에 오른 횟수
 int arr[10001];
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
     int n, m;
     while (cin >> n >> m) {
         if (n == 0) break;
         memset(arr, 0, sizeof(arr));
+
         while (n--)
             loop(i, m) {
-                int x; cin >> x;
-                ++arr[x];
-            }
+            int x; cin >> x;
+            ++arr[x];
+        }
 
-        // 임요환, 홍진호
         int Yim = 0, Hong = 0;
         loop(i, 10001) {
             if (arr[i] > Yim)
@@ -27,8 +26,9 @@ int main()
             else if (arr[i] > Hong)
                 Hong = arr[i];
         }
-        loop(i, 10001)
-            if (arr[i] == Hong) cout << i << ' ';
+
+        loop(i, 10001) if (arr[i] == Hong)
+            cout << i << ' ';
         cout << '\n';
     }
     return 0;
