bee708b9f591bce4ac84942cb9018ae022b978a6
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-08-31 10:47:44 +0900

    Double Queue

--- baekjoon/3000/3755.cpp
+++ baekjoon/3000/3755.cpp
@@ -2,36 +2,36 @@
 using namespace std;
 
 int main() {
-	cin.tie(0), cout.tie(0);
-	ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
-	map<int, int> mp;
-	map<int, int>::iterator it;
+    map<int, int> mp;
+    map<int, int>::iterator it;
 
-	for (int q, k, p;;) {
-		cin >> q;
-		switch (q) {
-		case 1:
-			cin >> k >> p;
-			mp[p] = k;
-			break;
-		case 2:
-			if (mp.empty()) cout << 0 << '\n';
-			else {
-				it = mp.end(); --it;
-				cout << it->second << '\n';
-				mp.erase(it);
-			}
-			break;
-		case 3:
-			if (mp.empty()) cout << 0 << '\n';
-			else {
-				it = mp.begin();
-				cout << it->second << '\n';
-				mp.erase(it);
-			}
-			break;
-		default: return 0;
-		}
-	}
+    for (int q, k, p;;) {
+        cin >> q;
+        switch (q) {
+        case 1:
+            cin >> k >> p;
+            mp[p] = k;
+            break;
+        case 2:
+            if (mp.empty()) cout << 0 << '\n';
+            else {
+                it = mp.end(); --it;
+                cout << it->second << '\n';
+                mp.erase(it);
+            }
+            break;
+        case 3:
+            if (mp.empty()) cout << 0 << '\n';
+            else {
+                it = mp.begin();
+                cout << it->second << '\n';
+                mp.erase(it);
+            }
+            break;
+        default: return 0;
+        }
+    }
 }
