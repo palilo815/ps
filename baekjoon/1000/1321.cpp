ba7a3db3be450f275c9ae5a667518ef2b1ccbd3e
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-08-31 17:58:49 +0900

    군인

--- baekjoon/1000/1321.cpp
+++ baekjoon/1000/1321.cpp
@@ -1,50 +1,46 @@
 #include <bits/stdc++.h>
+#define left (i<<1)
+#define right (i<<1|1)
 using namespace std;
 
-const int max_N = 500000;
+const int sgN = 524288;
 
-int arr[max_N + 1];
-int segT[524288 * 2 - 1];
+int segT[sgN << 1];
 
-int build_segT(int l, int r, int idx)
-{
-    if (l == r) return segT[idx] = arr[l];
-    int m = l + (r - l) / 2;
-    return segT[idx] = build_segT(l, m, 2 * idx + 1) + build_segT(m + 1, r, 2 * idx + 2);
+void update(int i, int v) {
+    for (i += sgN - 1; i; i >>= 1)
+        segT[i] += v;
 }
-void update(int qi, int qv, int l, int r, int idx)
-{
-    segT[idx] += qv;
-    if (l == r) return;
-    int m = l + (r - l) / 2;
-    if (qi <= m) update(qi, qv, l, m, 2 * idx + 1);
-    else update(qi, qv, m + 1, r, 2 * idx + 2);
+int query(int k) {
+    int i = 1;
+    while (i < sgN) {
+        if (segT[left] < k) {
+            k -= segT[left];
+            i = right;
+        }
+        else i = left;
+    }
+    return i - sgN + 1;
 }
-int query(int k, int l, int r, int idx)
-{
-    if (l == r) return l;
-    int m = l + (r - l) / 2;
-    if (segT[2 * idx + 1] >= k) return query(k, l, m, 2 * idx + 1);
-    else return query(k - segT[2 * idx + 1], m + 1, r, 2 * idx + 2);
-}
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
     int N; cin >> N;
-    for (int i = 0; i < N; ++i) cin >> arr[i];
+    for (int i = sgN; i < sgN + N; ++i)
+        cin >> segT[i];
 
-    build_segT(0, N - 1, 0);
+    for (int i = sgN - 1; i; --i)
+        segT[i] = segT[left] + segT[right];
 
     int Q; cin >> Q;
-    while (Q--) {
-        int q, a; cin >> q >> a;
+    for (int q, i, v; Q; --Q) {
+        cin >> q >> i;
         if (q == 1) {
-            int b; cin >> b;
-            update(a - 1, b, 0, N - 1, 0);
+            cin >> v;
+            update(i, v);
         }
-        else cout << query(a, 0, N - 1, 0) + 1 << '\n';
+        else cout << query(i) << '\n';
     }
     return 0;
 }
