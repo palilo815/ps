1050b9bad014c09603fdaae33b2e15a57c3f5c26
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-09-06 14:42:55 +0900

    사탕상자

--- baekjoon/2000/2243.cpp
+++ baekjoon/2000/2243.cpp
@@ -1,35 +1,40 @@
 #include <bits/stdc++.h>
+#define left (i<<1)
+#define right (i<<1|1)
 using namespace std;
 
-int segT[1048576 * 2 - 1];
-void update(int qi, int qv, int l, int r, int idx)
-{
-    segT[idx] += qv;
-    if (l == r) return;
-    int m = l + (r - l) / 2;
-    if (qi <= m) update(qi, qv, l, m, 2 * idx + 1);
-    else update(qi, qv, m + 1, r, 2 * idx + 2);
+const int sgN = 1048576;
+
+int segT[sgN << 1];
+
+void update(int i, int v) {
+    for (i += sgN; i; i >>= 1)
+        segT[i] += v;
 }
-int query(int k, int l, int r, int idx)
-{
-    --segT[idx];
-    if (l == r) return l;
-    int m = l + (r - l) / 2;
-    if (segT[2 * idx + 1] >= k) return query(k, l, m, 2 * idx + 1);
-    else return query(k - segT[2 * idx + 1], m + 1, r, 2 * idx + 2);
+int query(int k) {
+    int i = 1;
+    while (i < sgN) {
+        --segT[i];
+        if (segT[left] >= k) i = left;
+        else {
+            k -= segT[left];
+            i = right;
+        }
+    }
+    --segT[i];
+    return i - sgN;
 }
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
     int Q; cin >> Q;
-    while (Q--) {
-        int q, a; cin >> q >> a;
-        if (q == 1) cout << query(a, 0, 1000000, 0) << '\n';
+    for (int q, a, b; Q; --Q) {
+        cin >> q >> a;
+        if (q == 1) cout << query(a) << '\n';
         else {
-            int b; cin >> b;
-            update(a, b, 0, 1000000, 0);
+            cin >> b;
+            update(a, b);
         }
     }
     return 0;
