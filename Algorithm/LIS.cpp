d116e07943d6cc7d2c2034b0a41f1b3f6f015af2
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-09-11 21:07:24 +0900

    Update LIS.cpp

--- Algorithm/LIS.cpp
+++ Algorithm/LIS.cpp
@@ -1,30 +1,18 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
 using namespace std;
-const int max_N = 1000000;
 
-int arr[max_N], T[max_N + 1];
-int ceil_elem(int s, int e, int key) {
-    while (e - s > 1) {
-        int mid = s + (e - s) / 2;
-        if (T[mid] >= key)
-            e = mid;
-        else
-            s = mid;
-    }
-    return e;
-}
-int LIS(int N) {
-    int len = 1;
-    T[1] = arr[0];
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
 
-    for (int i = 1; i < N; ++i) {
-        if (arr[i] < T[1])
-            T[1] = arr[i];
-        else if (arr[i] > T[len])
-            T[++len] = arr[i];
-        else
-            T[ceil_elem(0, len, arr[i])] = arr[i];
+    vector<int> T(1);
+    int N; cin >> N >> T[0];
+
+    for (int x; --N;) {
+        cin >> x;
+        if (x > T.back()) T.emplace_back(x);
+        else *lower_bound(T.begin(), T.end(), x) = x;
     }
-    return len;
+    cout << T.size();
+    return 0;
 }
