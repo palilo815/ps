cb37747425f044bf486f1c91c72291fe491b9f2e
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-09-11 18:58:23 +0900

    가장 긴 증가하는 부분 수열 2

--- baekjoon/12000/12015.cpp
+++ baekjoon/12000/12015.cpp
@@ -5,14 +5,14 @@ int main() {
     cin.tie(0), cout.tie(0);
     ios::sync_with_stdio(0);
 
-    vector<int> T = {INT_MIN};
+    vector<int> T(1);
+    int N; cin >> N >> T[0];
 
-    int N; cin >> N;
-    for (int x; N--;) {
+    for (int x; --N;) {
         cin >> x;
         if (x > T.back()) T.emplace_back(x);
         else *lower_bound(T.begin(), T.end(), x) = x;
     }
-    cout << T.size() - 1;
+    cout << T.size();
     return 0;
 }
