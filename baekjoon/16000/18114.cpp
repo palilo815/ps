863d6ad594fd3a3294d5906dd208ebf083503a0e
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-09-11 15:11:09 +0900

    블랙 프라이데이

---
+++ baekjoon/16000/18114.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define loop(i,n) for(int i=0;i<n;++i)
+#define success do{cout<<1;return 0;}while(0)
+using namespace std;
+
+const int mxN = 5e3;
+
+int a[mxN];
+
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
+
+    int N, C; cin >> N >> C;
+    loop(i, N) cin >> a[i];
+
+    sort(a, a + N);
+    if (binary_search(a, a + N, C))
+        success;
+
+    for (int l = 0, r = N - 1, s; l ^ r;) {
+        if ((s = a[l] + a[r]) == C)
+            success;
+        if (s < C) {
+            if (C - s != a[l] && C - s != a[r] && binary_search(a, a + N, C - s))
+                success;
+            ++l;
+        }
+        else --r;
+    }
+    cout << 0;
+    return 0;
+}
