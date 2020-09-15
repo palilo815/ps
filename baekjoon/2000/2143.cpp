8401083f2415b43b22191f60b33766b59935273a
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-09-15 23:23:12 +0900

    두 배열의 합

--- baekjoon/2000/2143.cpp
+++ baekjoon/2000/2143.cpp
@@ -1,44 +1,36 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
+#define LOOP(i,n) for(int i=1;i<=n;++i)
 using namespace std;
-const int max_N = 1000;
 
-int psum_A[max_N + 1], psum_B[max_N + 1];
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+const int mxN = 1e3;
 
-    int T; cin >> T;
-    int len_A, len_B;
-    cin >> len_A;
-    loop(i, len_A) {
-        int x; cin >> x;
-        psum_A[i + 1] = psum_A[i] + x;
-    }
-    cin >> len_B;
-    loop(i, len_B) {
-        int x; cin >> x;
-        psum_B[i + 1] = psum_B[i] + x;
-    }
-    
-    // vt에 모든 A의 부분배열의 합을 저장
+int a[mxN + 1], b[mxN + 1];
+
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
+
+    int T, A, B; cin >> T;
+    cin >> A;
+    LOOP(i, A) cin >> a[i];
+    cin >> B;
+    LOOP(i, B) cin >> b[i];
+
+    partial_sum(a + 1, a + A + 1, a + 1);
+    partial_sum(b + 1, b + B + 1, b + 1);
+
     vector<int> vt;
-    loop(i, len_A)
-        for (int j = i + 1; j <= len_A; ++j)
-            vt.push_back(psum_A[j] - psum_A[i]);
-    sort(vt.begin(), vt.end());
+    LOOP(i, A) LOOP(j, i) vt.emplace_back(a[i] - a[j - 1]);
 
-    // ans는 int범위를 넘을 수 있다.
+    sort(vt.begin(), vt.end());
+    vt.emplace_back(INT_MAX);
+
     long long ans = 0;
-    loop(i,len_B)
-        for (int j = i + 1; j <= len_B; ++j) {
-            int key = T - (psum_B[j] - psum_B[i]);
-            // 값이 key인 부분배열이 하나가 아닐 수 있으므로 범위를 더해준다.
-            auto low = lower_bound(vt.begin(), vt.end(), key);
-            if (*low == key)
-                ans += upper_bound(vt.begin(), vt.end(), key) - low;
-        }
+    LOOP(i, B) LOOP(j, i) {
+        int key = T - (b[i] - b[j - 1]);
+        auto it = lower_bound(vt.begin(), vt.end(), key);
+        if (*it == key) ans += upper_bound(vt.begin(), vt.end(), key) - it;
+    }
     cout << ans;
     return 0;
 }
