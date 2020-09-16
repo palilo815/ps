fd24bd460457ba6d9d2a8cc92dadaad499e08bf8
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-09-17 03:05:27 +0900

    서로 다른 부분 문자열의 개수

---
+++ baekjoon/11000/11478.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("in", "r", stdin);
+	freopen("out", "w", stdout);
+#endif
+	string s;
+	cin >> s;
+	int N = s.size();
+
+	unordered_set<string> st;
+	for (int i = 0; i < N; ++i)
+		for (int j = i + 1; j <= N; ++j)
+			st.emplace(s.substr(i, j - i));
+	cout << st.size();
+	return 0;
+}
