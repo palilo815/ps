c0b99cbdfbabe356ceab635012284888e1e82bcf
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-09-17 18:28:28 +0900

    서로 다른 부분 문자열의 개수

--- baekjoon/11000/11478.cpp
+++ baekjoon/11000/11478.cpp
@@ -2,20 +2,20 @@
 using namespace std;
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 #ifndef ONLINE_JUDGE
-	freopen("in", "r", stdin);
-	freopen("out", "w", stdout);
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
 #endif
-	string s;
-	cin >> s;
-	int N = s.size();
+    string s;
+    cin >> s;
+    int N = s.size();
 
-	unordered_set<string> st;
-	for (int i = 0; i < N; ++i)
-		for (int j = i + 1; j <= N; ++j)
-			st.emplace(s.substr(i, j - i));
-	cout << st.size();
-	return 0;
+    unordered_set<string> st;
+    for (int i = 0; i < N; ++i)
+        for (int j = i + 1; j <= N; ++j)
+            st.emplace(s.substr(i, j - i));
+    cout << st.size();
+    return 0;
 }
