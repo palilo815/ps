49d391773a8a584726d06ec1fbcd4e02c77e30aa
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-09-22 16:12:33 +0900

    Update topological_sorting.cpp

--- Algorithm/topological_sorting.cpp
+++ Algorithm/topological_sorting.cpp
@@ -1,35 +1,10 @@
-#include <bits/stdc++.h>
-using namespace std;
+vector<int> vt;
+vector<bool> visited(N);
 
-const int max_N = 100000;
-
-stack<int> stk;
-vector<int> adj[max_N + 1];
-bool visited[max_N + 1];
-
-void DFS(int u) {
+function<void(int)> dfs = [&](int u) {
     visited[u] = true;
-    for (int v : adj[u]) if (!visited[v])
-        DFS(v);
-    stk.push(u);
-}
-int main() {
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
-
-    int N, M; cin >> N >> M;
-    while (M--) {
-        int u, v; cin >> u >> v;
-        adj[u].push_back(v);
-    }
-
-    for (int i = 1; i <= N; ++i) if (!visited[i])
-        DFS(i);
-
-    while (!stk.empty()) {
-        cout << stk.top() << ' ';
-        stk.pop();
-    }
-    return 0;
-
-}
+    for (int& v : adj[u])
+        if (!visited[v])
+            dfs(v);
+    vt.emplace_back(u);
+};
