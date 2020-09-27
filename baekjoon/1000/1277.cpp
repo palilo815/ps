3b70aed0aea4500daec23f36368806e17277cbc4
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-09-27 16:11:42 +0900

    발전소 설치

--- baekjoon/1000/1277.cpp
+++ baekjoon/1000/1277.cpp
@@ -1,52 +1,57 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
+#define loop(i, n) for (int i = 0; i < n; ++i)
 using namespace std;
-typedef pair<double, int> p;
+struct elem {
+    double d;
+    int u;
+    elem(double d, int u) : d(d), u(u) {}
+    bool operator<(const elem& rhs) const {
+        return d > rhs.d;
+    }
+};
 
-const int max_N = 1000;
+const int mxN = 1e3;
 
-int x[max_N], y[max_N];
-double adj[max_N][max_N];
-double dist[max_N];
+int x[mxN], y[mxN];
+double adj[mxN][mxN], dist[mxN];
 
 int main() {
-    cin.tie(0), cout.tie(0);
     ios::sync_with_stdio(0);
-
-    int N, W; cin >> N >> W;
-    double M; cin >> M;
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int N, W;
+    double M;
+    cin >> N >> W >> M;
     M += 1e-6;
 
     loop(i, N) cin >> x[i] >> y[i];
     loop(j, N) loop(i, j) {
         long long dx = x[i] - x[j], dy = y[i] - y[j];
-        adj[i][j] = adj[j][i] = sqrt(dx * dx + dy * dy);
+        double d = sqrt(dx * dx + dy * dy);
+        adj[i][j] = adj[j][i] = d > M ? DBL_MAX : d;
     }
-    while (W--) {
-        int u, v; cin >> u >> v;
+
+    for (int u, v; W--;) {
+        cin >> u >> v;
         --u, --v;
         adj[u][v] = adj[v][u] = 0;
     }
 
     fill(dist + 1, dist + N, DBL_MAX);
 
-    priority_queue<p, vector<p>, greater<p>> pq;
+    priority_queue<elem> pq;
     pq.emplace(0, 0);
 
-    int ans = -1;
-    while (!pq.empty()) {
-        auto [d, u] = pq.top(); pq.pop();
+    for (;;) {
+        auto [d, u] = pq.top();
+        pq.pop();
         if (dist[u] < d) continue;
-        if (u == N - 1) { ans = d * 1000; break; }
+        if (u == N - 1) return cout << (int)(d * 1000), 0;
 
-        loop(v, N) if (adj[u][v] <= M) {
-            double D = d + adj[u][v];
-            if (dist[v] > D) {
-                dist[v] = D;
-                pq.emplace(D, v);
-            }
-        }
+        loop(v, N) if (adj[u][v] != DBL_MAX && dist[v] > d + adj[u][v])
+            pq.emplace(dist[v] = d + adj[u][v], v);
     }
-    cout << ans;
-    return 0;
 }
