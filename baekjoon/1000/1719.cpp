f27fb29fa88e746406d6a7722d7d993e99a06bac
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-09-29 14:11:27 +0900

    택배

--- baekjoon/1000/1719.cpp
+++ baekjoon/1000/1719.cpp
@@ -1,70 +1,47 @@
 #include <bits/stdc++.h>
-#define LOOP(i,n) for(int i=1;i<=n;++i)
+#define loop(i, n) for (int i = 0; i < n; ++i)
 using namespace std;
-typedef pair<int, int> p;
 
-const int max_N = 200;
+const int mxN = 2e2;
+const int oo = 0x3f3f3f3f;
 
-int table[max_N + 1][max_N + 1];
-int dist[max_N + 1];
-vector<p> adj[max_N + 1];
+int adj[mxN][mxN], via[mxN][mxN];
 
-void Dijkstra(int src, int N)
-{
-    vector<int> parent(N + 1);
-    LOOP(i, N) dist[i] = INT_MAX;
-    dist[src] = 0;
-    priority_queue<p, vector<p>, greater<p>> pq;
-    pq.push({ 0,src });
-    while (!pq.empty()) {
-        int d = pq.top().first, u = pq.top().second; pq.pop();
-        if (dist[u] < d) continue;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int N, M;
+    cin >> N >> M;
 
-        for (p edge : adj[u]) {
-            int D = d + edge.first, v = edge.second;
-            if (dist[v] > D) {
-                dist[v] = D;
-                parent[v] = u;
-                pq.push({ D,v });
-            }
-        }
+    memset(adj, 0x3f, sizeof(adj));
+    memset(via, 0xff, sizeof(via));
+
+    for (int u, v, w; M--;) {
+        cin >> u >> v >> w;
+        --u, --v;
+        adj[u][v] = adj[v][u] = w;
     }
 
-    // table 만드는 과정인데, 더 간편한 과정이 있을듯
-    bool update = true;
-    while (update) {
-        update = false;
-        LOOP(i, N) if (parent[i]) {
-            if (parent[i] == src) {
-                table[src][i] = i;
-                update = true;
-                parent[i] = 0;
-            }
-            else if (table[src][parent[i]]) {
-                table[src][i] = table[src][parent[i]];
-                update = true;
-                parent[i] = 0;
-            }
-        }
-    }
-}
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+    loop(k, N) loop(i, N) if (adj[i][k] != oo)
+        loop(j, N) if (adj[k][j] != oo && adj[i][j] > adj[i][k] + adj[k][j])
+            adj[i][j] = adj[i][k] + adj[k][j],
+            via[i][j] = k;
 
-    int N, M; cin >> N >> M;
-    while (M--) {
-        int u, v, w; cin >> u >> v >> w;
-        adj[u].push_back({ w,v });
-        adj[v].push_back({ w,u });
-    }
-    LOOP(i, N) Dijkstra(i, N);
-    LOOP(i, N) {
-        LOOP(j, N) {
-            if (i == j) cout << '-';
-            else cout << table[i][j];
-            cout << ' ';
+    auto ans = [&](int u, int v) {
+        while (~via[u][v]) v = via[u][v];
+        return v + 1;
+    };
+
+    loop(i, N) {
+        loop(j, N) {
+            if (i == j)
+                cout << "- ";
+            else
+                cout << ans(i, j) << ' ';
         }
         cout << '\n';
     }
