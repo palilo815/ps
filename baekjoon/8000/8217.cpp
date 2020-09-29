806ff2d310e9026d8a707d244509f4612b0fa7bd
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-09-29 14:11:15 +0900

    유성

--- baekjoon/8000/8217.cpp
+++ baekjoon/8000/8217.cpp
@@ -1,80 +1,80 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
+#define loop(i, n) for (int i = 0; i < n; ++i)
 using namespace std;
 using ll = long long;
 
-struct meteo {
-    int l, r, w;
-};
+const int mx = 3e5;
 
-const int MAX = 300000;
+vector<int> state[mx];
+int N, M, req[mx];
+int l[mx], r[mx], w[mx];
+int lo[mx], hi[mx];
 
-int N, M, Q;
-vector<int> state[MAX];
-int req[MAX];
-ll fenwick[MAX + 1];
-
-meteo q[MAX];
-int lo[MAX], hi[MAX];
-vector<int> vt[MAX];
+ll fenwick[mx + 1];
 
 void update(int i, int v) {
-    for (++i; i <= M; i += i & -i)
+    for (; i <= M; i += i & -i)
         fenwick[i] += v;
 }
 ll read(int i) {
     ll ret = 0;
-    for (++i; i; i -= i & -i)
+    for (; i; i -= i & -i)
         ret += fenwick[i];
     return ret;
 }
-bool go() {
-    memset(fenwick + 1, 0, sizeof(ll) * M);
-    loop(i, Q) vt[i].clear();
-
-    bool ret = false;
-    loop(i, N) if (lo[i] ^ hi[i]) {
-        vt[(lo[i] + hi[i]) >> 1].emplace_back(i);
-        ret = true;
-    }
-    return ret;
-}
 int main() {
-    cin.tie(0), cout.tie(0);
     ios::sync_with_stdio(0);
-
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
     cin >> N >> M;
-    loop(i, M) {
-        int x; cin >> x;
-        state[x - 1].emplace_back(i);
+    for (int i = 1, owner; i <= M; ++i) {
+        cin >> owner;
+        state[owner - 1].emplace_back(i);
     }
     loop(i, N) cin >> req[i];
+
+    int Q;
     cin >> Q;
-    loop(i, Q) {
-        cin >> q[i].l >> q[i].r >> q[i].w;
-        --q[i].l;
-    }
+    loop(i, Q) cin >> l[i] >> r[i] >> w[i];
+    loop(i, Q) r[i]++;
+
     fill(hi, hi + N, Q);
 
-    while (go()) loop(m, Q) {
-        auto& [l, r, w] = q[m];
-        if (l < r) update(l, w), update(r, -w);
-        else update(0, w), update(r, -w), update(l, w);
+    for (;;) {
+        vector<vector<int>> vt(Q);
+        bool done = true;
 
-        for (int& i : vt[m]) {
-            ll total = 0;
-            for (int& area : state[i]) {
-                total += read(area);
-                if (total >= req[i]) break;
+        loop(i, N) if (lo[i] != hi[i]) {
+            int m = (lo[i] + hi[i]) >> 1;
+            vt[m].emplace_back(i);
+            done = false;
+        }
+        if (done) break;
+
+        memset(fenwick + 1, 0, sizeof(ll) * M);
+        loop(m, Q) {
+            if (l[m] < r[m])
+                update(l[m], w[m]), update(r[m], -w[m]);
+            else
+                update(1, w[m]), update(r[m], -w[m]), update(l[m], w[m]);
+
+            for (int& i : vt[m]) {
+                ll total = 0;
+                for (int& x : state[i]) {
+                    total += read(x);
+                    if (total >= req[i]) break;
+                }
+                total < req[i] ? (lo[i] = m + 1) : (hi[i] = m);
             }
-            total < req[i] ? (lo[i] = m + 1) : (hi[i] = m);
         }
     }
-
     loop(i, N) {
-        if (lo[i] == Q) cout << "NIE";
-        else cout << lo[i] + 1;
-        cout << '\n';
+        if (lo[i] == Q) cout << "NIE\n";
+        else
+            cout << lo[i] + 1 << '\n';
     }
     return 0;
 }
