49fe16c15fb946c612dc448670362c698d23fa16
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-09-30 14:55:38 +0900

    Cow Routing

--- baekjoon/10000/10673.cpp
+++ baekjoon/10000/10673.cpp
@@ -1,57 +1,64 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
 using namespace std;
-typedef long long ll;
-typedef pair<int, int> p;
-typedef tuple<ll, int, int> tup;
+using ll = long long;
+struct elem {
+    ll c;
+    int f, u;
+    elem(ll c, int f, int u) : c(c), f(f), u(u) {}
+    bool operator<(const elem& rhs) const {
+        return c == rhs.c ? f > rhs.f : c > rhs.c;
+    }
+};
 
-const int MAX = 1000;
+const int mxN = 1e3;
 
-vector<p> adj[MAX + 1];
-ll cost[MAX + 1];
-int flight[MAX + 1];
+vector<pair<int, int>> ticket[mxN];
+ll cost[mxN];
+int flight[mxN];
 
-vector<int> route[MAX];
-int pay[MAX];
+vector<int> route[mxN];
+int pay[mxN];
 
 int main() {
-    cin.tie(0), cout.tie(0);
     ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int src, dst, N;
+    cin >> src >> dst >> N;
+    --src, --dst;
 
-    int src, dst, M; cin >> src >> dst >> M;
-    loop(i, M) {
-        cin >> pay[i];
-        int cnt; cin >> cnt;
+    for (int i = 0, cnt; i < N; ++i) {
+        cin >> pay[i] >> cnt;
         route[i].resize(cnt);
-        loop(j, cnt) {
+
+        for (int j = 0; j < cnt; ++j) {
             cin >> route[i][j];
-            adj[route[i][j]].emplace_back(i, j);
+            ticket[--route[i][j]].emplace_back(i, j);
         }
     }
 
     memset(cost, 0x3f, sizeof(cost));
     memset(flight, 0x3f, sizeof(flight));
-    cost[src] = flight[src] = 0;
 
-    priority_queue<tup, vector<tup>, greater<tup>> pq;
-    pq.emplace(0, 0, src);
+    priority_queue<elem> pq;
+    pq.emplace(cost[src] = 0, flight[src] = 0, src);
 
     while (!pq.empty()) {
-        auto [c, f, u] = pq.top(); pq.pop();
-        if (cost[u] < c || (cost[u] == c && flight[u] < f))
+        auto [c, f, u] = pq.top();
+        pq.pop();
+        if (cost[u] != c || flight[u] != f)
             continue;
-        if (u == dst) { cout << c << ' ' << f; return 0; }
+        if (u == dst) return cout << c << ' ' << f, 0;
 
-        for (auto [idx, pos] : adj[u]) {
-            ll C = c + pay[idx];
-            int F = f;
-            while (++pos < route[idx].size()) {
-                ++F;
-                int v = route[idx][pos];
-                if (cost[v] > C || (cost[v] == C && flight[v] > F)) {
-                    cost[v] = C, flight[v] = F;
-                    pq.emplace(C, F, v);
-                }
+        for (auto& [route_num, idx] : ticket[u]) {
+            ll C = c + pay[route_num];
+            for (int F = f + 1, i = idx + 1; i < route[route_num].size(); ++i, ++F) {
+                int v = route[route_num][i];
+                if (cost[v] > C || (cost[v] == C && flight[v] > F))
+                    pq.emplace(cost[v] = C, flight[v] = F, v);
             }
         }
     }
