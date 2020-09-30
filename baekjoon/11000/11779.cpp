70b3bc25741bb678b141d16218c3cdbf4aefabb6
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-09-30 15:01:47 +0900

    최소비용 구하기 2

--- baekjoon/11000/11779.cpp
+++ baekjoon/11000/11779.cpp
@@ -1,58 +1,61 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
-#define P pair<int,int>
 using namespace std;
+struct elem {
+    int d, u;
+    elem(int d, int u) : d(d), u(u) {}
+    bool operator<(const elem& rhs) const {
+        return d > rhs.d;
+    }
+};
 
-int graph[1001][1001], cache[1001], dist[1001];
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+const int mxN = 1e3 + 1;
 
-    // ** initialize ** 
-    memset(graph, -1, sizeof(graph));
-    loop(i, 1001) dist[i] = INT_MAX;
-    int N, M; cin >> N >> M;
-    while (M--) {
-        int u, v, w; cin >> u >> v >> w;
-        if (graph[u][v] == -1) graph[u][v] = w;
-        else graph[u][v] = min(graph[u][v], w);
+vector<pair<int, int>> adj[mxN];
+int dist[mxN], par[mxN];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int N, M;
+    cin >> N >> M;
+
+    for (int u, v, w; M--;) {
+        cin >> u >> v >> w;
+        adj[u].emplace_back(v, w);
     }
 
-    // u에서 v로 가는 경로
-    int u, v; cin >> u >> v;
-    cache[u] = -1, dist[u] = 0;
-    priority_queue<P, vector<P>, greater<P>> pq;
-    pq.push({ 0,u });
+    int src, dst;
+    cin >> src >> dst;
 
-    while (1) {
-        int cost = pq.top().first, curr = pq.top().second; pq.pop();
-        if (curr == v) break;
-        loop(i, 1001)
-            if (graph[curr][i] != -1) {
-                int next_cost = cost + graph[curr][i];
-                if (next_cost < dist[i]) {
-                    dist[i] = next_cost;
-                    cache[i] = curr;
-                    pq.push({ next_cost,i });
-                }
+    memset(dist + 1, 0x3f, sizeof(int) * N);
+
+    priority_queue<elem> pq;
+    pq.emplace(dist[src] = 0, src);
+
+    for (;;) {
+        auto [d, u] = pq.top();
+        pq.pop();
+        if (dist[u] != d) continue;
+        if (u == dst) break;
+
+        for (auto& [v, w] : adj[u])
+            if (dist[v] > d + w) {
+                pq.emplace(dist[v] = d + w, v);
+                par[v] = u;
             }
     }
-    cout << dist[v] << '\n';
 
-    // cache[i]에는 i 이전에 방문한 도시의 번호가 들어있다.
-    // cache[u] = -1을 넣어놓은 상태
-    stack<int> stk;
-    while (v != -1) {
-        stk.push(v);
-        v = cache[v];
-    }
+    vector<int> path;
+    for (int v = dst; v; v = par[v])
+        path.emplace_back(v);
 
-    // 정답 역추적
-    cout << stk.size() << '\n';
-    while (!stk.empty()) {
-        cout << stk.top() << ' ';
-        stk.pop();
-    }
+    cout << dist[dst] << '\n'
+         << path.size() << '\n';
+    for (auto it = path.rbegin(); it != path.rend(); ++it)
+        cout << *it << ' ';
     return 0;
 }
