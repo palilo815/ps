594411942cef7d11069c481e62eafb9437a7375e
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-09-30 13:22:52 +0900

    도로포장

--- baekjoon/1000/1162.cpp
+++ baekjoon/1000/1162.cpp
@@ -1,12 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-using p = pair<int, int>;
 struct elem {
     ll d;
     int u, k;
     elem(ll d, int u, int k) : d(d), u(u), k(k) {}
-    bool operator <(const elem& rhs) const {
+    bool operator<(const elem& rhs) const {
         return d > rhs.d;
     }
 };
@@ -14,40 +13,43 @@ struct elem {
 const int mxN = 1e4 + 1;
 const int mxK = 20;
 
-vector<p> adj[mxN];
+vector<pair<int, int>> adj[mxN];
 ll dist[mxN][mxK + 1];
 
 int main() {
-    cin.tie(0), cout.tie(0);
     ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int N, M, K;
+    cin >> N >> M >> K;
 
-    int N, M, K; cin >> N >> M >> K;
     for (int u, v, w; M--;) {
         cin >> u >> v >> w;
-        adj[u].emplace_back(w, v);
-        adj[v].emplace_back(w, u);
+        adj[u].emplace_back(v, w);
+        adj[v].emplace_back(u, w);
     }
 
-    memset(dist, 0x3f, sizeof(dist));
-    dist[1][0] = 0;
+    for (int i = 2; i <= N; ++i)
+        memset(dist[i], 0x3f, sizeof(ll) * (K + 1));
 
     priority_queue<elem> pq;
-    pq.emplace(0, 1, 0);
+    for (int i = 0; i <= K; ++i)
+        pq.emplace(0, 1, i);
 
-    while (1) {
-        auto [d, u, k] = pq.top(); pq.pop();
-        if (u == N) {cout << d; break;}
+    for (;;) {
+        auto [d, u, k] = pq.top();
+        pq.pop();
+        if (d != dist[u][k]) continue;
+        if (u == N) return cout << d, 0;
 
-        for (auto& [w, v] : adj[u]) {
-            if (dist[v][k] > d + w) {
-                dist[v][k] = d + w;
-                pq.emplace(d + w, v, k);
-            }
-            if (k < K && dist[v][k + 1] > d) {
-                dist[v][k + 1] = d;
-                pq.emplace(d, v, k + 1);
-            }
+        for (auto& [v, w] : adj[u]) {
+            if (dist[v][k] > d + w)
+                pq.emplace(dist[v][k] = d + w, v, k);
+            if (k != K && dist[v][k + 1] > d)
+                pq.emplace(dist[v][k + 1] = d, v, k + 1);
         }
     }
-    return 0;
 }
