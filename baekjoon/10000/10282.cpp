2ca4f2938f47ed4ed1209c74bf933281a045623c
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-09-30 13:09:49 +0900

    해킹

--- baekjoon/10000/10282.cpp
+++ baekjoon/10000/10282.cpp
@@ -1,49 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef pair<int, int> p;
-
-const int max_N = 10000;
-
-vector<p> adj[max_N + 1];
-int dist[max_N + 1];
-
-int main() {
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
-
-    int T; cin >> T;
-    while (T--) {
-        int N, M, src; cin >> N >> M >> src;
-        fill(dist, dist + N + 1, INT_MAX);
-        for (int i = 1; i <= N; ++i) adj[i].clear();
-        while (M--) {
-            int v, u, w; cin >> v >> u >> w;
-            adj[u].push_back({ w,v });
-        }
-
-        priority_queue<p, vector<p>, greater<p>> pq;
-        pq.push({ 0,src });
-        dist[src] = 0;
-
-        while (!pq.empty()) {
-            auto [d, u] = pq.top(); pq.pop();
-            if (dist[u] < d) continue;
-
-            for (auto [w, v] : adj[u]) {
-                int D = d + w;
-                if (dist[v] > D) {
-                    dist[v] = D;
-                    pq.push({ D,v });
-                }
-            }
-        }
-
-        int ans = 0, cnt = 0;
-        for (int i = 1; i <= N; ++i) if (dist[i] != INT_MAX) {
-            ++cnt;
-            ans = max(ans, dist[i]);
-        }
-        cout << cnt << ' ' << ans << '\n';
+struct elem {
+    int d, u;
+    elem(int d, int u) : d(d), u(u) {}
+    bool operator<(const elem& rhs) const {
+        return d > rhs.d;
     }
+};
+
+const int mxN = 1e4 + 1;
+const int INF = 0x3f3f3f3f;
+
+vector<elem> adj[mxN];
+int dist[mxN];
+
+void solve() {
+    int N, M, s;
+    cin >> N >> M >> s;
+
+    for (int i = 1; i <= N; ++i)
+        adj[i].clear();
+
+    for (int u, v, w; M--;) {
+        cin >> v >> u >> w;
+        adj[u].emplace_back(v, w);
+    }
+
+    memset(dist + 1, 0x3f, sizeof(int) * N);
+
+    priority_queue<elem> pq;
+    pq.emplace(dist[s] = 0, s);
+
+    while (!pq.empty()) {
+        auto [d, u] = pq.top();
+        pq.pop();
+        if (dist[u] < d) continue;
+
+        for (auto& [v, w] : adj[u])
+            if (dist[v] > d + w)
+                pq.emplace(dist[v] = d + w, v);
+    }
+    cout << count_if(dist + 1, dist + N + 1, [&](int& x) { return x == INF ? (x = 0) : 1; }) << ' '
+         << *max_element(dist + 1, dist + N + 1) << '\n';
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int T;
+    cin >> T;
+    while (T--) solve();
     return 0;
 }
