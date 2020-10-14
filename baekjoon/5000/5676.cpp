a85150df8ef91a22af6c5b6d33fad91df8913678
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-10-14 15:35:42 +0900

    음주 코딩

--- baekjoon/5000/5676.cpp
+++ baekjoon/5000/5676.cpp
@@ -1,68 +1,62 @@
 #include <bits/stdc++.h>
+#define left (i << 1)
+#define right (i << 1 | 1)
 using namespace std;
 
-const int max_N = 100000;
+const int mxN = 1e5;
 
-int arr[max_N];
-int segT[131072 * 2 - 1];
+char segT[mxN << 1];
 
-int build_segT(int l, int r, int idx)
-{
-    if (l == r) return segT[idx] = arr[l];
-    int mid = l + (r - l) / 2;
-    return segT[idx] = build_segT(l, mid, 2 * idx + 1) * build_segT(mid + 1, r, 2 * idx + 2);
+void update(int i, int val) {
+    segT[i] = val ? val > 0 ? '+' : '-' : 0;
+    while (i >>= 1)
+        segT[i] = segT[left] && segT[right] ? segT[left] == segT[right] ? '+' : '-' : 0;
 }
-int query(int ql, int qr, int l, int r, int idx)
-{
-    if (ql <= l && r <= qr) return segT[idx];
-    if (r < ql || qr < l) return 1;
-    int mid = l + (r - l) / 2;
-    return query(ql, qr, l, mid, 2 * idx + 1) * query(ql, qr, mid + 1, r, 2 * idx + 2);
-}
-void update(int qi, int qv, int l, int r, int idx)
-{
-    if (l == r) {
-        arr[qi] = segT[idx] = qv;
-        return;
-    }
-    int mid = l + (r - l) / 2;
-    if (qi <= mid) update(qi, qv, l, mid, 2 * idx + 1);
-    else update(qi, qv, mid + 1, r, 2 * idx + 2);
-    segT[idx] = segT[2 * idx + 1] * segT[2 * idx + 2];
-}
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
-
-    int N, Q;
-    while (cin >> N >> Q) {
-        for (int i = 0; i < N; ++i) {
-            int x; cin >> x;
-            if (x > 0) arr[i] = 1;
-            else if (x < 0) arr[i] = -1;
-            else arr[i] = 0;
-        }
-
-        build_segT(0, N - 1, 0);
-
-        while (Q--) {
-            char q; int a, b;
-            cin >> q >> a >> b;
-            if (q == 'C') {
-                if (b > 0) b = 1;
-                else if (b < 0) b = -1;
-                if (arr[a - 1] != b)
-                    update(a - 1, b, 0, N - 1, 0);
-            }
+void query(int l, int r) {
+    char ret = '+';
+    for (; l != r; l >>= 1, r >>= 1) {
+        if (l & 1) {
+            if (segT[l]) ret = ret == segT[l] ? '+' : '-';
             else {
-                int res = query(a - 1, b - 1, 0, N - 1, 0);
-                if (res > 0) cout << '+';
-                else if (res < 0) cout << '-';
-                else cout << res;
+                cout << 0;
+                return;
+            }
+            ++l;
+        }
+        if (r & 1) {
+            --r;
+            if (segT[r]) ret = ret == segT[r] ? '+' : '-';
+            else {
+                cout << 0;
+                return;
             }
         }
-        cout << '\n';
+    }
+    cout << ret;
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    for (int N, K; cin >> N >> K; cout << '\n') {
+        memset(segT, 0, N << 1);
+        for (int i = 0, x; i < N; ++i) {
+            cin >> x;
+            if (x) segT[i + N] = x > 0 ? '+' : '-';
+        }
+
+        for (int i = N - 1; i; --i)
+            if (segT[left] && segT[right])
+                segT[i] = segT[left] == segT[right] ? '+' : '-';
+
+        for (int a, b; K--;) {
+            char q;
+            cin >> q >> a >> b;
+            q == 'C' ? update(a - 1 + N, b) : query(a - 1 + N, b + N);
+        }
     }
     return 0;
 }
