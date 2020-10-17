5a9fd57a192f19a93154c12ff78e36ae94ffbc71
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-10-17 15:53:52 +0900

    화성 지도

--- baekjoon/3000/3392.cpp
+++ baekjoon/3000/3392.cpp
@@ -1,51 +1,58 @@
 #include <bits/stdc++.h>
+#define left (i << 1)
+#define right (i << 1 | 1)
 using namespace std;
+
 struct line {
-    int x, y1, y2, w;
+    int x, y1, y2, val;
 };
 
-const int max_N = 10000;
-const int seg_N = 32768;
+const int mxN = 1e4;
+const int sgN = 32768;
 
-line a[max_N << 1];
-int cntT[seg_N << 1], segT[seg_N << 1];
+line a[mxN << 1];
+int cntT[sgN << 1], segT[sgN << 1];
 
-void apply(int i, int v, int k) {
-    cntT[i] += v;
-    if (k == 1) {
-        segT[i] = cntT[i] != 0;
-        i >>= 1, k <<= 1;
-    }
-    for (; i; i >>= 1, k <<= 1)
-        segT[i] = cntT[i] ? k : segT[i << 1] + segT[i << 1 | 1];
+inline void apply(int i, int v, int k) {
+    segT[i] = (cntT[i] += v) ? k : k == 1 ? 0 : segT[left] + segT[right];
 }
 void update(int l, int r, int v) {
-    l += seg_N, r += seg_N;
-    for (int k = 1; l < r; l >>= 1, r >>= 1, k <<= 1) {
+    int l0 = l += sgN, r0 = r += sgN;
+    for (int k = 1; l != r; l >>= 1, r >>= 1, k <<= 1) {
         if (l & 1) apply(l++, v, k);
         if (r & 1) apply(--r, v, k);
     }
+    segT[l0] = cntT[l0] != 0;
+    segT[r0] = cntT[r0] != 0;
+    for (int i = l0 >> 1, k = 2; i; i >>= 1, k <<= 1)
+        segT[i] = cntT[i] ? k : segT[left] + segT[right];
+    for (int i = r0 >> 1, k = 2; i; i >>= 1, k <<= 1)
+        segT[i] = cntT[i] ? k : segT[left] + segT[right];
 }
 int main() {
-    cin.tie(0), cout.tie(0);
     ios::sync_with_stdio(0);
-
-    int N; cin >> N;
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int N;
+    cin >> N;
     for (int i = 0, x1, y1, x2, y2; i < N << 1; i += 2) {
         cin >> x1 >> y1 >> x2 >> y2;
         a[i] = {x1, y1, y2, 1};
-        a[i + 1] = {x2, y1, y2, -1};
+        a[i | 1] = {x2, y1, y2, -1};
     }
-    sort(a, a + (N << 1), [&](line & a, line & b) -> bool {
+
+    sort(a, a + (N << 1), [&](auto& a, auto& b) {
         return a.x < b.x;
     });
 
     int ans = 0;
-    update(a[0].y1, a[0].y2, a[0].w);
+    update(a[0].y1, a[0].y2, a[0].val);
     for (int i = 1; i < N << 1; ++i) {
         ans += segT[1] * (a[i].x - a[i - 1].x);
-        update(a[i].y1, a[i].y2, a[i].w);
+        update(a[i].y1, a[i].y2, a[i].val);
     }
     cout << ans;
-    return 0;
 }
