aeb5f8895eef2a5aa945a49b3a63f3b45388b5f2
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-11-09 13:46:12 +0900

    데이터 구조

--- baekjoon/12000/12899.cpp
+++ baekjoon/12000/12899.cpp
@@ -1,29 +1,19 @@
 #include <bits/stdc++.h>
-#define left (i << 1)
-#define right (i << 1 | 1)
 using namespace std;
 
-const int sgN = 2097152;
+const int sgX = 2097152;
 
-int segT[sgN << 1];
+int bit[sgX + 1];
 
 void update(int i) {
-    ++segT[i += sgN];
-    while (i >>= 1)
-        ++segT[i];
+    for (; i <= sgX; i += i & -i)
+        ++bit[i];
 }
 void query(int k) {
-    int i = 1;
-    while (i < sgN) {
-        --segT[i];
-        if (segT[left] >= k) i = left;
-        else {
-            k -= segT[left];
-            i = right;
-        }
-    }
-    --segT[i];
-    cout << i - sgN << '\n';
+    int s = 0;
+    for (int len = sgX >> 1; len; len >>= 1)
+        bit[s | len] < k ? (k -= bit[s |= len]) : --bit[s | len];
+    cout << s + 1 << '\n';
 }
 int main() {
     ios::sync_with_stdio(0);
@@ -32,14 +22,12 @@ int main() {
     freopen("in", "r", stdin);
     freopen("out", "w", stdout);
 #endif
-    int N;
-    cin >> N;
-
+    int n, x;
     char q;
-    int x;
-    while (N--) {
+
+    cin >> n;
+    while (n--) {
         cin >> q >> x;
         q == '1' ? update(x) : query(x);
     }
-    return 0;
 }
