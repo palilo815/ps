b1d4305c9056636489f897ff5acf1e9a64724dc5
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-11-11 13:58:05 +0900

    요세푸스 문제 2

--- baekjoon/1000/1168.cpp
+++ baekjoon/1000/1168.cpp
@@ -1,24 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int sgN = 131072;
+
+int bit[sgN + 1];
+
+int query(int i) {
+    int ret = 0;
+    for (; i; i -= i & -i)
+        ret += bit[i];
+    return ret;
+}
+int kill(int k) {
+    int s = 0;
+    for (int len = sgN >> 1; len; len >>= 1)
+        bit[s | len] < k ? (k -= bit[s |= len]) : --bit[s | len];
+    return s + 1;
+}
 int main() {
-    cin.tie(0), cout.tie(0);
     ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int N, K;
+    cin >> N >> K;
 
-    int N, K; cin >> N >> K;
-    vector<int> vt(N);
-    iota(vt.begin(), vt.end(), 1);
+    for (int i = 1; i <= N; ++i)
+        for (int j = i; j <= sgN; j += j & -j)
+            ++bit[j];
 
-    --K;
+    for (int i = K; i <= sgN; i += i & -i)
+        --bit[i];
+
     int i = K;
-    cout << '<' << vt[i];
-    vt.erase(vt.begin() + i);
-
-    while (!vt.empty()) {
-        i = (i + K) % vt.size();
-        cout << ", " << vt[i];
-        vt.erase(vt.begin() + i);
+    cout << '<' << i;
+    for (int x; --N; cout << ", " << i) {
+        x = query(i - 1);
+        i = kill(N - x < K ? (K - N + x) % N ? (K - N + x) % N : N : x + K);
     }
     cout << '>';
-    return 0;
 }
