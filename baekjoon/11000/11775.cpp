5f25dedcfbc5777191235532aad3c0d49be80b6c
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-11-12 12:15:42 +0900

    SLON

--- baekjoon/11000/11775.cpp
+++ baekjoon/11000/11775.cpp
@@ -1,75 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+using ll = long long;
+
+const int mxN = 1e5;
 
 int P, M;
-stack<pair<int, int>> postfix;
+char s[mxN + 1];
+stack<pair<int, int>> num;
+stack<char> op;
 
-bool is_digit(char c) { return '0' <= c && c <= '9'; }
-void evaluate(char op) {
-    auto [coef1, num1] = postfix.top(); postfix.pop();
-    auto [coef2, num2] = postfix.top(); postfix.pop();
-    switch (op) {
-    case '+':
-        postfix.emplace((coef1 + coef2) % M, (num1 + num2) % M);
-        break;
-    case '-':
-        postfix.emplace((coef2 - coef1 + M) % M, (num2 - num1 + M) % M);
-        break;
-    case '*':
-        postfix.emplace(((ll)coef1 * num2 + (ll)coef2 * num1) % M, (ll)num1 * num2 % M);
+inline bool is_digit(char c) { return '0' <= c && c <= '9'; }
+void evaluate() {
+    auto [coef1, num1] = num.top();
+    num.pop();
+    auto [coef2, num2] = num.top();
+    num.pop();
+    switch (op.top()) {
+    case '+': num.emplace((coef1 + coef2) % M, (num1 + num2) % M); break;
+    case '-': num.emplace((coef2 - coef1 + M) % M, (num2 - num1 + M) % M); break;
+    default: num.emplace(((ll)coef1 * num2 + (ll)coef2 * num1) % M, (ll)num1 * num2 % M);
     }
+    op.pop();
 }
 int main() {
-    cin.tie(0), cout.tie(0);
     ios::sync_with_stdio(0);
-
-    string s;
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
     cin >> s >> P >> M;
-
-    stack<char> stk;
-    int len = s.size();
-    
-    for (int i = 0; i < len; ++i) {
+    for (int i = 0; s[i]; ++i) {
         if (is_digit(s[i])) {
-            int val = s[i] - '0', j = i + 1;
-            while (j < len && is_digit(s[j])) {
+            int val = (s[i] - '0') % M, j = i + 1;
+            for (; s[j] && is_digit(s[j]); ++j)
                 val = (val * 10 + s[j] - '0') % M;
-                ++j;
-            }
-            postfix.emplace(0, val);
+            num.emplace(0, val);
             i = j - 1;
-        }
-        else switch (s[i]) {
-        case '(': stk.emplace('(');
-            break;
-        case ')':
-            while (!stk.empty() && stk.top() != '(')
-                evaluate(stk.top()), stk.pop();
-            stk.pop();
-            break;
-        case '+':
-        case '-':
-            while (!stk.empty() && stk.top() != '(')
-                evaluate(stk.top()), stk.pop();
-            stk.emplace(s[i]);
-            break;
-        case '*':
-            while (!stk.empty() && stk.top() == '*')
-                evaluate(stk.top()), stk.pop();
-            stk.emplace('*');
-            break;
-        default: postfix.emplace(1, 0);
-        }
+        } else
+            switch (s[i]) {
+            case '(': op.emplace('('); break;
+            case ')':
+                while (!op.empty() && op.top() != '(') evaluate();
+                op.pop();
+                break;
+            case '+':
+            case '-':
+                while (!op.empty() && op.top() != '(') evaluate();
+                op.emplace(s[i]);
+                break;
+            case '*':
+                while (!op.empty() && op.top() == '*') evaluate();
+                op.emplace('*');
+                break;
+            default: num.emplace(1, 0);
+            }
     }
-    while (!stk.empty())
-        evaluate(stk.top()), stk.pop();
+    while (!op.empty())
+        evaluate();
 
-    auto [coef, num] = postfix.top();
-    for (ll i = 1; ; ++i)
-        if ((coef * i + num) % M == P) {
-            cout << i;
-            break;
-        }
-    return 0;
+    auto [c, n] = num.top();
+    for (ll i = 0;; ++i)
+        if ((c * i + n) % M == P)
+            return cout << i, 0;
 }
