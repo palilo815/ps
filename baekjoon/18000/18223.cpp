1283fce5c8408e797b9a79dc8655a92525b538f6
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-11-18 15:48:12 +0900

    민준이와 마산 그리고 건우

--- baekjoon/18000/18223.cpp
+++ baekjoon/18000/18223.cpp
@@ -1,58 +1,53 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
 using namespace std;
-typedef pair<int, int> P;
-typedef tuple<int, int, bool> tup;
-const int max_V = 5000;
 
-vector<P> adj[max_V + 1];
-int dist[max_V + 1];
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+const int mxV = 5e3;
 
-    int V, E, gunwoo; cin >> V >> E >> gunwoo;
-    while (E--) {
-        int u, v, w; cin >> u >> v >> w;
-        adj[u].push_back({ w,v });
-        adj[v].push_back({ w,u });
+struct elem {
+    int d, u;
+    elem(int d, int u) : d(d), u(u) {}
+    bool operator<(const elem& rhs) const {
+        return d > rhs.d;
+    }
+};
+
+vector<pair<int, int>> adj[mxV + 1];
+int dist[mxV + 1];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int V, E, P;
+    cin >> V >> E >> P;
+
+    for (int u, v, w; E--;) {
+        cin >> u >> v >> w;
+        w <<= 1;
+        adj[u].emplace_back(w, v);
+        adj[v].emplace_back(w, u);
     }
 
-    for (int i = 1; i <= V; ++i)
-        dist[i] = INT_MAX;
-    dist[1] = 0;
+    for (auto& [w, v] : adj[P])
+        --w;
 
-    bool ans = false;
-    priority_queue<tup, vector<tup>, greater<tup>> pq;
-    pq.push({ 0,1,false });
+    memset(dist + 1, 0x3f, sizeof(int) * V);
+
+    priority_queue<elem> pq;
+    pq.emplace(dist[1] = 0, 1);
+
     while (!pq.empty()) {
-        int d = get<0>(pq.top()), u = get<1>(pq.top());
-        bool save = get<2>(pq.top()); pq.pop();
+        auto [d, u] = pq.top();
+        pq.pop();
 
-        if (u == gunwoo) save = true;
-        // dst에 도착하더라도,
-        // 최딘경로가 여러 개일 수 있으므로 계속 진행해야 한다.
-        // 최단경로를 모두 찾았다면 (dist[v] < d) 끝
-        if (u == V) {
-            if (dist[V] < d) break;
-            ans |= save;
-            continue;
-        }
-        if (dist[u] < d) continue;
+        if (d != dist[u]) continue;
 
-        // dst로 가는 최단경로가 여러 개일 수 있으므로
-        // if(dist[v] > D) 가 아닌 if(dist[v] >= D)로 써야한다.
-        for (P p : adj[u]) {
-            int D = d + p.first, v = p.second;
-            if (dist[v] >= D) {
-                dist[v] = D;
-                pq.push({ D,v,save });
-            }
-        }
+        for (auto& [w, v] : adj[u])
+            if (dist[v] > d + w)
+                pq.emplace(dist[v] = d + w, v);
     }
-
-    if (ans) cout << "SAVE HIM";
-    else cout << "GOOD BYE";
-    return 0;
+    cout << (dist[V] & 1 ? "SAVE HIM" : "GOOD BYE");
 }
