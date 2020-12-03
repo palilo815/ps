41dd250145108d192c966b1e886e62d4501bcf38
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-12-03 14:40:35 +0900

    Parcel

--- baekjoon/16000/16287.cpp
+++ baekjoon/16000/16287.cpp
@@ -6,23 +6,30 @@ const int mxN = 5e3;
 int a[mxN];
 
 int main() {
-    cin.tie(0), cout.tie(0);
-    ios::sync_with_stdio(0);
-
-    int W, N; cin >> W >> N;
-    for (int i = 0; i < N; ++i)
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int w, n;
+    cin >> w >> n;
+    for (int i = 0; i < n; ++i)
         cin >> a[i];
 
-    unordered_set<int> st;
-    for (int i = 0; i < N - 1; ++i) {
-        for (int j = i + 1; j < N; ++j)
-            if (st.count(W - a[i] - a[j])) {
-                cout << "YES";
-                return 0;
-            }
-        for (int j = 0; j < i; ++j)
-            st.insert(a[i] + a[j]);
+    vector<bool> v(w + 1);
+
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            int val = w - a[i] - a[j];
+            if (val >= 0 && v[val])
+                return cout << "YES", 0;
+        }
+        for (int j = 0; j < i; ++j) {
+            int val = a[i] + a[j];
+            if (val <= w)
+                v[val] = true;
+        }
     }
     cout << "NO";
-    return 0;
 }
