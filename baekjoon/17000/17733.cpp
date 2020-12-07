c141ce574dad89df5b23ece12c870d172535dc7c
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-12-07 20:42:03 +0900

    Water Bottle

--- baekjoon/17000/17733.cpp
+++ baekjoon/17000/17733.cpp
@@ -1,28 +1,85 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
+#define loop(i, n) for (int i = 0; i < n; ++i)
 using namespace std;
-struct elem {
-    int a, b, c;
-    elem(int a, int b, int c): a(a), b(b), c(c) {}
-};
 
 const int mxN = 2e3;
 const int mxP = 2e5;
-const int mov[4][2] = { -1, 0, 0, -1, 0, 1, 1, 0};
+
+struct edge {
+    int u, v, w;
+    edge(int u, int v, int w) : u(u), v(v), w(w) {}
+};
 
 char mat[mxN + 2][mxN + 2];
-int area[mxN + 2][mxN + 2];
+int area[mxN + 2][mxN + 2], par[mxP + 1], lo[mxP], hi[mxP];
+pair<int, int> q[mxP];
 
-int par[mxP + 1];
-int qu[mxP], qv[mxP], lo[mxP], hi[mxP];
+int find(int u) {
+    return par[u] < 0 ? u : par[u] = find(par[u]);
+}
+void merge(int u, int v) {
+    u = find(u), v = find(v);
+    if (u == v) return;
 
-int _find(int u) {
-    return ~par[u] ? (par[u] = _find(par[u])) : u;
+    if (par[u] > par[v]) swap(u, v);
+    par[u] += par[v];
+    par[v] = u;
+}
+vector<edge> bfs(int P) {
+    const int mov[4][2] = {-1, 0, 0, -1, 0, 1, 1, 0};
+
+    queue<pair<int, int>> q;
+    for (int i = 1, r, c; i <= P; ++i) {
+        cin >> r >> c;
+        area[r][c] = i;
+        q.emplace(r << 11 | c, i);
+    }
+
+    memset(par + 1, -1, sizeof(int) * P);
+    vector<edge> edges;
+    vector<pair<int, int>> tmp;
+    for (int d = 0; !q.empty(); ++d) {
+        for (int cnt = q.size(); cnt; --cnt) {
+            int r = q.front().first >> 11,
+                c = q.front().first & 0x7ff,
+                id = q.front().second;
+            q.pop();
+
+            loop(i, 4) {
+                int R = r + mov[i][0], C = c + mov[i][1];
+                if (mat[R][C] == '#') continue;
+                if (!area[R][C]) {
+                    area[R][C] = id;
+                    q.emplace(R << 11 | C, id);
+                } else if (id != area[R][C] && find(id) != find(area[R][C])) {
+                    if (id < area[R][C]) {
+                        merge(id, area[R][C]);
+                        edges.emplace_back(id, area[R][C], d << 1);
+                    } else
+                        tmp.emplace_back(id, area[R][C]);
+                }
+            }
+        }
+        for (auto& e : tmp) {
+            int u = find(e.first), v = find(e.second);
+            if (u == v) continue;
+
+            edges.emplace_back(e.first, e.second, d << 1 | 1);
+            if (par[u] > par[v]) swap(u, v);
+            par[u] += par[v];
+            par[v] = u;
+        }
+        tmp.clear();
+    }
+    return edges;
 }
 int main() {
-    cin.tie(0), cout.tie(0);
-    ios::sync_with_stdio(0);
-
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
     int H, W, P, Q;
     cin >> H >> W >> P >> Q;
     for (int i = 1; i <= H; ++i)
@@ -33,56 +90,18 @@ int main() {
     for (int i = 1; i <= H; ++i)
         mat[i][0] = mat[i][W + 1] = '#';
 
-    queue<elem> q;
-    for (int i = 1, r, c; i <= P; ++i) {
-        cin >> r >> c;
-        area[r][c] = i;
-        q.emplace(r, c, i);
-    }
+    auto edges = bfs(P);
+    int N = edges.size();
 
-    vector<elem> edges;
-    for (int d = 0; !q.empty(); ++d) {
-        for (int cnt = q.size(); cnt; --cnt) {
-            auto [r, c, id] = q.front(); q.pop();
-
-            loop(i, 4) {
-                int R = r + mov[i][0], C = c + mov[i][1];
-                if (mat[R][C] == '#' || area[R][C] == id) continue;
-                if (area[R][C])
-                    edges.emplace_back(id, area[R][C], d * 2 + (id > area[R][C]));
-                else {
-                    area[R][C] = id;
-                    q.emplace(R, C, id);
-                }
-            }
-        }
-    }
-
-    sort(edges.begin(), edges.end(), [&](auto & a, auto & b) {
-        return a.c < b.c;
-    });
-    memset(par + 1, -1, sizeof(int) * P);
-
-    vector<elem> mst;
-    for (auto& e : edges) {
-        int u = _find(e.a), v = _find(e.b);
-        if (u ^ v) {
-            par[u] = v;
-            mst.emplace_back(e);
-            if (mst.size() == P - 1) break;
-        }
-    }
-
-    loop(i, Q) cin >> qu[i] >> qv[i];
-    int N = min(P - 1, (int)mst.size());
+    loop(i, Q) cin >> q[i].first >> q[i].second;
     fill(hi, hi + Q, N);
 
     for (;;) {
-        vector<vector<int>> vt(N);
+        vector<vector<int>> check(N);
         bool done = true;
 
-        loop(i, Q) if (lo[i] ^ hi[i]) {
-            vt[(lo[i] + hi[i]) >> 1].emplace_back(i);
+        loop(i, Q) if (lo[i] != hi[i]) {
+            check[(lo[i] + hi[i]) >> 1].emplace_back(i);
             done = false;
         }
         if (done) break;
@@ -90,15 +109,11 @@ int main() {
         memset(par + 1, -1, sizeof(int) * P);
 
         loop(m, N) {
-            int u = _find(mst[m].a), v = _find(mst[m].b);
-            par[u] = v;
-            for (int& i : vt[m]) {
-                u = _find(qu[i]), v = _find(qv[i]);
-                u ^ v ? (lo[i] = m + 1) : (hi[i] = m);
-            }
+            merge(edges[m].u, edges[m].v);
+            for (int& i : check[m])
+                find(q[i].first) == find(q[i].second) ? hi[i] = m : lo[i] = m + 1;
         }
     }
 
-    loop(i, Q) cout << (lo[i] == N ? -1 : mst[lo[i]].c) << '\n';
-    return 0;
+    loop(i, Q) cout << (lo[i] == N ? -1 : edges[lo[i]].w) << '\n';
 }
