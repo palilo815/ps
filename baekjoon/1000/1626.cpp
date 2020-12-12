11fdbf9ed7895c04975c133fa4ed56827227a2be
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-12-13 05:27:34 +0900

    두 번째로 작은 스패닝 트리

--- baekjoon/1000/1626.cpp
+++ baekjoon/1000/1626.cpp
@@ -1,118 +1,129 @@
 #include <bits/stdc++.h>
-#define x first
-#define y second
+#define m1 first
+#define m2 second
 using namespace std;
 using p = pair<int, int>;
 
-const int max_N = 50001;
-const int max_E = 200000;
+const int mxV = 5e4;
+const int mxE = 2e5;
 
 struct edge {
     int u, v, w, i;
-} e[max_E];
-vector<p> adj[max_N];
+};
 
-int N, M, t = -1;
-int sz[max_N], par[max_N], top[max_N], idx[max_N];
-p segT[max_N - 1 << 1];
+edge e[mxE];
+bitset<mxE> used;
+p segT[mxV << 1];
+vector<p> adj[mxV];
+int t, sz[mxV], par[mxV], top[mxV], in[mxV];
 
-int parent[max_N];
-bool used[max_E];
-
-p _max(p a, p b) {
-    if (a.x == b.x) return {a.x, max(a.y, b.y)};
-    return {max(a.x, b.x), max(min(a.x, b.x), max(a.y, b.y))};
+p max2(p lhs, p rhs) {
+    if (lhs.m1 > rhs.m1) return {lhs.m1, max(lhs.m2, rhs.m1)};
+    if (lhs.m1 < rhs.m1) return {rhs.m1, max(rhs.m2, lhs.m1)};
+    return {lhs.m1, max(lhs.m2, rhs.m2)};
 }
-int _find(int u) {
-    if (parent[u] < 0) return u;
-    return parent[u] = _find(parent[u]);
+int find(int u) {
+    return par[u] < 0 ? u : par[u] = find(par[u]);
 }
-int dfs(int u, int p) {
-    for (auto& [w, v] : adj[u])
-        if (v ^ p)
-            sz[u] += dfs(v, u);
-    return ++sz[u];
+void dfs(int u) {
+    sz[u] = 1;
+    for (auto& [w, v] : adj[u]) {
+        if (v == par[u]) continue;
+
+        par[v] = u;
+        dfs(v);
+        sz[u] += sz[v];
+
+        if (adj[u][0].second == par[u] || sz[v] > sz[adj[u][0].second])
+            swap(w, adj[u][0].first), swap(v, adj[u][0].second);
+    }
 }
-void hld(int u, int p, int d) {
-    int H = 0, HD = 0;
-    for (auto& [w, v] : adj[u])
-        if (v ^ p && sz[v] > sz[H])
-            H = v, HD = w;
-    for (auto& [w, v] : adj[u])
-        if (v ^ p && v ^ H)
-            hld(v, u, w);
+void hld(int u, int d) {
+    segT[t] = {d, -1};
+    in[u] = t++;
+    bool heavy = true;
+    for (auto& [w, v] : adj[u]) {
+        if (v == par[u]) continue;
 
-    if (!top[u]) top[u] = u;
-    if (H) top[H] = top[u], hld(H, u, HD);
-
-    par[u] = p, idx[u] = ++t;
-    segT[idx[u] + N] = {d, -1};
+        top[v] = heavy ? top[u] : v;
+        hld(v, w);
+        heavy = false;
+    }
 }
 p query(int l, int r) {
-    p ret = { -1, -1};
-    for (l += N, r += N; l < r; l >>= 1, r >>= 1) {
-        if (l & 1) ret = _max(ret, segT[l++]);
-        if (r & 1) ret = _max(ret, segT[--r]);
+    p ret = {-1, -1};
+    for (; l != r; l >>= 1, r >>= 1) {
+        if (l & 1) ret = max2(ret, segT[l++]);
+        if (r & 1) ret = max2(ret, segT[--r]);
     }
     return ret;
 }
 p solve(int u, int v) {
-    p ret = { -1, -1};
-    while (top[u] ^ top[v]) {
-        int& n = sz[top[u]] < sz[top[v]] ? u : v;
-        ret = _max(ret, query(idx[n], idx[top[n]] + 1));
-        n = par[top[n]];
+    p ret = {-1, -1};
+    for (; top[u] != top[v]; u = par[top[u]]) {
+        if (sz[top[u]] > sz[top[v]]) swap(u, v);
+        ret = max2(ret, query(in[top[u]], in[u] + 1));
     }
-    if (idx[u] > idx[v]) swap(u, v);
-    ret = _max(ret, query(idx[u], idx[v]));
+    if (in[u] > in[v]) swap(u, v);
+    ret = max2(ret, query(in[u] + 1, in[v] + 1));
     return ret;
 }
-int build_mst() {
-    memset(parent + 1, -1, sizeof(int) * N);
-    sort(e, e + M, [&](edge & a, edge & b) -> bool {
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int V, E;
+    cin >> V >> E;
+    if (E <= V - 1) return cout << -1, 0;
+
+    for (int i = 0; i < E; ++i) {
+        cin >> e[i].u >> e[i].v >> e[i].w;
+        --e[i].u, --e[i].v, e[i].i = i;
+    }
+
+    memset(par, -1, sizeof(int) * V);
+    sort(e, e + E, [&](auto& a, auto& b) {
         return a.w < b.w;
     });
 
-    int mst = 0, cnt = 0;
-    for (int j = 0; j < M; ++j) {
-        auto [u, v, w, i] = e[j];
-        u = _find(u), v = _find(v);
-        if (u ^ v) {
-            mst += w, ++cnt, parent[u] = v, used[i] = true;
-            adj[u].emplace_back(w, v);
-            adj[v].emplace_back(w, u);
-        }
-    }
-    return cnt == N - 1 ? mst : -1;
-}
-int main() {
-    cin.tie(0), cout.tie(0);
-    ios::sync_with_stdio(0);
-
-    cin >> N >> M;
-    for (int i = 0; i < M; ++i) {
-        cin >> e[i].u >> e[i].v >> e[i].w;
-        e[i].i = i;
+    int mst = 0;
+    for (int i = 0; i < E; ++i) {
+        auto merge = [&](edge& e) {
+            int u = find(e.u), v = find(e.v);
+            if (u == v) return;
+            if (par[u] > par[v]) swap(u, v);
+            par[u] += par[v];
+            par[v] = u;
+            mst += e.w;
+            used[e.i] = true;
+            adj[e.u].emplace_back(e.w, e.v);
+            adj[e.v].emplace_back(e.w, e.u);
+        };
+        merge(e[i]);
     }
 
-    int mst = build_mst();
-    if (mst == -1 || M == N - 1) {cout << -1; return 0;}
+    if (count_if(par, par + V, [&](auto& x) { return x < 0; }) > 1)
+        return cout << -1, 0;
 
-    dfs(1, 0);
-    hld(1, 0, 0);
+    t = V;
+    par[0] = -1;
+    dfs(0);
+    hld(0, 0);
 
-    for (int i = N - 1; i; --i)
-        segT[i] = _max(segT[i << 1], segT[i << 1 | 1]);
+    for (int i = V - 1; i; --i)
+        segT[i] = max2(segT[i << 1], segT[i << 1 | 1]);
 
     int ans = INT_MAX;
-    for (int j = 0; j < M; ++j) {
-        auto& [u, v, w, i] = e[j];
-        if (!used[i]) {
-            auto [w1, w2] = solve(u, v);
-            if (w > w1) ans = min(ans, w - w1);
-            else if (~w2 && w > w2) ans = min(ans, w - w2);
-        }
+    for (int i = 0; i < E; ++i) {
+        if (used[e[i].i]) continue;
+        auto [w1, w2] = solve(e[i].u, e[i].v);
+        if (e[i].w != w1)
+            ans = min(ans, e[i].w - w1);
+        else if (~w2)
+            ans = min(ans, e[i].w - w2);
     }
     cout << (ans == INT_MAX ? -1 : mst + ans);
-    return 0;
 }
