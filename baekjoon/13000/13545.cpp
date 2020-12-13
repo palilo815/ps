b977b1766b6550c7c5c2746c1bc8a2e45f68c7ba
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2020-12-13 21:50:11 +0900

    수열과 쿼리 0

--- baekjoon/13000/13545.cpp
+++ baekjoon/13000/13545.cpp
@@ -1,22 +1,22 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
 using namespace std;
+
 struct query {
     int l, r, i;
-    query(int l, int r, int i): l(l), r(r), i(i) {}
+    query(int l, int r, int i) : l(l), r(r), i(i) {}
 };
 
-const int M = 1e5;
+const int mxN = 1e5;
 const int max_sq = 317;
 
 vector<query> vt[max_sq];
-int a[M + 1], ans[M];
-int fst[M << 1 | 1], lst[M << 1 | 1];
+int a[mxN + 1], ans[mxN];
+int fst[mxN << 1 | 1], lst[mxN << 1 | 1];
 
 void solve(vector<query>& vt, int s, int e) {
     memset(fst, -1, sizeof(fst));
     memset(lst, -1, sizeof(lst));
-    sort(vt.begin(), vt.end(), [&](auto & a, auto & b) -> bool {
+    sort(vt.begin(), vt.end(), [&](auto& a, auto& b) {
         return a.r < b.r;
     });
 
@@ -25,7 +25,7 @@ void solve(vector<query>& vt, int s, int e) {
         for (; s < r; ++s) {
             lst[a[s]] = s;
             if (s >= e)
-                ~fst[a[s]] ? (res2 = max(res2, s - fst[a[s]])) : (fst[a[s]] = s);
+                ~fst[a[s]] ? res2 = max(res2, s - fst[a[s]]) : fst[a[s]] = s;
         }
 
         int res1 = 0;
@@ -35,25 +35,36 @@ void solve(vector<query>& vt, int s, int e) {
     }
 }
 int main() {
-    cin.tie(0), cout.tie(0);
-    ios::sync_with_stdio(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int n;
+    cin >> n;
 
-    int N; cin >> N;
-    loop(i, N) cin >> a[i + 1];
-    loop(i, N) a[i + 1] += a[i];
-    loop(i, N + 1) a[i] += M;
-    N = sqrt(N) + 1;
+    for (int i = 1; i <= n; ++i)
+        cin >> a[i];
+    partial_sum(a + 1, a + n + 1, a + 1);
+    for (int i = 0; i <= n; ++i)
+        a[i] += mxN;
 
-    int Q; cin >> Q;
-    loop(i, Q) {
-        int l, r; cin >> l >> r;
-        --l;
-        vt[l / N].emplace_back(l, ++r, i);
+    n = sqrt(n) + 1;
+
+    int q;
+    cin >> q;
+
+    for (int l, r, i = 0; i < q; ++i) {
+        cin >> l >> r;
+        --l, ++r;
+        vt[l / n].emplace_back(l, r, i);
     }
 
-    loop(i, N + 1) if (!vt[i].empty())
-        solve(vt[i], i * N, (i + 1) * N);
+    for (int i = 0; i < n; ++i)
+        if (!vt[i].empty())
+            solve(vt[i], i * n, (i + 1) * n);
 
-    loop(i, Q) cout << ans[i] << '\n';
-    return 0;
+    for (int i = 0; i < q; ++i)
+        cout << ans[i] << '\n';
 }
