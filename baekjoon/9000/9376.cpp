3de50edcf24e62bdffeb197847fea460247f5a46
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-01-16 14:16:57 +0900

    탈옥

--- baekjoon/9000/9376.cpp
+++ baekjoon/9000/9376.cpp
@@ -1,69 +1,73 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
 using namespace std;
-typedef pair<int, int> p;
 
 const int M = 102;
-const int mov[4][2] = { -1,0,0,-1,0,1,1,0 };
+const int mov[4][2] = {-1, 0, 0, -1, 0, 1, 1, 0};
 
-int row, col;
-char prison[M][M];
-int dist[3][M][M];
-deque<p> dq;
+char a[M][M];
+int row, col, dist[3][M][M];
 
-void BFS(int sr, int sc, int dist[][M]) {
+void bfs(int sr, int sc, int dist[][M]) {
+    deque<pair<int, int>> dq;
     dist[sr][sc] = 0;
     dq.emplace_back(sr, sc);
 
     while (!dq.empty()) {
-        auto [r, c] = dq.front(); dq.pop_front();
-        loop(i, 4) {
+        auto [r, c] = dq.front();
+        dq.pop_front();
+
+        for (int i = 0; i < 4; ++i) {
             int R = r + mov[i][0], C = c + mov[i][1];
-            if (R < 0 || R > row + 1 || C < 0 || C > col + 1 || dist[R][C] != -1 || prison[R][C] == '*')
-                continue;
-            if (prison[R][C] == '#') {
+            if (R == -1 || R == row + 2 || C == -1 || C == col + 2 || ~dist[R][C] || a[R][C] == '*') continue;
+
+            if (a[R][C] == '#') {
                 dist[R][C] = dist[r][c] + 1;
                 dq.emplace_back(R, C);
-            }
-            else {
+            } else {
                 dist[R][C] = dist[r][c];
                 dq.emplace_front(R, C);
             }
         }
     }
 }
-int main() {
-    cin.tie(0), cout.tie(0);
-    ios::sync_with_stdio(0);
+void solve() {
+    cin >> row >> col;
+    int cr1 = 0, cc1 = 0, cr2 = 0, cc2 = 0;
 
-    int T; cin >> T;
-    while (T--) {
-        cin >> row >> col;
-        int cr1 = 0, cc1 = 0, cr2 = 0, cc2 = 0;
-        loop(i, row + 2) loop(j, col + 2) {
-            if (i == 0 || i == row + 1 || j == 0 || j == col + 1)
-                prison[i][j] = '.';
-            else {
-                cin >> prison[i][j];
-                if (prison[i][j] == '$')
-                    cr1 ? (cr2 = i, cc2 = j) : (cr1 = i, cc1 = j);
-            }
+    memset(a[0], '.', col + 2);
+    memset(a[row + 1], '.', col + 2);
+    for (int i = 1; i <= row; ++i) {
+        for (int j = 1; j <= col; ++j) {
+            cin >> a[i][j];
+            if (a[i][j] == '$')
+                cr1 ? (cr2 = i, cc2 = j) : (cr1 = i, cc1 = j);
         }
-
-        memset(dist, -1, sizeof(dist));
-
-        BFS(0, 0, dist[0]);
-        BFS(cr1, cc1, dist[1]);
-        BFS(cr2, cc2, dist[2]);
-
-        int ans = INT_MAX;
-        loop(i, row + 2) loop(j, col + 2) if (prison[i][j] != '*') {
-            int tmp = 0;
-            loop(k, 3) tmp += dist[k][i][j];
-            if (prison[i][j] == '#') tmp -= 2;
-            ans = min(ans, tmp);
-        }
-        cout << ans << '\n';
+        a[i][0] = a[i][col + 1] = '.';
     }
-    return 0;
+
+    memset(dist, -1, sizeof(dist));
+
+    bfs(0, 0, dist[0]);
+    bfs(cr1, cc1, dist[1]);
+    bfs(cr2, cc2, dist[2]);
+
+    int ans = INT_MAX;
+    for (int i = 1; i <= row; ++i)
+        for (int j = 1; j <= col; ++j)
+            if (~dist[0][i][j]) {
+                int d = dist[0][i][j] + dist[1][i][j] + dist[2][i][j];
+                if (a[i][j] == '#') d -= 2;
+                ans = min(ans, d);
+            }
+    cout << ans << '\n';
+}
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifndef ONLINE_JUDGE
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int T;
+    cin >> T;
+    while (T--) solve();
 }
