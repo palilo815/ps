6b15b00b9cd125f7af9f374e5a6ed491f95b162e
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-01-25 00:11:59 +0900

    K번째 수

--- baekjoon/7000/7469.cpp
+++ baekjoon/7000/7469.cpp
@@ -1,59 +1,76 @@
 #include <bits/stdc++.h>
-#define val first
-#define idx second
 using namespace std;
-using p = pair<int, int>;
 
-const int mxN = 1e5;
-const int mxR = 17;
-const int mxC = 131072;
+template <typename T, unsigned mxB>
+struct wavelet_matrix {
+    static constexpr size_t BLOCK_SIZE = 64;
+    array<vector<uint64_t>, mxB> bit_array;
+    array<vector<int>, mxB> psum;
+    array<int, mxB> zeros;
 
-p a[mxN];
-int tr[mxR][mxC];
+    wavelet_matrix(vector<T>& a) {
+        const size_t block_count = a.size() / BLOCK_SIZE + 1;
+        vector<T> b(a.size());
+
+        for (auto pos = mxB; pos--;) {
+            bit_array[pos].resize(block_count);
+            psum[pos].resize(block_count);
+
+            int lo = 0, hi = a.size();
+            for (size_t i = 0; i < a.size(); ++i) {
+                if (a[i] & (T(1) << pos)) {
+                    bit_array[pos][i / BLOCK_SIZE] |= 1ull << (i % BLOCK_SIZE);
+                    b[--hi] = a[i];
+                } else
+                    b[lo++] = a[i];
+            }
+
+            for (size_t i = 1; i < block_count; ++i)
+                psum[pos][i] = psum[pos][i - 1] + __builtin_popcountll(bit_array[pos][i - 1]);
+
+            zeros[pos] = lo;
+            reverse(b.begin() + hi, b.end());
+            a.swap(b);
+        }
+    }
+    int count_1(int i, int r) const {
+        uint64_t mask = (1ull << (r % BLOCK_SIZE)) - 1;
+        return psum[i][r / BLOCK_SIZE] + __builtin_popcountll(bit_array[i][r / BLOCK_SIZE] & mask);
+    }
+    T kth(int l, int r, int k) const {
+        T ret = 0;
+        for (auto pos = mxB; pos--;) {
+            int lc = count_1(pos, l), rc = count_1(pos, r);
+            int zero = (r - l) - (rc - lc);
+            if (k >= zero) {
+                ret |= T(1) << pos;
+                l = lc + zeros[pos], r = rc + zeros[pos];
+                k -= zero;
+            } else
+                l -= lc, r -= rc;
+        }
+        return ret;
+    }
+};
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-#ifndef ONLINE_JUDGE
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef home
     freopen("in", "r", stdin);
     freopen("out", "w", stdout);
 #endif
-    int N, M;
-    cin >> N >> M;
+    int n, q;
+    cin >> n >> q;
 
-    if (N == 1) {
-        cin >> N;
-        while (M--) cout << N << '\n';
-        return 0;
+    constexpr int base = 1e9;
+
+    vector<int> a(n);
+    for (auto& x : a) cin >> x, x += base;
+
+    wavelet_matrix<int, 31> wm(a);
+    for (int l, r, k; q--;) {
+        cin >> l >> r >> k;
+        --l, --k;
+        cout << wm.kth(l, r, k) - base << '\n';
     }
-
-    for (int i = 0; i < N; ++i) {
-        cin >> a[i].val;
-        a[i].idx = i;
-    }
-
-    int R = 32 - __builtin_clz(N - 1);
-    int C = 1 << R;
-
-    sort(a, a + N);
-    for (int i = 0; i < N; ++i)
-        tr[0][i] = a[i].idx;
-    memset(tr[0] + N, 0x3f, sizeof(int) * (C - N));
-
-    for (int r = 0, k = 1; r + 1 < R; ++r, k <<= 1)
-        for (int* t = tr[r]; t < tr[r] + C; t += k << 1)
-            merge(t, t + k, t + k, t + k + k, t + mxC);
-
-    for (int i, j, k, lo, hi, r; M--;) {
-        cin >> i >> j >> k;
-        lo = 0, hi = C, r = R - 1;
-        for (--i, --j; ~r; --r) {
-            int m = (lo + hi) >> 1;
-            int cnt = upper_bound(tr[r] + lo, tr[r] + m, j) -
-                      lower_bound(tr[r] + lo, tr[r] + m, i);
-            k > cnt ? (lo = m, k -= cnt) : (hi = m);
-        }
-        cout << a[lo].val << '\n';
-    }
-    return 0;
 }
