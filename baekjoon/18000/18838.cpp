9961befa0b1661717d9583f424bf54e40fa80ec7
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-01-25 17:15:42 +0900

    가장 긴 증가하는 부분 수열 k

--- baekjoon/18000/18838.cpp
+++ baekjoon/18000/18838.cpp
@@ -1,64 +1,71 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
+
+constexpr int mxN = 1e5;
+constexpr int64_t INF = 1e18;
 
 struct node {
     int len;
-    ll cnt;
+    int64_t cnt;
+    void merge(const node& rhs) {
+        if (rhs.len > len)
+            len = rhs.len, cnt = rhs.cnt;
+        else if (rhs.len == len)
+            cnt = min(cnt + rhs.cnt, INF);
+    }
 };
 
-const int mxN = 1e5;
-const ll INF = 1e18;
+int a[mxN];
+node lis[mxN];
 
-int N, a[mxN];
-node bit[mxN + 1], lis[mxN];
+struct BIT {
+    vector<node> vt;
+    BIT(int n) : vt(n + 1) {}
+    void update(int i, node& val) {
+        ++val.len;
+        if (!val.cnt) val.cnt = 1;
 
-node merge(node& lhs, node& rhs) {
-    if (lhs.len != rhs.len) return lhs.len > rhs.len ? lhs : rhs;
-    return {lhs.len, lhs.cnt + rhs.cnt > INF ? INF : lhs.cnt + rhs.cnt};
-}
-void update(int i, node& n) {
-    ++n.len;
-    if (!n.cnt) n.cnt = 1;
+        for (; i; i -= i & -i)
+            vt[i].merge(val);
+    }
+    node query(int i) {
+        node ret = {0, 0};
+        for (++i; i < vt.size(); i += i & -i)
+            ret.merge(vt[i]);
+        return ret;
+    }
+};
 
-    for (; i <= N; i += i & -i)
-        bit[i] = merge(bit[i], n);
-}
-node query(int i) {
-    node ret = {0, 0};
-    for (; i; i -= i & -i)
-        ret = merge(ret, bit[i]);
-    return ret;
-}
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-#ifndef ONLINE_JUDGE
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef home
     freopen("in", "r", stdin);
     freopen("out", "w", stdout);
 #endif
-    ll K;
-    cin >> N >> K;
-    for (int i = 0; i < N; ++i)
+    int n;
+    int64_t k;
+    cin >> n >> k;
+    --k;
+
+    for (int i = 0; i < n; ++i)
         cin >> a[i];
 
-    for (int i = N - 1; ~i; --i)
-        update(N - a[i] + 1, lis[i] = query(N - a[i]));
+    BIT bit(n);
+    for (int i = n; i--;)
+        bit.update(a[i], lis[i] = bit.query(a[i]));
 
-    auto total = query(N);
-
-    if (total.cnt < K) return cout << -1, 0;
-    --K;
+    auto total = bit.query(0);
+    if (total.cnt <= k) return cout << -1, 0;
 
     vector<vector<int>> vt(total.len + 1);
-    for (int i = 0; i < N; ++i)
+    for (int i = 0; i < n; ++i)
         vt[lis[i].len].emplace_back(i);
 
     int prv_num = -1;
     for (int i = total.len; i; --i) {
         while (a[vt[i].back()] < prv_num) vt[i].pop_back();
-        while (lis[vt[i].back()].cnt <= K) {
-            K -= lis[vt[i].back()].cnt;
+        while (lis[vt[i].back()].cnt <= k) {
+            k -= lis[vt[i].back()].cnt;
             vt[i].pop_back();
         }
         cout << (prv_num = a[vt[i].back()]) << ' ';
