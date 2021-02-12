eb6b9094dd0d9bb49410f5c09419ccb14e6b66cc
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-02-12 17:13:12 +0900

    학급비 낭비하기

--- baekjoon/14000/14498.cpp
+++ baekjoon/14000/14498.cpp
@@ -1,8 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct query {
-    int x, y, op;
+struct bipartite_matching {
+    vector<vector<int>> adj;
+    vector<int> match, rev;
+    vector<bool> visited;
+    int n, m;
+
+    bipartite_matching(int n, int m) : n(n), m(m) {
+        adj.resize(n), match.resize(n, -1), rev.resize(m, -1), visited.resize(n);
+    }
+    void add_edge(int u, int v) { adj[u].emplace_back(v); }
+    bool dfs(int u) {
+        visited[u] = true;
+        for (const auto& v : adj[u]) {
+            if (rev[v] == -1 || (!visited[rev[v]] && dfs(rev[v]))) {
+                match[u] = v, rev[v] = u;
+                return true;
+            }
+        }
+        return false;
+    }
+    int maximum_matching() {
+        for (bool update = true; update;) {
+            fill(visited.begin(), visited.end(), false);
+            update = false;
+
+            for (int i = 0; i < n; ++i)
+                if (match[i] == -1 && dfs(i))
+                    update = true;
+        }
+        return n - count(match.begin(), match.end(), -1);
+    }
 };
 
 int main() {
@@ -14,36 +43,17 @@ int main() {
     int n, m, k;
     cin >> n >> m >> k;
 
-    vector<query> q(k);
-    for (auto& [x, y, op] : q)
-        cin >> x >> y >> op, --x, --y;
-
-    vector<vector<int>> adj(k);
-    for (int i = 0; i < k - 1; ++i)
-        for (int j = i + 1; j < k; ++j)
-            if (q[i].op != q[j].op && (q[i].x == q[j].x || q[i].y == q[j].y))
-                q[i].op ? adj[j].emplace_back(i) : adj[i].emplace_back(j);
-
-    vector<int> match(k, -1);
-    vector<bool> visited(k);
-    function<bool(int)> dfs = [&](int u) {
-        visited[u] = true;
-        for (const auto& v : adj[u]) {
-            if (match[v] == -1 || (!visited[match[v]] && dfs(match[v]))) {
-                match[u] = v, match[v] = u;
-                return true;
-            }
-        }
-        return false;
-    };
-
-    for (bool update = true; update;) {
-        fill(visited.begin(), visited.end(), false);
-        update = false;
-
-        for (int i = 0; i < k; ++i)
-            if (match[i] == -1 && dfs(i))
-                update = true;
+    vector<pair<int, int>> ppock, kkock;
+    // c = 0 -> 뽁뽁이
+    for (int x, y, c; k--;) {
+        cin >> x >> y >> c, --x, --y;
+        (c ? kkock : ppock).emplace_back(x, y);
     }
-    cout << (k - count(match.begin(), match.end(), -1) >> 1);
+
+    bipartite_matching solver(ppock.size(), kkock.size());
+    for (int i = 0; i < int(ppock.size()); ++i)
+        for (int j = 0; j < int(kkock.size()); ++j)
+            if (ppock[i].first == kkock[j].first || ppock[i].second == kkock[j].second)
+                solver.add_edge(i, j);
+    cout << solver.maximum_matching() << '\n';
 }
