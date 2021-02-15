c47485e78c42a2df7365f1305d5ba039a8651c12
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-02-15 17:19:14 +0900

    중량제한

--- baekjoon/1000/1939.cpp
+++ baekjoon/1000/1939.cpp
@@ -1,37 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 struct edge {
     int u, v, w;
 };
 
-const int mxN = 1e4 + 1;
-const int mxM = 1e5;
+struct disjoint_set {
+    vector<int> par, dist;
+    disjoint_set(int n) : par(n, -1), dist(n) {}
+    int find(int u) {
+        return par[u] < 0 ? u : par[u] = find(par[u]);
+    }
+    void merge(int u, int v, int w) {
+        u = find(u), v = find(v);
+        if (u == v) return;
 
-int par[mxN];
-edge e[mxM];
+        if (par[u] > par[v]) swap(u, v);
+        par[u] += par[v];
+        par[v] = u;
+        dist[v] = w;
+    }
+    int query(int u, int v) {
+        int ret;
+        for (; u != v; u = par[u]) {
+            if (dist[u] < dist[v]) swap(u, v);
+            ret = dist[u];
+        }
+        return ret;
+    }
+};
 
-int _find(int u) {
-    return ~par[u] ? (par[u] = _find(par[u])) : u;
-}
 int main() {
-    cin.tie(0), cout.tie(0);
-    ios::sync_with_stdio(0);
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef home
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int n, m;
+    cin >> n >> m;
 
-    int N, M; cin >> N >> M;
-    for (int i = 0; i < M; ++i)
-        cin >> e[i].u >> e[i].v >> e[i].w;
-    int x1, x2; cin >> x1 >> x2;
+    vector<edge> e(m);
+    for (auto& [u, v, w] : e)
+        cin >> u >> v >> w, --u, --v;
 
-    sort(e, e + M, [&](auto & a, auto & b) {
+    sort(e.begin(), e.end(), [&](auto& a, auto& b) {
         return a.w > b.w;
     });
-    memset(par + 1, -1, sizeof(int) * N);
 
-    int ans = 0;
-    for (int i = 0, u, v; _find(x1) ^ _find(x2); ++i) {
-        u = _find(e[i].u), v = _find(e[i].v), ans = e[i].w;
-        if (u ^ v) par[u] = v;
-    }
-    cout << ans;
-    return 0;
+    disjoint_set dsu(n);
+    for (const auto& [u, v, w] : e)
+        dsu.merge(u, v, w);
+
+    int u, v;
+    cin >> u >> v, --u, --v;
+    cout << dsu.query(u, v) << '\n';
 }
