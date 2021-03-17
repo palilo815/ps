434a0409d32d0b341760467c01d12273b12c345b
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-03-17 14:36:22 +0900

    님블

--- baekjoon/11000/11869.cpp
+++ baekjoon/11000/11869.cpp
@@ -1,13 +1,19 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
 
-int main()
-{
-    int N; scanf("%d", &N);
-    int x = 0, tmp;
-    while (N--) {
-        scanf("%d", &tmp);
-        x ^= tmp;
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef home
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int m;
+    cin >> m;
+
+    int xorsum = 0;
+    for (int p; m--;) {
+        cin >> p;
+        xorsum ^= p;
     }
-    printf("%s", x ? "koosaga" : "cubelover");
-    return 0;
+    cout << (xorsum ? "koosaga" : "cubelover");
 }
