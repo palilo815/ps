d7ea5446a7b4cb4095255003d44520b85f29cb6b
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-04-08 18:46:37 +0900

    추 정렬하기

---
+++ baekjoon/1000/1851.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef home
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int n;
+    cin >> n;
+
+    vector<pair<int, int>> a(n);
+    for (int i = 0; i < n; ++i)
+        cin >> a[i].first, a[i].second = i;
+
+    sort(a.begin(), a.end(), [&](auto& a, auto& b) {
+        return a.first < b.first;
+    });
+
+    int64_t ans = 0;
+    vector<bool> done(n);
+    for (int i = 0; i < n; ++i) {
+        if (done[i]) continue;
+
+        int cnt = 0;
+        for (int j = i; !done[j]; j = a[j].second) {
+            ans += a[j].first;
+            cnt += 1;
+            done[j] = true;
+        }
+        ans += min((cnt - 2LL) * a[i].first, a[i].first + (cnt + 1LL) * a[0].first);
+    }
+    cout << ans;
+}
