e62e6ee3149691ae211c9c8b58c45af16465c9fb
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-06-05 17:52:38 +0900

    Boss Battle

---
+++ baekjoon/15000/15600.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef home
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+
+    int n;
+    cin >> n;
+
+    cout << max(1, n - 2);
+}
