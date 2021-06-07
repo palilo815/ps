3d0674464b7c0cd276ce649b1bc893fc0bc2b56c
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-06-07 16:55:59 +0900

    지민이의 테러

--- baekjoon/1000/1688.cpp
+++ baekjoon/1000/1688.cpp
@@ -1,52 +1,99 @@
-#include <cstdio>
-#define loop(i,n) for(int i=0;i<n;++i)
-#define swap(a,b) do{auto tmp=a;a=b;b=tmp;}while(0)
-using ll = long long;
+#include <bits/stdc++.h>
+using namespace std;
 
-struct p {
-    int x, y;
-    bool operator >(const p& rhs) const {
-        return x == rhs.x ? y > rhs.y : x > rhs.x;
+template <typename T>
+struct point2D {
+    T x, y;
+    point2D() : x(0), y(0) {}
+    point2D(T _x, T _y) : x(_x), y(_y) {}
+    template <typename U>
+    explicit point2D(const point2D<U>& p) : x(p.x), y(p.y) {}
+
+    using P = point2D;
+
+    bool operator<(const P& p) const { return tie(x, y) < tie(p.x, p.y); }
+    bool operator==(const P& p) const { return tie(x, y) == tie(p.x, p.y); }
+    bool operator!=(const P& p) const { return tie(x, y) != tie(p.x, p.y); }
+
+    friend P operator+(const P& a, const P& b) { return P(a.x + b.x, a.y + b.y); }
+    friend P operator-(const P& a, const P& b) { return P(a.x - b.x, a.y - b.y); }
+    friend P operator*(const P& a, const T& scala) { return P(a.x * scala, a.y * scala); }
+    friend P operator*(const T& scala, const P& a) { return P(scala * a.x, scala * a.y); }
+    friend P operator/(const P& a, const T& scala) { return P(a.x / scala, a.y / scala); }
+
+    friend ostream& operator<<(ostream& o, const P& p) { return o << '(' << p.x << ", " << p.y << ')'; }
+    friend istream& operator>>(istream& i, P& p) { return i >> p.x >> p.y; }
+
+    T dot(const P& p) const { return x * p.x + y * p.y; }
+    T cross(const P& p) const { return x * p.y - y * p.x; }
+    T cross(const P& a, const P& b) const { return (a - *this).cross(b - *this); }
+
+    T dist2() const { return x * x + y * y; }
+    double dist() const { return sqrt(dist2()); }
+
+    P conj() const { return P(x, -y); }
+    P perp_cw() const { return P(y, -x); }
+    P perp_ccw() const { return P(-y, x); }
+
+    P unit() const { return *this / dist(); }
+    P normal() const { return perp_ccw().unit(); }
+
+    P unit_int() const { return x || y ? *this / gcd(x, y) : *this; }
+    P normal_int() const { return perp_ccw().unit_int(); }
+
+    bool same_dir(const P& p) const { return cross(p) == 0 && dot(p) > 0; }
+    bool on_segment(const P& s, const P& e) const {
+        if constexpr (is_integral_v<T>)
+            return cross(s, e) == 0 && (s - *this).dot(e - *this) <= 0;
+        else
+            return cross(s, e) == 0 && (s - *this).dot(e - *this) <= 1e-9;
     }
-    bool operator <=(const p& rhs) const {
-        return x == rhs.x ? y <= rhs.y : x < rhs.x;
+    int side_of(const P& s, const P& e) const {
+        if constexpr (is_integral_v<T>) {
+            auto c = s.cross(e, *this);
+            return (c > 0) - (c < 0);
+        } else {
+            auto a = (e - s).cross(*this - s);
+            double l = (e - s).dist() * 1e-9;
+            return (a > l) - (a < -l);
+        }
     }
-    bool operator ==(const p& rhs) const {
-        return x == rhs.x && y == rhs.y;
+
+    double angle() const { return atan2(y, x); }
+    P rotate(double radian) const {
+        return P(x * cos(radian) - y * sin(radian), x * sin(radian) + y * cos(radian));
     }
 };
 
-const int max_N = 10000;
-
-p poly[max_N + 4];
-
-int ccw(int a, int b, int c) {
-    ll ret = 1ll * (poly[b].x - poly[a].x) * (poly[c].y - poly[a].y) -
-             1ll * (poly[b].y - poly[a].y) * (poly[c].x - poly[a].x);
-    return ret ? (ret > 0 ? 1 : -1) : 0;
-}
-int cross(int a, int b, int c, int d) {
-    int A = ccw(a, b, c) * ccw(a, b, d);
-    int B = ccw(c, d, a) * ccw(c, d, b);
-    if (A | B) return A <= 0 && B <= 0;
-    if (poly[a] > poly[b]) swap(a, b);
-    if (poly[c] > poly[d]) swap(c, d);
-    return poly[a] <= poly[d] && poly[c] <= poly[b];
-}
-int main() {
-    int N; scanf("%d", &N);
-    for (int i = 0; i < N + 3; ++i)
-        scanf("%d %d", &poly[i].x, &poly[i].y);
-    poly[N + 3].x = -1;
-
-    loop(i, 3) {
-        poly[N + 3].y = poly[N + i].y + 1;
-        int cnt = 0;
-        loop(j, N) {
-            if (poly[j] == poly[N + i]) {cnt = 1; break;}
-            cnt += cross(j, (j + 1) % N, N + i, N + 3);
-        }
-        printf("%d\n", cnt & 1);
+template <typename T>
+bool in_polygon(vector<point2D<T>>& poly, point2D<T> p, bool strict) {
+    int cnt = 0, n = poly.size();
+    for (int i = 0; i < n; ++i) {
+        auto q = poly[(i + 1) % n];
+        if (p.on_segment(poly[i], q)) return !strict;
+        cnt ^= ((p.y < poly[i].y) - (p.y < q.y)) * p.cross(poly[i], q) > 0;
     }
-    return 0;
+    return cnt;
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef home
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+
+    int n;
+    cin >> n;
+
+    vector<point2D<int64_t>> poly(n);
+    for (auto& x : poly) cin >> x;
+
+    point2D<int64_t> p;
+    cin >> p;
+    cout << in_polygon(poly, p, false) << '\n';
+    cin >> p;
+    cout << in_polygon(poly, p, false) << '\n';
+    cin >> p;
+    cout << in_polygon(poly, p, false) << '\n';
 }
