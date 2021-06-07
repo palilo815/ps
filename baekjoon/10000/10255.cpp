0dfa7b3228d71764e88f829d06a643100990d875
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-06-08 02:00:27 +0900

    Intersections

---
+++ baekjoon/10000/10255.cpp
@@ -0,0 +1,132 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+struct point2D {
+    T x, y;
+    point2D() : x(0), y(0) {}
+    point2D(T _x, T _y) : x(_x), y(_y) {}
+    template <typename U>
+    explicit point2D(const point2D<U>& p) : x(p.x), y(p.y) {}
+
+    using P = point2D;
+
+    bool operator<(const P& p) const { return tie(x, y) < tie(p.x, p.y); }
+    bool operator==(const P& p) const { return tie(x, y) == tie(p.x, p.y); }
+    bool operator!=(const P& p) const { return tie(x, y) != tie(p.x, p.y); }
+
+    friend P operator+(const P& a, const P& b) { return P(a.x + b.x, a.y + b.y); }
+    friend P operator-(const P& a, const P& b) { return P(a.x - b.x, a.y - b.y); }
+    friend P operator*(const P& a, const T& scala) { return P(a.x * scala, a.y * scala); }
+    friend P operator*(const T& scala, const P& a) { return P(scala * a.x, scala * a.y); }
+    friend P operator/(const P& a, const T& scala) { return P(a.x / scala, a.y / scala); }
+
+    friend ostream& operator<<(ostream& o, const P& p) { return o << '(' << p.x << ", " << p.y << ')'; }
+    friend istream& operator>>(istream& i, P& p) { return i >> p.x >> p.y; }
+
+    T dot(const P& p) const { return x * p.x + y * p.y; }
+    T cross(const P& p) const { return x * p.y - y * p.x; }
+    T cross(const P& a, const P& b) const { return (a - *this).cross(b - *this); }
+
+    T dist2() const { return x * x + y * y; }
+    double dist() const { return sqrt(dist2()); }
+
+    P conj() const { return P(x, -y); }
+    P perp_cw() const { return P(y, -x); }
+    P perp_ccw() const { return P(-y, x); }
+
+    P unit() const { return *this / dist(); }
+    P normal() const { return perp_ccw().unit(); }
+
+    P unit_int() const { return x || y ? *this / gcd(x, y) : *this; }
+    P normal_int() const { return perp_ccw().unit_int(); }
+
+    bool same_dir(const P& p) const { return cross(p) == 0 && dot(p) > 0; }
+    bool on_segment(const P& s, const P& e) const {
+        if constexpr (is_integral_v<T>)
+            return cross(s, e) == 0 && (s - *this).dot(e - *this) <= 0;
+        else
+            return cross(s, e) == 0 && (s - *this).dot(e - *this) <= 1e-9;
+    }
+    int side_of(const P& s, const P& e) const {
+        if constexpr (is_integral_v<T>) {
+            auto c = s.cross(e, *this);
+            return (c > 0) - (c < 0);
+        } else {
+            auto a = (e - s).cross(*this - s);
+            double l = (e - s).dist() * 1e-9;
+            return (a > l) - (a < -l);
+        }
+    }
+
+    double angle() const { return atan2(y, x); }
+    P rotate(double radian) const {
+        return P(x * cos(radian) - y * sin(radian), x * sin(radian) + y * cos(radian));
+    }
+};
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef home
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+
+    using point = point2D<int>;
+    auto sgn = [](auto x) { return (x > 0) - (x < 0); };
+
+    array<point, 2> line;
+    array<point, 4> rect;
+    array<bool, 4> inter_corner;
+
+    set<point> corner;
+    int not_corner;
+    bool inf;
+
+    // check between the line and rect[i], rect[j]
+    auto segment_inter = [&](int i, int j) {
+        auto oa = rect[i].cross(rect[j], line[0]), ob = rect[i].cross(rect[j], line[1]),
+             oc = line[0].cross(line[1], rect[i]), od = line[0].cross(line[1], rect[j]);
+
+        if (sgn(oa) * sgn(ob) < 0 && sgn(oc) * sgn(od) < 0) {
+            point p = line[0] * ob - line[1] * oa;
+            if (p == rect[i] * (ob - oa))
+                corner.emplace(rect[i]);
+            else if (p == rect[j] * (ob - oa))
+                corner.emplace(rect[j]);
+            else
+                ++not_corner;
+        } else {
+            set<point> st;
+            if (rect[i].on_segment(line[0], line[1])) st.emplace(rect[i]);
+            if (rect[j].on_segment(line[0], line[1])) st.emplace(rect[j]);
+            if (line[0].on_segment(rect[i], rect[j])) st.emplace(line[0]);
+            if (line[1].on_segment(rect[i], rect[j])) st.emplace(line[1]);
+
+            if (st.size() > 1)
+                inf = true;
+            else if (st.size() == 1)
+                corner.emplace(*st.begin());
+        }
+    };
+
+    int T;
+    cin >> T;
+    while (T--) {
+        corner.clear();
+        not_corner = 0;
+        inf = false;
+
+        cin >> rect[0] >> rect[2] >> line[0] >> line[1];
+        rect[1] = rect[3] = rect[0];
+        rect[1].x += rect[2].x - rect[0].x;
+        rect[3].y += rect[2].y - rect[0].y;
+
+        segment_inter(0, 1);
+        segment_inter(1, 2);
+        segment_inter(2, 3);
+        segment_inter(3, 0);
+
+        cout << (inf ? 4 : corner.size() + not_corner) << '\n';
+    }
+}
