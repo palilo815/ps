b101a36cda3384c8980e406f65bcf2ba5a5a6a3f
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-06-11 14:03:30 +0900

    지민이의 테러

--- baekjoon/1000/1688.cpp
+++ baekjoon/1000/1688.cpp
@@ -1,10 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/* basics */
+namespace geo {
+#define EPS 1e-8
+template <typename T, enable_if_t<is_integral<T>::value, bool> = true>
+int sign(T x) { return (x > 0) - (x < 0); }
+template <typename T, enable_if_t<is_floating_point<T>::value, bool> = true>
+int sign(T x) { return (x > EPS) - (x < -EPS); }
+}; // namespace geo
+
+/* point2D */
+namespace geo {
 template <typename T>
 struct point2D {
     T x, y;
-    point2D() : x(0), y(0) {}
+    point2D() = default;
     point2D(T _x, T _y) : x(_x), y(_y) {}
     template <typename U>
     explicit point2D(const point2D<U>& p) : x(p.x), y(p.y) {}
@@ -17,9 +28,9 @@ struct point2D {
 
     friend P operator+(const P& a, const P& b) { return P(a.x + b.x, a.y + b.y); }
     friend P operator-(const P& a, const P& b) { return P(a.x - b.x, a.y - b.y); }
-    friend P operator*(const P& a, const T& scala) { return P(a.x * scala, a.y * scala); }
-    friend P operator*(const T& scala, const P& a) { return P(scala * a.x, scala * a.y); }
-    friend P operator/(const P& a, const T& scala) { return P(a.x / scala, a.y / scala); }
+    friend P operator*(const P& a, const T& scalar) { return P(a.x * scalar, a.y * scalar); }
+    friend P operator*(const T& scalar, const P& a) { return P(scalar * a.x, scalar * a.y); }
+    friend P operator/(const P& a, const T& scalar) { return P(a.x / scalar, a.y / scalar); }
 
     friend ostream& operator<<(ostream& o, const P& p) { return o << '(' << p.x << ", " << p.y << ')'; }
     friend istream& operator>>(istream& i, P& p) { return i >> p.x >> p.y; }
@@ -31,7 +42,6 @@ struct point2D {
     T dist2() const { return x * x + y * y; }
     double dist() const { return sqrt(dist2()); }
 
-    P conj() const { return P(x, -y); }
     P perp_cw() const { return P(y, -x); }
     P perp_ccw() const { return P(-y, x); }
 
@@ -42,20 +52,13 @@ struct point2D {
     P normal_int() const { return perp_ccw().unit_int(); }
 
     bool same_dir(const P& p) const { return cross(p) == 0 && dot(p) > 0; }
-    bool on_segment(const P& s, const P& e) const {
-        if constexpr (is_integral_v<T>)
-            return cross(s, e) == 0 && (s - *this).dot(e - *this) <= 0;
-        else
-            return cross(s, e) == 0 && (s - *this).dot(e - *this) <= 1e-9;
-    }
     int side_of(const P& s, const P& e) const {
         if constexpr (is_integral_v<T>) {
-            auto c = s.cross(e, *this);
-            return (c > 0) - (c < 0);
+            return sign(s.cross(e, *this));
         } else {
-            auto a = (e - s).cross(*this - s);
-            double l = (e - s).dist() * 1e-9;
-            return (a > l) - (a < -l);
+            auto res = (e - s).cross(*this - s);
+            double l = (e - s).dist() * EPS;
+            return (res > l) - (res < -l);
         }
     }
 
@@ -64,36 +67,65 @@ struct point2D {
         return P(x * cos(radian) - y * sin(radian), x * sin(radian) + y * cos(radian));
     }
 };
+}; // namespace geo
+
+namespace geo::point_in {
+template <typename T, enable_if_t<is_integral<T>::value, bool> = true>
+bool segment(const point2D<T>& p, const pair<point2D<T>, point2D<T>>& obj) {
+    return p.cross(obj.first, obj.second) == 0 &&
+           (obj.first - p).dot(obj.second - p) <= 0;
+}
+template <typename T, enable_if_t<is_floating_point<T>::value, bool> = true>
+bool segment(const point2D<T>& p, const pair<point2D<T>, point2D<T>>& obj) {
+    return fabs(p.cross(obj.first, obj.second)) < EPS &&
+           (obj.first - p).dot(obj.second - p) < EPS;
+}
 
 template <typename T>
-bool in_polygon(vector<point2D<T>>& poly, point2D<T> p, bool strict) {
-    int cnt = 0, n = poly.size();
-    for (int i = 0; i < n; ++i) {
-        auto q = poly[(i + 1) % n];
-        if (p.on_segment(poly[i], q)) return !strict;
-        cnt ^= ((p.y < poly[i].y) - (p.y < q.y)) * p.cross(poly[i], q) > 0;
+bool hull(const point2D<T>& p, const vector<point2D<T>>& obj, bool boundary) {
+    if (obj.size() < 3) return boundary && geo::point_in::segment(p, pair(obj.front(), obj.back()));
+
+    int lo = 1, hi = obj.size() - 1;
+    if (p.side_of(obj[0], obj[lo]) <= -boundary || p.side_of(obj[0], obj[hi]) >= boundary)
+        return false;
+
+    while (lo + 1 != hi) {
+        int mid = (lo + hi) >> 1;
+        (p.side_of(obj[0], obj[mid]) == 1 ? lo : hi) = mid;
     }
-    return cnt;
+    return p.side_of(obj[lo], obj[hi]) > -boundary;
 }
+
+template <typename T>
+bool polygon(const point2D<T>& p, const vector<point2D<T>>& obj, bool boundary) {
+    bool ret = false;
+    for (int i = 0; i < int(obj.size()); ++i) {
+        const auto s = obj[i], e = obj[i == int(obj.size()) - 1 ? 0 : i + 1];
+        if (point_in::segment(p, pair(s, e))) return boundary;
+        ret ^= ((p.y < s.y) - (p.y < e.y)) * p.cross(s, e) > 0;
+    }
+    return ret;
+}
+}; // namespace geo::point_in
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
-#ifdef home
+#ifdef palilo
     freopen("in", "r", stdin);
     freopen("out", "w", stdout);
 #endif
 
+    using point = geo::point2D<int64_t>;
+
     int n;
     cin >> n;
 
-    vector<point2D<int64_t>> poly(n);
-    for (auto& x : poly) cin >> x;
+    vector<point> a(n);
+    for (auto& p : a) cin >> p;
 
-    point2D<int64_t> p;
-    cin >> p;
-    cout << in_polygon(poly, p, false) << '\n';
-    cin >> p;
-    cout << in_polygon(poly, p, false) << '\n';
-    cin >> p;
-    cout << in_polygon(poly, p, false) << '\n';
+    for (int t = 3; t--;) {
+        point p;
+        cin >> p;
+        cout << geo::point_in::polygon(p, a, true) << '\n';
+    }
 }
