c4b72cc4717120fec40001d7cbbaf5fa0c8b5575
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-06-19 17:57:46 +0900

    Gratitude

---
+++ baekjoon/21000/21208.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef palilo
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+
+    int n, k;
+    cin >> n >> k;
+    cin.ignore();
+
+    vector<pair<pair<int, int>, string>> a;
+    unordered_map<string, int> mp;
+
+    for (int i = 0; i < 3 * n; ++i) {
+        string s;
+        getline(cin, s);
+        if (mp[s]) ++a[mp[s] - 1].first.first, a[mp[s] - 1].first.second = i;
+        else {
+            a.emplace_back(pair(1, i), s);
+            mp[s] = a.size();
+        }
+    }
+
+    stable_sort(a.begin(), a.end(), [&](const auto& lhs, const auto& rhs) {
+        return lhs.first > rhs.first;
+    });
+    a.resize(min(a.size(), size_t(k)));
+
+    for (const auto& [_, x] : a)
+        cout << x << '\n';
+}
