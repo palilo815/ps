77020755c9ddf4c6b1c1b1303a7c68f3a974c1f6
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-07-08 17:13:30 +0900

    Binary Search Tree

--- baekjoon/5000/5639.cpp
+++ baekjoon/5000/5639.cpp
@@ -1,24 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int pre_order[10000];
+namespace std {
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template <class Fun>
+class y_combinator_result {
+    Fun fun_;
 
-void print_post(int l, int r) {
-    if (l == r) return;
-    int rt = pre_order[l];
-    int R_sub = upper_bound(pre_order + l, pre_order + r, rt) - pre_order;
-    print_post(l + 1, R_sub);
-    print_post(R_sub, r);
-    cout << pre_order[l] << '\n';
+public:
+    template <class T>
+    explicit y_combinator_result(T&& fun) : fun_(std::forward<T>(fun)) {}
+
+    template <class... Args>
+    decltype(auto) operator()(Args&&... args) {
+        return fun_(std::ref(*this), std::forward<Args>(args)...);
+    }
+};
+
+template <class Fun>
+decltype(auto) y_combinator(Fun&& fun) {
+    return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
 }
-int main() {
-    cin.tie(0), cout.tie(0);
-    ios::sync_with_stdio(0);
-    
-    int n, sz = 0;
-    while (cin >> n)
-        pre_order[sz++] = n;
+}; // namespace std
 
-    print_post(0, sz);
-    return 0;
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef palilo
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    vector<int> ord;
+    for (int x; cin >> x; ord.emplace_back(x)) {
+        ;
+    }
+    y_combinator([&](auto self, auto l, auto r) -> void {
+        if (l == r) {
+            return;
+        }
+        const auto root = *l;
+        const auto pivot = upper_bound(l + 1, r, root);
+        self(l + 1, pivot);
+        self(pivot, r);
+        cout << root << '\n';
+    })(ord.begin(), ord.end());
 }
