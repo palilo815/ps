1640ddb431ce1ca3b96c675258bbefaf338f10ff
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-07-08 17:13:43 +0900

    Cheering up the Cows

--- baekjoon/1000/1185.cpp
+++ baekjoon/1000/1185.cpp
@@ -1,42 +1,60 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
 using namespace std;
-struct edge {
-    int u, v, w;
+
+class disjoint_set {
+    const int n;
+    vector<int> par;
+
+public:
+    disjoint_set(int _n) : n(_n), par(n, -1) {}
+    int find(int u) {
+        return par[u] < 0 ? u : par[u] = find(par[u]);
+    }
+    bool unite(int u, int v) {
+        u = find(u), v = find(v);
+        if (u == v) {
+            return false;
+        }
+        if (par[u] > par[v]) {
+            swap(u, v);
+        }
+        par[u] += par[v];
+        par[v] = u;
+        return true;
+    }
+    int size_of(int u) { return -par[find(u)]; }
+    bool is_same(int u, int v) { return find(u) == find(v); }
+    int num_components() const {
+        return count_if(par.begin(), par.end(), [&](auto& x) { return x < 0; });
+    }
 };
 
-const int mxN = 1e4;
-const int mxM = 1e5;
-
-int a[mxN], par[mxN];
-edge e[mxM];
-
-int _find(int u) {
-    return ~par[u] ? (par[u] = _find(par[u])) : u;
-}
 int main() {
-    cin.tie(0), cout.tie(0);
-    ios::sync_with_stdio(0);
-
-    int N, M; cin >> N >> M;
-    loop(i, N) cin >> a[i];
-    loop(i, M) {
-        cin >> e[i].u >> e[i].v >> e[i].w;
-        e[i].w = 2 * e[i].w + a[--e[i].u] + a[--e[i].v];
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef palilo
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    for (auto& x : a) {
+        cin >> x;
     }
-    sort(e, e + M, [&](auto & a, auto & b) {
-        return a.w < b.w;
+    vector<tuple<int, int, int>> edges(m);
+    for (auto& [u, v, w] : edges) {
+        cin >> u >> v >> w, --u, --v;
+        w += w + a[u] + a[v];
+    }
+    sort(edges.begin(), edges.end(), [&](const auto& lhs, const auto& rhs) {
+        return get<2>(lhs) < get<2>(rhs);
     });
-    memset(par, -1, sizeof(int) * N);
-
-    int ans = *min_element(a, a + N), cnt = N - 1;
-    loop(i, M) {
-        int u = _find(e[i].u), v = _find(e[i].v);
-        if (u ^ v) {
-            par[u] = v, ans += e[i].w;
-            if (--cnt == 0) break;
+    disjoint_set dsu(n);
+    int ans = *min_element(a.begin(), a.end());
+    for (const auto& [u, v, w] : edges) {
+        if (dsu.unite(u, v)) {
+            ans += w;
         }
     }
     cout << ans;
-    return 0;
 }
