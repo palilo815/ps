8a2c54cb8408f854af77f945e75213abed4ffc44
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-07-30 16:56:14 +0900

    소 운전한다.

--- baekjoon/19000/19701.cpp
+++ baekjoon/19000/19701.cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int mxN = 1e5;
-
 struct elem {
     int d, u;
     elem(int d, int u) : d(d), u(u) {}
@@ -11,50 +9,45 @@ struct elem {
     }
 };
 
-vector<tuple<int, int, int>> adj[mxN];
-int dist[mxN << 1];
-
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
-#ifndef ONLINE_JUDGE
+#ifdef palilo
     freopen("in", "r", stdin);
     freopen("out", "w", stdout);
 #endif
     int n, m;
     cin >> n >> m;
-
+    vector<vector<tuple<int, int, int>>> adj(n);
     for (int u, v, t, k; m--;) {
-        cin >> u >> v >> t >> k;
-        --u, --v;
+        cin >> u >> v >> t >> k, --u, --v;
         adj[u].emplace_back(t, k, v);
         adj[v].emplace_back(t, k, u);
     }
-
-    memset(dist, 0x3f, sizeof(int) * 2 * n);
-    dist[0] = 0;
-
+    vector dist(n << 1, INT_MAX);
     priority_queue<elem> pq;
     pq.emplace(dist[0] = 0, 0);
-
     while (!pq.empty()) {
-        auto [d, u] = pq.top();
+        const auto [d, u] = pq.top();
         pq.pop();
-
-        if (d != dist[u]) continue;
-
-        for (auto& [t, k, v] : adj[u >> 1]) {
+        if (d != dist[u]) {
+            continue;
+        }
+        for (const auto& [t, k, v] : adj[u >> 1]) {
             if (u & 1) {
-                if (dist[v << 1 | 1] > dist[u] + t)
+                if (dist[v << 1 | 1] > dist[u] + t) {
                     pq.emplace(dist[v << 1 | 1] = dist[u] + t, v << 1 | 1);
+                }
             } else {
-                if (dist[v << 1] > dist[u] + t)
+                if (dist[v << 1] > dist[u] + t) {
                     pq.emplace(dist[v << 1] = dist[u] + t, v << 1);
-                if (dist[v << 1 | 1] > dist[u] + t - k)
+                }
+                if (dist[v << 1 | 1] > dist[u] + t - k) {
                     pq.emplace(dist[v << 1 | 1] = dist[u] + t - k, v << 1 | 1);
+                }
             }
         }
     }
-
-    for (int i = 3; i < n << 1; i += 2)
+    for (int i = 3; i < n << 1; i += 2) {
         cout << dist[i] << '\n';
+    }
 }
