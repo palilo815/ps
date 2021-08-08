a62354bc06b5037746d2a453839f23a8acdc4935
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-08-08 21:40:22 +0900

    4 Values whose Sum is 0

--- baekjoon/7000/7453.cpp
+++ baekjoon/7000/7453.cpp
@@ -1,31 +1,49 @@
-#include<iostream>
-#include<algorithm>
-#define loop(i,n) for(int i=0;i<n;++i)
-#define ll long long
-#define max_N 4000
+#include <bits/stdc++.h>
 using namespace std;
 
-int A[max_N], B[max_N], C[max_N], D[max_N];
-int X[max_N * max_N], Y[max_N * max_N];
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
-
-    int n; cin >> n;
-    loop(i, n) cin >> A[i] >> B[i] >> C[i] >> D[i];
-    loop(i, n) loop(j, n) X[i * n + j] = A[i] + B[j];
-    loop(i, n) loop(j, n) Y[i * n + j] = C[i] + D[j];
-    n *= n;
-    sort(X, X + n);
-    sort(Y, Y + n);
-    ll ans = 0;
-    loop(i, n) {
-        int key = -X[i];
-        auto low = lower_bound(Y, Y + n, key), up = upper_bound(Y, Y + n, key);
-        if(low != up)
-            ans += (up - low);
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef palilo
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n), c(n), d(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i] >> b[i] >> c[i] >> d[i];
+    }
+    vector<int> ab(n * n), cd(n * n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            ab[i * n + j] = a[i] + b[j];
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cd[i * n + j] = c[i] + d[j];
+        }
+    }
+    sort(ab.begin(), ab.end());
+    sort(cd.begin(), cd.end(), greater());
+    auto it = ab.begin(), jt = cd.begin();
+    int64_t ans = 0;
+    for (;;) {
+        if (*it + *jt < 0) {
+            if (++it == ab.end()) {
+                break;
+            }
+        } else if (*it + *jt > 0) {
+            if (++jt == cd.end()) {
+                break;
+            }
+        } else {
+            const auto it2 = find_if(it, ab.end(), [&](const auto& x) { return x != *it; });
+            const auto jt2 = find_if(jt, cd.end(), [&](const auto& x) { return x != *jt; });
+            ans += (it2 - it) * (jt2 - jt);
+            if (it2 == ab.end() || jt2 == cd.end()) break;
+            it = it2, jt = jt2;
+        }
     }
     cout << ans;
-    return 0;
 }
