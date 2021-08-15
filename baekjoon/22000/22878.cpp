a5f82a967f023a1c8a60e64e58fd880d90e1c67a
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-08-15 13:53:59 +0900

    간단한 문제

---
+++ baekjoon/22000/22878.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef palilo
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int n;
+    cin >> n;
+    vector<int> p(n), q(n);
+    for (auto& x : p) cin >> x;
+    for (auto& x : q) cin >> x;
+    transform(p.begin(), p.end(), p.begin(), [&](const auto& x) { return x << 1; });
+    transform(q.begin(), q.end(), q.begin(), [&](const auto& x) { return x << 1; });
+    vector<int> r(n), s(n);
+    transform(p.begin(), p.end(), q.begin(), r.begin(), plus());
+    transform(p.begin(), p.end(), q.begin(), s.begin(), minus());
+    auto f = [&](vector<int>& v) {
+        sort(v.begin(), v.end());
+        int64_t ret = 0;
+        for (int i = 0; i < n; ++i) {
+            ret += int64_t(2 * i + 1 - n) * v[i];
+        }
+        return ret;
+    };
+    cout << f(p) + f(q) - (f(r) + f(s)) / 2;
+}
