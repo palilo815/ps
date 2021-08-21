1f4c8ddc14a4418c13db6597878bc9072baed4dd
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-08-21 15:39:05 +0900

    볼록 껍질

--- baekjoon/1000/1708.cpp
+++ baekjoon/1000/1708.cpp
@@ -1,45 +1,103 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
-#define x first
-#define y second
 using namespace std;
-using ll = long long;
-using p = pair<int, int>;
 
-const int M = 100000;
+namespace geo {
+/* basics */
+#define EPS 1e-8
+template <typename T, enable_if_t<is_integral<T>::value, bool> = true>
+int sign(T x) { return (x > 0) - (x < 0); }
+template <typename T, enable_if_t<is_floating_point<T>::value, bool> = true>
+int sign(T x) { return (x > EPS) - (x < -EPS); }
 
-p poly[M];
+/* point2D */
+template <typename T>
+struct point2D {
+    T x, y;
+    point2D() = default;
+    point2D(T _x, T _y) : x(_x), y(_y) {}
+    template <typename U>
+    explicit point2D(const point2D<U>& p) : x(p.x), y(p.y) {}
 
-int ccw(p& a, p& b, p& c) {
-    ll ret = 1LL * (b.x - a.x) * (c.y - a.y) -
-             1LL * (b.y - a.y) * (c.x - a.x);
-    return ret ? (ret > 0 ? 1 : -1) : 0;
-}
-ll dist(p& v) {
-    ll dx = poly[0].x - v.x, dy = poly[0].y - v.y;
-    return dx * dx + dy * dy;
-}
-int main() {
-    cin.tie(0), cout.tie(0);
-    ios::sync_with_stdio(0);
+    using P = point2D;
 
-    int N; cin >> N;
-    loop(i, N) cin >> poly[i].x >> poly[i].y;
+    bool operator<(const P& p) const { return tie(x, y) < tie(p.x, p.y); }
+    bool operator==(const P& p) const { return tie(x, y) == tie(p.x, p.y); }
+    bool operator!=(const P& p) const { return tie(x, y) != tie(p.x, p.y); }
 
-    swap(poly[0], *min_element(poly, poly + N, [](p & a, p & b) -> bool {
-        return a.x == b.x ? a.y < b.y : a.x < b.x;
-    }));
-    sort(poly + 1, poly + N, [](p & a, p & b) -> bool {
-        int ret = ccw(poly[0], a, b);
-        return ret ? ret == 1 : dist(a) < dist(b);
-    });
+    friend P operator+(const P& a, const P& b) { return P(a.x + b.x, a.y + b.y); }
+    friend P operator-(const P& a, const P& b) { return P(a.x - b.x, a.y - b.y); }
+    friend P operator*(const P& a, const T& scalar) { return P(a.x * scalar, a.y * scalar); }
+    friend P operator*(const T& scalar, const P& a) { return P(scalar * a.x, scalar * a.y); }
+    friend P operator/(const P& a, const T& scalar) { return P(a.x / scalar, a.y / scalar); }
 
-    vector<p> hull;
-    loop(i, N) {
-        while (hull.size() > 1 && ccw(hull[hull.size() - 2], hull[hull.size() - 1], poly[i]) <= 0)
-            hull.pop_back();
-        hull.emplace_back(poly[i]);
+    friend ostream& operator<<(ostream& o, const P& p) { return o << '(' << p.x << ", " << p.y << ')'; }
+    friend istream& operator>>(istream& i, P& p) { return i >> p.x >> p.y; }
+
+    T dot(const P& p) const { return x * p.x + y * p.y; }
+    T cross(const P& p) const { return x * p.y - y * p.x; }
+    T cross(const P& a, const P& b) const { return (a - *this).cross(b - *this); }
+
+    T dist2() const { return x * x + y * y; }
+    double dist() const { return sqrt(dist2()); }
+
+    P perp_cw() const { return P(y, -x); }
+    P perp_ccw() const { return P(-y, x); }
+
+    P unit() const { return *this / dist(); }
+    P normal() const { return perp_ccw().unit(); }
+
+    P unit_int() const { return x || y ? *this / gcd(x, y) : *this; }
+    P normal_int() const { return perp_ccw().unit_int(); }
+
+    bool same_dir(const P& p) const { return cross(p) == 0 && dot(p) > 0; }
+    int side_of(const P& s, const P& e) const {
+        if constexpr (is_integral_v<T>) {
+            return sign(s.cross(e, *this));
+        } else {
+            auto res = (e - s).cross(*this - s);
+            double l = (e - s).dist() * EPS;
+            return (res > l) - (res < -l);
+        }
     }
-    cout << hull.size();
-    return 0;
+
+    double angle() const { return atan2(y, x); }
+    P rotate(double radian) const {
+        return P(x * cos(radian) - y * sin(radian), x * sin(radian) + y * cos(radian));
+    }
+};
+}; // namespace geo
+
+namespace geo {
+template <typename T>
+vector<point2D<T>> convex_hull(vector<point2D<T>>& pts) {
+    if (pts.size() <= 1) return pts;
+    sort(pts.begin(), pts.end());
+    vector<point2D<T>> hull(pts.size() + 1);
+    size_t s = 0, t = 0;
+    for (const auto& p : pts) {
+        while (t >= s + 2 && hull[t - 2].cross(hull[t - 1], p) <= 0) --t;
+        hull[t++] = p;
+    }
+    s = --t;
+    reverse(pts.begin(), pts.end());
+    for (const auto& p : pts) {
+        while (t >= s + 2 && hull[t - 2].cross(hull[t - 1], p) <= 0) t--;
+        hull[t++] = p;
+    }
+    return {hull.begin(), hull.begin() + t - 1 - (t == 2 && hull[0] == hull[1])};
+}
+}; // namespace geo
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef palilo
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    using point = geo::point2D<int64_t>;
+    int n;
+    cin >> n;
+    vector<point> a(n);
+    for (auto& x : a) cin >> x;
+    cout << geo::convex_hull(a).size();
 }
