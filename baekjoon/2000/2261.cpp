3ff74cc7cdb899ae1bf26bd2352b17d29992e5d2
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-08-22 20:27:37 +0900

    가장 가까운 두 점

--- baekjoon/2000/2261.cpp
+++ baekjoon/2000/2261.cpp
@@ -1,58 +1,113 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
 using namespace std;
-typedef pair<int, int> p;
 
-const int max_N = 100000;
-p dot[max_N];
+template <class T>
+bool chmin(T& _old, T _new) { return _old > _new && (_old = _new, true); }
+template <class T>
+bool chmax(T& _old, T _new) { return _old < _new && (_old = _new, true); }
 
-auto cmp_y = [](p a, p b)
-{
-    if (a.second == b.second) return a.first < b.first;
-    return a.second < b.second;
-};
-int dist(p a, p b)
-{
-    int dx = a.first - b.first, dy = a.second - b.second;
-    return dx * dx + dy * dy;
-}
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+namespace geo {
+/* basics */
+#define EPS 1e-8
+template <typename T, enable_if_t<is_integral<T>::value, bool> = true>
+int sign(T x) { return (x > 0) - (x < 0); }
+template <typename T, enable_if_t<is_floating_point<T>::value, bool> = true>
+int sign(T x) { return (x > EPS) - (x < -EPS); }
 
-    // 출처)
-    // https://www.acmicpc.net/blog/view/25
+/* point2D */
+template <typename T>
+struct point2D {
+    T x, y;
+    point2D() = default;
+    point2D(T _x, T _y) : x(_x), y(_y) {}
+    template <typename U>
+    explicit point2D(const point2D<U>& p) : x(p.x), y(p.y) {}
 
-    int N; cin >> N;
-    loop(i, N) cin >> dot[i].first >> dot[i].second;
-    sort(dot, dot + N);
+    using P = point2D;
 
-    set<p, decltype(cmp_y)> st(cmp_y);
-    st.insert(dot[0]), st.insert(dot[1]);
-    int ans = dist(dot[0], dot[1]);
-    int start = 0;
+    bool operator<(const P& p) const { return tie(x, y) < tie(p.x, p.y); }
+    bool operator==(const P& p) const { return tie(x, y) == tie(p.x, p.y); }
+    bool operator!=(const P& p) const { return tie(x, y) != tie(p.x, p.y); }
 
-    for (int i = 2; i < N; ++i) {
-        p& here = dot[i];
-        while (start < i) {
-            int dx = dot[start].first - here.first;
-            if (dx * dx > ans) st.erase(dot[start++]);
-            else break;
+    friend P operator+(const P& a, const P& b) { return P(a.x + b.x, a.y + b.y); }
+    friend P operator-(const P& a, const P& b) { return P(a.x - b.x, a.y - b.y); }
+    friend P operator*(const P& a, const T& scalar) { return P(a.x * scalar, a.y * scalar); }
+    friend P operator*(const T& scalar, const P& a) { return P(scalar * a.x, scalar * a.y); }
+    friend P operator/(const P& a, const T& scalar) { return P(a.x / scalar, a.y / scalar); }
+
+    friend ostream& operator<<(ostream& o, const P& p) { return o << '(' << p.x << ", " << p.y << ')'; }
+    friend istream& operator>>(istream& i, P& p) { return i >> p.x >> p.y; }
+
+    T dot(const P& p) const { return x * p.x + y * p.y; }
+    T cross(const P& p) const { return x * p.y - y * p.x; }
+    T cross(const P& a, const P& b) const { return (a - *this).cross(b - *this); }
+
+    T dist2() const { return x * x + y * y; }
+    double dist() const { return sqrt(dist2()); }
+
+    P perp_cw() const { return P(y, -x); }
+    P perp_ccw() const { return P(-y, x); }
+
+    P unit() const { return *this / dist(); }
+    P normal() const { return perp_ccw().unit(); }
+
+    P unit_int() const { return x || y ? *this / gcd(x, y) : *this; }
+    P normal_int() const { return perp_ccw().unit_int(); }
+
+    bool same_dir(const P& p) const { return cross(p) == 0 && dot(p) > 0; }
+    int side_of(const P& s, const P& e) const {
+        if constexpr (is_integral_v<T>) {
+            return sign(s.cross(e, *this));
+        } else {
+            auto res = (e - s).cross(*this - s);
+            double l = (e - s).dist() * EPS;
+            return (res > l) - (res < -l);
         }
-
-        int d = ceil(sqrt(ans));
-        p lower_p = { -10000,here.second - d };
-        p upper_p = { 10000,here.second + d };
-        auto lower = st.lower_bound(lower_p);
-        auto upper = st.upper_bound(upper_p);
-
-        for (auto it = lower; it != upper; ++it) {
-            int d = dist(here, *it);
-            if (d < ans) ans = d;
-        }
-        st.insert(here);
     }
-    cout << ans;
-    return 0;
+
+    double angle() const { return atan2(y, x); }
+    P rotate(double radian) const {
+        return P(x * cos(radian) - y * sin(radian), x * sin(radian) + y * cos(radian));
+    }
+};
+}; // namespace geo
+
+namespace geo {
+template <typename T>
+T closest_pair(vector<point2D<T>> v) {
+    assert(v.size() > 1);
+    sort(v.begin(), v.end(), [&](const auto& lhs, const auto& rhs) {
+        return lhs.y < rhs.y;
+    });
+    set<point2D<T>> st;
+    T ret = numeric_limits<T>::max();
+    size_t i = 0;
+    for (const auto& p : v) {
+        point2D<T> d(1 + T(sqrt(ret)), 0);
+        while (v[i].y <= p.y - d.x) st.erase(v[i++]);
+        auto lo = st.lower_bound(p - d);
+        auto hi = st.upper_bound(p + d);
+        for (; lo != hi; ++lo) {
+            chmin(ret, (*lo - p).dist2());
+        }
+        st.emplace(p);
+    }
+    return ret;
+}
+}; // namespace geo
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef palilo
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    using point = geo::point2D<int>;
+    int n;
+    cin >> n;
+    vector<point> a(n);
+    for (auto& x : a) {
+        cin >> x;
+    }
+    cout << geo::closest_pair(move(a));
 }
