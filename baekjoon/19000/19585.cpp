78374d841cd1438500ef1c222651d63ae5ad6f23
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-09-03 15:38:13 +0900

    전설

--- baekjoon/19000/19585.cpp
+++ baekjoon/19000/19585.cpp
@@ -1,6 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct hash_t {
+    uint64_t x;
+    hash_t() = default;
+    hash_t(uint64_t x) : x(x) {}
+#define OP(O, A, B)                         \
+    hash_t operator O(hash_t o) {           \
+        uint64_t r = x;                     \
+        asm(A "addq %%rdx, %0\n adcq $0,%0" \
+            : "+a"(r)                       \
+            : B);                           \
+        return r;                           \
+    }
+    OP(+, , "d"(o.x))
+    OP(*, "mul %1\n", "r"(o.x)
+        : "rdx")
+    hash_t operator-(const hash_t& o) { return *this + ~o.x; }
+    uint64_t get() const { return x + !~x; }
+    bool operator==(const hash_t& o) const { return get() == o.get(); }
+    bool operator<(const hash_t& o) const { return get() < o.get(); }
+};
+
+static const hash_t C = 1e11 + 3;
+
 #include <bits/extc++.h>
 struct splitmix64_hash {
     // http://xorshift.di.unimi.it/splitmix64.c
@@ -11,9 +34,9 @@ struct splitmix64_hash {
         return x ^ (x >> 31);
     }
 
-    size_t operator()(uint64_t x) const {
+    size_t operator()(hash_t x) const {
         static const uint64_t FIXED_RANDOM = std::chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
+        return splitmix64(x.get() + FIXED_RANDOM);
     }
 };
 
@@ -29,44 +52,35 @@ int main() {
     freopen("in", "r", stdin);
     freopen("out", "w", stdout);
 #endif
-    constexpr int64_t m1 = 1610612741;
-    constexpr int64_t m2 = 1919191969;
-    constexpr int64_t p1 = 307;
-    constexpr int64_t p2 = 409;
     int c, n;
     cin >> c >> n;
     char buf[2001];
-    vector<pair<int, int>> hash_colour(c);
-    for (auto& [h1, h2] : hash_colour) {
+    vector<hash_t> hash_colour(c);
+    for (auto& h : hash_colour) {
         cin >> buf;
         for (auto it = buf; *it; ++it) {
-            h1 = (h1 * p1 + *it) % m1;
-            h2 = (h2 * p2 + *it) % m2;
+            h = h * C + *it;
         }
     }
-    hash_set<int64_t> table;
-    for (int i = 0; i < n; ++i) {
+    hash_set<hash_t> table;
+    for (hash_t h, pw; n--;) {
         cin >> buf;
-        int64_t h1 = 0, h2 = 0, pw1 = 1, pw2 = 1;
+        h = 0, pw = 1;
         for (auto it = buf; *it; ++it) {
-            h1 = (h1 * p1 + *it) % m1;
-            h2 = (h2 * p2 + *it) % m2;
-            pw1 = pw1 * p1 % m1;
-            pw2 = pw2 * p2 % m2;
+            h = h * C + *it;
+            pw = pw * C;
         }
-        for (const auto& [c1, c2] : hash_colour) {
-            int64_t combined1 = (c1 * pw1 + h1) % m1;
-            int64_t combined2 = (c2 * pw2 + h2) % m2;
-            table.insert(combined1 << 32 | combined2);
+        for (auto hc : hash_colour) {
+            table.insert(hc * pw + h);
         }
     }
-    for (cin >> n; n--;) {
+    cin >> n;
+    for (hash_t h; n--;) {
         cin >> buf;
-        int64_t h1 = 0, h2 = 0;
+        h = 0;
         for (auto it = buf; *it; ++it) {
-            h1 = (h1 * p1 + *it) % m1;
-            h2 = (h2 * p2 + *it) % m2;
+            h = h * C + *it;
         }
-        cout << (table.find(h1 << 32 | h2) == table.end() ? "No\n" : "Yes\n");
+        cout << (table.find(h) == table.end() ? "No\n" : "Yes\n");
     }
 }
