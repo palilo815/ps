4c58e72fd5da6633c4c33c7051268578e49ece3f
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-11-12 22:44:16 +0900

    수열과 쿼리 5

--- baekjoon/13000/13547.cpp
+++ baekjoon/13000/13547.cpp
@@ -1,55 +1,60 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
 using namespace std;
 
-int sqrt_N;
-struct _query {
-    int i, s, e;
-    bool operator <(_query& b) {
-        int A = s / sqrt_N, B = b.s / sqrt_N;
-        return A == B ? e < b.e : A < B;
-    }
+struct query_t {
+    int l, r, i;
 };
 
-const int max_N = 100001;
-const int max_A = 1000001;
-
-int A[max_N];
-int ans[max_N];
-int cnt[max_A];
-_query query[max_N];
-
 int main() {
-    cin.tie(0), cout.tie(0);
-    ios::sync_with_stdio(0);
-
-    int N; cin >> N;
-    for (int i = 1; i <= N; ++i)
-        cin >> A[i];
-    int Q; cin >> Q;
-    loop(i, Q) {
-        query[i].i = i;
-        cin >> query[i].s >> query[i].e;
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef palilo
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto& x : a) {
+        cin >> x;
     }
-
-    sqrt_N = sqrt(N);
-    sort(query, query + Q);
-
+    auto cnt = [&]() {
+        auto compressed(a);
+        sort(compressed.begin(), compressed.end());
+        compressed.erase(unique(compressed.begin(), compressed.end()), compressed.end());
+        for (auto& x : a) {
+            x = lower_bound(compressed.begin(), compressed.end(), x) - compressed.begin();
+        }
+        return vector<int>(compressed.size());
+    }();
+    int m;
+    cin >> m;
+    vector<query_t> q(m);
+    for (int i = 0; i < m; ++i) {
+        cin >> q[i].l >> q[i].r, --q[i].l;
+        q[i].i = i;
+    }
+    constexpr int BLOCK = 512;
+    sort(q.begin(), q.end(), [&](const auto& lhs, const auto& rhs) {
+        if (lhs.l / BLOCK != rhs.l / BLOCK) return lhs.l / BLOCK < rhs.l / BLOCK;
+        return (lhs.l / BLOCK) & 1 ? lhs.r > rhs.r : lhs.r < rhs.r;
+    });
     int res = 0;
-    int s = query[0].s, e = query[0].e;
-    for (int i = s; i <= e; ++i)
-        res += (++cnt[A[i]] == 1);
-    ans[query[0].i] = res;
-
-    for (int i = 1; i < Q; ++i) {
-        auto& [I, S, E] = query[i];
-        while (s < S) res -= (--cnt[A[s++]] == 0);
-        while (s > S) res += (++cnt[A[--s]] == 1);
-        while (e < E) res += (++cnt[A[++e]] == 1);
-        while (e > E) res -= (--cnt[A[e--]] == 0);
-        ans[I] = res;
+    const auto add = [&](const int id) {
+        if (++cnt[a[id]] == 1) ++res;
+    };
+    const auto del = [&](const int id) {
+        if (--cnt[a[id]] == 0) --res;
+    };
+    vector<int> ans(m);
+    int pl = q[0].l, pr = q[0].l;
+    for (const auto& [l, r, i] : q) {
+        while (l < pl) add(--pl);
+        while (pr < r) add(pr++);
+        while (pl < l) del(pl++);
+        while (r < pr) del(--pr);
+        ans[i] = res;
     }
-
-    loop(i, Q) cout << ans[i] << '\n';
-    return 0;
+    for (const auto& x : ans) {
+        cout << x << '\n';
+    }
 }
