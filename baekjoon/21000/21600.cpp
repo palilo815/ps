a05f06d5b618190b9a94bee095f385aa43487339
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-11-18 15:35:30 +0900

    계단

---
+++ baekjoon/21000/21600.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <class T>
+bool chmin(T& _old, T _new) { return _old > _new && (_old = _new, true); }
+template <class T>
+bool chmax(T& _old, T _new) { return _old < _new && (_old = _new, true); }
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef palilo
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int n;
+    cin >> n;
+    int ans = 0;
+    for (int x, cur = 0; n--;) {
+        cin >> x;
+        if (x > cur) {
+            chmax(ans, ++cur);
+        } else {
+            cur = x;
+        }
+    }
+    cout << ans;
+}
