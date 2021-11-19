6f8fa49a75d7c37934cfa68746b6bd831c6156e5
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-11-19 15:09:19 +0900

    Friendship Graphs

--- baekjoon/23000/23569.cpp
+++ baekjoon/23000/23569.cpp
@@ -13,19 +13,19 @@ int main() {
     freopen("out", "w", stdout);
 #endif
     constexpr size_t MX_N = 1e3;
-    array<array<bool, MX_N>, MX_N> adj {};
+    array<bitset<MX_N>, MX_N> adj {};
     int n, m;
     cin >> n >> m;
+    for (int i = 0; i < n; ++i) {
+        adj[i]._Unchecked_set(i);
+    }
     for (int u, v; m--;) {
         cin >> u >> v, --u, --v;
-        adj[u][v] = adj[v][u] = true;
-    }
-    for (int i = 0; i < n; ++i) {
-        adj[i][i] = true;
+        adj[u]._Unchecked_set(v);
+        adj[v]._Unchecked_set(u);
     }
     vector<int> stk(n);
     vector<char> colour(n);
-    int alone = 0;
     auto split = [&](int s) -> int {
         int black = 1, white = 0;
         colour[s] = 'b';
@@ -33,7 +33,7 @@ int main() {
         for (int ptr = 1; ptr;) {
             const auto u = stk[--ptr];
             for (int v = 0; v < n; ++v) {
-                if (adj[u][v]) continue;
+                if (adj[u]._Unchecked_test(v)) continue;
                 if (colour[v]) {
                     if (colour[u] == colour[v]) return -1;
                 } else {
@@ -42,26 +42,19 @@ int main() {
                 }
             }
         }
-        if (black == 1 && white == 0) ++alone, --black;
         return abs(black - white);
     };
-    vector<int> item;
+    bitset<2048> dp;
+    dp._Unchecked_set(1024);
     for (int i = 0; i < n; ++i) {
         if (colour[i]) continue;
-        auto res = split(i);
-        if (res == -1) return cout << -1, 0;
-        if (res) item.emplace_back(res);
-    }
-    bitset<1024> dp;
-    dp._Unchecked_set(0);
-    for (const auto& x : item) {
+        const auto x = split(i);
+        if (x == -1) return cout << -1, 0;
         dp = dp << x | dp >> x;
     }
-    int ans = INT_MAX;
-    for (int i = 0; i <= n; ++i) {
-        if (dp._Unchecked_test(i)) {
-            chmin(ans, i < alone ? ((alone - i) & 1) : i - alone);
+    for (int i = 0;; ++i) {
+        if (dp._Unchecked_test(1024 - i) || dp._Unchecked_test(1024 + i)) {
+            return cout << i, 0;
         }
     }
-    cout << ans;
 }
