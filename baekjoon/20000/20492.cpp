3253e3933be5e6808d1529ee2fcf67bfc1fcf945
PaliLo <58646761+PaliLo815@users.noreply.github.com>
2021-11-28 00:16:48 +0900

    세금

---
+++ baekjoon/20000/20492.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef palilo
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int n;
+    cin >> n;
+    cout << n / 100 * 78 << ' '
+         << n / 1000 * 956;
+}
