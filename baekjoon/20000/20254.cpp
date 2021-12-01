822097af1124cdd132667975b4102415c29b9bfc
palilo815 <hyunsoo1397@gmail.com>
2021-12-01 21:33:39 +0900

    Site Score

---
+++ baekjoon/20000/20254.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+int main() {
+    using namespace std;
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << 56 * a + 24 * b + 14 * c + 6 * d;
+}
+
