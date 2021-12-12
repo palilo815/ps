81c9c29ae54b0a824caf838fddb1a11214aea26e
palilo815 <hyunsoo1397@gmail.com>
2021-12-12 21:53:23 +0900

    문자열 압축 해제

---
+++ baekjoon/23000/23746.rs
@@ -0,0 +1,71 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+    ($iter:expr, bytes) => {
+        read_value!($iter, String).bytes().collect::<Vec<u8>>()
+    };
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    use std::io::Write;
+    let out = std::io::stdout();
+    let mut out = std::io::BufWriter::new(out.lock());
+    input! {
+        n: usize,
+        pat: [(chars, char); n],
+        s: chars,
+        l: usize1,
+        r: usize,
+    }
+    let mut conv: [Vec<char>; 26] = Default::default();
+    for (to, from) in pat.iter() {
+        conv[*from as usize - 'A' as usize] = to.clone();
+    }
+    let mut t = vec![];
+    for &x in s.iter() {
+        let mut unzip = conv[x as usize - 'A' as usize].clone();
+        t.append(&mut unzip);
+    }
+    for i in l..r {
+        write!(out, "{}", t[i]).ok();
+    }
+}
