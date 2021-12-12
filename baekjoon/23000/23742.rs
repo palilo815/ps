6b224f27c33d255c8557ae77c7898590e46af0ea
palilo815 <hyunsoo1397@gmail.com>
2021-12-12 17:29:18 +0900

    Player-based Team Distribution

---
+++ baekjoon/23000/23742.rs
@@ -0,0 +1,89 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+    ($iter:expr, bytes) => {
+        read_value!($iter, String).bytes().collect::<Vec<u8>>()
+    };
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+use std::io::Write;
+
+pub trait ChangeMinMax {
+    fn chmin(&mut self, x: Self) -> bool;
+    fn chmax(&mut self, x: Self) -> bool;
+}
+
+impl<T: PartialOrd> ChangeMinMax for T {
+    fn chmin(&mut self, x: Self) -> bool {
+        *self > x && {
+            *self = x;
+            true
+        }
+    }
+    fn chmax(&mut self, x: Self) -> bool {
+        *self < x && {
+            *self = x;
+            true
+        }
+    }
+}
+
+fn main() {
+    let out = std::io::stdout();
+    let mut out = std::io::BufWriter::new(out.lock());
+    input! {
+        n: usize,
+        a: [i64; n],
+    }
+    let mut a = a;
+    a.sort_unstable();
+    a.reverse();
+    let mut solos: i64 = a.iter().sum();
+    let mut group = 0;
+    let mut ans = solos;
+    for i in 0..n {
+        group += a[i];
+        solos -= a[i];
+        ans.chmax(group * (i as i64 + 1) + solos);
+    }
+    writeln!(out, "{}", ans).ok();
+}
