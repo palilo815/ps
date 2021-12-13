582644f9c90d375c57ee68a5fb0b2392a792180f
palilo815 <hyunsoo1397@gmail.com>
2021-12-13 23:54:21 +0900

    회의실 배정 4

---
+++ baekjoon/19000/19623.rs
@@ -0,0 +1,95 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+    ($iter:expr, bytes) => {
+        read_value!($iter, String).bytes().collect::<Vec<u8>>()
+    };
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+pub trait ChangeMinMax {
+    fn chmin(&mut self, x: Self) -> bool;
+    fn chmax(&mut self, x: Self) -> bool;
+}
+
+impl<T: PartialOrd> ChangeMinMax for T {
+    fn chmin(&mut self, x: Self) -> bool {
+        *self > x && {
+            *self = x;
+            true
+        }
+    }
+    fn chmax(&mut self, x: Self) -> bool {
+        *self < x && {
+            *self = x;
+            true
+        }
+    }
+}
+
+fn main() {
+    use std::io::Write;
+    let out = std::io::stdout();
+    let mut out = std::io::BufWriter::new(out.lock());
+    input! {
+        n: usize,
+        a: [(u32, u32, u32); n],
+    }
+    let mut vals = a.iter().map(|x| x.0).collect::<Vec<_>>();
+    vals.append(&mut a.iter().map(|x| x.1).collect::<Vec<_>>());
+    vals.sort_unstable();
+    let n = n << 1;
+    let mut nxt = vec![(0, 0); n];
+    for &(x, y, z) in a.iter() {
+        let x = vals.binary_search(&x).unwrap();
+        let y = vals.binary_search(&y).unwrap();
+        nxt[x] = (y, z);
+    }
+    let mut dp = vec![0; n];
+    let mut best = 0;
+    for i in 0..n {
+        best.chmax(dp[i]);
+        if nxt[i].0 != 0 {
+            dp[nxt[i].0] = best + nxt[i].1;
+        }
+    }
+    writeln!(out, "{}", best).ok();
+}
