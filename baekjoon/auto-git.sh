e69f93ffa4eb9ce2d80fdb05d11a4b3dd5e7151b
palilo815 <hyunsoo1397@gmail.com>
2021-12-14 15:17:01 +0900

    auto git tool

---
+++ baekjoon/auto-git.sh
@@ -0,0 +1,14 @@
+if [ $# -ne 3 ]; then
+    echo "Usage --- {file name} {problem #} {'problem name'}"
+    echo "e.g. ---- a.rs 1000 'A + B'"
+    exit 1
+fi
+
+FILE=("$1")
+EXTENSION=${FILE##*.} # rs, cpp, py, txt ...
+
+let D=$2/1000*1000
+
+cp $FILE $D/$2.$EXTENSION
+gaa
+gcmsg '$3'
