fc736746ecdce5d628dd9ed1620ff1565e5fa4a1
palilo815 <hyunsoo1397@gmail.com>
2021-12-18 12:30:48 +0900

    중앙값 제거

---
+++ baekjoon/23000/23758.rs
@@ -0,0 +1,53 @@
+use std::io::Write;
+
+#[allow(dead_code)]
+mod scanner {
+    use std::str::FromStr;
+    pub struct Scanner<'a> {
+        it: std::str::SplitWhitespace<'a>,
+    }
+    impl<'a> Scanner<'a> {
+        pub fn new(s: &'a String) -> Scanner<'a> {
+            Scanner {
+                it: s.split_whitespace(),
+            }
+        }
+        pub fn next<T: FromStr>(&mut self) -> T {
+            self.it.next().unwrap().parse::<T>().ok().unwrap()
+        }
+        pub fn next_bytes(&mut self) -> Vec<u8> {
+            self.it.next().unwrap().bytes().collect()
+        }
+        pub fn next_chars(&mut self) -> Vec<char> {
+            self.it.next().unwrap().chars().collect()
+        }
+        pub fn next_vec<T: FromStr>(&mut self, len: usize) -> Vec<T> {
+            (0..len).map(|_| self.next()).collect()
+        }
+    }
+}
+
+fn main() {
+    use std::io::Read;
+    let mut s = String::new();
+    std::io::stdin().read_to_string(&mut s).unwrap();
+    let mut sc = scanner::Scanner::new(&s);
+    let out = std::io::stdout();
+    let mut out = std::io::BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+fn run<W: Write>(sc: &mut scanner::Scanner, out: &mut std::io::BufWriter<W>) {
+    let n = sc.next::<usize>();
+    let mut cnt = [0; 32];
+    for _ in 0..n {
+        cnt[31 ^ sc.next::<u32>().leading_zeros() as usize] += 1;
+    }
+    let mut n = (n + 1) / 2;
+    let mut ans = 1;
+    for (i, &c) in cnt.iter().enumerate() {
+        ans += n.min(c) * i;
+        n -= n.min(c);
+    }
+    writeln!(out, "{}", ans).ok();
+}
