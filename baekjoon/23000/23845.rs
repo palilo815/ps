fe2c6d064dabbe11dce3f1fecd062013e02f3239
palilo815 <hyunsoo1397@gmail.com>
2021-12-19 16:07:40 +0900

    마트료시카

---
+++ baekjoon/23000/23845.rs
@@ -0,0 +1,55 @@
+use std::io::Write;
+
+#[allow(dead_code)]
+mod scanner {
+    use std::str::FromStr;
+    pub struct Scanner<'a> {
+        it: std::str::SplitWhitespace<'a>,
+    }
+    impl<'a> Scanner<'a> {
+        pub fn new(s: &'a String) -> Scanner<'a> {
+            Scanner {
+                it: s.split_whitespace(),
+            }
+        }
+        pub fn next<T: FromStr>(&mut self) -> T {
+            self.it.next().unwrap().parse::<T>().ok().unwrap()
+        }
+        pub fn next_bytes(&mut self) -> Vec<u8> {
+            self.it.next().unwrap().bytes().collect()
+        }
+        pub fn next_chars(&mut self) -> Vec<char> {
+            self.it.next().unwrap().chars().collect()
+        }
+        pub fn next_vec<T: FromStr>(&mut self, len: usize) -> Vec<T> {
+            (0..len).map(|_| self.next()).collect()
+        }
+    }
+}
+
+fn main() {
+    use std::io::Read;
+    let mut s = String::new();
+    std::io::stdin().read_to_string(&mut s).unwrap();
+    let mut sc = scanner::Scanner::new(&s);
+    let out = std::io::stdout();
+    let mut out = std::io::BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+fn run<W: Write>(sc: &mut scanner::Scanner, out: &mut std::io::BufWriter<W>) {
+    let n = sc.next::<usize>();
+    let mut cnt = [0; 100_002]; // cnt[1e5 + 1] as sentinel
+    for _ in 0..n {
+        cnt[sc.next::<usize>()] += 1;
+    }
+    let mut stk = Vec::with_capacity(n);
+    let mut ans = 0;
+    for (i, &x) in cnt.iter().enumerate() {
+        while stk.len() > x {
+            ans += (i - 1) * (i - stk.pop().unwrap());
+        }
+        stk.append(&mut vec![i; x - stk.len()]);
+    }
+    writeln!(out, "{}", ans).ok();
+}
