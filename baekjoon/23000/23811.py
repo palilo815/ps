2e8a6be4d4d44d94099c097f3b31fb1755cb95af
palilo815 <hyunsoo1397@gmail.com>
2021-12-26 21:47:47 +0900

    골뱅이 찍기 - ㅌ

---
+++ baekjoon/23000/23811.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(input())
+    print(('@' * 5 * n + '\n') * n, end='')
+    print(('@' * n + '\n') * n, end='')
+    print(('@' * 5 * n + '\n') * n, end='')
+    print(('@' * n + '\n') * n, end='')
+    print(('@' * 5 * n + '\n') * n, end='')
