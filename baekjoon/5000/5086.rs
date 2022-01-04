9dbfd23f0d9537abb6e95c75a122dea6285edd60
palilo815 <hyunsoo1397@gmail.com>
2022-01-04 19:31:28 +0900

    Factors And Multiples

---
+++ baekjoon/5000/5086.rs
@@ -0,0 +1,60 @@
+#[allow(dead_code)]
+mod scanner {
+    use std::str::FromStr;
+    pub struct Scanner<'a> {
+        it: std::str::SplitWhitespace<'a>,
+    }
+    impl<'a> Scanner<'a> {
+        pub fn new(s: &'a String) -> Scanner<'a> {
+            Scanner {
+                it: s.split_whitespace(),
+            }
+        }
+        pub fn next<T: FromStr>(&mut self) -> T {
+            self.it.next().unwrap().parse::<T>().ok().unwrap()
+        }
+        pub fn next_bytes(&mut self) -> Vec<u8> {
+            self.it.next().unwrap().bytes().collect()
+        }
+        pub fn next_chars(&mut self) -> Vec<char> {
+            self.it.next().unwrap().chars().collect()
+        }
+        pub fn next_vec<T: FromStr>(&mut self, len: usize) -> Vec<T> {
+            (0..len).map(|_| self.next()).collect()
+        }
+    }
+}
+
+use std::io::Write;
+
+fn main() {
+    use std::io::Read;
+    let mut s = String::new();
+    std::io::stdin().read_to_string(&mut s).unwrap();
+    let mut sc = scanner::Scanner::new(&s);
+    let out = std::io::stdout();
+    let mut out = std::io::BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+fn run<W: Write>(sc: &mut scanner::Scanner, out: &mut std::io::BufWriter<W>) {
+    loop {
+        let a = sc.next::<u32>();
+        let b = sc.next::<u32>();
+        if a == 0 && b == 0 {
+            break;
+        }
+        writeln!(
+            out,
+            "{}",
+            if a % b == 0 {
+                "multiple"
+            } else if b % a == 0 {
+                "factor"
+            } else {
+                "neither"
+            }
+        )
+        .ok();
+    }
+}
