9387d7b42981790a62f0c13a3fb4fbcdc0c300f8
palilo815 <hyunsoo1397@gmail.com>
2022-01-10 17:36:10 +0900

    Lonely Photo

---
+++ baekjoon/23000/23878.rs
@@ -0,0 +1,70 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+    ($iter:expr, bytes) => {
+        read_value!($iter, String).bytes().collect::<Vec<u8>>()
+    };
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+
+fn main() {
+    use std::io::Write;
+    let out = std::io::stdout();
+    let mut out = std::io::BufWriter::new(out.lock());
+    input! {
+        n: i32,
+        s: bytes,
+    }
+    let mut ans = 0_i64;
+    let mut g = [-1, -1];
+    let mut h = [-1, -1];
+    for i in 0..n {
+        if s[i as usize] == b'G' {
+            g[0] = std::mem::replace(&mut g[1], i);
+        } else {
+            h[0] = std::mem::replace(&mut h[1], i);
+        }
+        ans += (0.max(g[1].min(i - 2) - g[0])) as i64;
+        ans += (0.max(h[1].min(i - 2) - h[0])) as i64;
+    }
+    writeln!(out, "{}", ans).ok();
+}
