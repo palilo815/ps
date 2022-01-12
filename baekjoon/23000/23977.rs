aa9c3471a7b0dc9149b58b4a2d2156c8fac701e5
palilo815 <hyunsoo1397@gmail.com>
2022-01-13 00:03:33 +0900

    To Find Password

---
+++ baekjoon/23000/23977.rs
@@ -0,0 +1,68 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+    ($iter:expr, bytes) => {
+        read_value!($iter, String).bytes().collect::<Vec<u8>>()
+    };
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    use std::io::Write;
+    let out = std::io::stdout();
+    let mut out = std::io::BufWriter::new(out.lock());
+    input! {
+        k: i64,
+        n: usize,
+        a: [i64; n],
+    }
+    let ans = a.iter().fold(a[0], |a, &b| {
+        let c = a * b;
+        let mut a = a;
+        let mut b = b;
+        while a != 0 {
+            std::mem::swap(&mut a, &mut b);
+            a %= b;
+        }
+        c / b
+    }) - k;
+    writeln!(out, "{}", ans).ok();
+}
