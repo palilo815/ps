edd92c7707f7a5414c1835b3da1b0eec8aff4bad
palilo815 <hyunsoo1397@gmail.com>
2022-01-29 00:08:12 +0900

    remove all `pub`s, impl `iter_roots`

--- lib/data/dsu.rs
+++ lib/data/dsu.rs
@@ -1,17 +1,21 @@
 /**
-* @date     2022-01-21
+* @date     2022-01-29
 * @author   palilo
 * @brief    disjoint-set-union (a.k.a. union-find)
 */
-pub struct DisjointSet {
+struct DisjointSet {
     p: Vec<i32>,
 }
 
+#[allow(dead_code)]
 impl DisjointSet {
-    pub fn new(n: usize) -> Self {
+    fn new(n: usize) -> Self {
         Self { p: vec![-1; n] }
     }
-    pub fn find(&mut self, u: usize) -> usize {
+    fn clear(&mut self) {
+        self.p.fill(-1);
+    }
+    fn find(&mut self, u: usize) -> usize {
         if self.p[u] < 0 {
             u
         } else {
@@ -20,7 +24,7 @@ impl DisjointSet {
             rt
         }
     }
-    pub fn unite(&mut self, u: usize, v: usize) -> bool {
+    fn unite(&mut self, u: usize, v: usize) -> bool {
         let mut u = self.find(u);
         let mut v = self.find(v);
         if u == v {
@@ -33,17 +37,23 @@ impl DisjointSet {
         self.p[v] = u as i32;
         true
     }
-    pub fn same(&mut self, u: usize, v: usize) -> bool {
+    fn same(&mut self, u: usize, v: usize) -> bool {
         self.find(u) == self.find(v)
     }
-    pub fn size_of(&mut self, u: usize) -> usize {
+    fn size_of(&mut self, u: usize) -> usize {
         let root = self.find(u);
         (-self.p[root]) as usize
     }
-    pub fn num_components(&self) -> usize {
+    fn num_components(&self) -> usize {
         self.p.iter().filter(|x| x.is_negative()).count()
     }
-    pub fn clear(&mut self) {
-        self.p.fill(-1);
+    fn iter_roots(&self) -> impl Iterator<Item = usize> + '_ {
+        self.p
+            .iter()
+            .enumerate()
+            .filter_map(|(i, x)| match x.is_negative() {
+                true => Some(i),
+                false => None,
+            })
     }
 }
