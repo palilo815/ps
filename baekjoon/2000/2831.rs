a48dd79bfedf90e2d7f07faba5c8eacbc25cfd38
palilo815 <hyunsoo1397@gmail.com>
2022-02-03 16:53:16 +0900

    PLES

---
+++ baekjoon/2000/2831.rs
@@ -0,0 +1,76 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+    ($iter:expr, bytes) => {
+        read_value!($iter, String).bytes().collect::<Vec<u8>>()
+    };
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    use std::io::Write;
+    let out = std::io::stdout();
+    let mut out = std::io::BufWriter::new(out.lock());
+    input! {
+        n: usize,
+        a: [i32; n],
+        b: [i32; n],
+    }
+    let mut a = a;
+    let mut b = b;
+    a.sort_unstable();
+    b.sort_unstable();
+    let fence_a = a.partition_point(|x| x.is_negative());
+    let fence_b = b.partition_point(|x| x.is_negative());
+    let mut i = 0;
+    for &girl in b[fence_b..].iter().rev() {
+        if i != fence_a && a[i] + girl < 0 {
+            i += 1;
+        }
+    }
+    let mut j = 0;
+    for &boy in a[fence_a..].iter().rev() {
+        if j != fence_b && b[j] + boy < 0 {
+            j += 1;
+        }
+    }
+    writeln!(out, "{}", i + j).ok();
+}
