a8c8d2563faf70f2c8187263de756259d040b5af
palilo815 <hyunsoo1397@gmail.com>
2022-02-03 17:37:45 +0900

    가장 쉬운 문제를 찾는 문제

---
+++ baekjoon/22000/22966.rs
@@ -0,0 +1,72 @@
+use std::io::Write;
+
+#[allow(dead_code)]
+mod scanner {
+    use std::str::FromStr;
+    pub struct Scanner<'a> {
+        it: std::str::SplitWhitespace<'a>,
+    }
+    impl<'a> Scanner<'a> {
+        pub fn new(s: &'a str) -> Scanner<'a> {
+            Scanner {
+                it: s.split_whitespace(),
+            }
+        }
+        pub fn next<T: FromStr>(&mut self) -> T {
+            self.it.next().unwrap().parse::<T>().ok().unwrap()
+        }
+        pub fn next_bytes(&mut self) -> Vec<u8> {
+            self.it.next().unwrap().bytes().collect()
+        }
+        pub fn next_chars(&mut self) -> Vec<char> {
+            self.it.next().unwrap().chars().collect()
+        }
+        pub fn next_vec<T: FromStr>(&mut self, len: usize) -> Vec<T> {
+            (0..len).map(|_| self.next()).collect()
+        }
+    }
+}
+
+fn main() {
+    use std::io::Read;
+    let mut s = String::new();
+    std::io::stdin().read_to_string(&mut s).unwrap();
+    let mut sc = scanner::Scanner::new(&s);
+    let out = std::io::stdout();
+    let mut out = std::io::BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+trait ChMinMax {
+    fn chmin(&mut self, x: Self) -> bool;
+    fn chmax(&mut self, x: Self) -> bool;
+}
+
+impl<T: PartialOrd> ChMinMax for T {
+    fn chmin(&mut self, x: Self) -> bool {
+        *self > x && {
+            *self = x;
+            true
+        }
+    }
+    fn chmax(&mut self, x: Self) -> bool {
+        *self < x && {
+            *self = x;
+            true
+        }
+    }
+}
+
+fn run<W: Write>(sc: &mut scanner::Scanner, out: &mut std::io::BufWriter<W>) {
+    let n = sc.next::<u32>();
+    let mut ans = vec![];
+    let mut min = u32::MAX;
+    for _ in 0..n {
+        let s = sc.next_bytes();
+        let x = sc.next::<u32>();
+        if min.chmin(x) {
+            ans = s;
+        }
+    }
+    writeln!(out, "{}", String::from_utf8(ans).unwrap()).ok();
+}
