25b74925a47accafa00227ee44e8c37024de405a
palilo815 <hyunsoo1397@gmail.com>
2022-02-06 16:05:04 +0900

    Showstopper

--- baekjoon/1000/1637.cpp
+++ baekjoon/1000/1637.cpp
@@ -1,32 +1,45 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(ui i=0;i<n;++i)
-using namespace std;
-using ui = unsigned;
 
-const ui M = 20000;
-
-ui N, A[M], B[M], C[M];
-
-ui solve(ui m) {
-    ui ret = 0;
-    loop(i, N) if (m >= A[i])
-        ret += (min(C[i], m) - A[i]) / B[i] + 1;
-    return ret;
-}
 int main() {
-    cin.tie(0), cout.tie(0);
-    ios::sync_with_stdio(0);
-
-    cin >> N;
-    loop(i, N) cin >> A[i] >> C[i] >> B[i];
-
-    ui l = 0, r = INT_MIN;
-    while (l < r) {
-        ui m = (l + r) >> 1;
-        solve(m) & 1 ? (r = m) : (l = m + 1);
+    using namespace std;
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef palilo
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    struct acb {
+        unsigned a, c, b;
+    };
+    int n;
+    cin >> n;
+    vector<acb> vt(n);
+    for (auto& [a, c, b] : vt) {
+        cin >> a >> c >> b;
     }
-
-    if (l <= INT_MAX) cout << l << ' ' << solve(l) - solve(l - 1);
-    else cout << "NOTHING";
-    return 0;
+    auto less_or_equal = [&]<typename T>(T mid) {
+        // 홀짝만 판별하면 되므로 오버플로우 생겨도 상관 x
+        return accumulate(vt.begin(), vt.end(), T(0), [&](const auto& acc, const auto& x) {
+            return mid < x.a ? acc : (acc + (min(mid, x.c) - x.a) / x.b + 1);
+        });
+    };
+    auto parametric = [&]<typename T>(T lo, T hi) {
+        // first true
+        while (lo != hi) {
+            auto mid = lo + (hi - lo) / 2;
+            less_or_equal(mid)& 1 ? hi = mid : lo = mid + 1;
+        }
+        // // last true
+        // while (lo != hi) {
+        //     auto mid = hi - (hi - lo) / 2;
+        //     f(mid) ? lo = mid : hi = mid - 1;
+        // }
+        return lo;
+    };
+    // INT_MAX = (1 << 31) - 1
+    const auto res = parametric(0u, 1u << 31);
+    if (res == (1u << 31)) {
+        cout << "NOTHING";
+    } else {
+        cout << res << ' ' << less_or_equal(res) - less_or_equal(res - 1);
+    }
 }
