7491ce0460fdaa5146777a7b7b159b12ab369e8a
palilo815 <hyunsoo1397@gmail.com>
2022-02-06 16:02:46 +0900

    풍선 공장

--- baekjoon/15000/15810.cpp
+++ baekjoon/15000/15810.cpp
@@ -1,31 +1,37 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
-using namespace std;
-using ll = long long;
 
-const int mxN = 1e6;
-
-int N, a[mxN];
-ll M;
-
-bool enough(ll m) {
-    ll cnt = 0;
-    loop(i, N) if ((cnt += m / a[i]) >= M)
-        return true;
-    return false;
-}
 int main() {
-    cin.tie(0), cout.tie(0);
-    ios::sync_with_stdio(0);
-
-    cin >> N >> M;
-    loop(i, N) cin >> a[i];
-
-    ll lo = 1, hi = 1e12;
-    while (lo ^ hi) {
-        ll m = (lo + hi) >> 1;
-        enough(m) ? (hi = m) : (lo = m + 1);
+    using namespace std;
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef palilo
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+    int n;
+    int64_t m;
+    cin >> n >> m;
+    vector<int> a(n);
+    for (auto& x : a) {
+        cin >> x;
     }
-    cout << lo;
-    return 0;
+    auto parametric = [&]<typename T>(T lo, T hi) {
+        auto f = [&](T mid) -> bool {
+            int64_t balloon = 0;
+            return ranges::any_of(a, [&](const auto& t) {
+                return (balloon += mid / t) >= m;
+            });
+        };
+        // first true
+        while (lo != hi) {
+            auto mid = lo + (hi - lo) / 2;
+            f(mid) ? hi = mid : lo = mid + 1;
+        }
+        // // last true
+        // while (lo != hi) {
+        //     auto mid = hi - (hi - lo) / 2;
+        //     f(mid) ? lo = mid : hi = mid - 1;
+        // }
+        return lo;
+    };
+    cout << parametric(int64_t(1), int64_t(1e12));
 }
