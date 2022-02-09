76bdb5894d3bb2310fa232e013cf4334b7d1f4b2
palilo815 <hyunsoo1397@gmail.com>
2022-02-09 16:06:52 +0900

    알고리즘 수업 - 너비 우선 탐색 2

---
+++ baekjoon/24000/24445.rs
@@ -0,0 +1,71 @@
+use std::io::Write;
+
+#[allow(dead_code)]
+mod scanner {
+    use std::str::FromStr;
+    pub struct Scanner<'a> {
+        it: std::str::SplitWhitespace<'a>,
+    }
+    impl<'a> Scanner<'a> {
+        pub fn new(s: &'a str) -> Scanner<'a> {
+            Scanner {
+                it: s.split_whitespace(),
+            }
+        }
+        pub fn next<T: FromStr>(&mut self) -> T {
+            self.it.next().unwrap().parse::<T>().ok().unwrap()
+        }
+        pub fn next_bytes(&mut self) -> Vec<u8> {
+            self.it.next().unwrap().bytes().collect()
+        }
+        pub fn next_chars(&mut self) -> Vec<char> {
+            self.it.next().unwrap().chars().collect()
+        }
+        pub fn next_vec<T: FromStr>(&mut self, len: usize) -> Vec<T> {
+            (0..len).map(|_| self.next()).collect()
+        }
+    }
+}
+
+fn main() {
+    use std::io::Read;
+    let mut s = String::new();
+    std::io::stdin().read_to_string(&mut s).unwrap();
+    let mut sc = scanner::Scanner::new(&s);
+    let out = std::io::stdout();
+    let mut out = std::io::BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+fn run<W: Write>(sc: &mut scanner::Scanner, out: &mut std::io::BufWriter<W>) {
+    let n = sc.next::<usize>();
+    let m = sc.next::<usize>();
+    let s = sc.next::<usize>() - 1;
+    let mut adj = vec![vec![]; n];
+    for _ in 0..m {
+        let u = sc.next::<usize>() - 1;
+        let v = sc.next::<usize>() - 1;
+        adj[u].push(v);
+        adj[v].push(u);
+    }
+    for x in adj.iter_mut() {
+        x.sort_unstable();
+    }
+    let mut dq = std::collections::VecDeque::new();
+    dq.push_back(s);
+    let mut tin = vec![0; n];
+    let mut timer = 0;
+    while let Some(u) = dq.pop_front() {
+        timer += 1;
+        tin[u] = timer;
+        for &v in adj[u].iter().rev() {
+            if tin[v] == 0 {
+                tin[v] = tin[u] + 1;
+                dq.push_back(v);
+            }
+        }
+    }
+    for x in tin {
+        writeln!(out, "{}", x).ok();
+    }
+}
