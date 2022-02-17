585dcb6899b28609e4ecca74a8ea83561f2d8d5a
palilo815 <hyunsoo1397@gmail.com>
2022-02-17 18:32:29 +0900

    가장 긴 증가하는 부분 수열 3

---
+++ baekjoon/12000/12738.rs
@@ -0,0 +1,55 @@
+use std::io::Write;
+
+#[allow(dead_code)]
+mod scanner {
+    use std::str::FromStr;
+    pub struct Scanner<'a> {
+        it: std::str::SplitWhitespace<'a>,
+    }
+    impl<'a> Scanner<'a> {
+        pub fn new(s: &'a str) -> Scanner<'a> {
+            Scanner {
+                it: s.split_whitespace(),
+            }
+        }
+        pub fn next<T: FromStr>(&mut self) -> T {
+            self.it.next().unwrap().parse::<T>().ok().unwrap()
+        }
+        pub fn next_bytes(&mut self) -> Vec<u8> {
+            self.it.next().unwrap().bytes().collect()
+        }
+        pub fn next_chars(&mut self) -> Vec<char> {
+            self.it.next().unwrap().chars().collect()
+        }
+        pub fn next_vec<T: FromStr>(&mut self, len: usize) -> Vec<T> {
+            (0..len).map(|_| self.next()).collect()
+        }
+    }
+}
+
+fn main() {
+    use std::io::Read;
+    let mut s = String::new();
+    std::io::stdin().read_to_string(&mut s).unwrap();
+    let mut sc = scanner::Scanner::new(&s);
+    let out = std::io::stdout();
+    let mut out = std::io::BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+fn run<W: Write>(sc: &mut scanner::Scanner, out: &mut std::io::BufWriter<W>) {
+    let n = sc.next::<usize>();
+    const INF: i32 = 0x3f3f3f3f;
+    let mut dp = vec![INF; n];
+    for _ in 0..n {
+        let x = sc.next::<i32>();
+        let i = dp.partition_point(|&elem| elem < x);
+        dp[i] = x;
+    }
+    writeln!(
+        out,
+        "{}",
+        dp.into_iter().position(|x| x == INF).unwrap_or(n)
+    )
+    .ok();
+}
