299d5a82f1cddbd8f67efac6483e4518f1495c44
palilo815 <hyunsoo1397@gmail.com>
2022-02-23 20:52:46 +0900

    알고리즘 수업 - 깊이 우선 탐색 3

---
+++ baekjoon/24000/24481.rs
@@ -0,0 +1,66 @@
+use std::io::Write;
+
+#[allow(dead_code)]
+mod scanner {
+    use std::str::FromStr;
+    pub struct Scanner<'a> {
+        it: std::str::SplitWhitespace<'a>,
+    }
+    impl<'a> Scanner<'a> {
+        pub fn new(s: &'a str) -> Scanner<'a> {
+            Scanner {
+                it: s.split_whitespace(),
+            }
+        }
+        pub fn next<T: FromStr>(&mut self) -> T {
+            self.it.next().unwrap().parse::<T>().ok().unwrap()
+        }
+        pub fn next_bytes(&mut self) -> Vec<u8> {
+            self.it.next().unwrap().bytes().collect()
+        }
+        pub fn next_chars(&mut self) -> Vec<char> {
+            self.it.next().unwrap().chars().collect()
+        }
+        pub fn next_vec<T: FromStr>(&mut self, len: usize) -> Vec<T> {
+            (0..len).map(|_| self.next()).collect()
+        }
+    }
+}
+
+fn main() {
+    use std::io::Read;
+    let mut s = String::new();
+    std::io::stdin().read_to_string(&mut s).unwrap();
+    let mut sc = scanner::Scanner::new(&s);
+    let out = std::io::stdout();
+    let mut out = std::io::BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+fn run<W: Write>(sc: &mut scanner::Scanner, out: &mut std::io::BufWriter<W>) {
+    let n = sc.next::<usize>();
+    let m = sc.next::<usize>();
+    let s = sc.next::<usize>() - 1;
+    let mut adj = vec![vec![]; n];
+    for _ in 0..m {
+        let u = sc.next::<usize>() - 1;
+        let v = sc.next::<usize>() - 1;
+        adj[u].push(v);
+        adj[v].push(u);
+    }
+    adj.iter_mut().for_each(|x| x.sort_unstable());
+    fn dfs(adj: &[Vec<usize>], dep: &mut [i32], u: usize) {
+        for &v in adj[u].iter() {
+            if dep[v] == -1 {
+                dep[v] = dep[u] + 1;
+                dfs(adj, dep, v);
+            }
+        }
+    }
+    let mut dep = vec![-1; n];
+    dep[s] = 0;
+    dfs(&adj, &mut dep, s);
+    for x in dep {
+        writeln!(out, "{}", x).ok();
+    }
+}
