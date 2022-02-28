19c00f6feb48a205eb78510f11d8fc851fbb13f0
palilo815 <hyunsoo1397@gmail.com>
2022-02-28 09:37:12 +0900

    튜터-튜티 관계의 수

---
+++ baekjoon/24000/24542.rs
@@ -0,0 +1,92 @@
+use std::io::Write;
+
+#[allow(dead_code)]
+mod scanner {
+    use std::str::FromStr;
+    pub struct Scanner<'a> {
+        it: std::str::SplitWhitespace<'a>,
+    }
+    impl<'a> Scanner<'a> {
+        pub fn new(s: &'a str) -> Scanner<'a> {
+            Scanner {
+                it: s.split_whitespace(),
+            }
+        }
+        pub fn next<T: FromStr>(&mut self) -> T {
+            self.it.next().unwrap().parse::<T>().ok().unwrap()
+        }
+        pub fn next_bytes(&mut self) -> Vec<u8> {
+            self.it.next().unwrap().bytes().collect()
+        }
+        pub fn next_chars(&mut self) -> Vec<char> {
+            self.it.next().unwrap().chars().collect()
+        }
+        pub fn next_vec<T: FromStr>(&mut self, len: usize) -> Vec<T> {
+            (0..len).map(|_| self.next()).collect()
+        }
+    }
+}
+
+fn main() {
+    use std::io::Read;
+    let mut s = String::new();
+    std::io::stdin().read_to_string(&mut s).unwrap();
+    let mut sc = scanner::Scanner::new(&s);
+    let out = std::io::stdout();
+    let mut out = std::io::BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+struct DisjointSet {
+    p: Vec<i32>,
+}
+
+#[allow(dead_code)]
+impl DisjointSet {
+    fn new(n: usize) -> Self {
+        Self { p: vec![-1; n] }
+    }
+    fn find(&mut self, u: usize) -> usize {
+        if self.p[u] < 0 {
+            u
+        } else {
+            let rt = self.find(self.p[u] as usize);
+            self.p[u] = rt as i32;
+            rt
+        }
+    }
+    fn unite(&mut self, u: usize, v: usize) -> bool {
+        let mut u = self.find(u);
+        let mut v = self.find(v);
+        if u == v {
+            return false;
+        }
+        if self.p[u] > self.p[v] {
+            std::mem::swap(&mut u, &mut v);
+        }
+        self.p[u] += self.p[v];
+        self.p[v] = u as i32;
+        true
+    }
+    fn iter_size(&self) -> impl Iterator<Item = i32> + '_ {
+        self.p.iter().filter_map(|x| match x.is_negative() {
+            true => Some(-x),
+            false => None,
+        })
+    }
+}
+
+fn run<W: Write>(sc: &mut scanner::Scanner, out: &mut std::io::BufWriter<W>) {
+    let n = sc.next::<usize>();
+    let m = sc.next::<usize>();
+    let mut dsu = DisjointSet::new(n);
+    for _ in 0..m {
+        dsu.unite(sc.next::<usize>() - 1, sc.next::<usize>() - 1);
+    }
+    const MOD: u64 = 1_000_000_007;
+    let mut ans = 1;
+    for x in dsu.iter_size() {
+        ans = ans * x as u64 % MOD;
+    }
+    writeln!(out, "{}", ans).ok();
+}
