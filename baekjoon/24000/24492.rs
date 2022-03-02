b46b1a587a2f822c94b284e2cf1d1715586e3338
palilo815 <hyunsoo1397@gmail.com>
2022-03-02 12:18:38 +0900

    Cow Frisbee

---
+++ baekjoon/24000/24492.rs
@@ -0,0 +1,68 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+    ($iter:expr, $var:ident:$t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ($($t:tt), *)) => {
+        ($(read_value!($iter, $t)), *)
+    };
+    ($iter:expr, [$t:tt; $len:expr]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+    ($iter:expr, bytes) => {
+        read_value!($iter, String).bytes().collect::<Vec<u8>>()
+    };
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    use std::io::Write;
+    let out = std::io::stdout();
+    let mut out = std::io::BufWriter::new(out.lock());
+    input! {
+        n: usize,
+        h: [u32; n],
+    }
+    let mut stk = vec![];
+    let mut ans = 0;
+    for (i, &x) in h.iter().enumerate() {
+        while !stk.is_empty() && h[*stk.last().unwrap()] < x {
+            ans += i - stk.pop().unwrap() + 1;
+        }
+        if let Some(j) = stk.last() {
+            ans += i - j + 1;
+        }
+        stk.push(i);
+    }
+    writeln!(out, "{}", ans).ok();
+}
