c2fc0764aa68bbe7fda61fb843ecc88f2a06452c
palilo815 <hyunsoo1397@gmail.com>
2022-03-05 11:32:39 +0900

    Wifi Setup

---
+++ baekjoon/5000/5864.rs
@@ -0,0 +1,74 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+    ($iter:expr, $var:ident:$t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ($($t:tt), *)) => {
+        ($(read_value!($iter, $t)), *)
+    };
+    ($iter:expr, [$t:tt; $len:expr]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+    ($iter:expr, bytes) => {
+        read_value!($iter, String).bytes().collect::<Vec<u8>>()
+    };
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    use std::io::Write;
+    let out = std::io::stdout();
+    let mut out = std::io::BufWriter::new(out.lock());
+    input! {
+        n: usize,
+        a: u32,
+        b: u32,
+        x: [u32; n],
+    }
+    let a = a << 1;
+    let mut x = x;
+    x.iter_mut().for_each(|x| *x <<= 1);
+    x.sort_unstable();
+    const INF: u32 = 0x3f3f3f3f;
+    let mut dp = vec![INF; n + 1];
+    dp[0] = 0;
+    for i in 0..n {
+        for j in i..n {
+            dp[j + 1] = dp[j + 1].min(dp[i] + a + b * (x[j] - x[i]) / 2);
+        }
+    }
+    write!(out, "{}", dp[n] >> 1).ok();
+    if dp[n] & 1 == 1 {
+        write!(out, ".5").ok();
+    }
+}
