0321a93c62c73ce94102d00d031e408a9ec86cb6
palilo815 <hyunsoo1397@gmail.com>
2022-03-10 22:59:02 +0900

    LOPOV

---
+++ baekjoon/1000/1202.rs
@@ -0,0 +1,75 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+    ($iter:expr, $var:ident:$t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ($($t:tt), *)) => {
+        ($(read_value!($iter, $t)), *)
+    };
+    ($iter:expr, [$t:tt; $len:expr]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+    ($iter:expr, bytes) => {
+        read_value!($iter, String).bytes().collect::<Vec<u8>>()
+    };
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    use std::io::Write;
+    let out = std::io::stdout();
+    let mut out = std::io::BufWriter::new(out.lock());
+    input! {
+        n: usize,
+        k: usize,
+        jews: [(u32, u32); n],
+        bags: [u32; k],
+    }
+    let mut jews = jews;
+    let mut bags = bags;
+    jews.sort_unstable_by(|lhs, rhs| lhs.0.cmp(&rhs.0));
+    bags.sort_unstable();
+    let mut pq = std::collections::BinaryHeap::new();
+    let mut i = 0;
+    let mut ans = 0;
+    for c in bags.into_iter() {
+        while i != n && jews[i].0 <= c {
+            pq.push(jews[i].1);
+            i += 1;
+        }
+        if let Some(value) = pq.pop() {
+            ans += value as u64;
+        }
+    }
+    writeln!(out, "{}", ans).ok();
+}
