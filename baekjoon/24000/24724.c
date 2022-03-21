a2eb5bdb02df0f3382f1dde3185d4c62257bb049
palilo <58646761+palilo815@users.noreply.github.com>
2022-03-21 22:35:55 +0900

    현대모비스와 함께하는 부품 관리

--- baekjoon/24000/24724.c
+++ baekjoon/24000/24724.c
@@ -2,9 +2,9 @@
 
 int main() {
     int t;
-	scanf("%d", &t);
-	for (int i = 1; i <= t; ++i) {
+    scanf("%d", &t);
+    for (int i = 1; i <= t; ++i) {
         printf("Material Management %d\n", i);
-		puts("Classification ---- End!");
-	}
+        puts("Classification ---- End!");
+    }
 }
