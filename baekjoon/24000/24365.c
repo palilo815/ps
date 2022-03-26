f26cc77437f7694bdb0635e89c804af4d37b29c5
palilo <58646761+palilo815@users.noreply.github.com>
2022-03-26 22:11:16 +0900

    ПЧЕЛИЧКАТА МАЯ

--- baekjoon/24000/24365.c
+++ baekjoon/24000/24365.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int a, b, c;
-    scanf("%d%d%d", &a, &b, &c);
-    const int m = (a + b + c) / 3;
-    printf("%d", c - m + (b + c - m - m));
+    int a, _, c;
+    scanf("%d%d%d", &a, &_, &c);
+    printf("%d", c - a);
 }
