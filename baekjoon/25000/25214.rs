70ae7b39c452a7b3b7372b9c831e1a7095c1708a
palilo815 <hyunsoo1397@gmail.com>
2022-06-14 23:40:19 +0900

    크림 파스타

---
+++ baekjoon/25000/25214.rs
@@ -0,0 +1,39 @@
+use std::io::Write;
+
+pub struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    use std::io::Read;
+    let mut s = String::new();
+    std::io::stdin().read_to_string(&mut s).unwrap();
+    let mut sc = Scanner::new(&s);
+    let out = std::io::stdout();
+    let mut out = std::io::BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+fn run<W: Write>(sc: &mut Scanner, out: &mut std::io::BufWriter<W>) {
+    let n = sc.read::<usize>();
+    let mut mn = u32::MAX;
+    let mut gap = 0;
+    for _ in 0..n {
+        let x = sc.read::<u32>();
+        mn = mn.min(x);
+        gap = gap.max(x - mn);
+        write!(out, "{} ", gap).ok();
+    }
+}
+
