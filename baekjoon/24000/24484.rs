d7977e74b0c31745090ebb28ff829ae3041cb7bf
palilo <58646761+palilo815@users.noreply.github.com>
2022-06-16 16:05:48 +0900

    알고리즘 수업 - 깊이 우선 탐색 6

---
+++ baekjoon/24000/24484.rs
@@ -0,0 +1,63 @@
+use std::io::Write;
+
+pub struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    pub fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    use std::io::Read;
+    let mut s = String::new();
+    std::io::stdin().read_to_string(&mut s).unwrap();
+    let mut sc = Scanner::new(&s);
+    let out = std::io::stdout();
+    let mut out = std::io::BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+fn run<W: Write>(sc: &mut Scanner, out: &mut std::io::BufWriter<W>) {
+    let n = sc.read::<usize>();
+    let m = sc.read::<usize>();
+    let s = sc.read::<usize>() - 1;
+    let mut adj = vec![vec![]; n];
+    for _ in 0..m {
+        let u = sc.read::<usize>() - 1;
+        let v = sc.read::<usize>() - 1;
+        adj[u].push(v);
+        adj[v].push(u);
+    }
+    adj.iter_mut().for_each(|x| x.sort_unstable());
+    let mut stk = Vec::with_capacity(n);
+    let mut dep = vec![-1; n];
+    let mut check = vec![false; n];
+    let mut ans = 0;
+    let mut t = 0;
+    dep[s] = 0;
+    stk.push(s);
+    while let Some(u) = stk.pop() {
+        if !check[u] {
+            t += 1;
+            ans += dep[u] as usize * t;
+            check[u] = true;
+        }
+        while let Some(v) = adj[u].pop() {
+            if dep[v] == -1 {
+                dep[v] = dep[u] + 1;
+                stk.push(u);
+                stk.push(v);
+                break;
+            }
+        }
+    }
+    writeln!(out, "{}", ans).ok();
+}
