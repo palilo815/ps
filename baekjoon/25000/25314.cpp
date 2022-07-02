a4a072a4206616655c38a32efc8f701be5d21499
palilo815 <hyunsoo1397@gmail.com>
2022-07-02 23:29:30 +0900

    코딩은 체육과목 입니다

---
+++ baekjoon/25000/25314.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+auto main() -> int {
+    using namespace std;
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int n;
+    cin >> n, n >>= 2;
+    while (n--) cout << "long ";
+    cout << "int";
+}
