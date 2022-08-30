c7b42917b4c42f73949f26c48444bce1b4dee5b8
palilo815 <hyunsoo1397@gmail.com>
2022-08-30 21:00:23 +0900

    Missing Number

---
+++ cses/introductory_problems/missing_number.rs
@@ -0,0 +1,36 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut s = String::new();
+    stdin().read_to_string(&mut s).unwrap();
+    let mut sc = Scanner::new(&s);
+    let out = stdout();
+    let mut out = BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+fn run<W: Write>(sc: &mut Scanner, out: &mut BufWriter<W>) {
+    let n = sc.read::<u32>();
+    let ans = match n & 3 {
+        0 => n,
+        1 => 1,
+        2 => n + 1,
+        _ => 0,
+    };
+    writeln!(out, "{}", (1..n).fold(ans, |acc, _| acc ^ sc.read::<u32>())).ok();
+}
