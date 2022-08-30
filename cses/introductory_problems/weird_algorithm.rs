2a2ab83a7dfd3017c904523f26eeb3bc31ee42c7
palilo815 <hyunsoo1397@gmail.com>
2022-08-30 20:48:32 +0900

    Weird Algorithm

---
+++ cses/introductory_problems/weird_algorithm.rs
@@ -0,0 +1,36 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut s = String::new();
+    stdin().read_to_string(&mut s).unwrap();
+    let mut sc = Scanner::new(&s);
+    let out = stdout();
+    let mut out = BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+fn run<W: Write>(sc: &mut Scanner, out: &mut BufWriter<W>) {
+    let mut x = sc.read::<u64>();
+    loop {
+        write!(out, "{} ", x).ok();
+        if x == 1 {
+            break;
+        }
+        x = if (x & 1) == 1 { 3 * x + 1 } else { x >> 1 };
+    }
+}
