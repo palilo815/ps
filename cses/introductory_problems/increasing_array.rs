6e62bf02fc365b70f270555b8ff25c7340c8c277
palilo815 <hyunsoo1397@gmail.com>
2022-08-30 21:10:29 +0900

    Increasing Array

---
+++ cses/introductory_problems/increasing_array.rs
@@ -0,0 +1,40 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut s = String::new();
+    stdin().read_to_string(&mut s).unwrap();
+    let mut sc = Scanner::new(&s);
+    let out = stdout();
+    let mut out = BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+fn run<W: Write>(sc: &mut Scanner, out: &mut BufWriter<W>) {
+    let n = sc.read::<usize>();
+    let ans = (1..n)
+        .fold((0, sc.read::<u64>()), |(acc, p), _| {
+            let x = sc.read::<u64>();
+            if p > x {
+                (acc + p - x, p)
+            } else {
+                (acc, x)
+            }
+        })
+        .0;
+    writeln!(out, "{}", ans).ok();
+}
