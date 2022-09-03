9c462bf0895ece05f2783c7bef1bc20b3449e308
palilo815 <hyunsoo1397@gmail.com>
2022-09-03 10:57:30 +0900

    Dice Combinations

---
+++ cses/dynamic_programming/dice_combinations.rs
@@ -0,0 +1,40 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut s = String::new();
+    stdin().read_to_string(&mut s).unwrap();
+    let mut sc = Scanner::new(&s);
+    let out = stdout();
+    let mut out = BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+fn run<W: Write>(sc: &mut Scanner, out: &mut BufWriter<W>) {
+    let n = sc.read::<usize>();
+    let mut dp = vec![0; n + 1];
+    dp[0] = 1;
+    for i in 1..=n {
+        for eye in 1..=6 {
+            if i >= eye {
+                dp[i] += dp[i - eye];
+                dp[i] %= 1_000_000_007;
+            }
+        }
+    }
+    writeln!(out, "{}", dp[n]).ok();
+}
