3a28b10199acf5b4c1e4211ad19a0d307bf234e0
palilo815 <hyunsoo1397@gmail.com>
2022-09-04 21:56:34 +0900

    Maximum Subarray Sum

---
+++ cses/sorting_and_searching/maximum_subarray_sum.rs
@@ -0,0 +1,36 @@
+use std::io::*;
+ 
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+ 
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+ 
+fn main() {
+    let mut s = String::new();
+    stdin().read_to_string(&mut s).unwrap();
+    let mut sc = Scanner::new(&s);
+    let out = stdout();
+    let mut out = BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+ 
+fn run<W: Write>(sc: &mut Scanner, out: &mut BufWriter<W>) {
+    let n = sc.read::<i32>();
+    let mut acc = sc.read::<i64>();
+    let mut ans = acc;
+    for _ in 1..n {
+        acc = if acc < 0 { 0 } else { acc } + sc.read::<i64>();
+        ans = ans.max(acc);
+    }
+    writeln!(out, "{}", ans).ok();
+}
