a5c0730fbaa1a880c3fd010f43e774fe99049d9e
palilo815 <hyunsoo1397@gmail.com>
2022-09-04 23:19:05 +0900

    Towers

---
+++ cses/sorting_and_searching/towers.rs
@@ -0,0 +1,40 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut s = String::new();
+    stdin().read_to_string(&mut s).unwrap();
+    let mut sc = Scanner::new(&s);
+    let out = stdout();
+    let mut out = BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+fn run<W: Write>(sc: &mut Scanner, out: &mut BufWriter<W>) {
+    let n = sc.read::<usize>();
+    let mut top = Vec::with_capacity(n);
+    for _ in 0..n {
+        let k = sc.read::<u32>();
+        let i = top.partition_point(|&x| x <= k);
+        if i == top.len() {
+            top.push(k);
+        } else {
+            top[i] = k;
+        }
+    }
+    writeln!(out, "{}", top.len()).ok();
+}
