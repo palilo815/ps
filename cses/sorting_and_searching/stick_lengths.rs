5a220b82bd10199fd693641ea4a64af92c5d7040
palilo815 <hyunsoo1397@gmail.com>
2022-09-04 21:59:59 +0900

    Stick Lengths

---
+++ cses/sorting_and_searching/stick_lengths.rs
@@ -0,0 +1,38 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_vec<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.read()).collect()
+    }
+}
+
+fn main() {
+    let mut s = String::new();
+    stdin().read_to_string(&mut s).unwrap();
+    let mut sc = Scanner::new(&s);
+    let out = stdout();
+    let mut out = BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+fn run<W: Write>(sc: &mut Scanner, out: &mut BufWriter<W>) {
+    let n = sc.read::<usize>();
+    let mut a = sc.read_vec::<u32>(n);
+    a.select_nth_unstable(n / 2);
+    let c = a[n / 2];
+    let l = a.iter().take(n / 2).fold(0, |acc, x| acc + (c - x) as u64);
+    let r = a.iter().skip(n / 2).fold(0, |acc, x| acc + (x - c) as u64);
+    writeln!(out, "{}", l + r).ok();
+}
