861d0b64ca123fbef57f819d2d139c519d582674
palilo815 <hyunsoo1397@gmail.com>
2022-09-04 23:14:06 +0900

    Playlist

---
+++ cses/sorting_and_searching/playlist.rs
@@ -0,0 +1,44 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_vec<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.read()).collect()
+    }
+}
+
+fn main() {
+    let mut s = String::new();
+    stdin().read_to_string(&mut s).unwrap();
+    let mut sc = Scanner::new(&s);
+    let out = stdout();
+    let mut out = BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+fn run<W: Write>(sc: &mut Scanner, out: &mut BufWriter<W>) {
+    let n = sc.read::<usize>();
+    let k = sc.read_vec::<usize>(n);
+    let mut set = std::collections::HashSet::new();
+    let mut ans = 0;
+    let mut j = 0;
+    for x in k.iter() {
+        while j != n && set.insert(k[j]) {
+            j += 1;
+        }
+        ans = ans.max(set.len());
+        set.remove(x);
+    }
+    writeln!(out, "{}", ans).ok();
+}
