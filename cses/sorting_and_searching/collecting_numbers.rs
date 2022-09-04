d9fd86886a3cc64de8c8d156d91b940ef041623a
palilo815 <hyunsoo1397@gmail.com>
2022-09-04 22:07:01 +0900

    Collecting Numbers

---
+++ cses/sorting_and_searching/collecting_numbers.rs
@@ -0,0 +1,39 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut s = String::new();
+    stdin().read_to_string(&mut s).unwrap();
+    let mut sc = Scanner::new(&s);
+    let out = stdout();
+    let mut out = BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+fn run<W: Write>(sc: &mut Scanner, out: &mut BufWriter<W>) {
+    let n = sc.read::<usize>();
+    let mut check = vec![false; n + 1];
+    let mut ans = 0;
+    for _ in 0..n {
+        let x = sc.read::<usize>();
+        if !check[x - 1] {
+            ans += 1;
+        }
+        check[x] = true;
+    }
+    writeln!(out, "{}", ans).ok();
+}
