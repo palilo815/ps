bf55ecfd8082e0721fa252222899bf3c7bd4f800
palilo815 <hyunsoo1397@gmail.com>
2022-09-04 22:04:03 +0900

    Missing Coin Sum

---
+++ cses/sorting_and_searching/missing_coin_sum.rs
@@ -0,0 +1,42 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_vec<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.read()).collect()
+    }
+}
+
+fn main() {
+    let mut s = String::new();
+    stdin().read_to_string(&mut s).unwrap();
+    let mut sc = Scanner::new(&s);
+    let out = stdout();
+    let mut out = BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+fn run<W: Write>(sc: &mut Scanner, out: &mut BufWriter<W>) {
+    let n = sc.read::<usize>();
+    let mut x = sc.read_vec::<u32>(n);
+    x.sort_unstable();
+    let mut m = 1;
+    for x in x.into_iter() {
+        if m < x as u64 {
+            break;
+        }
+        m += x as u64;
+    }
+    writeln!(out, "{}", m).ok();
+}
