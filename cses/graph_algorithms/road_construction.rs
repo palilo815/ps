641ca242f4d6651fd1a08701034b7b7c009c71fc
palilo815 <hyunsoo1397@gmail.com>
2022-09-14 00:06:49 +0900

    Road Construction

---
+++ cses/graph_algorithms/road_construction.rs
@@ -0,0 +1,78 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut s = String::new();
+    stdin().read_to_string(&mut s).unwrap();
+    let mut sc = Scanner::new(&s);
+    let out = stdout();
+    let mut out = BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+struct DisjointSet {
+    p: Vec<i32>,
+}
+
+impl DisjointSet {
+    fn new(n: usize) -> Self {
+        Self { p: vec![-1; n] }
+    }
+    fn find(&mut self, u: usize) -> usize {
+        if self.p[u] < 0 {
+            u
+        } else {
+            let rt = self.find(self.p[u] as usize);
+            self.p[u] = rt as i32;
+            rt
+        }
+    }
+    fn unite(&mut self, u: usize, v: usize) -> bool {
+        let mut u = self.find(u);
+        let mut v = self.find(v);
+        if u == v {
+            return false;
+        }
+        if self.p[u] > self.p[v] {
+            std::mem::swap(&mut u, &mut v);
+        }
+        self.p[u] += self.p[v];
+        self.p[v] = u as i32;
+        true
+    }
+    fn size_of(&mut self, u: usize) -> usize {
+        let root = self.find(u);
+        (-self.p[root]) as usize
+    }
+}
+
+fn run<W: Write>(sc: &mut Scanner, out: &mut BufWriter<W>) {
+    let n = sc.read::<usize>();
+    let m = sc.read::<usize>();
+    let mut dsu = DisjointSet::new(n);
+    let mut components = n;
+    let mut largest = 0;
+    for _ in 0..m {
+        let u = sc.read::<usize>() - 1;
+        let v = sc.read::<usize>() - 1;
+        if dsu.unite(u, v) {
+            components -= 1;
+            largest = largest.max(dsu.size_of(u));
+        }
+        writeln!(out, "{} {}", components, largest).ok();
+    }
+}
