693cf1cf6bb38b7cee1a54fd916b66552f8b8340
palilo815 <hyunsoo1397@gmail.com>
2022-11-01 10:27:22 +0900

    안녕 클레오파트라 세상에서 제일가는 포테이토칩

---
+++ baekjoon/25000/25904.rs
@@ -0,0 +1,41 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_vec<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.read()).collect()
+    }
+}
+
+fn main() {
+    let mut s = String::new();
+    stdin().read_to_string(&mut s).unwrap();
+    let mut sc = Scanner::new(&s);
+    let out = stdout();
+    let mut out = BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+fn run<W: Write>(sc: &mut Scanner, out: &mut BufWriter<W>) {
+    let n = sc.read::<usize>();
+    let x = sc.read::<u32>();
+    let t = sc.read_vec::<u32>(n);
+    let r = (x..)
+        .zip(t.iter().cycle())
+        .position(|(x, &t)| x > t)
+        .unwrap()
+        % n
+        + 1;
+    writeln!(out, "{}", r).ok();
+}
