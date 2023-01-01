175acddc4c2accb21f53b4461c658da92aecf47b
palilo815 <hyunsoo1397@gmail.com>
2023-01-01 14:11:54 +0900

    Cow College

---
+++ baekjoon/26000/26975.rs
@@ -0,0 +1,62 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_vec<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.read()).collect()
+    }
+}
+
+fn main() {
+    let mut s = String::new();
+    stdin().read_to_string(&mut s).unwrap();
+    let mut sc = Scanner::new(&s);
+    let out = stdout();
+    let mut out = BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+trait ChMinMax {
+    fn chmin(&mut self, x: Self) -> bool;
+    fn chmax(&mut self, x: Self) -> bool;
+}
+
+impl<T: PartialOrd> ChMinMax for T {
+    fn chmin(&mut self, x: Self) -> bool {
+        *self > x && {
+            *self = x;
+            true
+        }
+    }
+    fn chmax(&mut self, x: Self) -> bool {
+        *self < x && {
+            *self = x;
+            true
+        }
+    }
+}
+
+fn run<W: Write>(sc: &mut Scanner, out: &mut BufWriter<W>) {
+    let n = sc.read::<usize>();
+    let mut willing = sc.read_vec::<u32>(n);
+    willing.sort_unstable();
+    let mut ans = 0;
+    let mut pay = 0;
+    for (i, t) in willing.into_iter().enumerate() {
+        if ans.chmax((n - i) * t as usize) {
+            pay = t;
+        }
+    }
+    writeln!(out, "{} {}", ans, pay).ok();
+}
