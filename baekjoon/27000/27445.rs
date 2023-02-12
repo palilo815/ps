3363536db4ce77db60589a140ec11fd6726ec00d
palilo815 <hyunsoo1397@gmail.com>
2023-02-12 17:36:09 +0900

    Gorani Command

---
+++ baekjoon/27000/27445.rs
@@ -0,0 +1,35 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut s = String::new();
+    stdin().read_to_string(&mut s).unwrap();
+    let mut sc = Scanner::new(&s);
+    let out = stdout();
+    let mut out = BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+fn run<W: Write>(sc: &mut Scanner, out: &mut BufWriter<W>) {
+    let n = sc.read::<usize>();
+    let _ = sc.read::<usize>();
+    let (r, c) = (0..n)
+        .map(|i| (i, sc.read::<usize>()))
+        .min_by_key(|x| x.1)
+        .unwrap();
+    writeln!(out, "{} {}", r + 1, c + 1).ok();
+}
