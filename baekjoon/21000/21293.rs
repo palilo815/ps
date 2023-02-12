768f35e686bfa05264144de787b7bbeebfbc16ba
palilo815 <hyunsoo1397@gmail.com>
2023-02-12 17:53:40 +0900

    No Description 2

---
+++ baekjoon/21000/21293.rs
@@ -0,0 +1,36 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut s = String::new();
+    stdin().read_to_string(&mut s).unwrap();
+    let mut sc = Scanner::new(&s);
+    let out = stdout();
+    let mut out = BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+fn run<W: Write>(sc: &mut Scanner, out: &mut BufWriter<W>) {
+    (0..23).for_each(|_| {
+        sc.read::<i32>();
+    });
+    let b = sc.read::<i32>();
+    let a = sc.read::<i32>();
+    writeln!(out, "{}", a - b).ok();
+    writeln!(out, "{}", a * b).ok();
+    writeln!(out, "{}", a + b + 1).ok();
+}
