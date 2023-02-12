d05804e3daaca202c071633184ba9aeb54736cd7
palilo815 <hyunsoo1397@gmail.com>
2023-02-12 17:32:08 +0900

    수학 퀴즈

---
+++ baekjoon/27000/27467.rs
@@ -0,0 +1,36 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut s = String::new();
+    stdin().read_to_string(&mut s).unwrap();
+    let mut sc = Scanner::new(&s);
+    let out = stdout();
+    let mut out = BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+fn run<W: Write>(sc: &mut Scanner, out: &mut BufWriter<W>) {
+    let n = sc.read::<usize>();
+    let mut w = [0; 3];
+    for _ in 0..n {
+        w[sc.read::<usize>() % 3] += 1;
+    }
+    w[0] -= w[2];
+    w[1] -= w[2];
+    writeln!(out, "{} {}", w[1], w[0]).ok();
+}
