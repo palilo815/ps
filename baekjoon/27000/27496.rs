c6bcf03bcb0eb1cb5a612a6c89a5377dac2de59b
palilo815 <hyunsoo1397@gmail.com>
2023-02-20 16:13:05 +0900

    발머의 피크 이론

---
+++ baekjoon/27000/27496.rs
@@ -0,0 +1,44 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_vec<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.read()).collect()
+    }
+}
+
+fn main() {
+    let mut s = String::new();
+    stdin().read_to_string(&mut s).unwrap();
+    let mut sc = Scanner::new(&s);
+    let out = stdout();
+    let mut out = BufWriter::new(out.lock());
+    run(&mut sc, &mut out);
+}
+
+fn run<W: Write>(sc: &mut Scanner, out: &mut BufWriter<W>) {
+    let n = sc.read::<usize>();
+    let l = sc.read::<usize>();
+    let a = sc.read_vec::<u32>(n);
+    let mut acc = 0;
+    let mut res = 0;
+    for i in 0..n {
+        acc += a[i];
+        if i >= l {
+            acc -= a[i - l];
+        }
+        res += (129..=138).contains(&acc) as u32;
+    }
+    writeln!(out, "{res}").ok();
+}
