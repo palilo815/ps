bd3b4ac7ea1f08e045bb25eb234b140d1ebc2ca3
palilo815 <hyunsoo1397@gmail.com>
2023-03-08 21:53:46 +0900

    현대모비스 소프트웨어 아카데미

---
+++ baekjoon/26000/26091.rs
@@ -0,0 +1,41 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_vec<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.read()).collect()
+    }
+}
+
+fn main() {
+    let (mut sc, mut out) = unsafe {
+        static mut S: String = String::new();
+        stdin().read_to_string(&mut S).unwrap();
+        (Scanner::new(&S), BufWriter::new(stdout().lock()))
+    };
+    let n = sc.read::<usize>();
+    let m = sc.read::<u32>();
+    let mut a = sc.read_vec::<u32>(n);
+    a.sort_unstable();
+    let mut j = n - 1;
+    for i in 0.. {
+        if i >= j {
+            break;
+        }
+        if a[i] + a[j] >= m {
+            j -= 1;
+        }
+    }
+    writeln!(out, "{}", n - j - 1).ok();
+}
