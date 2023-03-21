0dd9aceca7b4d54761386710b540b7b0fd385f42
palilo <58646761+palilo815@users.noreply.github.com>
2023-03-22 08:45:45 +0900

    금광

--- baekjoon/27000/27915.rs
+++ baekjoon/27000/27915.rs
@@ -26,22 +26,10 @@ fn main() {
     for p in p.iter_mut().skip(1) {
         *p = sc.read::<usize>() - 1;
     }
-    let mut dp = vec![(0, 0); n];
+    let mut dp = vec![(1, 0); n];
     for (i, p) in p.into_iter().enumerate().skip(1).rev() {
-        if dp[i].1 == 0 {
-            dp[i].0 += 1;
-        } else {
-            dp[i].1 -= 1;
-        }
         dp[p].0 += dp[i].1;
         dp[p].1 += dp[i].0;
     }
-    let n = n as i32;
-    let (mut x, mut y) = dp[0];
-    if y == 0 {
-        x += 1;
-    } else {
-        y -= 1;
-    }
-    writeln!(out, "{}", (n - x - y) / 2 + x.max(y)).ok();
+    writeln!(out, "{}", dp[0].0.max(dp[0].1)).ok();
 }
