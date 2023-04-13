c105e1eab29b3937f36099cde8eccbbd0efa1b2b
palilo815 <hyunsoo1397@gmail.com>
2023-04-14 00:49:11 +0900

    진법 변환 2

--- baekjoon/11000/11005.cpp
+++ baekjoon/11000/11005.cpp
@@ -1,23 +1,39 @@
 #include <bits/stdc++.h>
-#define loop(i,n) for(int i=0;i<n;++i)
-using namespace std;
 
-int main()
-{
-    cin.tie(NULL), cout.tie(NULL);
-    ios::sync_with_stdio(false);
+namespace std {
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template <class Fun>
+class y_combinator_result {
+    Fun fun_;
 
-    int n, b; cin >> n >> b;
-    stack<int> ans;
-    while (n) {
-        ans.push(n % b);
-        n /= b;
+public:
+    template <class T>
+    explicit y_combinator_result(T&& fun) : fun_(std::forward<T>(fun)) {}
+
+    template <class... Args>
+    auto operator()(Args&&... args) -> decltype(auto) {
+        return fun_(std::ref(*this), std::forward<Args>(args)...);
     }
-    while (!ans.empty()) {
-        int x = ans.top();
-        ans.pop();
-        if (x < 10) cout << x;
-        else cout << (char)('A' + x - 10);
-    }
-    return 0;
+};
+
+template <class Fun>
+auto y_combinator(Fun&& fun) -> decltype(auto) {
+    return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+}
+}; // namespace std
+
+auto main() -> int {
+    using namespace std;
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int n, b;
+    cin >> n >> b;
+    y_combinator([&](auto self, int x) -> void {
+        if (!x) return;
+        self(x / b);
+        if (const auto d = x % b; d < 10) {
+            cout << d;
+        } else {
+            cout << static_cast<char>(d - 10 + 'A');
+        }
+    })(n);
 }
