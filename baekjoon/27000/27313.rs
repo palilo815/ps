ee516bfc1375ccaa1a83d5d5a82eafde417a6fe3
palilo815 <hyunsoo1397@gmail.com>
2023-04-30 21:58:51 +0900

    효율적인 애니메이션 감상

---
+++ baekjoon/27000/27313.rs
@@ -0,0 +1,55 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_vec<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.read()).collect()
+    }
+}
+
+fn main() {
+    let (mut sc, mut out) = unsafe {
+        static mut S: String = String::new();
+        stdin().read_to_string(&mut S).unwrap();
+        (Scanner::new(&S), BufWriter::new(stdout().lock()))
+    };
+    let n = sc.read::<usize>();
+    let m = sc.read::<u32>();
+    let k = sc.read::<usize>();
+    let mut a = sc.read_vec::<u32>(n);
+    a.sort_unstable();
+    let parametric = |mut lo, mut hi| {
+        let f = |mid| -> bool {
+            let mut acc = 0;
+            for t in a.iter().take(mid).rev().step_by(k) {
+                acc += t;
+                if acc > m {
+                    return false;
+                }
+            }
+            true
+        };
+        // last true
+        while lo != hi {
+            let mid = hi - (hi - lo) / 2;
+            if f(mid) {
+                lo = mid;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        lo
+    };
+    writeln!(out, "{}", parametric(0, n)).ok();
+}
