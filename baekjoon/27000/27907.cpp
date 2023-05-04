4fbbf411a735cea862fcd660d7df1b43bf2a0be8
palilo815 <hyunsoo1397@gmail.com>
2023-05-04 23:32:29 +0900

    The primes contain arbitrarily long arithmetic progressions

---
+++ baekjoon/27000/27907.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+auto main() -> int {
+    using namespace std;
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int n;
+    cin >> n;
+    while (n--) cout << "2 ";
+}
