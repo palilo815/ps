a9d9a5052f3cc3f40ce4feff36d47e12ac2d158d
palilo <hyunsoo1397@gmail.com>
2023-09-04 00:24:59 +0900

    레몬 따기

---
+++ baekjoon/28000/28061.rs
@@ -0,0 +1,27 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let (mut sc, mut out) = unsafe {
+        static mut S: String = String::new();
+        stdin().read_to_string(&mut S).unwrap();
+        (Scanner::new(&S), BufWriter::new(stdout().lock()))
+    };
+    let n = sc.read::<usize>();
+    let ans = (0..n).map(|i| sc.read::<i32>() - (n - i) as i32).max().unwrap().max(0);
+    writeln!(out, "{ans}").ok();
+}
