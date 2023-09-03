a8ea0b17d446841f278f897e7845a52f9d2b81e5
palilo <hyunsoo1397@gmail.com>
2023-09-04 00:27:06 +0900

    SW 수열 구하기

---
+++ baekjoon/28000/28065.rs
@@ -0,0 +1,31 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let (mut sc, mut out) = unsafe {
+        static mut S: String = String::new();
+        stdin().read_to_string(&mut S).unwrap();
+        (Scanner::new(&S), BufWriter::new(stdout().lock()))
+    };
+    let n = sc.read::<usize>();
+    for x in (1..).zip((1..=n as i32).rev()).take(n / 2) {
+        write!(out, "{} {} ", x.0, x.1).ok();
+    }
+    if n & 1 == 1 {
+        write!(out, "{}", n / 2 + 1).ok();
+    }
+}
