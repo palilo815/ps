d81a7ca95d0b9eaae19b21452621763a1df9dc8f
palilo <hyunsoo1397@gmail.com>
2023-09-04 00:29:36 +0900

    K512에서 피자 먹기

---
+++ baekjoon/28000/28072.rs
@@ -0,0 +1,33 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let (mut sc, mut out) = unsafe {
+        static mut S: String = String::new();
+        stdin().read_to_string(&mut S).unwrap();
+        (Scanner::new(&S), BufWriter::new(stdout().lock()))
+    };
+    let n = sc.read::<usize>();
+    let k = sc.read::<usize>();
+    let mut cnt = vec![0; k];
+    let mut acc = 0;
+    for _ in 0..n {
+        acc = (acc + sc.read::<usize>()) % k;
+        cnt[acc] += 1;
+    }
+    writeln!(out, "{}", cnt.into_iter().max().unwrap()).ok();
+}
