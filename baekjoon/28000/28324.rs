9b9ece5af6e021390ffc90714b8243ddeacf0f9f
palilo <hyunsoo1397@gmail.com>
2023-09-11 22:54:32 +0900

    스케이트 연습

---
+++ baekjoon/28000/28324.rs
@@ -0,0 +1,38 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_vec<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.read()).collect()
+    }
+}
+
+fn main() {
+    let (mut sc, mut out) = unsafe {
+        static mut S: String = String::new();
+        stdin().read_to_string(&mut S).unwrap();
+        (Scanner::new(&S), BufWriter::new(stdout().lock()))
+    };
+    let n = sc.read::<usize>();
+    let a = sc.read_vec::<u32>(n);
+    let ans = a
+        .into_iter()
+        .rev()
+        .fold((0, 0), |(acc, next), x| {
+            let x = x.min(next + 1);
+            (acc + x as u64, x)
+        })
+        .0;
+    writeln!(out, "{ans}").ok();
+}
