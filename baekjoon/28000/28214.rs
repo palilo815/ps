277fe2cd8f38c7d99fcacd03be23e70dff3ec0a5
palilo <hyunsoo1397@gmail.com>
2023-09-11 22:58:05 +0900

    크림빵

---
+++ baekjoon/28000/28214.rs
@@ -0,0 +1,31 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let (mut sc, mut out) = unsafe {
+        static mut S: String = String::new();
+        stdin().read_to_string(&mut S).unwrap();
+        (Scanner::new(&S), BufWriter::new(stdout().lock()))
+    };
+    let n = sc.read::<usize>();
+    let k = sc.read::<usize>();
+    let p = sc.read::<usize>();
+    let ans = (0..n).fold(0, |acc, _| {
+        acc + ((0..k).filter(|_| sc.read::<usize>() == 0).count() < p) as usize
+    });
+    writeln!(out, "{ans}").ok();
+}
