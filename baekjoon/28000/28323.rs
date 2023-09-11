fc62e5984ff757571f611f9d6cf656049bf52345
palilo <hyunsoo1397@gmail.com>
2023-09-11 22:56:41 +0900

    불안정한 수열

---
+++ baekjoon/28000/28323.rs
@@ -0,0 +1,35 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Scanner<'a> {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let (mut sc, mut out) = unsafe {
+        static mut S: String = String::new();
+        stdin().read_to_string(&mut S).unwrap();
+        (Scanner::new(&S), BufWriter::new(stdout().lock()))
+    };
+    let n = sc.read::<usize>();
+    let mut ans = 0;
+    let mut prev = 2;
+    for _ in 0..n {
+        let curr = sc.read::<usize>() & 1;
+        if curr != prev {
+            prev = curr;
+            ans += 1;
+        }
+    }
+    writeln!(out, "{ans}").ok();
+}
