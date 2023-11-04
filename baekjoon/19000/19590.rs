fa70c5160218945c303128e24af265cab1521f0d
palilo815 <hyunsoo1397@gmail.com>
2023-11-04 18:25:13 +0900

    비드맨

---
+++ baekjoon/19000/19590.rs
@@ -0,0 +1,33 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &str) -> Scanner {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_vec<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.read()).collect()
+    }
+}
+
+fn main() {
+    let (mut sc, mut out) = unsafe {
+        static mut S: String = String::new();
+        stdin().read_to_string(&mut S).unwrap();
+        (Scanner::new(&S), BufWriter::new(stdout().lock()))
+    };
+    let n = sc.read::<usize>();
+    let a = sc.read_vec::<u32>(n);
+    let total = a.iter().fold(0, |acc, &x| acc + x as u64);
+    let mx = a.into_iter().max().unwrap() as u64;
+    let left = if mx * 2 > total { 2 * mx - total } else { total & 1 };
+    writeln!(out, "{left}").ok();
+}
