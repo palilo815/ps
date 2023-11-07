3507170231f0e320e886eb190e5186c6761f6b66
palilo815 <hyunsoo1397@gmail.com>
2023-11-08 00:20:13 +0900

    Natatorium

---
+++ baekjoon/30000/30580.rs
@@ -0,0 +1,37 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &str) -> Scanner {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let (mut sc, mut out) = unsafe {
+        static mut S: String = String::new();
+        stdin().read_to_string(&mut S).unwrap();
+        (Scanner::new(&S), BufWriter::new(stdout().lock()))
+    };
+    let c = sc.read::<u64>();
+    let n = sc.read::<usize>();
+    let mut set = std::collections::HashSet::new();
+    for _ in 0..n {
+        let x = sc.read::<u64>();
+        if c % x == 0 {
+            if let Some(&y) = set.get(&(c / x)) {
+                writeln!(out, "{} {}", x.min(y), x.max(y)).ok();
+                return;
+            }
+            set.insert(x);
+        }
+    }
+}
