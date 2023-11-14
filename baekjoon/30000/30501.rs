a672802786e75c4f106705bf8aca18ab90d073a9
palilo815 <hyunsoo1397@gmail.com>
2023-11-14 22:10:01 +0900

    관공... 어찌하여 목만 오셨소...

---
+++ baekjoon/30000/30501.rs
@@ -0,0 +1,35 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &str) -> Scanner {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn raw(&mut self) -> &str {
+        self.it.next().unwrap()
+    }
+}
+
+fn main() {
+    let (mut sc, mut out) = unsafe {
+        static mut S: String = String::new();
+        stdin().read_to_string(&mut S).unwrap();
+        (Scanner::new(&S), BufWriter::new(stdout().lock()))
+    };
+    let n = sc.read::<usize>();
+    for _ in 0..n {
+        let s = sc.raw();
+        if s.find('S').is_some() {
+            writeln!(out, "{}", s).ok();
+            return;
+        }
+    }
+}
