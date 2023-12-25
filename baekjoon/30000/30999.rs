a3f113e4c61845d15d4d0d0ff23ca54b3b20bf06
palilo815 <hyunsoo1397@gmail.com>
2023-12-25 23:06:08 +0900

    민주주의

---
+++ baekjoon/30000/30999.rs
@@ -0,0 +1,31 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &str) -> Scanner {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn raw(&mut self) -> &str {
+        self.it.next().unwrap()
+    }
+}
+
+fn main() {
+    let (mut sc, mut out) = unsafe {
+        static mut S: String = String::new();
+        stdin().read_to_string(&mut S).unwrap();
+        (Scanner::new(&S), BufWriter::new(stdout().lock()))
+    };
+    let n = sc.read::<usize>();
+    let m = sc.read::<usize>();
+    let ans = (0..n).filter(|_| sc.raw().bytes().filter(|&x| x == b'O').count() > m / 2).count();
+    writeln!(out, "{ans}").ok();
+}
