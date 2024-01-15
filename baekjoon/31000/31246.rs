08bb94bec8328a8c8ee89d3e2ca8527335838779
palilo815 <hyunsoo1397@gmail.com>
2024-01-15 22:59:51 +0900

    모바일 광고 입찰

---
+++ baekjoon/31000/31246.rs
@@ -0,0 +1,29 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &str) -> Scanner {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let (mut sc, mut out) = unsafe {
+        static mut S: String = String::new();
+        stdin().read_to_string(&mut S).unwrap();
+        (Scanner::new(&S), BufWriter::new(stdout().lock()))
+    };
+    let n = sc.read::<usize>();
+    let k = sc.read::<usize>();
+    let mut a = (0..n).map(|_| (-sc.read::<i32>() + sc.read::<i32>())).collect::<Vec<_>>();
+    a.select_nth_unstable(k - 1);
+    writeln!(out, "{}", a[k - 1].max(0)).ok();
+}
