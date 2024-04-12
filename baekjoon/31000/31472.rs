cc68d15ceeec6793ced694b9ec525be84da2f755
palilo815 <hyunsoo1397@gmail.com>
2024-04-12 22:05:34 +0900

    갈래의 색종이 자르기

---
+++ baekjoon/31000/31472.rs
@@ -0,0 +1,26 @@
+use std::io::*;
+
+struct Scanner<'a> {
+    it: std::str::SplitWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &str) -> Scanner {
+        Scanner {
+            it: s.split_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let (mut sc, mut out) = unsafe {
+        static mut S: String = String::new();
+        stdin().read_to_string(&mut S).unwrap();
+        (Scanner::new(&S), BufWriter::new(stdout().lock()))
+    };
+    let w = sc.read::<f64>();
+    writeln!(out, "{}", (2.0 * w).sqrt() * 4.0).ok();
+}
