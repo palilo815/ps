ea52c3b13e18deec4ff8ef4a123ac241723ac04f
palilo815 <hyunsoo1397@gmail.com>
2024-04-14 18:34:29 +0900

    매우 어려운 문제

---
+++ baekjoon/31000/31738.rs
@@ -0,0 +1,31 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self {
+            it: s.leak().split_ascii_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<u64>();
+    let m = sc.read::<u64>();
+    let ans = if n < m {
+        (1..=n).fold(1, |prod, x| prod * x % m)
+    } else {
+        0
+    };
+    writeln!(bw, "{ans}").ok();
+}
