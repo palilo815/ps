f312ab24bccd851f57fc403e04440aab3cf15ac6
palilo815 <hyunsoo1397@gmail.com>
2024-05-29 01:52:54 +0900

    나는 북극곰입니다

---
+++ baekjoon/31000/31932.rs
@@ -0,0 +1,85 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self {
+            it: s.leak().split_ascii_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+struct Csr<T> {
+    values: Box<[(usize, T)]>,
+    pref: Box<[usize]>,
+}
+
+impl<T> std::ops::Index<usize> for Csr<T> {
+    type Output = [(usize, T)];
+    fn index(&self, i: usize) -> &Self::Output {
+        &self.values[self.pref[i]..self.pref[i + 1]]
+    }
+}
+
+impl<T: Copy + Default> Csr<T> {
+    fn from_undirected_edges(n: usize, edges: Vec<(usize, usize, T)>) -> Self {
+        let mut values = vec![(0, T::default()); edges.len() * 2].into_boxed_slice();
+        let mut pref = vec![0; n + 1].into_boxed_slice();
+        for &(u, v, _) in edges.iter() {
+            pref[u] += 1;
+            pref[v] += 1;
+        }
+        for i in 0..n {
+            pref[i + 1] += pref[i];
+        }
+        for (u, v, w) in edges {
+            pref[u] -= 1;
+            values[pref[u]] = (v, w);
+            pref[v] -= 1;
+            values[pref[v]] = (u, w);
+        }
+        Self { values, pref }
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let m = sc.read::<usize>();
+    let edges = (0..m)
+        .map(|_| {
+            (
+                sc.read::<usize>() - 1,
+                sc.read::<usize>() - 1,
+                (sc.read::<i32>(), sc.read::<i32>()),
+            )
+        })
+        .collect::<Vec<_>>();
+    let csr = Csr::from_undirected_edges(n, edges);
+    let mut pq = std::collections::BinaryHeap::new();
+    pq.push((i32::MAX, n - 1));
+    let mut deadline = vec![-1; n];
+    deadline[n - 1] = i32::MAX;
+    while let Some((cur, u)) = pq.pop() {
+        if cur != deadline[u] {
+            continue;
+        }
+        for &(v, (d, t)) in csr[u].iter() {
+            let pre = t.min(cur) - d;
+            if pre > deadline[v] {
+                deadline[v] = pre;
+                pq.push((pre, v));
+            }
+        }
+    }
+    writeln!(bw, "{}", deadline[0]).ok();
+}
