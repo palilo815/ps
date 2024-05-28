0067b6e55ca977445becc19a5ba1902d97539404
palilo815 <hyunsoo1397@gmail.com>
2024-05-29 01:49:34 +0900

    Waterworld

---
+++ baekjoon/31000/31905.rs
@@ -0,0 +1,26 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self {
+            it: s.leak().split_ascii_whitespace(),
+        }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>() * sc.read::<usize>();
+    let sum = (0..n).fold(0, |acc, _| acc + sc.read::<u64>());
+    writeln!(bw, "{:.8}", sum as f64 / n as f64).ok();
+}
