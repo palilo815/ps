b199c8ef17af28625e5aab002ce26284396f3345
palilo815 <hyunsoo1397@gmail.com>
2024-06-02 23:54:45 +0900

    데이터 구조

--- baekjoon/12000/12899.rs
+++ baekjoon/12000/12899.rs
@@ -1,69 +1,71 @@
-use std::io::Write;
+use std::io::*;
 
-#[allow(dead_code)]
-mod scanner {
-    use std::str::FromStr;
-    pub struct Scanner<'a> {
-        it: std::str::SplitWhitespace<'a>,
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self {
+            it: s.leak().split_ascii_whitespace(),
+        }
     }
-    impl<'a> Scanner<'a> {
-        pub fn new(s: &'a String) -> Scanner<'a> {
-            Scanner {
-                it: s.split_whitespace(),
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+struct DynamicKth<T> {
+    size: usize,
+    data: Box<[T]>,
+}
+
+impl DynamicKth<i32> {
+    fn new(size: usize) -> Self {
+        let size = size.next_power_of_two();
+        Self {
+            size,
+            data: vec![0; size + 1].into(),
+        }
+    }
+    fn add(&mut self, mut i: usize, x: i32) {
+        i += 1;
+        while i <= self.size {
+            self.data[i] += x;
+            i += i & i.wrapping_neg();
+        }
+    }
+    fn remove_kth(&mut self, mut k: i32) -> usize {
+        let mut i = 0;
+        let mut len = self.size >> 1;
+        while len != 0 {
+            if self.data[i | len] < k {
+                i |= len;
+                k -= self.data[i];
+            } else {
+                self.data[i | len] -= 1;
             }
+            len >>= 1;
         }
-        pub fn next<T: FromStr>(&mut self) -> T {
-            self.it.next().unwrap().parse::<T>().ok().unwrap()
-        }
-        pub fn next_bytes(&mut self) -> Vec<u8> {
-            self.it.next().unwrap().bytes().collect()
-        }
-        pub fn next_chars(&mut self) -> Vec<char> {
-            self.it.next().unwrap().chars().collect()
-        }
-        pub fn next_vec<T: FromStr>(&mut self, len: usize) -> Vec<T> {
-            (0..len).map(|_| self.next()).collect()
-        }
+        i
     }
 }
 
 fn main() {
-    use std::io::Read;
-    let mut s = String::new();
-    std::io::stdin().read_to_string(&mut s).unwrap();
-    let mut sc = scanner::Scanner::new(&s);
-    let out = std::io::stdout();
-    let mut out = std::io::BufWriter::new(out.lock());
-    run(&mut sc, &mut out);
-}
-
-fn run<W: Write>(sc: &mut scanner::Scanner, out: &mut std::io::BufWriter<W>) {
-    const SIZE: usize = 1 << 21;
-    let mut bit = [0; SIZE | 1];
-    let q = sc.next::<usize>();
-    for _ in 0..q {
-        let op = sc.next::<char>();
-        let x = sc.next::<u32>();
-        if op == '1' {
-            let mut i = (x + 1) as usize;
-            while i <= SIZE {
-                bit[i] += 1;
-                i += i & i.wrapping_neg();
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let mut dk = DynamicKth::new(2_000_001);
+    for _ in 0..n {
+        match sc.read::<char>() {
+            '1' => {
+                dk.add(sc.read::<usize>(), 1);
             }
-        } else {
-            let mut x = x;
-            let mut i = 0;
-            let mut len = SIZE >> 1;
-            while len != 0 {
-                if bit[i | len] < x {
-                    i |= len;
-                    x -= bit[i];
-                } else {
-                    bit[i | len] -= 1;
-                }
-                len >>= 1;
+            _ => {
+                writeln!(bw, "{}", dk.remove_kth(sc.read::<i32>())).ok();
             }
-            writeln!(out, "{}", i).ok();
         }
     }
 }
