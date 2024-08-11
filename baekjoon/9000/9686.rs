ca77fa49ea72970390002cc329e56d6b33a64158
palilo815 <hyunsoo1397@gmail.com>
2024-08-11 22:39:28 +0900

    The Exam

---
+++ baekjoon/9000/9686.rs
@@ -0,0 +1,32 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let k = sc.read::<usize>();
+    if n / 2 < k {
+        writeln!(bw, "NIE").ok();
+        return;
+    }
+    for s in (1..=k).rev() {
+        for i in (s..=n).step_by(k) {
+            write!(bw, "{i} ").ok();
+        }
+    }
+}
