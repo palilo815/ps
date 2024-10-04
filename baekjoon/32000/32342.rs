2485cca17962766ce71d406a65cd5776d647f767
palilo815 <hyunsoo1397@gmail.com>
2024-10-04 19:28:27 +0900

    와우와 쿼리

---
+++ baekjoon/32000/32342.rs
@@ -0,0 +1,30 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_bytes(&mut self) -> Vec<u8> {
+        self.it.next().unwrap().bytes().collect()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let q = sc.read::<usize>();
+    for _ in 0..q {
+        let s = sc.read_bytes();
+        let ans = s.windows(3).filter(|w| w.eq(b"WOW")).count();
+        writeln!(bw, "{ans}").ok();
+    }
+}
