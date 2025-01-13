e29e10b54fbe6875025f23ce4db9c44987a2e348
palilo815 <hyunsoo1397@gmail.com>
2025-01-14 00:06:22 +0900

    Account Names

---
+++ baekjoon/29000/29881.rs
@@ -0,0 +1,32 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn raw(&mut self) -> &str {
+        self.it.next().unwrap()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let mut map = std::collections::HashMap::new();
+    for _ in 0..n {
+        map.insert(sc.raw().to_string(), sc.raw().to_string());
+    }
+    for _ in 0..n {
+        writeln!(bw, "{} {}", map.get(sc.raw()).unwrap(), sc.raw()).ok();
+    }
+}
