c36d7091adc87b0a0f42247e4515a0c1750b8171
palilo815 <hyunsoo1397@gmail.com>
2025-03-01 21:21:57 +0900

    트리 순회

--- baekjoon/22000/22856.rs
+++ baekjoon/22000/22856.rs
@@ -1,56 +1,38 @@
-use std::io::Write;
+use std::io::*;
 
-#[allow(dead_code)]
-mod scanner {
-    use std::str::FromStr;
-    pub struct Scanner<'a> {
-        it: std::str::SplitWhitespace<'a>,
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
     }
-    impl<'a> Scanner<'a> {
-        pub fn new(s: &'a str) -> Scanner<'a> {
-            Scanner {
-                it: s.split_whitespace(),
-            }
-        }
-        pub fn next<T: FromStr>(&mut self) -> T {
-            self.it.next().unwrap().parse::<T>().ok().unwrap()
-        }
-        pub fn next_bytes(&mut self) -> Vec<u8> {
-            self.it.next().unwrap().bytes().collect()
-        }
-        pub fn next_chars(&mut self) -> Vec<char> {
-            self.it.next().unwrap().chars().collect()
-        }
-        pub fn next_vec<T: FromStr>(&mut self, len: usize) -> Vec<T> {
-            (0..len).map(|_| self.next()).collect()
-        }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn raw(&mut self) -> &str {
+        self.it.next().unwrap()
     }
 }
 
 fn main() {
-    use std::io::Read;
-    let mut s = String::new();
-    std::io::stdin().read_to_string(&mut s).unwrap();
-    let mut sc = scanner::Scanner::new(&s);
-    let out = std::io::stdout();
-    let mut out = std::io::BufWriter::new(out.lock());
-    run(&mut sc, &mut out);
-}
-
-fn run<W: Write>(sc: &mut scanner::Scanner, out: &mut std::io::BufWriter<W>) {
-    let n = sc.next::<usize>();
-    let mut right_child = vec![0; n];
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let mut rchild = vec![0; n];
     for _ in 0..n {
-        let i = sc.next::<usize>() - 1;
-        let _ = sc.next::<i32>();
-        let r = sc.next::<i32>() - 1;
-        right_child[i] = r;
+        let i = sc.read::<usize>() - 1;
+        sc.raw();
+        rchild[i] = sc.read::<i32>() - 1;
     }
-    let mut ans = (n - 1) << 1;
+    let mut ans = 2 * (n - 1);
     let mut u = 0;
-    while right_child[u as usize] != -2 {
-        u = right_child[u as usize];
+    while rchild[u as usize] != -2 {
+        u = rchild[u as usize];
         ans -= 1;
     }
-    writeln!(out, "{}", ans).ok();
+    writeln!(bw, "{ans}").ok();
 }
