837c9bf36066aead1017f146eeb8a21568faf7df
palilo815 <hyunsoo1397@gmail.com>
2025-03-02 17:37:50 +0900

    로프

---
+++ baekjoon/2000/2217.rs
@@ -0,0 +1,29 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_vec<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.read()).collect()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let mut a = sc.read_vec::<i32>(n);
+    a.sort_unstable();
+    let ans = a.into_iter().rev().zip(1..).map(|(w, i)| i * w).max().unwrap();
+    writeln!(bw, "{ans}").ok();
+}
