326eab335167b2dc62abb2ec1f0845c175b6f9cc
palilo815 <hyunsoo1397@gmail.com>
2025-04-22 23:43:14 +0900

    예산

---
+++ baekjoon/2000/2512.rs
@@ -0,0 +1,42 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_vec<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.read()).collect()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let a = sc.read_vec::<u32>(n);
+    let m = sc.read::<u32>();
+    let parametric = |mut lo, mut hi| {
+        let f = |mid| -> bool { a.iter().fold(0, |acc, &x| acc + if x > mid { mid } else { x }) <= m };
+        // last true
+        while lo != hi {
+            let mid = hi - (hi - lo) / 2;
+            if f(mid) {
+                lo = mid;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        lo
+    };
+    writeln!(bw, "{}", parametric(0, *a.iter().max().unwrap())).ok();
+}
+
