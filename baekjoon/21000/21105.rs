f86804b8d5895497bb64852b275c71ab5f35aad3
palilo815 <hyunsoo1397@gmail.com>
2025-04-22 23:40:55 +0900

    New Financial Year

---
+++ baekjoon/21000/21105.rs
@@ -0,0 +1,28 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    for _ in 0..n {
+        let p = sc.read::<f64>();
+        let c = sc.read::<f64>();
+        writeln!(bw, "{:.8}", 100.0 * p / (c + 100.0)).ok();
+    }
+}
+
