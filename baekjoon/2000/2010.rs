7d4a7f7fe166266fc2f48a63f7caa66391474b0f
palilo815 <hyunsoo1397@gmail.com>
2025-04-22 23:50:41 +0900

    플러그

---
+++ baekjoon/2000/2010.rs
@@ -0,0 +1,24 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let ans = (0..n).fold(1, |acc, _| acc + sc.read::<i32>() - 1);
+    writeln!(bw, "{ans}").ok();
+}
