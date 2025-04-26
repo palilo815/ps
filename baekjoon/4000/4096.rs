990a6b465ffb669d624c92b6c410b03f5821e1eb
palilo815 <hyunsoo1397@gmail.com>
2025-04-27 00:37:15 +0900

    팰린드로미터

---
+++ baekjoon/4000/4096.rs
@@ -0,0 +1,52 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read_bytes(&mut self) -> Vec<u8> {
+        self.it.next().unwrap().bytes().collect()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    loop {
+        let s = sc.read_bytes();
+        if s == vec![b'0'] {
+            break;
+        }
+        let mut t = s.clone();
+        let half = s.len() / 2;
+        t.copy_within(..half, s.len() - half);
+        t[s.len() - half..].reverse();
+        if t < s {
+            let (mut i, mut j) = if s.len() % 2 == 0 {
+                t[half - 1] += 1;
+                t[half] += 1;
+                (half - 1, half)
+            } else {
+                t[half] += 1;
+                (half, half)
+            };
+            while t[i] > b'9' {
+                t[i] = b'0';
+                t[j] = b'0';
+                i -= 1;
+                j += 1;
+                t[i] += 1;
+                t[j] += 1;
+            }
+        }
+        let s = String::from_utf8(s).unwrap().parse::<u64>().unwrap();
+        let t = String::from_utf8(t).unwrap().parse::<u64>().unwrap();
+        writeln!(bw, "{}", t - s).ok();
+    }
+}
