f19ce3a3f4792cf1f40a072c769ee9faecd0c242
palilo815 <hyunsoo1397@gmail.com>
2025-05-07 21:58:31 +0900

    입국심사

---
+++ baekjoon/3000/3079.rs
@@ -0,0 +1,50 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_vec<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.read()).collect()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let m = sc.read::<u64>();
+    let a = sc.read_vec::<u32>(n);
+    let parametric = |mut lo, mut hi| {
+        let f = |mid| -> bool {
+            let mut acc = 0;
+            for &x in a.iter() {
+                acc += mid / x as u64;
+                if acc >= m {
+                    return true;
+                }
+            }
+            false
+        };
+        // first true
+        while lo != hi {
+            let mid = lo + (hi - lo) / 2;
+            if f(mid) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        lo
+    };
+    writeln!(bw, "{}", parametric(1, 1_000_000_000_000_000_000_u64)).ok();
+}
