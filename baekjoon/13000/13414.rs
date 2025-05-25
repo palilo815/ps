25fdc462c555809c6119fac71388f6ed704accd9
palilo815 <hyunsoo1397@gmail.com>
2025-05-25 13:43:05 +0900

    수강신청

--- baekjoon/13000/13414.rs
+++ baekjoon/13000/13414.rs
@@ -1,74 +1,40 @@
-macro_rules! input {
-    (source = $s:expr, $($r:tt)*) => {
-        let mut iter = $s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-    ($($r:tt)*) => {
-        let s = {
-            use std::io::Read;
-            let mut s = String::new();
-            std::io::stdin().read_to_string(&mut s).unwrap();
-            s
-        };
-        let mut iter = s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
 }
 
-macro_rules! input_inner {
-    ($iter:expr) => {};
-    ($iter:expr, ) => {};
-    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
-        let $var = read_value!($iter, $t);
-        input_inner!{$iter $($r)*}
-    };
-}
-
-macro_rules! read_value {
-    ($iter:expr, ( $($t:tt),* )) => {
-        ( $(read_value!($iter, $t)),* )
-    };
-    ($iter:expr, [ $t:tt ; $len:expr ]) => {
-        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
-    };
-    ($iter:expr, chars) => {
-        read_value!($iter, String).chars().collect::<Vec<char>>()
-    };
-    ($iter:expr, bytes) => {
-        read_value!($iter, String).bytes().collect::<Vec<u8>>()
-    };
-    ($iter:expr, usize1) => {
-        read_value!($iter, usize) - 1
-    };
-    ($iter:expr, $t:ty) => {
-        $iter.next().unwrap().parse::<$t>().expect("Parse error")
-    };
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_vec<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.read()).collect()
+    }
 }
 
 fn main() {
-    use std::io::Write;
-    let out = std::io::stdout();
-    let mut out = std::io::BufWriter::new(out.lock());
-    input! {
-        n: usize,
-        m: usize,
-        a: [u32; m],
-    }
-    let mut last_click = std::collections::HashMap::new();
-    for (i, x) in a.iter().enumerate() {
-        last_click.insert(*x, i);
-    }
-    a.into_iter()
-        .enumerate()
-        .filter_map(|(i, x)| {
-            if *last_click.get(&x).unwrap() == i {
-                Some(x)
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let m = sc.read::<usize>();
+    let a = sc.read_vec::<u32>(m);
+    let history = a.iter().enumerate().map(|(i, &x)| (x, i)).collect::<std::collections::HashMap<_, _>>();
+    let ans = (0..m)
+        .filter_map(|i| {
+            if Some(&i) == history.get(&a[i]) {
+                Some(a[i])
             } else {
                 None
             }
         })
-        .take(n)
-        .for_each(|x| {
-            writeln!(out, "{:0>8}", x).ok();
-        });
+        .take(n);
+    for x in ans {
+        writeln!(bw, "{:0>8}", x).ok();
+    }
 }
