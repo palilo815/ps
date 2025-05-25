599d1bbb59567355d36aaa813bd89392152b1a67
palilo815 <hyunsoo1397@gmail.com>
2025-05-25 14:08:40 +0900

    알바생 강호

---
+++ baekjoon/1000/1758.rs
@@ -0,0 +1,35 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_vec<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.read()).collect()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let mut a = sc.read_vec::<i32>(n);
+    a.sort_unstable();
+    let ans = a
+        .into_iter()
+        .rev()
+        .zip(0..)
+        .map(|(x, i)| x - i)
+        .take_while(|&x| x > 0)
+        .fold(0, |acc, x| acc + x as i64);
+    writeln!(bw, "{ans}").ok();
+}
