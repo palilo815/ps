e68da402461d3acc5169ce47a295f99b07259b25
palilo815 <hyunsoo1397@gmail.com>
2025-05-25 13:39:20 +0900

    클리크 조절

---
+++ baekjoon/33000/33884.rs
@@ -0,0 +1,25 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let p1 = (0..n).map(|_| (sc.read::<i32>(), sc.read::<i32>())).min().unwrap();
+    let p2 = (0..n).map(|_| (sc.read::<i32>(), sc.read::<i32>())).min().unwrap();
+    writeln!(bw, "{} {}", p2.0 - p1.0, p2.1 - p1.1).ok();
+}
