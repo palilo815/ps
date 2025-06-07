e15ac7209a73b001a131c6e77e4984475f24f369
palilo815 <hyunsoo1397@gmail.com>
2025-06-07 21:06:00 +0900

    Sumy i różnice

---
+++ baekjoon/8000/8660.rs
@@ -0,0 +1,24 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let negs = (0..n).fold(0, |acc, _| acc + sc.read::<i32>().is_negative() as usize);
+    writeln!(bw, "{}", (n - 1) * negs).ok();
+}
