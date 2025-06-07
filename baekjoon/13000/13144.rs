6eb378881c49ccc5079fba05be3d7cf15e8caed2
palilo815 <hyunsoo1397@gmail.com>
2025-06-07 21:04:03 +0900

    List of Unique Numbers

--- baekjoon/13000/13144.rs
+++ baekjoon/13000/13144.rs
@@ -1,77 +1,38 @@
-macro_rules! input {
-    (source = $s:expr, $($r:tt)*) => {
-        let mut iter = $s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-    ($($r:tt)*) => {
-        let s = {
-            use std::io::Read;
-            let mut s = String::new();
-            std::io::stdin().read_to_string(&mut s).unwrap();
-            s
-        };
-        let mut iter = s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
 }
 
-macro_rules! input_inner {
-    ($iter:expr) => {};
-    ($iter:expr, ) => {};
-    ($iter:expr, $var:ident:$t:tt $($r:tt)*) => {
-        let $var = read_value!($iter, $t);
-        input_inner!{$iter $($r)*}
-    };
-}
-
-macro_rules! read_value {
-    ($iter:expr, ($($t:tt), *)) => {
-        ($(read_value!($iter, $t)), *)
-    };
-    ($iter:expr, [$t:tt; $len:expr]) => {
-        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
-    };
-    ($iter:expr, chars) => {
-        read_value!($iter, String).chars().collect::<Vec<char>>()
-    };
-    ($iter:expr, bytes) => {
-        read_value!($iter, String).bytes().collect::<Vec<u8>>()
-    };
-    ($iter:expr, usize1) => {
-        read_value!($iter, usize) - 1
-    };
-    ($iter:expr, $t:ty) => {
-        $iter.next().unwrap().parse::<$t>().expect("Parse error")
-    };
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_vec<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.read()).collect()
+    }
 }
 
 fn main() {
-    use std::io::Write;
-    let out = std::io::stdout();
-    let mut out = std::io::BufWriter::new(out.lock());
-    input! {
-        n: usize,
-        a: [usize; n],
-    }
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let a = sc.read_vec::<usize>(n);
     let mut check = [false; 100_001];
-    let mut dup = usize::MAX;
-    let mut l = 0;
     let mut ans = 0;
-    for r in 0..n {
-        if check[a[r]] {
-            dup = a[r];
-        } else {
+    let mut r = 0;
+    for l in 0..n {
+        while r != n && !check[a[r]] {
             check[a[r]] = true;
+            r += 1;
         }
-        while dup != usize::MAX {
-            if dup == a[l] {
-                dup = usize::MAX;
-            } else {
-                check[a[l]] = false;
-            }
-            l += 1;
-        }
-        ans += r - l + 1;
+        ans += r - l;
+        check[a[l]] = false;
     }
-    writeln!(out, "{}", ans).ok();
+    writeln!(bw, "{}", ans).ok();
 }
