786f4f69f894c5547955199201730fc1c951af00
palilo815 <hyunsoo1397@gmail.com>
2025-06-11 00:35:35 +0900

    Counting Sheep

---
+++ baekjoon/7000/7366.rs
@@ -0,0 +1,30 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn raw(&mut self) -> &str {
+        self.it.next().unwrap()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let t = sc.read::<usize>();
+    for t in 1..=t {
+        let n = sc.read::<usize>();
+        let ans = (0..n).filter(|_| sc.raw() == "sheep").count();
+        writeln!(bw, "Case {t}: This list contains {ans} sheep.\n").ok();
+    }
+}
