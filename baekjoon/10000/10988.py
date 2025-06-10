61e4dc621256e6f786e2ad8543648fb9b69766b9
palilo815 <hyunsoo1397@gmail.com>
2025-06-11 00:28:31 +0900

    팰린드롬인지 확인하기

--- baekjoon/10000/10988.py
+++ baekjoon/10000/10988.py
@@ -1,12 +1,2 @@
-def pallin(s):
-    l, r = 0, len(s) - 1
-    while l < r:
-        if s[l] != s[r]:
-            return 0
-        l += 1
-        r -= 1
-    return 1
-
-
 s = input()
-print(pallin(s))
+print(int(s == s[::-1]))
