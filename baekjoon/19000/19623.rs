086a273029998938b3daf3a318fb67feef909d69
palilo815 <hyunsoo1397@gmail.com>
2025-06-12 21:22:19 +0900

    회의실 배정 4

--- baekjoon/19000/19623.rs
+++ baekjoon/19000/19623.rs
@@ -1,86 +1,32 @@
-macro_rules! input {
-    (source = $s:expr, $($r:tt)*) => {
-        let mut iter = $s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-    ($($r:tt)*) => {
-        let s = {
-            use std::io::Read;
-            let mut s = String::new();
-            std::io::stdin().read_to_string(&mut s).unwrap();
-            s
-        };
-        let mut iter = s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
 }
 
-macro_rules! input_inner {
-    ($iter:expr) => {};
-    ($iter:expr, ) => {};
-    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
-        let $var = read_value!($iter, $t);
-        input_inner!{$iter $($r)*}
-    };
-}
-
-macro_rules! read_value {
-    ($iter:expr, ( $($t:tt),* )) => {
-        ( $(read_value!($iter, $t)),* )
-    };
-    ($iter:expr, [ $t:tt ; $len:expr ]) => {
-        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
-    };
-    ($iter:expr, chars) => {
-        read_value!($iter, String).chars().collect::<Vec<char>>()
-    };
-    ($iter:expr, bytes) => {
-        read_value!($iter, String).bytes().collect::<Vec<u8>>()
-    };
-    ($iter:expr, usize1) => {
-        read_value!($iter, usize) - 1
-    };
-    ($iter:expr, $t:ty) => {
-        $iter.next().unwrap().parse::<$t>().expect("Parse error")
-    };
-}
-
-pub trait ChangeMinMax {
-    fn chmin(&mut self, x: Self) -> bool;
-    fn chmax(&mut self, x: Self) -> bool;
-}
-
-impl<T: PartialOrd> ChangeMinMax for T {
-    fn chmin(&mut self, x: Self) -> bool {
-        *self > x && {
-            *self = x;
-            true
-        }
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
     }
-    fn chmax(&mut self, x: Self) -> bool {
-        *self < x && {
-            *self = x;
-            true
-        }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
     }
 }
 
 fn main() {
-    use std::io::Write;
-    let out = std::io::stdout();
-    let mut out = std::io::BufWriter::new(out.lock());
-    input! {
-        n: usize,
-        a: [(u32, u32, u32); n],
-    }
-    let mut a = a;
-    a.sort_unstable();
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let mut a = (0..n).map(|_| (sc.read::<u32>(), sc.read::<u32>(), sc.read::<u32>())).collect::<Vec<_>>();
+    a.sort_unstable_by(|l, r| l.0.cmp(&r.0));
     let mut dp = vec![0; n + 1];
     for i in 0..n {
         let cur = dp[i];
-        dp[i + 1].chmax(cur);
-        let idx = a[(i + 1)..n].binary_search(&(a[i].1, 0, 0)).unwrap_err() + i + 1;
-        dp[idx].chmax(cur + a[i].2);
+        dp[i + 1] = dp[i + 1].max(cur);
+        let j = i + 1 + a[i + 1..].partition_point(|o| o.0 < a[i].1);
+        dp[j] = dp[j].max(cur + a[i].2);
     }
-    writeln!(out, "{}", dp[n]).ok();
+    writeln!(bw, "{}", dp[n]).ok();
 }
