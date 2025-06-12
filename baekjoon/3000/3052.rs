8bd3ebb684456bb77b104ca2b6613a723d63bc7f
palilo815 <hyunsoo1397@gmail.com>
2025-06-12 21:29:57 +0900

    나머지

---
+++ baekjoon/3000/3052.rs
@@ -0,0 +1,23 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let ans = (0..10).map(|_| sc.read::<u32>() % 42).collect::<std::collections::HashSet<_>>().len();
+    writeln!(bw, "{ans}").ok();
+}
