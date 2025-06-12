69991c50637972e1b85bc6e39106eb4d751928a7
palilo815 <hyunsoo1397@gmail.com>
2025-06-12 21:23:18 +0900

    아름다운 문자열

--- baekjoon/24000/24524.rs
+++ baekjoon/24000/24524.rs
@@ -1,65 +1,31 @@
-macro_rules! input {
-    (source = $s:expr, $($r:tt)*) => {
-        let mut iter = $s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-    ($($r:tt)*) => {
-        let s = {
-            use std::io::Read;
-            let mut s = String::new();
-            std::io::stdin().read_to_string(&mut s).unwrap();
-            s
-        };
-        let mut iter = s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
 }
 
-macro_rules! input_inner {
-    ($iter:expr) => {};
-    ($iter:expr, ) => {};
-    ($iter:expr, $var:ident:$t:tt $($r:tt)*) => {
-        let $var = read_value!($iter, $t);
-        input_inner!{$iter $($r)*}
-    };
-}
-
-macro_rules! read_value {
-    ($iter:expr, ($($t:tt), *)) => {
-        ($(read_value!($iter, $t)), *)
-    };
-    ($iter:expr, [$t:tt; $len:expr]) => {
-        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
-    };
-    ($iter:expr, chars) => {
-        read_value!($iter, String).chars().collect::<Vec<char>>()
-    };
-    ($iter:expr, bytes) => {
-        read_value!($iter, String).bytes().collect::<Vec<u8>>()
-    };
-    ($iter:expr, usize1) => {
-        read_value!($iter, usize) - 1
-    };
-    ($iter:expr, $t:ty) => {
-        $iter.next().unwrap().parse::<$t>().expect("Parse error")
-    };
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read_bytes(&mut self) -> Vec<u8> {
+        self.it.next().unwrap().bytes().collect()
+    }
 }
 
 fn main() {
-    use std::io::Write;
-    let out = std::io::stdout();
-    let mut out = std::io::BufWriter::new(out.lock());
-    input! {
-        s: bytes,
-        t: bytes,
-    }
-    let len = t.len();
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let s = sc.read_bytes();
+    let t = sc.read_bytes();
     let mut ord = [usize::MAX; 26];
+    let mut dp = vec![0; t.len() + 1];
     for (i, x) in t.into_iter().enumerate() {
         ord[(x - b'a') as usize] = i;
     }
-    let mut dp = [0; 27];
-    dp[0] = i32::MAX;
+    dp[0] = u32::MAX;
     for x in s {
         let x = (x - b'a') as usize;
         if ord[x] == usize::MAX {
@@ -70,5 +36,5 @@ fn main() {
             dp[ord[x] + 1] += 1;
         }
     }
-    writeln!(out, "{}", dp[len]).ok();
+    writeln!(bw, "{}", dp.last().unwrap()).ok();
 }
