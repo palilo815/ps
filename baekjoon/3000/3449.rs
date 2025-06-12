b435e4955ee35d8acaff42648cb2fa7c87d8895b
palilo815 <hyunsoo1397@gmail.com>
2025-06-12 21:26:08 +0900

    해밍 거리

---
+++ baekjoon/3000/3449.rs
@@ -0,0 +1,31 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_bytes(&mut self) -> Vec<u8> {
+        self.it.next().unwrap().bytes().collect()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    for _ in 0..n {
+        let s1 = sc.read_bytes();
+        let s2 = sc.read_bytes();
+        let dist = s1.into_iter().zip(s2).map(|(x, y)| x ^ y).sum::<u8>();
+        writeln!(bw, "Hamming distance is {dist}.").ok();
+    }
+}
