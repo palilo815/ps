566076a7ef2c8d3e0008989362dae9add412e927
palilo815 <hyunsoo1397@gmail.com>
2025-06-18 22:57:56 +0900

    큐 2

---
+++ baekjoon/18000/18258.rs
@@ -0,0 +1,37 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn raw(&mut self) -> &str {
+        self.it.next().unwrap()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let mut dq = std::collections::VecDeque::new();
+    for _ in 0..n {
+        match sc.raw() {
+            "push" => dq.push_back(sc.read::<i32>()),
+            "pop" => writeln!(bw, "{}", dq.pop_front().unwrap_or(-1)).unwrap(),
+            "size" => writeln!(bw, "{}", dq.len()).unwrap(),
+            "empty" => writeln!(bw, "{}", dq.is_empty() as usize).unwrap(),
+            "front" => writeln!(bw, "{}", dq.front().unwrap_or(&-1)).unwrap(),
+            "back" => writeln!(bw, "{}", dq.back().unwrap_or(&-1)).unwrap(),
+            _ => unreachable!(),
+        }
+    }
+}
