59dfc7e3285d60bc97ca2e613470e0a1bb4eb8f5
palilo815 <hyunsoo1397@gmail.com>
2025-06-23 22:22:39 +0900

    알고리즘 수업 - 깊이 우선 탐색 1

--- baekjoon/24000/24479.rs
+++ baekjoon/24000/24479.rs
@@ -1,74 +1,86 @@
-use std::io::Write;
+use std::io::*;
 
-#[allow(dead_code)]
-mod scanner {
-    use std::str::FromStr;
-    pub struct Scanner<'a> {
-        it: std::str::SplitWhitespace<'a>,
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
     }
-    impl<'a> Scanner<'a> {
-        pub fn new(s: &'a str) -> Scanner<'a> {
-            Scanner {
-                it: s.split_whitespace(),
-            }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+struct Csr<T> {
+    values: Box<[(usize, T)]>,
+    pref: Box<[usize]>,
+}
+
+impl<T> std::ops::Index<usize> for Csr<T> {
+    type Output = [(usize, T)];
+    fn index(&self, i: usize) -> &Self::Output {
+        &self.values[self.pref[i]..self.pref[i + 1]]
+    }
+}
+
+impl<T> std::ops::IndexMut<usize> for Csr<T> {
+    fn index_mut(&mut self, i: usize) -> &mut Self::Output {
+        &mut self.values[self.pref[i]..self.pref[i + 1]]
+    }
+}
+
+impl<T: Copy + Default> Csr<T> {
+    fn from_undirected_edges(n: usize, edges: Vec<(usize, usize, T)>) -> Self {
+        let mut values = vec![(0, T::default()); edges.len() * 2].into_boxed_slice();
+        let mut pref = vec![0; n + 1].into_boxed_slice();
+        for &(u, v, _) in edges.iter() {
+            pref[u] += 1;
+            pref[v] += 1;
         }
-        pub fn next<T: FromStr>(&mut self) -> T {
-            self.it.next().unwrap().parse::<T>().ok().unwrap()
+        for i in 0..n {
+            pref[i + 1] += pref[i];
         }
-        pub fn next_bytes(&mut self) -> Vec<u8> {
-            self.it.next().unwrap().bytes().collect()
+        for (u, v, w) in edges {
+            pref[u] -= 1;
+            values[pref[u]] = (v, w);
+            pref[v] -= 1;
+            values[pref[v]] = (u, w);
         }
-        pub fn next_chars(&mut self) -> Vec<char> {
-            self.it.next().unwrap().chars().collect()
-        }
-        pub fn next_vec<T: FromStr>(&mut self, len: usize) -> Vec<T> {
-            (0..len).map(|_| self.next()).collect()
-        }
+        Self { values, pref }
     }
 }
 
 fn main() {
-    use std::io::Read;
-    let mut s = String::new();
-    std::io::stdin().read_to_string(&mut s).unwrap();
-    let mut sc = scanner::Scanner::new(&s);
-    let out = std::io::stdout();
-    let mut out = std::io::BufWriter::new(out.lock());
-    run(&mut sc, &mut out);
-}
-
-fn run<W: Write>(sc: &mut scanner::Scanner, out: &mut std::io::BufWriter<W>) {
-    let n = sc.next::<usize>();
-    let m = sc.next::<usize>();
-    let s = sc.next::<usize>() - 1;
-    let mut adj = vec![vec![]; n];
-    for _ in 0..m {
-        let u = sc.next::<usize>() - 1;
-        let v = sc.next::<usize>() - 1;
-        adj[u].push(v);
-        adj[v].push(u);
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let m = sc.read::<usize>();
+    let s = sc.read::<usize>() - 1;
+    let edges = (0..m).map(|_| (sc.read::<usize>() - 1, sc.read::<usize>() - 1, ())).collect::<Vec<_>>();
+    let mut csr = Csr::from_undirected_edges(n, edges);
+    for i in 0..n {
+        csr[i].sort_unstable_by(|l, r| r.0.cmp(&l.0));
     }
-    adj.iter_mut().for_each(|x| x.sort_unstable());
-    let mut timer = 0;
     let mut tin = vec![0; n];
-    let mut stk = Vec::with_capacity(n);
-    stk.push((s, usize::MAX));
-    while let Some((u, mut i)) = stk.pop() {
-        if i == usize::MAX {
-            timer += 1;
-            tin[u] = timer;
+    let mut stack = vec![s];
+    let mut tick = 0;
+    while let Some(u) = stack.pop() {
+        if tin[u] != 0 {
+            continue;
         }
-        i += 1;
-        while i != adj[u].len() {
-            if tin[adj[u][i]] == 0 {
-                stk.push((u, i));
-                stk.push((adj[u][i], usize::MAX));
-                break;
+        tick += 1;
+        tin[u] = tick;
+        for &(v, _) in csr[u].iter() {
+            if tin[v] == 0 {
+                stack.push(v);
             }
-            i += 1;
         }
     }
-    for x in tin {
-        writeln!(out, "{}", x).ok();
+    for t in tin {
+        writeln!(bw, "{t}").ok();
     }
 }
