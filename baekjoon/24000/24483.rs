1f527dc7f5145bac5bfbfcca69315b7aa30ebf1a
palilo815 <hyunsoo1397@gmail.com>
2025-06-23 22:26:36 +0900

    알고리즘 수업 - 깊이 우선 탐색 5

--- baekjoon/24000/24483.rs
+++ baekjoon/24000/24483.rs
@@ -1,64 +1,86 @@
-use std::io::Write;
+use std::io::*;
 
-pub struct Scanner<'a> {
-    it: std::str::SplitWhitespace<'a>,
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
 }
 
-impl<'a> Scanner<'a> {
-    pub fn new(s: &'a str) -> Scanner<'a> {
-        Scanner {
-            it: s.split_whitespace(),
-        }
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
     }
-    pub fn next<T: std::str::FromStr>(&mut self) -> T {
+    fn read<T: std::str::FromStr>(&mut self) -> T {
         self.it.next().unwrap().parse::<T>().ok().unwrap()
     }
 }
 
-fn main() {
-    use std::io::Read;
-    let mut s = String::new();
-    std::io::stdin().read_to_string(&mut s).unwrap();
-    let mut sc = Scanner::new(&s);
-    let out = std::io::stdout();
-    let mut out = std::io::BufWriter::new(out.lock());
-    run(&mut sc, &mut out);
+struct Csr<T> {
+    values: Box<[(usize, T)]>,
+    pref: Box<[usize]>,
 }
 
-fn run<W: Write>(sc: &mut Scanner, out: &mut std::io::BufWriter<W>) {
-    let n = sc.next::<usize>();
-    let m = sc.next::<usize>();
-    let s = sc.next::<usize>() - 1;
-    let mut adj = vec![vec![]; n];
-    for _ in 0..m {
-        let u = sc.next::<usize>() - 1;
-        let v = sc.next::<usize>() - 1;
-        adj[u].push(v);
-        adj[v].push(u);
+impl<T> std::ops::Index<usize> for Csr<T> {
+    type Output = [(usize, T)];
+    fn index(&self, i: usize) -> &Self::Output {
+        &self.values[self.pref[i]..self.pref[i + 1]]
     }
-    adj.iter_mut()
-        .for_each(|x| x.sort_unstable_by(|l, r| r.cmp(l)));
-    let mut stk = Vec::with_capacity(n);
-    let mut dep = vec![-1; n];
-    let mut check = vec![false; n];
-    let mut ans = 0;
-    let mut t = 0;
-    dep[s] = 0;
-    stk.push(s);
-    while let Some(u) = stk.pop() {
-        if !check[u] {
-            t += 1;
-            ans += dep[u] as usize * t;
-            check[u] = true;
+}
+
+impl<T> std::ops::IndexMut<usize> for Csr<T> {
+    fn index_mut(&mut self, i: usize) -> &mut Self::Output {
+        &mut self.values[self.pref[i]..self.pref[i + 1]]
+    }
+}
+
+impl<T: Copy + Default> Csr<T> {
+    fn from_undirected_edges(n: usize, edges: Vec<(usize, usize, T)>) -> Self {
+        let mut values = vec![(0, T::default()); edges.len() * 2].into_boxed_slice();
+        let mut pref = vec![0; n + 1].into_boxed_slice();
+        for &(u, v, _) in edges.iter() {
+            pref[u] += 1;
+            pref[v] += 1;
         }
-        while let Some(v) = adj[u].pop() {
+        for i in 0..n {
+            pref[i + 1] += pref[i];
+        }
+        for (u, v, w) in edges {
+            pref[u] -= 1;
+            values[pref[u]] = (v, w);
+            pref[v] -= 1;
+            values[pref[v]] = (u, w);
+        }
+        Self { values, pref }
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let m = sc.read::<usize>();
+    let s = sc.read::<usize>() - 1;
+    let edges = (0..m).map(|_| (sc.read::<usize>() - 1, sc.read::<usize>() - 1, ())).collect::<Vec<_>>();
+    let mut csr = Csr::from_undirected_edges(n, edges);
+    for i in 0..n {
+        csr[i].sort_unstable_by(|l, r| r.0.cmp(&l.0));
+    }
+    let mut dep = vec![-1; n];
+    let mut stack = vec![s];
+    let mut tick = 0;
+    let mut ans = 0;
+    while let Some(u) = stack.pop() {
+        if dep[u] != -1 {
+            continue;
+        }
+        tick += 1;
+        dep[u] = csr[u].iter().map(|&(v, _)| dep[v]).max().unwrap_or(-1) + 1;
+        ans += tick * dep[u] as i64;
+        for &(v, _) in csr[u].iter() {
             if dep[v] == -1 {
-                dep[v] = dep[u] + 1;
-                stk.push(u);
-                stk.push(v);
-                break;
+                stack.push(v);
             }
         }
     }
-    writeln!(out, "{}", ans).ok();
+    writeln!(bw, "{ans}").ok();
 }
