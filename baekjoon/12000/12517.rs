5a351d967f15aaeed0ef96cc663cf34ac8991e1a
palilo815 <hyunsoo1397@gmail.com>
2025-06-28 16:15:04 +0900

    Centauri Prime (Small1)

---
+++ baekjoon/12000/12517.rs
@@ -0,0 +1,35 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn raw(&mut self) -> &str {
+        self.it.next().unwrap()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let t = sc.read::<usize>();
+    for t in 1..=t {
+        let name = sc.raw();
+        let end = name.chars().last().unwrap();
+        let who = match end {
+            'y' | 'Y' => "nobody",
+            'a' | 'e' | 'i' | 'o' | 'u' | 'A' | 'E' | 'I' | 'O' | 'U' => "a queen",
+            _ => "a king",
+        };
+        writeln!(bw, "Case #{t}: {name} is ruled by {who}.").ok();
+    }
+}
