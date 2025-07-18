b771ef6f2709931290219c4caa8bb44268e3855a
palilo815 <hyunsoo1397@gmail.com>
2025-07-19 00:09:13 +0900

    사탕 선생 고창영

---
+++ baekjoon/2000/2547.rs
@@ -0,0 +1,27 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let t = sc.read::<usize>();
+    for _ in 0..t {
+        let n = sc.read::<u64>();
+        let yes = (0..n).fold(0, |acc, _| (acc + sc.read::<u64>()) % n) == 0;
+        writeln!(bw, "{}", if yes { "YES" } else { "NO" }).ok();
+    }
+}
