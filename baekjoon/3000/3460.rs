bf26cf0aeaf3b3cc79b3f0bf8e9affec7fe4dba3
palilo815 <hyunsoo1397@gmail.com>
2025-07-19 00:10:10 +0900

    Binary numbers

---
+++ baekjoon/3000/3460.rs
@@ -0,0 +1,31 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let t = sc.read::<usize>();
+    for _ in 0..t {
+        let n = sc.read::<u32>();
+        for i in 0..20 {
+            if n >> i & 1 == 1 {
+                write!(bw, "{i} ").ok();
+            }
+        }
+        writeln!(bw).ok();
+    }
+}
