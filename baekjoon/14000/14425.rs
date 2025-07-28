c554aa901f48c2392198594afe5f40eace4ec3e4
palilo815 <hyunsoo1397@gmail.com>
2025-07-28 23:28:34 +0900

    문자열 집합

---
+++ baekjoon/14000/14425.rs
@@ -0,0 +1,26 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let m = sc.read::<usize>();
+    let set = (0..n).map(|_| sc.read::<String>()).collect::<std::collections::HashSet<_>>();
+    let ans = (0..m).filter(|_| set.contains(&sc.read::<String>())).count();
+    writeln!(bw, "{ans}").ok();
+}
