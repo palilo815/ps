712b5b175f7b3a80f97c92c94d04d8834e0622a6
palilo815 <hyunsoo1397@gmail.com>
2025-07-28 23:31:55 +0900

    Moo Volume

---
+++ baekjoon/2000/2399.rs
@@ -0,0 +1,30 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_vec<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.read()).collect()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let mut a = sc.read_vec::<i64>(n);
+    a.sort_unstable();
+    let n = n as i64;
+    let ans = a.into_iter().zip(0..).fold(0, |acc, (x, i)| acc + x * 2 * (2 * i - n + 1));
+    writeln!(bw, "{ans}").ok();
+}
