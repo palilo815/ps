98220da5ae79f7884c1bb0fe74219a192ec1cbd1
palilo815 <hyunsoo1397@gmail.com>
2025-08-02 16:12:52 +0900

    DORO

---
+++ baekjoon/34000/34073.rs
@@ -0,0 +1,29 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn raw(&mut self) -> &str {
+        self.it.next().unwrap()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    for _ in 0..n {
+        let s = sc.raw();
+        write!(bw, "{s}DORO ",).ok();
+    }
+}
