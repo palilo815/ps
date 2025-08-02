bbadfa3b93f5a8347e2a4dce5a28648f47383985
palilo815 <hyunsoo1397@gmail.com>
2025-08-02 16:04:20 +0900

    급상승

--- baekjoon/23000/23978.rs
+++ baekjoon/23000/23978.rs
@@ -1,78 +1,48 @@
-macro_rules! input {
-    (source = $s:expr, $($r:tt)*) => {
-        let mut iter = $s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-    ($($r:tt)*) => {
-        let s = {
-            use std::io::Read;
-            let mut s = String::new();
-            std::io::stdin().read_to_string(&mut s).unwrap();
-            s
-        };
-        let mut iter = s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
 }
 
-macro_rules! input_inner {
-    ($iter:expr) => {};
-    ($iter:expr, ) => {};
-    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
-        let $var = read_value!($iter, $t);
-        input_inner!{$iter $($r)*}
-    };
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_vec<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.read()).collect()
+    }
 }
-
-macro_rules! read_value {
-    ($iter:expr, ( $($t:tt),* )) => {
-        ( $(read_value!($iter, $t)),* )
-    };
-    ($iter:expr, [ $t:tt ; $len:expr ]) => {
-        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
-    };
-    ($iter:expr, chars) => {
-        read_value!($iter, String).chars().collect::<Vec<char>>()
-    };
-    ($iter:expr, bytes) => {
-        read_value!($iter, String).bytes().collect::<Vec<u8>>()
-    };
-    ($iter:expr, usize1) => {
-        read_value!($iter, usize) - 1
-    };
-    ($iter:expr, $t:ty) => {
-        $iter.next().unwrap().parse::<$t>().expect("Parse error")
-    };
-}
-
-use std::io::Write;
 
 fn main() {
-    let out = std::io::stdout();
-    let mut out = std::io::BufWriter::new(out.lock());
-    input! {
-        n: usize,
-        k: i64,
-        a: [i32; n],
-    }
-    let mut lo: i64 = 1;
-    let mut hi: i64 = 1414213562;
-    let f = |m| {
-        let mut earn = (m + 1) * m / 2;
-        for i in 1..n {
-            // m + (m - 1) + (m - 2) + ... + 1
-            let interval = std::cmp::min((a[i] - a[i - 1]) as i64, m);
-            earn += (m + m + 1 - interval) * interval / 2;
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let k = sc.read::<i64>();
+    let a = sc.read_vec::<i32>(n);
+    let parametric = |mut lo, mut hi| {
+        let f = |mid: i64| -> bool {
+            let earn = a.windows(2).fold((mid + 1) * mid / 2, |acc, w| {
+                let interval = mid.min((w[1] - w[0]) as i64);
+                acc + (mid + mid + 1 - interval) * interval / 2
+            });
+            earn >= k
+        };
+        // first true
+        while lo != hi {
+            let mid = lo + (hi - lo) / 2;
+            if f(mid) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
         }
-        earn >= k
+        lo
     };
-    while lo != hi {
-        let m = lo + (hi - lo) / 2;
-        if f(m) {
-            hi = m;
-        } else {
-            lo = m + 1;
-        }
-    }
-    writeln!(out, "{}", lo).ok();
+    let ans = parametric(1, 1414213562);
+    writeln!(bw, "{ans}").ok();
 }
