ea657b116443986fd3497d8ee648fe62dce54a1d
palilo815 <hyunsoo1397@gmail.com>
2025-08-02 16:03:07 +0900

    Extending Rock-Paper-Scissors

---
+++ baekjoon/16000/16104.rs
@@ -0,0 +1,27 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    for i in 1..=n {
+        for j in (i + 1..).take(n / 2) {
+            writeln!(bw, "{i} {}", if j > n { j - n } else { j }).ok();
+        }
+    }
+}
