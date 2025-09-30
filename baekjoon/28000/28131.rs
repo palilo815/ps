0341ba774fe4fbdf925be812c4fcf3594ee8ce87
palilo815 <hyunsoo1397@gmail.com>
2025-09-30 22:55:06 +0900

    K-지폐

--- baekjoon/28000/28131.rs
+++ baekjoon/28000/28131.rs
@@ -1,72 +1,97 @@
 use std::io::*;
 
-struct Scanner<'a> {
-    it: std::str::SplitWhitespace<'a>,
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
 }
 
-impl<'a> Scanner<'a> {
-    fn new(s: &'a str) -> Scanner<'a> {
-        Scanner {
-            it: s.split_whitespace(),
-        }
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
     }
     fn read<T: std::str::FromStr>(&mut self) -> T {
         self.it.next().unwrap().parse::<T>().ok().unwrap()
     }
 }
 
-#[derive(Eq, PartialEq)]
-struct HeapNode(u32, usize, usize);
+struct Csr<T> {
+    values: Box<[(usize, T)]>,
+    pref: Box<[usize]>,
+}
 
-impl std::cmp::Ord for HeapNode {
+impl<T> std::ops::Index<usize> for Csr<T> {
+    type Output = [(usize, T)];
+    fn index(&self, i: usize) -> &Self::Output {
+        &self.values[self.pref[i]..self.pref[i + 1]]
+    }
+}
+
+impl<T: Copy + Default> Csr<T> {
+    fn from_directed_edges(n: usize, edges: Vec<(usize, usize, T)>) -> Self {
+        let mut values = vec![(0, T::default()); edges.len()].into_boxed_slice();
+        let mut pref = vec![0; n + 1].into_boxed_slice();
+        for &(u, _, _) in edges.iter() {
+            pref[u] += 1;
+        }
+        for i in 0..n {
+            pref[i + 1] += pref[i];
+        }
+        for (u, v, w) in edges {
+            pref[u] -= 1;
+            values[pref[u]] = (v, w);
+        }
+        Self { values, pref }
+    }
+}
+
+#[derive(Eq, PartialEq)]
+struct HeapNode<T>(T, usize);
+
+impl<T: Ord> std::cmp::Ord for HeapNode<T> {
     fn cmp(&self, rhs: &Self) -> std::cmp::Ordering {
         rhs.0.cmp(&(self.0))
     }
 }
 
-impl std::cmp::PartialOrd for HeapNode {
+impl<T: Ord> std::cmp::PartialOrd for HeapNode<T> {
     fn partial_cmp(&self, rhs: &Self) -> Option<std::cmp::Ordering> {
         Some(self.cmp(rhs))
     }
 }
 
 fn main() {
-    let (mut sc, mut out) = unsafe {
-        static mut S: String = String::new();
-        stdin().read_to_string(&mut S).unwrap();
-        (Scanner::new(&S), BufWriter::new(stdout().lock()))
-    };
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
     let n = sc.read::<usize>();
     let m = sc.read::<usize>();
     let k = sc.read::<usize>();
     let s = sc.read::<usize>() - 1;
     let t = sc.read::<usize>() - 1;
-    let mut adj = vec![vec![]; n];
-    for _ in 0..m {
-        let u = sc.read::<usize>() - 1;
-        let v = sc.read::<usize>() - 1;
-        let w = sc.read::<usize>();
-        adj[u].push((w, v));
-    }
-    let mut dist = vec![[u32::MAX; 50]; n];
+    let edges = (0..m)
+        .map(|_| (sc.read::<usize>() - 1, sc.read::<usize>() - 1, sc.read::<usize>()))
+        .collect::<Vec<_>>();
+    let csr = Csr::from_directed_edges(n, edges);
+    let mut dist = vec![[usize::MAX; 50]; n];
     dist[s][0] = 0;
     let mut pq = std::collections::BinaryHeap::new();
-    pq.push(HeapNode(0, s, 0));
-    while let Some(HeapNode(d, u, rem)) = pq.pop() {
-        if dist[u][rem] != d {
+    pq.push(HeapNode(0, s));
+    while let Some(HeapNode(d, u)) = pq.pop() {
+        if d != dist[u][d % k] {
             continue;
         }
-        for &(w, v) in adj[u].iter() {
-            let rem = (rem + w) % k;
-            if dist[v][rem] > d + w as u32 {
-                dist[v][rem] = d + w as u32;
-                pq.push(HeapNode(dist[v][rem], v, rem));
+        for &(v, w) in csr[u].iter() {
+            let d = d + w;
+            let r = d % k;
+            if dist[v][r] > d {
+                dist[v][r] = d;
+                pq.push(HeapNode(d, v));
             }
         }
     }
-    if dist[t][0] == u32::MAX {
-        writeln!(out, "IMPOSSIBLE").ok();
+    if dist[t][0] == usize::MAX {
+        writeln!(bw, "IMPOSSIBLE").ok();
     } else {
-        writeln!(out, "{}", dist[t][0]).ok();
+        writeln!(bw, "{}", dist[t][0]).ok();
     }
 }
