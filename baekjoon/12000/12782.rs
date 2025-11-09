87473326e6a3e8cde41e35ca8542928e441b1616
palilo815 <hyunsoo1397@gmail.com>
2025-11-09 20:36:29 +0900

    비트 우정지수

---
+++ baekjoon/12000/12782.rs
@@ -0,0 +1,39 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_bytes(&mut self) -> Vec<u8> {
+        self.it.next().unwrap().bytes().collect()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let t = sc.read::<usize>();
+    for _ in 0..t {
+        let s = sc.read_bytes();
+        let t = sc.read_bytes();
+        let (zero, one) = s.into_iter().zip(t.into_iter()).fold((0, 0), |acc, (x, y)| {
+            if x == y {
+                acc
+            } else if x == b'0' {
+                (acc.0 + 1, acc.1)
+            } else {
+                (acc.0, acc.1 + 1)
+            }
+        });
+        writeln!(bw, "{}", zero + one - zero.min(one)).ok();
+    }
+}
