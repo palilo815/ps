398a75ddada3be78fa2c20019ae9ac3c720d3936
palilo815 <hyunsoo1397@gmail.com>
2025-11-11 20:18:02 +0900

    줄 세우기

---
+++ baekjoon/2000/2252.rs
@@ -0,0 +1,70 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+struct Csr<T> {
+    values: Box<[(usize, T)]>,
+    pref: Box<[usize]>,
+}
+
+impl<T> std::ops::Index<usize> for Csr<T> {
+    type Output = [(usize, T)];
+    fn index(&self, i: usize) -> &Self::Output {
+        &self.values[self.pref[i]..self.pref[i + 1]]
+    }
+}
+
+impl<T: Copy + Default> Csr<T> {
+    fn from_directed_edges(n: usize, edges: Vec<(usize, usize, T)>) -> Self {
+        let mut values = vec![(0, T::default()); edges.len()].into_boxed_slice();
+        let mut pref = vec![0; n + 1].into_boxed_slice();
+        for &(u, _, _) in edges.iter() {
+            pref[u] += 1;
+        }
+        for i in 0..n {
+            pref[i + 1] += pref[i];
+        }
+        for (u, v, w) in edges {
+            pref[u] -= 1;
+            values[pref[u]] = (v, w);
+        }
+        Self { values, pref }
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let m = sc.read::<usize>();
+    let edges = (0..m).map(|_| (sc.read::<usize>() - 1, sc.read::<usize>() - 1, ())).collect::<Vec<_>>();
+    let mut indeg = vec![0; n];
+    for &(_, v, _) in edges.iter() {
+        indeg[v] += 1;
+    }
+    let csr = Csr::from_directed_edges(n, edges);
+    let mut dq = std::collections::VecDeque::with_capacity(n);
+    dq.extend((0..n).filter(|&i| indeg[i] == 0));
+    while let Some(u) = dq.pop_front() {
+        write!(bw, "{} ", u + 1).ok();
+        for &(v, _) in csr[u].iter() {
+            indeg[v] -= 1;
+            if indeg[v] == 0 {
+                dq.push_back(v);
+            }
+        }
+    }
+}
