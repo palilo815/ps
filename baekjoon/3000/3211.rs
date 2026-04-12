403c5759132cbfec9fcd91e5cc3b4447f40888ef
palilo815 <hyunsoo1397@gmail.com>
2026-04-12 11:51:18 +0900

    kino

---
+++ baekjoon/3000/3211.rs
@@ -0,0 +1,33 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).ok();
+        Self { it: s.leak().split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_vec<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.read()).collect()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let mut a = sc.read_vec::<usize>(n);
+    a.sort_unstable();
+    for (i, x) in a.into_iter().enumerate() {
+        if x <= i {
+            writeln!(bw, "{}", i + 1).ok();
+            break;
+        }
+    }
+}
