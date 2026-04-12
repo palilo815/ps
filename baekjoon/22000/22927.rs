f59c4c5efb5d7f700b9fd5befccdb628f4db87b1
palilo815 <hyunsoo1397@gmail.com>
2026-04-12 11:54:30 +0900

    Mountainous Palindromic Subarray

--- baekjoon/22000/22927.rs
+++ baekjoon/22000/22927.rs
@@ -1,57 +1,39 @@
-macro_rules! input {
-    (source = $s:expr, $($r:tt)*) => {
-        let mut iter = $s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-    ($($r:tt)*) => {
-        let s = {
-            use std::io::Read;
-            let mut s = String::new();
-            std::io::stdin().read_to_string(&mut s).unwrap();
-            s
-        };
-        let mut iter = s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
 }
 
-macro_rules! input_inner {
-    ($iter:expr) => {};
-    ($iter:expr, ) => {};
-    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
-        let $var = read_value!($iter, $t);
-        input_inner!{$iter $($r)*}
-    };
+impl Scanner {
+    fn new() -> Self {
+        let mut buf = Vec::new();
+        stdin().lock().read_to_end(&mut buf).ok();
+        let s = unsafe { std::str::from_utf8_unchecked(Box::leak(buf.into_boxed_slice())) };
+        Self { it: s.split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+    fn read_vec<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.read()).collect()
+    }
 }
-
-macro_rules! read_value {
-    ($iter:expr, ( $($t:tt),* )) => {
-        ( $(read_value!($iter, $t)),* )
-    };
-    ($iter:expr, [ $t:tt ; $len:expr ]) => {
-        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
-    };
-    ($iter:expr, chars) => {
-        read_value!($iter, String).chars().collect::<Vec<char>>()
-    };
-    ($iter:expr, bytes) => {
-        read_value!($iter, String).bytes().collect::<Vec<u8>>()
-    };
-    ($iter:expr, usize1) => {
-        read_value!($iter, usize) - 1
-    };
-    ($iter:expr, $t:ty) => {
-        $iter.next().unwrap().parse::<$t>().expect("Parse error")
-    };
-}
-
 
 fn main() {
-    use std::io::Write;
-    let out = std::io::stdout();
-    let mut out = std::io::BufWriter::new(out.lock());
-    input! {
-        n: usize,
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let a = sc.read_vec::<u32>(n);
+    let mut ans = 1;
+    let mut r = 1;
+    while r < n - 1 {
+        let mut l = r;
+        while l != 0 && r != n - 1 && a[l - 1] < a[l] && a[l - 1] == a[r + 1] {
+            l -= 1;
+            r += 1;
+        }
+        ans = ans.max(r - l + 1);
+        r += 1;
     }
-    // TODO
+    writeln!(bw, "{}", if ans == 1 { -1 } else { ans as i32 }).ok();
 }
