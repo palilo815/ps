9167e52c53ceff2fdfc78d9b43ff3013375e26c5
palilo815 <hyunsoo1397@gmail.com>
2026-04-21 20:17:30 +0900

    자세히 보아야 예쁘다

---
+++ baekjoon/33000/33572.rs
@@ -0,0 +1,26 @@
+use std::io::*;
+
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut buf = Vec::new();
+        stdin().lock().read_to_end(&mut buf).ok();
+        let s = unsafe { std::str::from_utf8_unchecked(Box::leak(buf.into_boxed_slice())) };
+        Self { it: s.split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let n = sc.read::<usize>();
+    let m = sc.read::<u64>();
+    let k = (0..n).fold(0, |acc, _| acc + sc.read::<u64>() - 1);
+    writeln!(bw, "{}", if m > k { "OUT" } else { "DIMI" }).ok();
+}
