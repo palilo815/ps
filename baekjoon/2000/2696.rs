e88e6d6f844a8a8e9126bbc10995caa106ace064
palilo815 <hyunsoo1397@gmail.com>
2026-04-21 20:12:37 +0900

    Running Median

--- baekjoon/2000/2696.rs
+++ baekjoon/2000/2696.rs
@@ -1,40 +1,19 @@
-use std::io::Write;
+use std::io::*;
 
-#[allow(dead_code)]
-mod scanner {
-    use std::str::FromStr;
-    pub struct Scanner<'a> {
-        it: std::str::SplitWhitespace<'a>,
-    }
-    impl<'a> Scanner<'a> {
-        pub fn new(s: &'a str) -> Scanner<'a> {
-            Scanner {
-                it: s.split_whitespace(),
-            }
-        }
-        pub fn next<T: FromStr>(&mut self) -> T {
-            self.it.next().unwrap().parse::<T>().ok().unwrap()
-        }
-        pub fn next_bytes(&mut self) -> Vec<u8> {
-            self.it.next().unwrap().bytes().collect()
-        }
-        pub fn next_chars(&mut self) -> Vec<char> {
-            self.it.next().unwrap().chars().collect()
-        }
-        pub fn next_vec<T: FromStr>(&mut self, len: usize) -> Vec<T> {
-            (0..len).map(|_| self.next()).collect()
-        }
-    }
+struct Scanner {
+    it: std::str::SplitAsciiWhitespace<'static>,
 }
 
-fn main() {
-    use std::io::Read;
-    let mut s = String::new();
-    std::io::stdin().read_to_string(&mut s).unwrap();
-    let mut sc = scanner::Scanner::new(&s);
-    let out = std::io::stdout();
-    let mut out = std::io::BufWriter::new(out.lock());
-    run(&mut sc, &mut out);
+impl Scanner {
+    fn new() -> Self {
+        let mut buf = Vec::new();
+        stdin().lock().read_to_end(&mut buf).ok();
+        let s = unsafe { std::str::from_utf8_unchecked(Box::leak(buf.into_boxed_slice())) };
+        Self { it: s.split_ascii_whitespace() }
+    }
+    fn read<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse::<T>().ok().unwrap()
+    }
 }
 
 struct RunningMedian<T> {
@@ -42,17 +21,7 @@ struct RunningMedian<T> {
     right: std::collections::BinaryHeap<std::cmp::Reverse<T>>,
 }
 
-#[allow(dead_code)]
-impl<T> RunningMedian<T>
-where
-    T: std::cmp::Ord + std::marker::Copy,
-{
-    fn new() -> Self {
-        Self {
-            left: Default::default(),
-            right: Default::default(),
-        }
-    }
+impl<T: Clone + Copy + std::cmp::Ord> RunningMedian<T> {
     fn with_capacity(n: usize) -> Self {
         Self {
             left: std::collections::BinaryHeap::with_capacity((n + 1) >> 1),
@@ -65,16 +34,17 @@ where
     }
     fn push(&mut self, mut x: T) {
         if self.left.len() == self.right.len() {
-            if let Some(mut right_min) = self.right.peek_mut() {
-                if x > right_min.0 {
-                    std::mem::swap(&mut x, &mut right_min.0);
-                }
+            if let Some(mut r) = self.right.peek_mut()
+                && x > r.0
+            {
+                std::mem::swap(&mut x, &mut r.0);
             }
             self.left.push(x);
         } else {
-            let mut left_max = self.left.peek_mut().unwrap();
-            if x < *left_max {
-                std::mem::swap(&mut x, &mut left_max);
+            if let Some(mut l) = self.left.peek_mut()
+                && x < *l
+            {
+                std::mem::swap(&mut x, &mut l);
             }
             self.right.push(std::cmp::Reverse(x));
         }
@@ -84,22 +54,24 @@ where
     }
 }
 
-fn run<W: Write>(sc: &mut scanner::Scanner, out: &mut std::io::BufWriter<W>) {
-    let t = sc.next::<usize>();
-    let mut running_median = RunningMedian::with_capacity(10000);
+fn main() {
+    let mut sc = Scanner::new();
+    let mut bw = BufWriter::new(stdout().lock());
+    let t = sc.read::<usize>();
+    let mut rm = RunningMedian::with_capacity(10000);
     for _ in 0..t {
-        running_median.clear();
-        let n = sc.next::<usize>();
-        writeln!(out, "{}", (n + 1) >> 1).ok();
+        rm.clear();
+        let n = sc.read::<usize>();
+        writeln!(bw, "{}", (n + 1) >> 1).ok();
         for i in 0..n {
-            running_median.push(sc.next::<i32>());
+            rm.push(sc.read::<i32>());
             if i & 1 == 0 {
                 if i % 20 == 0 && i != 0 {
-                    writeln!(out).ok();
+                    writeln!(bw).ok();
                 }
-                write!(out, "{} ", running_median.get_median().unwrap()).ok();
+                write!(bw, "{} ", rm.get_median().unwrap()).ok();
             }
         }
-        writeln!(out).ok();
+        writeln!(bw).ok();
     }
 }
