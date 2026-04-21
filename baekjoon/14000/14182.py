90c7d69d49b4cd9d4309f9fbd2d90475f03e6fde
palilo815 <hyunsoo1397@gmail.com>
2026-04-21 20:17:50 +0900

    Tax

--- baekjoon/14000/14182.py
+++ baekjoon/14000/14182.py
@@ -1,9 +1,11 @@
-while 1:
-    n = int(input())
-    if n == 0:
-        break
-    if n > 5000000:
-        n = n * 8 // 10
-    elif n > 1000000:
-        n = n * 9 // 10
-    print(n)
+def main():
+    while (n := int(input())) != 0:
+        if n > 5_000_000:
+            n = n * 8 // 10
+        elif n > 1_000_000:
+            n = n * 9 // 10
+        print(n)
+
+
+if __name__ == "__main__":
+    main()
