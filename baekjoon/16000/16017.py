8f8e4348a1890a29ef533149a8090e41a3a6451d
palilo815 <hyunsoo1397@gmail.com>
2026-04-21 20:15:44 +0900

    Telemarketer or not?

--- baekjoon/16000/16017.py
+++ baekjoon/16000/16017.py
@@ -1,7 +1,7 @@
-arr = []
-for _ in range(4):
-    arr.append(int(input()))
-if arr[1] == arr[2] and (arr[0] == 8 or arr[0] == 9) and (arr[3] == 8 or arr[3] == 9):
-    print("ignore")
-else:
-    print("answer")
+def main():
+    a = [int(input()) for _ in range(4)]
+    print("ignore" if a[1] == a[2] and a[0] in (8, 9) and a[3] in (8, 9) else "answer")
+
+
+if __name__ == "__main__":
+    main()
