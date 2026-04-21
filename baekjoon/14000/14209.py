ae6802068102def13274d57ed52f34644a5a3beb
palilo815 <hyunsoo1397@gmail.com>
2026-04-21 20:19:19 +0900

    Bridž

--- baekjoon/14000/14209.py
+++ baekjoon/14000/14209.py
@@ -1,14 +1,16 @@
-N = int(input())
-ans = 0
-for _ in range(N):
-    s = input()
-    for i in s:
-        if i == 'A':
-            ans += 4
-        elif i == 'K':
-            ans += 3
-        elif i == 'Q':
-            ans += 2
-        elif i == 'J':
-            ans += 1
-print(ans)
+import sys
+
+
+def main():
+    data = sys.stdin.read()
+    ans = (
+        (data.count("A") * 4)
+        + (data.count("K") * 3)
+        + (data.count("Q") * 2)
+        + data.count("J")
+    )
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
